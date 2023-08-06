563ed6df8f61eb756e6adc0dc5af1bcb6c1f1273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:15 +0000

    Add package finvader/1.0.0/finvader-1.0.0.tar.gz

---
+++ packages/finvader/finvader-1.0.0.tar.gz/finvader-1.0.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+    setuptools.setup(
+        name="finvader",
+        version="1.0.0",
+        author="Petr Koráb",
+        author_email="xpetrkorab@gmail.com",
+        packages=["finvader"],
+        description="Python package for exploratory text data analysis",
+        long_description=description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/PetrKorab/FinVADER",
+        python_requires='>=3.8',
+        install_requires = ['nltk==3.8.1'],
+        license='MIT License'
+    )
