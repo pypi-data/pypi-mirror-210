9ac95db857398abc14a694f8e222b89b19d5bfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:05 +0000

    Add package sonatype-nexus-sdk/0.1.0/sonatype_nexus_sdk-0.1.0-py3-none-any.whl

---
+++ packages/sonatype-nexus-sdk/sonatype_nexus_sdk-0.1.0-py3-none-any.whl/nexus_sdk/apis/paths/beta_replicationtarget_repository_repository_name_enable.py
@@ -0,0 +1,9 @@
+from nexus_sdk.paths.beta_replicationtarget_repository_repository_name_enable.delete import (
+    ApiFordelete,
+)
+
+
+class BetaReplicationtargetRepositoryRepositoryNameEnable(
+    ApiFordelete,
+):
+    pass
