9a6618f50fe35c938608b44ebb8fd54baa1ba8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:17 +0000

    Add package vsiew/2.1.1/vsiew-2.1.1.tar.gz

---
+++ packages/vsiew/vsiew-2.1.1.tar.gz/vsiew-2.1.1/vsiew/__init__.py
@@ -0,0 +1,32 @@
+__all__ = [
+    'tools',
+    'pyplugin',
+    'kernels',
+    'exprs',
+    'rg',
+    'masks',
+    'aa',
+    'scale',
+    'denoise',
+    'dehalo',
+    'deband',
+    'deint',
+    'source',
+
+    'vs', 'core'
+]
+
+import vsaa as aa
+import vsdeband as deband
+import vsdehalo as dehalo
+import vsdeinterlace as deint
+import vsdenoise as denoise
+import vsexprtools as exprs
+import vskernels as kernels
+import vsmasktools as masks
+import vssource as source
+import vsrgtools as rg
+import vsscale as scale
+import vstools as tools
+
+from vstools import core, vs
