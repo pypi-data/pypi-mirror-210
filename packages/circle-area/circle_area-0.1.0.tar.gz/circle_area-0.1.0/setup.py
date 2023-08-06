cc7b2d1414b1bb10c30c70020dd3fa9a19b6b5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:37 +0000

    Add package circle-area/0.1.0/circle_area-0.1.0.tar.gz

---
+++ packages/circle-area/circle_area-0.1.0.tar.gz/circle_area-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='circle_area',
+    version='0.1.0',
+    author='James-Wachuka',
+    author_email='jewachu26@email.com',
+    description='A package to compute the area of a circle',
+    packages=['circle_area'],
+)
