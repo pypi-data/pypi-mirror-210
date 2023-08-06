b6810820c5440b62eee16aac14a64094c1528b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:44 +0000

    Add package camminapy/0.8.17/camminapy-0.8.17.tar.gz

---
+++ packages/camminapy/camminapy-0.8.17.tar.gz/camminapy-0.8.17/README.rst
@@ -0,0 +1,20 @@
+=========
+camminapy
+=========
+
+
+
+
+Install via ``pip``:
+
+.. code-block:: bash
+
+   pip install camminapy
+
+
+
+Install via ``poetry``:
+
+.. code-block:: bash
+
+   poetry add camminapy
---
+++ packages/camminapy/camminapy-0.8.17.tar.gz/camminapy-0.8.17/camminapy/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for camminapy."""
+from camminapy.utils.config import config
+from camminapy.utils.logger import logger
+
+__all__ = ["config", "logger"]
+__author__ = """Thomas Camminady"""
+__email__ = "0milieux_member@icloud.com"
