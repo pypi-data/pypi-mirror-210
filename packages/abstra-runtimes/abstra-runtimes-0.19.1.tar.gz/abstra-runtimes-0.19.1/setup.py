399879ea2d6ae527824020e3ad162e8382451361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:47 +0000

    Add package abstra-runtimes/0.19.1/abstra-runtimes-0.19.1.tar.gz

---
+++ packages/abstra-runtimes/abstra-runtimes-0.19.1.tar.gz/abstra-runtimes-0.19.1/setup.py
@@ -0,0 +1,31 @@
+import os
+import re
+import pathlib
+from setuptools import setup
+
+regex = "^v(\d+\.\d+\.\d+)$"
+TAG = os.getenv("TAG", "v0.0.0")
+if not TAG or not re.search(regex, TAG):
+    raise ValueError("TAG environment variable must be in the format v1.2.3")
+version = re.search(regex, TAG).group(1)
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# The list of requirements
+requirements = (HERE / "requirements.txt").read_text().split("\n")
+
+setup(
+    name="abstra-runtimes",
+    version=version,
+    description="Abstra Runtimes Lib",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abstra-app/abstra-runtimes-lib",
+    license="MIT",
+    packages=["abstra_runtimes", "abstra_runtimes.dashes_module"],
+    install_requires=requirements,
+)
