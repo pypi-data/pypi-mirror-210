ffd10abe1f7b68ba0513581ee08e9fcea4d818d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:56 +0000

    Add package hapy/0.5.2/Hapy-0.5.2-py3-none-any.whl

---
+++ packages/hapy/Hapy-0.5.2-py3-none-any.whl/hapy/main.py
@@ -0,0 +1,51 @@
+from .exector import run, run2
+from .transpiler import transpile
+import math
+
+def do(code: str):
+   run2(transpile(code))
+
+if __name__ == "__main__":
+   # Just testing das'all...
+#    code = """
+#          import something;
+
+#          # I'm not sure if 'someting.Person' will work...
+#          Person = something.Person;
+
+#          class Man inherits Person {
+#           has name;
+#           has has_pp;
+#           has age;
+#           has title = 'Mr.';
+#           has gender = 'male';
+
+#           use Person(name);
+
+#           def __startwith__() {
+#               print("Man was created!")
+#           };
+
+#          };
+
+#          frank = Man(name="Frank Abgna", has_pp=True, age=20);
+
+#          print(frank.say_hello());
+#          print(issubclass(Man, Person));
+
+#             hello(age=20.3, year=1999);
+#             dict = {1:2};
+#             print(dict);
+#          """
+
+    code = """
+        age = 10;
+
+        in (age > 20) {
+            printo("Age is greater!");
+        } imbahakaba {
+            printo("Age is less!");
+        };
+    """
+
+    run2(transpile(code))
