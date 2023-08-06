b8e88de5ea6f5be355b0cf6baa7ef93283c9d384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:49 +0000

    Add package tr0nz0d/0.1.6/tr0nz0d-0.1.6-py3-none-any.whl

---
+++ packages/tr0nz0d/tr0nz0d-0.1.6-py3-none-any.whl/tr0nz0d/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+TR0NZ0D Utils Library
+~~~~~~~~~~~~~~~~~~~
+
+Library created to facilitate the access and usage of common tools.
+
+:copyright: (c) 2021 TR0NZ0D
+:license: MIT, see LICENSE for more details.
+
+"""
+# Imports
+import logging
+
+__title__ = 'tr0nz0d'
+__author__ = 'TR0NZ0D'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 TR0NZ0D'
+__version__ = "0.1.6"
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# Instances
+logging.getLogger(__name__).addHandler(logging.NullHandler())
---
+++ packages/tr0nz0d/tr0nz0d-0.1.6-py3-none-any.whl/tr0nz0d/security/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+tr0nz0d.security
+~~~~~~~~~~~~~~~~~~~
+
+Module containing tools for cryptography management and general security.
+
+:copyright: (c) 2021 TR0NZ0D
+:license: MIT, see LICENSE for more details.
+"""
