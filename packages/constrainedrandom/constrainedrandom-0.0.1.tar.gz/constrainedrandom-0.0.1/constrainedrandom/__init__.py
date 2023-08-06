6546636036d9918c1ff76c0dc7fe460c1b730d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:39 +0000

    Add package constrainedrandom/0.0.1/constrainedrandom-0.0.1.tar.gz

---
+++ packages/constrainedrandom/constrainedrandom-0.0.1.tar.gz/constrainedrandom-0.0.1/constrainedrandom/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2023 Imagination Technologies Ltd. All Rights Reserved
+
+from constrainedrandom.random import Random
+from constrainedrandom.randobj import RandObj
+
+__all__ = ['Random', 'RandObj']
