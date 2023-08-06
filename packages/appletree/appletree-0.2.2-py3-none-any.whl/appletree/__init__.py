3310dca8474c1b45cc84b00eaa54f33791c1291a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:37 +0000

    Add package appletree/0.2.2/appletree-0.2.2-py3-none-any.whl

---
+++ packages/appletree/appletree-0.2.2-py3-none-any.whl/appletree/__init__.py
@@ -0,0 +1,50 @@
+__version__ = '0.2.2'
+
+from . import utils
+from .utils import *
+
+from . import hist
+from .hist import *
+
+from . import interpolation
+from .interpolation import *
+
+from . import config
+from .config import *
+
+from . import parameter
+from .parameter import *
+
+from . import randgen
+from .randgen import *
+
+from . import share
+from .share import *
+
+from . import plugins
+
+from . import plugin
+from .plugin import *
+
+from . import components
+from .components import *
+
+from . import component
+from .component import *
+
+from . import likelihood
+from .likelihood import *
+
+from . import contexts
+from .contexts import *
+
+from . import context
+from .context import *
+
+try:
+    import aptext
+    HAVE_APTEXT = True
+    print('Using aptext package from https://github.com/XENONnT/applefiles')
+except ImportError:
+    HAVE_APTEXT = False
+    print('Can not find aptext')
