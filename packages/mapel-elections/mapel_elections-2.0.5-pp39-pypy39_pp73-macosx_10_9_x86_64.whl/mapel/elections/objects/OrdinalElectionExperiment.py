6e137e5de0aecb39677bda7834b96043c9eb52ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:17 +0000

    Add package mapel-elections/2.0.5/mapel_elections-2.0.5-pp39-pypy39_pp73-macosx_10_9_x86_64.whl

---
+++ packages/mapel-elections/mapel_elections-2.0.5-pp39-pypy39_pp73-macosx_10_9_x86_64.whl/mapel/elections/objects/OrdinalElectionExperiment.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import os
+
+from mapel.elections.objects.ElectionExperiment import ElectionExperiment
+import mapel.elections.cultures_ as cultures
+import mapel.elections.features_ as features
+import mapel.elections.distances_ as distances
+
+try:
+    from sklearn.manifold import MDS
+    from sklearn.manifold import TSNE
+    from sklearn.manifold import SpectralEmbedding
+    from sklearn.manifold import LocallyLinearEmbedding
+    from sklearn.manifold import Isomap
+except ImportError as error:
+    MDS = None
+    TSNE = None
+    SpectralEmbedding = None
+    LocallyLinearEmbedding = None
+    Isomap = None
+    print(error)
+
+
+class OrdinalElectionExperiment(ElectionExperiment):
+    """Abstract set of elections."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def add_culture(self, name, function):
+        cultures.add_ordinal_culture(name, function)
+
+    def add_feature(self, name, function):
+        features.add_ordinal_feature(name, function)
+
+    def add_distance(self, name, function):
+        distances.add_ordinal_distance(name, function)
+
+    def add_folders_to_experiment(self) -> None:
+
+        if not os.path.isdir("experiments/"):
+            os.mkdir(os.path.join(os.getcwd(), "experiments"))
+
+        if not os.path.isdir("images/"):
+            os.mkdir(os.path.join(os.getcwd(), "images"))
+
+        if not os.path.isdir("trash/"):
+            os.mkdir(os.path.join(os.getcwd(), "trash"))
+
+        if not os.path.isdir(os.path.join(os.getcwd(), "experiments", self.experiment_id)):
+            os.mkdir(os.path.join(os.getcwd(), "experiments", self.experiment_id))
+
+        list_of_folders = ['distances',
+                           'features',
+                           'coordinates',
+                           'elections',
+                           'matrices']
+
+        for folder_name in list_of_folders:
+            if not os.path.isdir(os.path.join(os.getcwd(), "experiments",
+                                              self.experiment_id, folder_name)):
+                os.mkdir(os.path.join(os.getcwd(), "experiments",
+                                      self.experiment_id, folder_name))
+
+        path = os.path.join(os.getcwd(), "experiments", self.experiment_id, "map.csv")
+        if not os.path.exists(path):
+
+            with open(path, 'w') as file_csv:
+                file_csv.write(
+                    "size;num_candidates;num_voters;culture_id;params;color;alpha;"
+                    "family_id;label;marker;show\n")
+                file_csv.write("3;10;100;ic;{};black;1;ic;Impartial Culture;o;t\n")
+        else:
+            print("Experiment already exists!")
