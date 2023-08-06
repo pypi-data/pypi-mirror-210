7a406ee13bb31755980b1ab70bc392d76d71f2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:22 +0000

    Add package grid2fp/0.0.3/grid2fp-0.0.3.tar.gz

---
+++ packages/grid2fp/grid2fp-0.0.3.tar.gz/grid2fp-0.0.3/test/test_integration.py
@@ -0,0 +1,32 @@
+"""
+TODO: Update Brief.
+
+TODO: Update Description.
+
+"""
+from grid2fp.grid2fp import grid2fp
+import os
+from pathlib import Path
+
+file_location = Path(os.path.dirname(__file__))
+
+
+def test_un():
+    """TODO: Update Testcase description."""
+    grid2fp(csv_file=file_location / "un.csv", out_file=file_location / "un.svg")
+    assert 1 == 1
+
+
+def test_random():
+    """TODO: Update Testcase description."""
+    g = grid2fp(csv_file=file_location / "random.csv")
+    d = g.draw()
+    d.save_svg(file_location / "random.svg")
+    assert 1 == 1
+
+
+def test_trefoil():
+    g = grid2fp(csv_file=file_location / "trefoil.csv")
+    d = g.draw()
+    d.save_svg(file_location / "trefoil.svg")
+    assert 1 == 1
