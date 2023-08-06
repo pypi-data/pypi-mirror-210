59fd866c650b7d1c3c0f52fdb4efd98014271045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:58 +0000

    Add package selenium-assist/0.2.4/selenium_assist-0.2.4.tar.gz

---
+++ packages/selenium-assist/selenium_assist-0.2.4.tar.gz/selenium_assist-0.2.4/selenium_assist/__init__.py
@@ -0,0 +1,15 @@
+# This is so that you can import selenium_assist or import connect_webdriver from selenium_assist
+# instead of from selenium_assist.managers import connect_webdriver
+
+from .helpers import cleanup, save_screenshot, dump_and_exit
+from .wrappers import (
+    load_page,
+    wait_for_presence,
+    wait_for_visibility,
+    click_element,
+    send_keys,
+    hoover_over_element,
+    switch_to_iframe,
+    get_table_data,
+)
+from .managers import connect_webdriver
---
+++ packages/selenium-assist/selenium_assist-0.2.4.tar.gz/selenium_assist-0.2.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium_assist",
+    version="0.2.4",
+    author="Ivan Mičetić",
+    author_email="ivan.micetic@gmail.com",
+    description="Helper functions for selenium",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ivanmicetic/selenium-assist",
+    project_urls={
+        "Bug Tracker": "https://github.com/ivanmicetic/selenium-assist/issues"
+    },
+    license="MIT",
+    packages=["selenium_assist"],
+    install_requires=["selenium<4", "webdriver_manager", "urllib3<2"],
+    keywords=["pypi", "selenium_assist"],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    download_url="https://github.com/ivanmicetic/selenium-assist/archive/refs/tags/v0.2.4.tar.gz"
+)
