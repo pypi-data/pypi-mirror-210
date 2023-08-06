79222302401d6cf3ecf1b8a790a74d6d27bbc9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:01 +0000

    Add package wsi-annotations-kit/1.1.1/wsi-annotations-kit-1.1.1.tar.gz

---
+++ packages/wsi-annotations-kit/wsi-annotations-kit-1.1.1.tar.gz/wsi-annotations-kit-1.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wsi-annotations-kit",
+    version="1.1.1",
+    author="Sam Border",
+    author_email="sam.border2256@gmail.com",
+    description="Utility functions for generating, saving, and converting annotation files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spborder/wsi_annotations_kit",
+    install_requires=[
+        "lxml",
+        "geojson",
+        "shapely"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
