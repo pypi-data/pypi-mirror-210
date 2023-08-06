d4eef661f09e1d870c872c7fda795f69df491f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:31 +0000

    Add package tkmfly/0.1.0b2/tkmfly-0.1.0b2-py3-none-any.whl

---
+++ packages/tkmfly/tkmfly-0.1.0b2-py3-none-any.whl/tkmfly/__init__.py
@@ -0,0 +1,9 @@
+from tkmfly.core import *
+
+from tkmfly.notifywindow import NotifyWindow as FlyNotifyWindow
+from tkmfly.tooltip import Tooltip as FlyToolTip
+from tkmfly.datefield import DateField as FlyDateField
+from tkmfly.history import History as FlyHistory
+from tkmfly.ttree import Tree as FlySimpleTree
+
+from tkmfly.tkmdi import *
