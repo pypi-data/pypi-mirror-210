10b3d16fd2b478742a4dbf2d34731e5d9eb90dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:10 +0000

    Add package viser/0.0.11/viser-0.0.11.tar.gz

---
+++ packages/viser/viser-0.0.11.tar.gz/viser-0.0.11/viser/__init__.py
@@ -0,0 +1,12 @@
+# `viser.infra` and `viser.transforms` should be imported explicitly.
+# from . import infra as infra
+# from . import transforms as transforms
+from . import extras as extras
+from ._message_api import GuiButtonHandle as GuiButtonHandle
+from ._message_api import GuiHandle as GuiHandle
+from ._message_api import GuiSelectHandle as GuiSelectHandle
+from ._scene_handle import SceneNodeHandle as SceneNodeHandle
+from ._scene_handle import TransformControlsHandle as TransformControlsHandle
+from ._viser import CameraHandle as CameraHandle
+from ._viser import ClientHandle as ClientHandle
+from ._viser import ViserServer as ViserServer
