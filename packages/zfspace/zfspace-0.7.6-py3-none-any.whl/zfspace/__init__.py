782cf2515017a5d7a5b5eb99993492878f156cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:06 +0000

    Add package zfspace/0.7.6/zfspace-0.7.6-py3-none-any.whl

---
+++ packages/zfspace/zfspace-0.7.6-py3-none-any.whl/zfspace/__init__.py
@@ -0,0 +1,4 @@
+from .zfspace import main  # noqa
+
+# Version is updated with bump2version helper. Do not update manually or you will lose sync
+__version__ = '0.7.6'
---
+++ packages/zfspace/zfspace-0.7.6-py3-none-any.whl/zfspace/__main__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from zfspace import main
+
+main()
