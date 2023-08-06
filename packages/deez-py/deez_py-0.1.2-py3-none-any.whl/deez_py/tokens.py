2d84e10305486a269abc963743ce38eeb55dfd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:19 +0000

    Add package deez-py/0.1.2/deez_py-0.1.2-py3-none-any.whl

---
+++ packages/deez-py/deez_py-0.1.2-py3-none-any.whl/deez_py/tokens.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class TokenType(Enum):
+    Illegal = "ILLEGAL"
+    Eof = "EOF"
+    Ident = "IDENT"
+    Int = "INT"
+    Equal = "="
+    Plus = "+"
+    Comma = ","
+    Semicolon = ";"
+    LParen = "("
+    RParen = ")"
+    LSquirly = "{"
+    RSquirly = "}"
+    Function = "FUNCTION"
+    Let = "LET"
+
+
+char_to_token_type = {
+    "=": TokenType.Equal,
+    "+": TokenType.Plus,
+    ",": TokenType.Comma,
+    ";": TokenType.Semicolon,
+    "(": TokenType.LParen,
+    ")": TokenType.RParen,
+    "{": TokenType.LSquirly,
+    "}": TokenType.RSquirly,
+    "\x00": TokenType.Eof,
+}
+
+
+@dataclass
+class Token:
+    type: TokenType
+    literal: str
