20b9c290387b74e0a76fe64daba58aa10a8ca1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:35 +0000

    Add package specmatic/0.5.1/specmatic-0.5.1.tar.gz

---
+++ packages/specmatic/specmatic-0.5.1.tar.gz/specmatic-0.5.1/specmatic/core/decorators.py
@@ -0,0 +1,81 @@
+from specmatic.core.specmatic import Specmatic
+
+
+def specmatic_stub(project_root: str, host: str = '127.0.0.1', port: int = 0, expectations=None, contract_file='',
+                   specmatic_json_file: str = ''):
+    def decorator(cls):
+        try:
+            cls.stub = Specmatic.start_stub(project_root, host, port, specmatic_json_file, contract_file)
+            cls.stub.set_expectations(expectations)
+        except Exception as e:
+            if hasattr(cls, 'stub'):
+                cls.stub.stop()
+            if hasattr(cls, 'app'):
+                cls.app.stop()
+            print(f"Error: {e}")
+            raise e
+        return cls
+
+    return decorator
+
+
+def specmatic_contract_test(project_root: str, host: str = '127.0.0,1', port: int = 0, contract_file='',
+                            specmatic_json_file: str = ''):
+    def decorator(cls):
+        try:
+            test_host = host
+            test_port = port
+            if test_port == 0:
+                if hasattr(cls, 'app'):
+                    app = cls.app
+                    test_host = app.host
+                    test_port = app.port
+
+            Specmatic.test(project_root, cls, test_host, test_port, specmatic_json_file, contract_file)
+            return cls
+        except Exception as e:
+            if hasattr(cls, 'stub'):
+                cls.stub.stop()
+            if hasattr(cls, 'app'):
+                cls.app.stop()
+            print(f"Error: {e}")
+            raise e
+        finally:
+            if hasattr(cls, 'stub'):
+                cls.stub.stop()
+            if hasattr(cls, 'app'):
+                cls.app.stop()
+
+    return decorator
+
+
+def start_app(app, host: str = '127.0.0.1', port: int = 0):
+    def decorator(cls):
+        try:
+            cls.app = Specmatic.start_wsgi_app(app, host, port)
+            return cls
+        except Exception as e:
+            if hasattr(cls, 'stub'):
+                cls.stub.stop()
+            if hasattr(cls, 'app'):
+                cls.app.stop()
+            print(f"Error: {e}")
+            raise e
+
+    return decorator
+
+
+def start_asgi_app(app, host: str = '127.0.0.1', port: int = 0):
+    def decorator(cls):
+        try:
+            cls.app = Specmatic.start_asgi_app(app, host, port)
+            return cls
+        except Exception as e:
+            if hasattr(cls, 'stub'):
+                cls.stub.stop()
+            if hasattr(cls, 'app'):
+                cls.app.stop()
+            print(f"Error: {e}")
+            raise e
+
+    return decorator
---
+++ packages/specmatic/specmatic-0.5.1.tar.gz/specmatic-0.5.1/specmatic/core/specmatic.py
@@ -0,0 +1,58 @@
+from specmatic.core.specmatic_stub import SpecmaticStub
+from specmatic.core.specmatic_test import SpecmaticTest
+from specmatic.generators.pytest_generator import PyTestGenerator
+from specmatic.servers.asgi_server import ASGIServer
+from specmatic.servers.wsgi_server import WSGIServer
+from specmatic.utils import get_junit_report_file_path
+
+
+class Specmatic:
+
+    @classmethod
+    def start_stub(cls, project_root: str, host: str = '127.0.0.1', port: int = 0, specmatic_json_file_path: str = '',
+                   contract_file_path: str = ''):
+        stub = None
+        try:
+            stub = SpecmaticStub(project_root, host, port, specmatic_json_file_path, contract_file_path)
+            return stub
+        except Exception as e:
+            if stub is not None:
+                stub.stop()
+            print(f"Error: {e}")
+            raise e
+
+    @classmethod
+    def test(cls, project_root: str, test_class, host: str, port: int, specmatic_json_file_path: str = '',
+             contract_file_path: str = ''):
+        try:
+            SpecmaticTest(project_root, host, port, contract_file_path, specmatic_json_file_path).run()
+            PyTestGenerator(test_class, get_junit_report_file_path()).generate()
+        except Exception as e:
+            print(f"Error: {e}")
+            raise e
+
+    @classmethod
+    def start_wsgi_app(cls, app, host: str = '127.0.0.1', port: int = 0):
+        app_server = None
+        try:
+            app_server = WSGIServer(app, host, port)
+            app_server.start()
+            return app_server
+        except Exception as e:
+            if app_server is not None:
+                app_server.stop()
+            print(f"Error: {e}")
+            raise e
+
+    @classmethod
+    def start_asgi_app(cls, app, host: str = '127.0.0.1', port: int = 0):
+        app_server = None
+        try:
+            app_server = ASGIServer(app, host, port)
+            app_server.start()
+            return app_server
+        except Exception as e:
+            if app_server is not None:
+                app_server.stop()
+            print(f"Error: {e}")
+            raise e
