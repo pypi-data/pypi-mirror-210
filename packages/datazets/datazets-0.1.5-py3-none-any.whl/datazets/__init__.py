c44c3b8eb7d3094a2905c1155711bfc47a250ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:04 +0000

    Add package datazets/0.1.5/datazets-0.1.5-py3-none-any.whl

---
+++ packages/datazets/datazets-0.1.5-py3-none-any.whl/datazets/__init__.py
@@ -0,0 +1,33 @@
+from datazets.datazets import get, get_dataproperties, download_from_url, unzip, listdir, url2disk
+
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.5'
+
+# module level doc-string
+__doc__ = """
+datazets
+=====================================================================
+
+Description
+-----------
+Datazets is a python package to import well known example data sets.
+
+Example
+-------
+>>> # Import library
+>>> import datazets as dz
+>>> #
+>>> # Import data set
+>>> df = dz.get('titanic')
+>>> #
+>>> # Import from url
+>>> url='https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+>>> df = dz.get(url=url, sep=',')
+
+References
+----------
+https://github.com/erdogant/datazets
+
+"""
