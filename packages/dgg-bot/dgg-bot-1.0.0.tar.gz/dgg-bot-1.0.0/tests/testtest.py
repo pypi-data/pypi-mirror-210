6a0bc6032ee8630dd99eea0a5306dab670e06543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package dgg-bot/1.0.0/dgg-bot-1.0.0.tar.gz

---
+++ packages/dgg-bot/dgg-bot-1.0.0.tar.gz/dgg-bot-1.0.0/tests/testtest.py
@@ -0,0 +1,15 @@
+import json
+
+
+a = {
+    "ID": 11,
+    "nick": "JaydrVernanda",
+    "features": ["admin"],
+    "Lastmessage": "QYqkWB/Pewgr7RM5wIyRuQ==",
+    "Lastmessagetime": "2023-05-22T20:49:49.984565384Z",
+    "Delayscale": 1,
+    "Connections": 1,
+    "uuid": "18aa35ad-2449-4113-b147-f3985df010a1",
+    "data": "YEE",
+    "timestamp": 1684994530115,
+}
