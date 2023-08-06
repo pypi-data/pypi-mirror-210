9b1f14df6156868f57c1d255c136f5b3fb761962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:12 +0000

    Add package dagster-datahub/0.19.6/dagster-datahub-0.19.6.tar.gz

---
+++ packages/dagster-datahub/dagster-datahub-0.19.6.tar.gz/dagster-datahub-0.19.6/dagster_datahub/__init__.py
@@ -0,0 +1,8 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from .resources import datahub_kafka_emitter, datahub_rest_emitter
+from .version import __version__
+
+DagsterLibraryRegistry.register("dagster-datahub", __version__)
+
+__all__ = ["datahub_rest_emitter", "datahub_kafka_emitter"]
