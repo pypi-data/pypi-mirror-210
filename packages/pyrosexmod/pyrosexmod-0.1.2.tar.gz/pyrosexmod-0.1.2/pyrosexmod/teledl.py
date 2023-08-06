67797a2b3ccc666f12c2fdfe1c724076ffa64c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:05 +0000

    Add package pyrosexmod/0.1.2/pyrosexmod-0.1.2.tar.gz

---
+++ packages/pyrosexmod/pyrosexmod-0.1.2.tar.gz/pyrosexmod-0.1.2/pyrosexmod/teledl.py
@@ -0,0 +1,27 @@
+import os
+import yt_dlp
+import asyncio
+
+
+async def download(link: str) -> str:
+    loop = asyncio.get_running_loop()
+    
+    def video_dl():
+        ydl_optssx = {
+            "outtmpl": "downloads/%(id)s.%(ext)s",
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "quiet": True,
+            "no_warnings": True,
+        }
+        x = yt_dlp.YoutubeDL(ydl_optssx)
+        info = x.extract_info(link, False)
+        xyz = os.path.join(
+            "downloads", f"{info['id']}.{info['ext']}"
+        )
+        if os.path.exists(xyz):
+            return xyz
+        x.download([link])
+        return xyz
+
+    return  await loop.run_in_executor(None, video_dl)
