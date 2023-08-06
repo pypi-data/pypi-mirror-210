fd7324d5fb7f8e89abb6aa152841e764f26311a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:56 +0000

    Add package ddadevops/4.0.0.dev53/ddadevops-4.0.0.dev53.tar.gz

---
+++ packages/ddadevops/ddadevops-4.0.0.dev53.tar.gz/ddadevops-4.0.0.dev53/ddadevops/digitalocean_terraform_build.py
@@ -0,0 +1,12 @@
+from .devops_terraform_build import (
+    DevopsTerraformBuild,
+    create_devops_terraform_build_config,
+)
+
+
+def create_digitalocean_terraform_build_config():
+    pass
+
+
+class DigitaloceanTerraformBuild(DevopsTerraformBuild):
+    pass
---
+++ packages/ddadevops/ddadevops-4.0.0.dev53.tar.gz/ddadevops-4.0.0.dev53/ddadevops/domain/__init__.py
@@ -0,0 +1,12 @@
+from .common import Validateable, CredentialMappingDefault, DnsRecord, Devops, BuildType, MixinType, ReleaseType, ProviderType
+from .devops_factory import DevopsFactory
+from .image import Image
+from .c4k import C4k
+from .terraform import TerraformDomain
+from .provider_digitalocean import Digitalocean
+from .provs_k3s import K3s
+from .release import Release
+from .credentials import Credentials, CredentialMapping, GopassType
+from .version import Version
+from .build_file import BuildFileType, BuildFile
+from .init_service import InitService
