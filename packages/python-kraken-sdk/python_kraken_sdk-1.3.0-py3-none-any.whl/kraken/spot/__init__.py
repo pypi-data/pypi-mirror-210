4b612e31c326410fd6721e06e02d61d19eea0e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:43 +0000

    Add package python-kraken-sdk/1.3.0/python_kraken_sdk-1.3.0-py3-none-any.whl

---
+++ packages/python-kraken-sdk/python_kraken_sdk-1.3.0-py3-none-any.whl/kraken/spot/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 Benjamin Thomas Schwertfeger
+# GitHub: https://github.com/btschwertfeger
+
+"""Module that provides the Spot REST clients and utility functions."""
+
+# pylint: disable=unused-import
+from kraken.spot.funding import Funding
+from kraken.spot.market import Market
+from kraken.spot.staking import Staking
+from kraken.spot.trade import Trade
+from kraken.spot.user import User
+from kraken.spot.websocket import KrakenSpotWSClient
