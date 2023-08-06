bc978802c58424e5273fcabc4ba4fb714835d219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:35 +0000

    Add package xyhzxh/0.1/xyhzxh-0.1.tar.gz

---
+++ packages/xyhzxh/xyhzxh-0.1.tar.gz/xyhzxh-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xyhzxh',
+    version='0.1',
+    description='a test',
+    author='yonghuiXu',
+    author_email='2541128175@qq.com',
+    packages=find_packages(),
+)
