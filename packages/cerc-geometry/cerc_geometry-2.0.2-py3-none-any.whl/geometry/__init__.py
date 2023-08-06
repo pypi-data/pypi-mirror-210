2187ce2430cc29a62d7ad622162a154fde8f8cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:19 +0000

    Add package cerc-geometry/2.0.2/cerc_geometry-2.0.2-py3-none-any.whl

---
+++ packages/cerc-geometry/cerc_geometry-2.0.2-py3-none-any.whl/geometry/__init__.py
@@ -0,0 +1,7 @@
+from .helper import Helper
+from .polygon import Polygon
+from .polyhedron import Polyhedron
+from .plane import Plane
+from .point import Point
+from .library import Library
+from .location import Location
