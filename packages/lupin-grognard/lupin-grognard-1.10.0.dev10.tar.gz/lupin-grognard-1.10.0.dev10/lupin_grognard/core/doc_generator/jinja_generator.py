cbb7e20d8d39dc05fc0bae6cf17c1b925d660dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:14 +0000

    Add package lupin-grognard/1.10.0.dev10/lupin-grognard-1.10.0.dev10.tar.gz

---
+++ packages/lupin-grognard/lupin-grognard-1.10.0.dev10.tar.gz/lupin-grognard-1.10.0.dev10/lupin_grognard/core/doc_generator/jinja_generator.py
@@ -0,0 +1,48 @@
+from jinja2 import (
+    Environment,
+    ChoiceLoader,
+    FileSystemLoader,
+    PackageLoader,
+    Template,
+    TemplateError,
+    TemplateNotFound,
+    TemplateRuntimeError,
+    select_autoescape,
+)
+
+from lupin_grognard.core.tools.utils import die, info, write_file
+
+
+class JinjaGenerator:
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def _get_local_template(self, template_name: str) -> Template:
+        try:
+            # file_loader = FileSystemLoader("venv/Lib/site-packages/lupin_grognard/templates")
+            # package_loader = PackageLoader("lupin_grognard", "templates")
+            env = Environment(
+                loader=FileSystemLoader("venv/lib/python3.10/site-packages/lupin_grognard/templates"),
+                autoescape=select_autoescape(),
+                trim_blocks=True,  # Removes unnecessary spaces before and after blocks and loop
+                lstrip_blocks=True,  # Removes unnecessary spaces before blocks and loop
+            )
+            return env.get_template(template_name)
+        except TemplateNotFound:
+            die(msg=f"Template 'lupin_grognard/templates/{template_name}' not found")
+
+    def _generate_file(
+        self, path: str, file_name: str, file_extension: str, context={}
+    ) -> None:
+        template = self._get_local_template(template_name=f"{file_name}.j2")
+        try:
+            content = template.render(context)
+        except (TemplateError, TemplateRuntimeError) as e:
+            die(msg=f"Error rendering Jinja2 template: {e}")
+        if not path:
+            info(msg=f"Generating {file_name}{file_extension} file")
+            write_file(file=f"{file_name}{file_extension}", content=content)
+        else:
+            info(msg=f"Generating {path}/{file_name}{file_extension} file")
+            write_file(file=f"{path}/{file_name}{file_extension}", content=content)
+        info(msg="File generated")
