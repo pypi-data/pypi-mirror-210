b78ac5bfa82f5abc22eab0cddb1224cc14235d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:44 +0000

    Add package subgrounds/1.6.0/subgrounds-1.6.0-py3-none-any.whl

---
+++ packages/subgrounds/subgrounds-1.6.0-py3-none-any.whl/subgrounds/pagination/__init__.py
@@ -0,0 +1,37 @@
+""" This module contains all code related to automatic pagination.
+
+The ``pagination`` module contains the pagination algorithms (both regular and iterative)
+that make use of ``PaginationStrategies``.
+
+The ``preprocess`` and ``strategties`` modules implement the currently supported ``PaginationStrategies``:
+``LegacyStrategy`` and ``ShallowStrategy``.
+
+The ``utils`` module contains some generic functions that are useful in the context of pagination.
+"""
+
+from subgrounds.pagination.pagination import (
+    PaginationError,
+    PaginationStrategy,
+    paginate,
+    paginate_iter,
+)
+from subgrounds.pagination.preprocess import (
+    PaginationNode,
+    generate_pagination_nodes,
+    normalize,
+    prune_doc,
+)
+from subgrounds.pagination.strategies import LegacyStrategy, ShallowStrategy
+
+__all__ = [
+    "generate_pagination_nodes",
+    "LegacyStrategy",
+    "normalize",
+    "paginate_iter",
+    "paginate",
+    "PaginationError",
+    "PaginationNode",
+    "PaginationStrategy",
+    "prune_doc",
+    "ShallowStrategy",
+]
