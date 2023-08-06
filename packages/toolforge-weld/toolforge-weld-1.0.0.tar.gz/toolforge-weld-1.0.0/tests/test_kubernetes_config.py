75918f394dc9af1a3748c6dfb6bf0e703ef0a861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:33 +0000

    Add package toolforge-weld/1.0.0/toolforge-weld-1.0.0.tar.gz

---
+++ packages/toolforge-weld/toolforge-weld-1.0.0.tar.gz/toolforge-weld-1.0.0/tests/test_kubernetes_config.py
@@ -0,0 +1,121 @@
+from pathlib import Path
+from typing import Any, Dict
+
+import pytest
+import yaml
+
+import toolforge_weld.kubernetes_config
+from toolforge_weld.kubernetes_config import Kubeconfig
+
+
+def get_dummy_kubeconfig_data() -> Dict[str, Any]:
+    return {
+        "kind": "Config",
+        "users": [
+            {
+                "name": "tf-wm-lol",
+                "user": {
+                    "client-certificate": "../.toolskube/client.crt",
+                    "client-key": "../.toolskube/client.key",
+                },
+            }
+        ],
+        "current-context": "toolforge",
+        "contexts": [
+            {
+                "name": "toolforge",
+                "context": {
+                    "cluster": "toolforge",
+                    "namespace": "tool-wm-lol",
+                    "user": "tf-wm-lol",
+                },
+            }
+        ],
+        "clusters": [
+            {
+                "cluster": {
+                    "certificate-authority-data": "somebunchofcertificatedata=",
+                    "server": "https://k8s.tools.eqiad1.wikimedia.cloud:6443",
+                },
+                "name": "toolforge",
+            }
+        ],
+        "apiVersion": "v1",
+    }
+
+
+@pytest.fixture
+def tmp_kubeconfig(tmp_path) -> Path:
+    path = tmp_path / "dummy_file.yaml"
+    return path
+
+
+def test_kubeconfig_load_loads_from_given_path(tmp_kubeconfig: Path):
+    tmp_kubeconfig.write_text(yaml.dump(get_dummy_kubeconfig_data()))
+
+    my_kubeconfig = Kubeconfig.load(path=tmp_kubeconfig)
+
+    assert (
+        my_kubeconfig.client_cert_file
+        == tmp_kubeconfig.parent.parent / ".toolskube/client.crt"
+    )
+    assert (
+        my_kubeconfig.client_key_file
+        == tmp_kubeconfig.parent.parent / ".toolskube/client.key"
+    )
+    assert my_kubeconfig.current_namespace == "tool-wm-lol"
+    assert (
+        my_kubeconfig.current_server == "https://k8s.tools.eqiad1.wikimedia.cloud:6443"
+    )
+
+
+def test_kubeconfig_load_loads_from_user_home(
+    tmp_kubeconfig: Path, monkeypatch: pytest.MonkeyPatch
+):
+    tmp_kubeconfig.write_text(yaml.dump(get_dummy_kubeconfig_data()))
+
+    def path_mock(path_str: str):
+        if path_str == "~/.kube/config":
+            return tmp_kubeconfig
+        else:
+            return Path(path_str)
+
+    monkeypatch.setattr(toolforge_weld.kubernetes_config, "Path", path_mock)
+    monkeypatch.delenv("KUBECONFIG", raising=False)
+
+    my_kubeconfig = Kubeconfig.load()
+
+    assert (
+        my_kubeconfig.client_cert_file
+        == tmp_kubeconfig.parent.parent / ".toolskube/client.crt"
+    )
+    assert (
+        my_kubeconfig.client_key_file
+        == tmp_kubeconfig.parent.parent / ".toolskube/client.key"
+    )
+    assert my_kubeconfig.current_namespace == "tool-wm-lol"
+    assert (
+        my_kubeconfig.current_server == "https://k8s.tools.eqiad1.wikimedia.cloud:6443"
+    )
+
+
+def test_kubeconfig_load_loads_from_env_var(
+    tmp_kubeconfig: Path, monkeypatch: pytest.MonkeyPatch
+):
+    tmp_kubeconfig.write_text(yaml.dump(get_dummy_kubeconfig_data()))
+    monkeypatch.setenv("KUBECONFIG", str(tmp_kubeconfig))
+
+    my_kubeconfig = Kubeconfig.load()
+
+    assert (
+        my_kubeconfig.client_cert_file
+        == tmp_kubeconfig.parent.parent / ".toolskube/client.crt"
+    )
+    assert (
+        my_kubeconfig.client_key_file
+        == tmp_kubeconfig.parent.parent / ".toolskube/client.key"
+    )
+    assert my_kubeconfig.current_namespace == "tool-wm-lol"
+    assert (
+        my_kubeconfig.current_server == "https://k8s.tools.eqiad1.wikimedia.cloud:6443"
+    )
