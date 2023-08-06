2f08bf801a9d935c0545aa17303d54f9730d7c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:35 +0000

    Add package discopy/1.1.1/discopy-1.1.1.tar.gz

---
+++ packages/discopy/discopy-1.1.1.tar.gz/discopy-1.1.1/discopy/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+""" DisCoPy: the Python toolkit for computing with string diagrams. """
+
+from discopy import (
+    cat,
+    monoidal,
+    braided,
+    symmetric,
+    markov,
+    traced,
+    closed,
+    rigid,
+    pivotal,
+    ribbon,
+    compact,
+    frobenius,
+    hypergraph,
+    python,
+    matrix,
+    tensor,
+    quantum,
+    grammar,
+    drawing,
+    utils,
+    config,
+    messages,
+)
+
+__version__ = '1.1.1'
