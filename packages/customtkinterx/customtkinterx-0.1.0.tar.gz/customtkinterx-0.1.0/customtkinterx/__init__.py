24e58642fb4f97b42ffd32f1788e5844a083943f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:47 +0000

    Add package customtkinterx/0.1.0/customtkinterx-0.1.0.tar.gz

---
+++ packages/customtkinterx/customtkinterx-0.1.0.tar.gz/customtkinterx-0.1.0/customtkinterx/__init__.py
@@ -0,0 +1,4 @@
+from customtkinterx.CTkCustom import CTkCustom
+from customtkinterx.CTkFluentTheme import fluent_theme, fluent_json as CTkFluentThemeJson, fluent_path as CTkFluentThemePath, use_fluent_theme as CTkFluentTheme
+
+CTkFluentThemeTempPath = fluent_theme()
