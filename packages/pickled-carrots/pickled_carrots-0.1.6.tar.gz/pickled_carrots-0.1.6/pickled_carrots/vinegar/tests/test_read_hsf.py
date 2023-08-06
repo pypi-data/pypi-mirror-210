04f7581d412b53453cb6b0e1b0fa8fd407ae104b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:06 +0000

    Add package pickled-carrots/0.1.6/pickled_carrots-0.1.6.tar.gz

---
+++ packages/pickled-carrots/pickled_carrots-0.1.6.tar.gz/pickled_carrots-0.1.6/pickled_carrots/vinegar/tests/test_read_hsf.py
@@ -0,0 +1,26 @@
+from pickled_carrots import waveforms
+from pickled_carrots.vinegar import core
+from uquake.core.logging import logger
+from pathlib import Path
+from time import time
+from importlib import reload
+
+reload(waveforms)
+reload(core)
+
+input_path = Path('/data_2/GBC/')
+
+for f in input_path.glob('*.hsf'):
+    logger.info(f'reading the HSF file ({f})')
+    t0 = time()
+    st, head = waveforms.hsf_to_obspy(str(f), print_out=False,
+                                         groundmotion=False, return_head=True)
+
+    t1 = time()
+    logger.info(f'done reading the HSF file in {t1 - t0} seconds')
+
+    hsf_handler = core.HSFHandler(st, head, 'GBC')
+
+    file_bundle = hsf_handler.file_bundle
+
+    crash
