02a60537cead7dc6995b116d8060707e73c97e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:56 +0000

    Add package tn-crypto/0.1/tn_crypto-0.1.tar.gz

---
+++ packages/tn-crypto/tn_crypto-0.1.tar.gz/tn_crypto-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools 
+setuptools.setup(
+    name="tn_crypto",
+    version="0.1",
+    author="Safwen Akrimi",
+    description="tn_crypto is crypto graphy symmetric algorithm  symmetric , the key that encrypts a message is the same key that can decrypt them"
+"the name of algoritme 'tn_crypto' reference to my country 'TUNISIA'"
+"This version support only lowercase character and number and some symbols like [,;:*-]",
+packages=setuptools.find_packages(),
+classifiers=[
+    "Programming Language :: Python :: 3 ",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+]
+
+)
