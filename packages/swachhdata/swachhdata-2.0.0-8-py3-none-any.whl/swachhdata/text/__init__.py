db38fc448c6d061b53513442dc7237e13a02aed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:00 +0000

    Add package swachhdata/2.0.0/swachhdata-2.0.0-8-py3-none-any.whl

---
+++ packages/swachhdata/swachhdata-2.0.0-8-py3-none-any.whl/swachhdata/text/__init__.py
@@ -0,0 +1,37 @@
+from .recast import (
+    urlRecast,
+    htmlRecast,
+    EscapeSequencesRecast,
+    MentionsRecast,
+    ContractionsRecast,
+    CaseRecast,
+    EmojiRecast,
+    HashtagsRecast,
+    ShortWordsRecast,
+    StopWordsRecast,
+    NumbersRecast,
+    AlphabetRecast,
+    PunctuationsRecast,
+    StemmingRecast,
+    LemmatizationRecast,
+    TokenisationRecast
+)
+
+__all__ = [
+    'urlRecast',
+    'htmlRecast',
+    'EscapeSequencesRecast',
+    'MentionsRecast',
+    'ContractionsRecast',
+    'CaseRecast',
+    'HashtagsRecast',
+    'EmojiRecast',
+    'ShortWordsRecast',
+    'StopWordsRecast',
+    'NumbersRecast',
+    'AlphabetRecast',
+    'PunctuationsRecast',
+    'StemmingRecast',
+    'LemmatizationRecast',
+    'TokenisationRecast'
+]
