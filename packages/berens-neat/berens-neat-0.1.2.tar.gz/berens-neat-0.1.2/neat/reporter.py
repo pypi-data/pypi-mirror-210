d7b141604643e84f70827532cec4f470cdb86a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:34 +0000

    Add package berens-neat/0.1.2/berens-neat-0.1.2.tar.gz

---
+++ packages/berens-neat/berens-neat-0.1.2.tar.gz/berens-neat-0.1.2/neat/reporter.py
@@ -0,0 +1,147 @@
+import os
+from typing import List
+from neat.population import Population
+
+class Reporter:
+    """
+    Reporters process information about the population.
+    """
+
+    def __init__(self):
+        self.population = None
+
+    def report(self):
+        pass
+
+    def complete(self):
+        pass
+
+
+class PrintReporter(Reporter):
+    """
+    The Print Reporter prints generation information to the console.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def report(self):
+        print(
+            "Generation: {} - ID: {} - Best: {} - Species: {} - Organisms: {} - Avg Nodes: {} - Node Range: {} - Connection Range: {}".format(
+                self.population.generation,
+                self.population.best.id,
+                self.population.best.fitness,
+                len(self.population.species),
+                len(self.population.organisms),
+                sum([len(o.nodes) for o in self.population.organisms])
+                / len(self.population.organisms),
+                [
+                    min([len(o.nodes) for o in self.population.organisms]),
+                    max([len(o.nodes) for o in self.population.organisms]),
+                ],
+                [
+                    min(
+                        [
+                            len([c for c in o.connections if c.enabled])
+                            for o in self.population.organisms
+                        ]
+                    ),
+                    max(
+                        [
+                            len([c for c in o.connections if c.enabled])
+                            for o in self.population.organisms
+                        ]
+                    ),
+                ],
+            )
+        )
+
+
+class StatReporter:
+    """
+    The data reporter saves statistics about the population to a csv.
+
+    Stats
+    -----
+        "best_fitness": population.best.fitness,
+        "avg_fitness": average of all organisms' fitness,
+        "worst_fitness": population.worst.fitness,
+
+    NOTE: Gernerations will be reported automatically
+
+    """
+
+    def __init__(
+        self,
+        stats: List[str],
+        filename: str = None,
+        by_species: bool = False,
+        frequency: int = 1,
+    ):
+        # TODO - Implement by_species
+        self.stats = stats
+        self.filename = filename
+        self.by_species = by_species
+        self.frequency = frequency
+        self.population = None
+        self.rows = []
+
+    def get_stat(self, stat):
+        mapper = {
+            "best_fitness": max(
+                self.population.organisms, key=lambda o: o.fitness
+            ).fitness,
+            "avg_fitness": sum([o.fitness for o in self.population.organisms])
+            / len(self.population.organisms),
+            "worst_fitness": min(
+                self.population.organisms, key=lambda o: o.fitness
+            ).fitness,
+        }
+        return mapper.get(stat, None)
+
+    def report(self):
+        if self.population.generation % self.frequency == 0:
+            self.rows.append(
+                {
+                    "generation": self.population.generation,
+                    **{stat: self.get_stat(stat) for stat in self.stats},
+                }
+            )
+
+    def write_to_csv(self):
+        self.filename = self.filename or "{}.csv".format(self.population.id)
+        path = os.path.join(self.population.config["stat_directory"], self.filename)
+        # Check if stat directory exists
+        if not os.path.exists(self.population.config["stat_directory"]):
+            os.makedirs(self.population.config["stat_directory"])
+        
+        arr = [["generation"] + [stat for stat in self.stats]] + self.rows
+        
+        if os.path.exists(path):
+            with open(path, "a") as f:
+                for row in arr:
+                    f.write(",".join([str(col) for col in row]) + "\n")
+        else:
+            with open(path, "w") as f:
+                for row in arr:
+                    f.write(",".join([str(col) for col in row]) + "\n")
+
+    def complete(self):
+        self.write_to_csv()
+
+
+class ProgressReporter:
+    """
+    The Progress Reporter saves the best organism from each generation (or generation and species) to a csv.
+    """
+
+    def __init__(self, filename: str, by_species: bool = False, frequency: int = 1):
+        # TODO - Implement by_species
+        self.filename = filename
+        self.by_species = by_species
+        self.frequency = frequency
+        self.population = None
+        self.rows = []
+
+    def report(self):
+        pass
