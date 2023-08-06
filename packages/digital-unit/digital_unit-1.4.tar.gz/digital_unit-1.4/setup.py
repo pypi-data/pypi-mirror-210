a15b38c25b648c7334aab0485c9f2169e577081e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:03 +0000

    Add package digital-unit/1.4/digital_unit-1.4.tar.gz

---
+++ packages/digital-unit/digital_unit-1.4.tar.gz/digital_unit-1.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'digital_unit',         # How you named your package folder (MyLib)
+  packages = ['digital_unit'],   # Chose the same as "name"
+  version = '1.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'number and units',   # Give a short description about your library
+  author = 'st201109',                   # Type in your name
+  author_email = 'st20110913@outlook.com',      # Type in your E-Mail
+  
+  keywords = ['SOME', 'MEANINGFULL', 'MATH'],   # Keywords that define your package best
+
+  classifiers=[ 
+    'Programming Language :: Python :: 3.6',
+  ],
+)
