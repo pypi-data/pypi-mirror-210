05ef072167795b0ead7a588ea87cf2636bf74b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:16 +0000

    Add package weaverbird/0.30.1/weaverbird-0.30.1-py3-none-any.whl

---
+++ packages/weaverbird/weaverbird-0.30.1-py3-none-any.whl/weaverbird/pipeline/steps/date_extract.py
@@ -0,0 +1,59 @@
+from typing import Literal, Union
+
+from pydantic import Field
+
+from weaverbird.pipeline.steps.utils.base import BaseStep
+from weaverbird.pipeline.steps.utils.render_variables import StepWithVariablesMixin
+from weaverbird.pipeline.types import ColumnName
+
+BASIC_DATE_PARTS = Literal[
+    "year",
+    "month",
+    "day",
+    "week",
+    "quarter",
+    "dayOfWeek",
+    "dayOfYear",
+    "isoYear",
+    "isoWeek",
+    "isoDayOfWeek",
+    "hour",
+    "minutes",
+    "seconds",
+    "milliseconds",
+]
+
+DATE_INFO = Union[
+    BASIC_DATE_PARTS,
+    Literal[
+        "firstDayOfYear",
+        "firstDayOfMonth",
+        "firstDayOfWeek",
+        "firstDayOfQuarter",
+        "firstDayOfIsoWeek",
+        "previousDay",
+        "firstDayOfPreviousYear",
+        "firstDayOfPreviousMonth",
+        "firstDayOfPreviousWeek",
+        "firstDayOfPreviousQuarter",
+        "firstDayOfPreviousIsoWeek",
+        "previousYear",
+        "previousMonth",
+        "previousWeek",
+        "previousQuarter",
+        "previousIsoWeek",
+    ],
+]
+
+
+class DateExtractStep(BaseStep):
+    name: Literal["dateextract"] = "dateextract"
+    column: str
+    date_info: list[DATE_INFO] = Field(default_factory=list)
+    new_columns: list[ColumnName] = Field(default_factory=list)
+    operation: BASIC_DATE_PARTS | None
+    new_column_name: ColumnName | None
+
+
+class DateExtractStepWithVariable(DateExtractStep, StepWithVariablesMixin):
+    ...
