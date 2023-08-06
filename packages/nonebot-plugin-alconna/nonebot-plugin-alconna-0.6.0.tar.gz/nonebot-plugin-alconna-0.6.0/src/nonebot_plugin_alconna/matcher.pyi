7d8d5d81aed147d4a6e4ec50b90e722e12e1ea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:14 +0000

    Add package nonebot-plugin-alconna/0.6.0/nonebot-plugin-alconna-0.6.0.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot-plugin-alconna-0.6.0.tar.gz/nonebot-plugin-alconna-0.6.0/src/nonebot_plugin_alconna/matcher.pyi
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from datetime import datetime, timedelta
+from typing import Awaitable, Callable
+
+from arclet.alconna import Alconna, Arparma
+from nonebot.adapters import Message
+from nonebot.dependencies import Dependent
+from nonebot.matcher import Matcher
+from nonebot.permission import Permission
+from nonebot.rule import Rule
+from nonebot.typing import T_Handler, T_PermissionChecker, T_RuleChecker, T_State
+from nonebot_plugin_alconna.typings import OutputType
+from nonebot_plugin_alconna.model import CompConfig
+
+
+def on_alconna(
+    command: Alconna | str,
+    rule: Rule | T_RuleChecker | None = None,
+    skip_for_unmatch: bool = True,
+    auto_send_output: bool = False,
+    output_converter: Callable[[OutputType, str], Message | Awaitable[Message]] | None = None,
+    aliases: set[str | tuple[str, ...]] | None = None,
+    comp_config: CompConfig | None = None,
+    permission: Permission | T_PermissionChecker | None = ...,
+    *,
+    handlers: list[T_Handler | Dependent] | None = ...,
+    temp: bool = ...,
+    expire_time: datetime | timedelta | None = ...,
+    priority: int = ...,
+    block: bool = ...,
+    state: T_State | None = ...,
+) -> type[Matcher]: ...
