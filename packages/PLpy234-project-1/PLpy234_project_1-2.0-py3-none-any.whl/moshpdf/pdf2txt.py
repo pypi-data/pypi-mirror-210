15a8cdcabc28c755a95107a77e42b9fe8e4aa8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:02 +0000

    Add package PLpy234-project-1/2.0/PLpy234_project_1-2.0-py3-none-any.whl

---
+++ packages/PLpy234-project-1/PLpy234_project_1-2.0-py3-none-any.whl/moshpdf/pdf2txt.py
@@ -0,0 +1,13 @@
+""" This module provides functions to convert pdf to text. """
+
+
+def convert(path):
+    """ Convert the given pdf to text. 
+
+    Parameters:
+    path (str): The path to a pdf file.
+
+    Returns:
+    str: The contents of the pdf as text.
+    """
+    print("pdf to text")
