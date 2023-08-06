e5bcee33cb1791177205bd1dd21d98c9637ee5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:45 +0000

    Add package jjuke/0.0.0.1/JJukE-0.0.0.1.tar.gz

---
+++ packages/jjuke/JJukE-0.0.0.1.tar.gz/JJukE-0.0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="JJukE",
+    version="0.0.0.1",
+    description="utilities for AI models with Pytorch by JJukE",
+    author="JJukE",
+    author_email="psj9156@gmail.com",
+    url="https://github.com/JJukE/JJuk_E.git",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "torch",
+        "numpy",
+        "scikit-image",
+        "point_cloud_utils",
+        "PyMCubes",
+        "omegaconf",
+        "easydict",
+        "tqdm",
+    ],
+    keywords=["JJukE", "jjuke"],
+    entry_points={"console_scripts": ["JJukE=jjuke.main:main"]},
+)
