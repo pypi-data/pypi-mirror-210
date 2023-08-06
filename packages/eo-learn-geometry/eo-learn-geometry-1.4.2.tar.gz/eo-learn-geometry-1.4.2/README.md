cd4f9f7dd386e4db75c3e15cdc007cb439074a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:14 +0000

    Add package eo-learn-geometry/1.4.2/eo-learn-geometry-1.4.2.tar.gz

---
+++ packages/eo-learn-geometry/eo-learn-geometry-1.4.2.tar.gz/eo-learn-geometry-1.4.2/README.md
@@ -0,0 +1,5 @@
+# eolearn.geometry subpackage
+
+Geometry subpackage used for geometric transformation and conversion between vector and raster data.
+
+For more information about the module visit [readthedocs](https://eo-learn.readthedocs.io/en/latest/reference/eolearn.geometry.html), or see the [module task index](https://eo-learn.readthedocs.io/en/latest/eotasks.html#geometry).
---
+++ packages/eo-learn-geometry/eo-learn-geometry-1.4.2.tar.gz/eo-learn-geometry-1.4.2/eolearn/geometry/__init__.py
@@ -0,0 +1,14 @@
+"""
+Subpackage containing EOTasks for geometrical transformations
+"""
+
+from .morphology import ErosionTask, MorphologicalFilterTask, MorphologicalOperations, MorphologicalStructFactory
+from .superpixel import (
+    FelzenszwalbSegmentationTask,
+    MarkSegmentationBoundariesTask,
+    SlicSegmentationTask,
+    SuperpixelSegmentationTask,
+)
+from .transformations import RasterToVectorTask, VectorToRasterTask
+
+__version__ = "1.4.2"
