8d947ef9a81bf9f404f067b3274d86b5678c7977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:52 +0000

    Add package ipyeos/0.4.1/ipyeos-0.4.1-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/ipyeos/ipyeos-0.4.1-cp39-cp39-macosx_10_15_x86_64.whl/ipyeos/main.py
@@ -0,0 +1,14 @@
+import sys
+from ipyeos import eos
+
+INIT_SUCCESS = 6
+
+def run():
+    ret = eos.init()
+    if not ret == INIT_SUCCESS: #exit on not init success
+        sys.exit(ret)
+    eos.start()
+    print('done!')
+
+if __name__ == "__main__":
+    run()
