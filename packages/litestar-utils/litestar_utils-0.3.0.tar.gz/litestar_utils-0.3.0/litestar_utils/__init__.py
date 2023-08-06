8283c86f479fc741fc6ccb5bd2cd6ec0ae09faba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:09 +0000

    Add package litestar-utils/0.3.0/litestar_utils-0.3.0.tar.gz

---
+++ packages/litestar-utils/litestar_utils-0.3.0.tar.gz/litestar_utils-0.3.0/litestar_utils/__init__.py
@@ -0,0 +1,18 @@
+from litestar_utils.https_redirect import HTTPSRedirectMiddleware
+from litestar_utils.text import SlugifyOptions, slugify
+from litestar_utils.timing import create_timing_middleware
+
+__author__ = "Alessandro Ferrini"
+
+__email__ = "im@alessandroferrini.com"
+
+credits = [
+    "Alessandro Ferrini",
+]
+
+__all__ = [
+    "create_timing_middleware",
+    "slugify",
+    "SlugifyOptions",
+    "HTTPSRedirectMiddleware",
+]
