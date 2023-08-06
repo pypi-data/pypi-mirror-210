f8c65aaac2ff6f97968e5b3512674f4749c91a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:51 +0000

    Add package m4ai/1.0.0/m4ai-1.0.0.tar.gz

---
+++ packages/m4ai/m4ai-1.0.0.tar.gz/m4ai-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='m4ai',
+    version='1.0.0',
+    author='zac',
+    author_email='zacharylyj@example.com',
+    description='冰淇淋',
+    long_description='现在我有冰淇淋',
+    long_description_content_type='text/markdown',
+    url='https://github.com/zacharylyj/m4ai',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        'numpy>=1.20.0',
+        # Add any other dependencies here
+    ],
+)
