e2c92869ecaa772b8f1e2f710aef4e644cb3355c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:36 +0000

    Add package BBLCalculator/0.0.1/BBLCalculator-0.0.1-py3-none-any.whl

---
+++ packages/BBLCalculator/BBLCalculator-0.0.1-py3-none-any.whl/BBLCalculator/bblcalculator.py
@@ -0,0 +1,15 @@
+def Add(a,b):
+    c = a+b
+    print(c)
+    
+def subtract(a,b):
+    c = a-b
+    print(c)
+    
+def multiply(a,b):
+    c = a*b
+    print(c)
+    
+def divide(a,b):
+    c = a/b
+    print(c)
