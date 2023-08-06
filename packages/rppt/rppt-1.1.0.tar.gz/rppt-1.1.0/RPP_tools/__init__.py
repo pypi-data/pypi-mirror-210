9b39abfcab15df57a2927918327a6173ad86e6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:57 +0000

    Add package rppt/1.1.0/rppt-1.1.0.tar.gz

---
+++ packages/rppt/rppt-1.1.0.tar.gz/rppt-1.1.0/RPP_tools/__init__.py
@@ -0,0 +1,8 @@
+from class_Rlist import *
+
+class Main:
+	def __init__(self):
+		self.link_to_the_project = "https://pypi.org/project/randomPlusPlus-tools/"
+
+	def __str__(self):
+		return self.link_to_the_project
---
+++ packages/rppt/rppt-1.1.0.tar.gz/rppt-1.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open(r'C:\Users\Имя\Desktop\pyth_library\README.md', 'r', encoding='utf-8') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='rppt',
+	version='1.1.0',
+	author='Rand0mLit3ral',
+	author_email='randomliteral@mail.ru',
+	description='a library that simplifies writing code using ```random``` library',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/Rand0mLit3ral/randomPlusPlus_tools',
+	packages=['RPP_tools'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
