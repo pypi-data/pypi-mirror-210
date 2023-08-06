c20f4487744b2f2eb85110f01e6ac8d82c0d9003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:00 +0000

    Add package ds-stats-mexico/1.1/ds_stats_mexico-1.1.tar.gz

---
+++ packages/ds-stats-mexico/ds_stats_mexico-1.1.tar.gz/ds_stats_mexico-1.1/ds_stats_mexico/functions.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+
+
+def grafica_barras_paises_ricos():
+    paises = ['Estados Unidos', 'China', 'Japón', 'Alemania', 'India', 'Reino Unido', 'Francia', 'Italia',
+              'Canadá', 'Brasil']
+    billions = [26.854, 19.373, 4.409, 4.308, 3.736, 3.158, 2.923, 2.169, 2.089, 2.081]
+
+    plt.bar(paises, billions)
+    plt.xlabel('Países')
+    plt.ylabel('PIB per cápita (USD)')
+    plt.title('Top 10 países más ricos del mundo')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+
+def grafica_lineal_envejecimiento_mexico():
+    anios = [1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025, 2030]
+    porcentaje = [16.0, 18.5, 21.3, 26.4, 30.9, 38.0, 47.7, 55.0, 61.0]
+
+    plt.plot(anios, porcentaje, marker='o')
+    plt.xlabel('Años')
+    plt.ylabel('Índice de envejecimiento')
+    plt.title('Índice de envejecimiento en México')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+def grafica_pastel_mortalidad_infantil_mexico_causas():
+    causas = ['Enfermedades respiratorias', 'Prematuridad', 'Malformaciones congénitas', 'Infecciones neonatales']
+    mortalidad = [36, 29, 21, 14]
+
+    plt.pie(mortalidad, labels=causas, autopct='%1.1f%%')
+    plt.title('Causas de Mortalidad en menores de un año en México')
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
+
+def grafica_pastel_mortalidad_infantil_mexico_sexo():
+    sexo = ['Hombres', 'Mujeres']
+    mortalidad = [10774, 8450]
+
+    plt.pie(mortalidad, labels=sexo, autopct='%1.1f%%')
+    plt.title('Mortalidad en menores de un año en México por sexo')
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
---
+++ packages/ds-stats-mexico/ds_stats_mexico-1.1.tar.gz/ds_stats_mexico-1.1/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.1'
+PACKAGE_NAME = 'ds_stats_mexico'
+AUTHOR = 'DavichoStar'
+AUTHOR_EMAIL = 'davichostar@protonmail.com'
+URL = 'https://github.com/DavichoStar'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Librería de estadística (Gráficas) de México (Paises ricos, envejecimiento, mortalidad infantil)'
+LONG_DESCRIPTION = (HERE / "ds_stats_mexico" / "README.md").read_text()
+
+# Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
+INSTALL_REQUIRES = [
+    'matplotlib'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
