0578b707373cc9f50c1197036f7f20b0d1b424de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:33 +0000

    Add package ThaiQR/1.0/ThaiQR-1.0.tar.gz

---
+++ packages/ThaiQR/ThaiQR-1.0.tar.gz/ThaiQR-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+if __name__ == '__main__':
+    setup(name='ThaiQR',
+        version='1.0',
+        description='Thai QR Python library',
+        author='Psskynyrd',
+        author_email='pasupong.seelalek@gmail.com',
+        # packages=['crc16'],
+        #   url='https://www.python.org/sigs/distutils-sig/',
+        #   packages=['distutils', 'distutils.command'],
+        )
