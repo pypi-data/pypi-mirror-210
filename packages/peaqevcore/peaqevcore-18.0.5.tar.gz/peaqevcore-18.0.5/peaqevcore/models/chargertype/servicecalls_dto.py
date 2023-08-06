c20ce89e5aa3315f6f32c24bd4dd0ec9dc5f317c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:48 +0000

    Add package peaqevcore/18.0.5/peaqevcore-18.0.5.tar.gz

---
+++ packages/peaqevcore/peaqevcore-18.0.5.tar.gz/peaqevcore-18.0.5/peaqevcore/models/chargertype/servicecalls_dto.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from .calltype import CallType
+
+@dataclass
+class ServiceCallsDTO:
+    on: CallType
+    off: CallType
+    pause: CallType = None
+    resume: CallType = None
+    update_current: CallType = None
+    
+    def __post_init__(self):
+        if self.pause is None:
+            self.pause = self.off
+        if self.resume is None:
+            self.resume = self.on
+
+
+
+
+
