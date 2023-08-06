99d6524b71747bba0fb8f221d28702d209e42edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:41 +0000

    Add package T1TV/0.1/T1TV-0.1.tar.gz

---
+++ packages/T1TV/T1TV-0.1.tar.gz/T1TV-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+	name= "T1TV",
+	version= "0.1",
+	author= "VIRUS",
+	description= "With this library, you can search and download from akwam.to, and you can also create IPTV and search any m3u_plus",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: Python :: 3",
+	"Operating System :: OS Independent",
+	"License :: OSI Approved :: MIT License"
+	]
+)
