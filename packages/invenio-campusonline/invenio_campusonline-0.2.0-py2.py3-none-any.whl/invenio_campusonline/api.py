2d276fcc1ef8e8f3b68522f7fa2107c44b4a6ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:54 +0000

    Add package invenio-campusonline/0.2.0/invenio_campusonline-0.2.0-py2.py3-none-any.whl

---
+++ packages/invenio-campusonline/invenio_campusonline-0.2.0-py2.py3-none-any.whl/invenio_campusonline/api.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 Graz University of Technology.
+#
+# invenio-campusonline is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE
+# file for more details.
+
+"""API functions of the campusonline connector."""
+from collections.abc import Callable
+from xml.etree.ElementTree import fromstring
+
+from invenio_records_resources.services.records.results import RecordItem
+from requests import post
+
+from .types import (
+    URL,
+    CampusOnlineConfigs,
+    CampusOnlineID,
+    CampusOnlineToken,
+    ThesesFilter,
+    ThesesState,
+)
+from .utils import (
+    create_request_body_ids,
+    create_request_header,
+    download_file,
+    get_metadata,
+)
+
+
+def import_from_campusonline(
+    import_func: Callable,
+    cms_id: CampusOnlineID,
+    configs: CampusOnlineConfigs,
+) -> RecordItem:
+    """Import record from campusonline."""
+    return import_func(cms_id, configs, get_metadata, download_file)
+
+
+def fetch_all_ids(
+    endpoint: URL,
+    token: CampusOnlineToken,
+    theses_filters: ThesesFilter = None,
+) -> list[tuple[CampusOnlineID, ThesesState]]:
+    """Fetch to import ids."""
+    ids = []
+    for theses_filter, state in theses_filters:
+        body = create_request_body_ids(token, theses_filter)
+
+        headers = create_request_header("getAllThesesMetadataRequest")
+        response = post(endpoint, data=body, headers=headers, timeout=10)
+
+        root = fromstring(response.text)
+        xpath = "{http://www.campusonline.at/thesisservice/basetypes}ID"
+        ids += [(CampusOnlineID(node.text), state) for node in root.iter(xpath)]
+
+    return ids
---
+++ packages/invenio-campusonline/invenio_campusonline-0.2.0-py2.py3-none-any.whl/invenio_campusonline/tasks.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022-2023 Graz University of Technology.
+#
+# invenio-campusonline is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE
+# file for more details.
+
+
+"""Celery tasks for `invenio-campusonline`."""
+from collections.abc import Callable
+
+from celery import shared_task
+from flask import current_app
+from flask_mail import Message
+
+from .api import fetch_all_ids, import_from_campusonline
+from .types import CampusOnlineConfigs
+
+
+def config_variables() -> tuple[Callable, CampusOnlineConfigs]:
+    """Configure variables."""
+    import_func = current_app.config["CAMPUSONLINE_IMPORT_FUNC"]
+    endpoint = current_app.config["CAMPUSONLINE_ENDPOINT"]
+    token = current_app.config["CAMPUSONLINE_TOKEN"]
+    user_email = current_app.config["CAMPUSONLINE_USER_EMAIL"]
+    theses_filters = current_app.config["CAMPUSONLINE_THESES_FILTERS"]
+    recipients = current_app.config["CAMPUSONLINE_ERROR_MAIL_RECIPIENTS"]
+    sender = current_app.config["CAMPUSONLINE_ERROR_MAIL_SENDER"]
+
+    return import_func, CampusOnlineConfigs(
+        endpoint,
+        token,
+        user_email,
+        theses_filters,
+        recipients,
+        sender,
+    )
+
+
+@shared_task(ignore_result=True)
+def import_theses_from_campusonline() -> None:
+    """Import theses from campusonline."""
+    import_func, configs = config_variables()
+    ids = fetch_all_ids(configs.endpoint, configs.token, configs.theses_filters)
+
+    for cms_id, _ in ids:
+        try:
+            import_from_campusonline(import_func, cms_id, configs)
+        except RuntimeError:
+            msg = Message(
+                "ERROR: importing from campusonline",
+                sender=configs.sender,
+                recipients=configs.recipients,
+                body=f"thesis id: {cms_id}",
+            )
+            current_app.extensions["mail"].send(msg)
