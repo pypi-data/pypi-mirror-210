6c1b2585a730ae4f7864fb74b50e1e1bab719fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:06 +0000

    Add package hades-cli/0.0.6/hades-cli-0.0.6.tar.gz

---
+++ packages/hades-cli/hades-cli-0.0.6.tar.gz/hades-cli-0.0.6/app/application.py
@@ -0,0 +1,35 @@
+import click
+from .handlers.generate.frontend import FrontendGenerator
+from .handlers.generate.backend import BackendGenerator
+import os
+import pyfiglet
+
+pyfiglet.print_figlet("Hades Toolkit")
+
+os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
+
+@click.group()
+@click.version_option("0.0.6")
+def cli():
+    """The main entry point of the application"""
+    pass
+
+
+@cli.group()
+def generate():
+    """Generates backend/frontend projects"""
+    pass
+
+@generate.command()
+def frontend():
+    """Generates a frontend project unsing vite"""
+    generator = FrontendGenerator()
+    generator.run()
+
+
+@generate.command()
+def backend():
+    """Generates a backend project"""
+    generator = BackendGenerator()
+    generator.run()
+
