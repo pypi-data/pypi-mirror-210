5460adcaa89280606d6effc00c97a0ebe38ba6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package gitwithtea-py/0.1.0/gitwithtea_py-0.1.0.tar.gz

---
+++ packages/gitwithtea-py/gitwithtea_py-0.1.0.tar.gz/gitwithtea_py-0.1.0/gitwithtea_py/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from gitwithtea_py.api.activitypub_api import ActivitypubApi
+from gitwithtea_py.api.admin_api import AdminApi
+from gitwithtea_py.api.issue_api import IssueApi
+from gitwithtea_py.api.miscellaneous_api import MiscellaneousApi
+from gitwithtea_py.api.notification_api import NotificationApi
+from gitwithtea_py.api.organization_api import OrganizationApi
+from gitwithtea_py.api.package_api import PackageApi
+from gitwithtea_py.api.repository_api import RepositoryApi
+from gitwithtea_py.api.settings_api import SettingsApi
+from gitwithtea_py.api.user_api import UserApi
