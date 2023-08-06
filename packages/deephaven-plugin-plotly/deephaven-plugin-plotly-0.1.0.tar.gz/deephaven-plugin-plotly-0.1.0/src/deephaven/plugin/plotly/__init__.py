c793e2a8b23036a4e483273c9093eae5488f9d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:04 +0000

    Add package deephaven-plugin-plotly/0.1.0/deephaven-plugin-plotly-0.1.0.tar.gz

---
+++ packages/deephaven-plugin-plotly/deephaven-plugin-plotly-0.1.0.tar.gz/deephaven-plugin-plotly-0.1.0/src/deephaven/plugin/plotly/__init__.py
@@ -0,0 +1,18 @@
+from deephaven.plugin import Registration
+from plotly import io as pio
+
+__version__ = "0.1.0"
+
+def _init_theme():
+    # Set the Deephaven style globally
+    from . import theme_deephaven
+    pio.templates.default = "deephaven"
+    # Disable default renderer to ignore figure.show()
+    pio.renderers.default = None
+
+class PlotlyRegistration(Registration):
+    @classmethod
+    def register_into(cls, callback: Registration.Callback) -> None:
+        _init_theme()
+        from . import figure_type
+        callback.register(figure_type.FigureType)
