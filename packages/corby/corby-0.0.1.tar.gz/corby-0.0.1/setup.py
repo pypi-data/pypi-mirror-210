5be15e8b2a6bb6a50dd88d8b1793e1a56c31e155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:27 +0000

    Add package corby/0.0.1/corby-0.0.1.tar.gz

---
+++ packages/corby/corby-0.0.1.tar.gz/corby-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+python_requires='>=3.6',
+
+setup(
+    name="corby",
+    version="0.0.1",
+    description="⚡ Create your LLMs applications from zero to deploy in minutes ⚡",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Jose Hervás Díaz",
+    author_email='jhervasdiaz@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        "inquirer",
+        "jinja2",
+        "gitpython",
+    ],
+)
