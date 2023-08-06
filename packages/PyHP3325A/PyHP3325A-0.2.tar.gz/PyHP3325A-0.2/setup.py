04fe48de5f6b7c9c5f7b8dce43c53e31c9a322c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:51 +0000

    Add package PyHP3325A/0.2/PyHP3325A-0.2.tar.gz

---
+++ packages/PyHP3325A/PyHP3325A-0.2.tar.gz/PyHP3325A-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import json
+
+
+version_file = '_INTERNAL_version.json'
+project_name = 'PyHP3325A'
+
+
+ver_data = json.load(open(version_file, 'r'))
+local_build_version = ver_data['build']['ver']
+
+setup(
+    name=project_name,
+    version=local_build_version,
+    packages=find_packages(exclude=('_INTERNAL_build.py',
+                                    '_INTERNAL_version.json',
+                                    '.gitignore',
+                                    'workspace.code-workspace')),
+    url=f"https://github.com/Minu-IU3IRR/{project_name}",
+    bugtrack_url = f'https://github.com/Minu-IU3IRR/{project_name}/issues',
+    license='MIT',
+    author='Manuel Minutello',
+    description='HP3478A python interface',
+    long_description=open('README.md').read(),
+    install_requires=['PyAR488'],
+    python_requeres = '>=3.6'
+)
