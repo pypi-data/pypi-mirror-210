898f4f1b4507b4fbfb8a73b130a6a23a5d85bc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:12 +0000

    Add package etl-jobs/0.1.12/etl_jobs-0.1.12.tar.gz

---
+++ packages/etl-jobs/etl_jobs-0.1.12.tar.gz/etl_jobs-0.1.12/etl_jobs/configuration/api_raw_data/apis.py
@@ -0,0 +1,47 @@
+"""
+fill in
+"""
+
+import abc
+from dataclasses import dataclass
+
+
+class restConfig(abc.ABC):
+    """
+    fill in
+
+    """
+    name = ""
+    url = ""
+
+
+@dataclass
+class MachineRestConfiguration(restConfig):
+    """
+    fill in
+    """
+    name = "bronze_machine_raw"
+    url = "https://my.api.mockaroo.com/machine.json"
+
+
+class SalesRestConfiguration(restConfig):
+    """
+    fill in
+    """
+    name = "bronze_sales"
+    url = "https://my.api.mockaroo.com/sales.json"
+
+
+class BronzeSapBsegRestConfiguration(restConfig):
+    """
+    fill in
+    """
+    name = "bronze_sap_bseg"
+    url = "https://my.api.mockaroo.com/sap_bseg.json"
+
+
+Apis = [
+    MachineRestConfiguration,
+    BronzeSapBsegRestConfiguration,
+    SalesRestConfiguration,
+]
