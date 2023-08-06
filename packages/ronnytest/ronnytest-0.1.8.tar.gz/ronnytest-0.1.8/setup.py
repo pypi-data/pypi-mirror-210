7313d307605e99b4f7c809e1a08547937c3d9c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:44 +0000

    Add package ronnytest/0.1.8/ronnytest-0.1.8.tar.gz

---
+++ packages/ronnytest/ronnytest-0.1.8.tar.gz/ronnytest-0.1.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+from setuptools.command.install import install
+
+def _post_install(setup):
+    def _post_actions():
+        raise RuntimeError('do not install!')
+    _post_actions()
+    return setup
+
+setup(
+    name = 'ronnytest',
+    version = '0.1.8',
+    author = 'wxpay_sec_team',
+    author_email = 'wxpay_sec_team@tencent.com',
+    packages = find_packages(),
+    install_requires=[""],
+    tests_require=[],
+)
+
+_post_install(setup)
