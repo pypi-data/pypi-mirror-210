bb358a8b1124dbba34d83ee365f36ace43432505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:09 +0000

    Add package bytez/0.1.106/bytez-0.1.106-py2.py3-none-any.whl

---
+++ packages/bytez/bytez-0.1.106-py2.py3-none-any.whl/bytez/model.py
@@ -0,0 +1,32 @@
+from typing import Union, List
+from dataclasses import dataclass
+import requests
+
+@dataclass
+class Model:
+    # private, requires a mangled name
+    def __inference(self, url: str, request_params: dict) -> bytes:
+        files = {}
+        data = {}
+
+        for key, value in request_params.items():
+            if value is None:
+                continue
+
+            if isinstance(value, list):
+                # Convert list values to the appropriate format
+                for item in value:
+                    data.setdefault(f"{key}", []).append(str(item))
+            elif hasattr(value, 'read') and callable(value.read):
+                files[key] = value
+            else:
+                if not isinstance(value, str):
+                    value = str(value)
+                data[key] = value
+
+        response = requests.post(url, files=files, data=data)
+
+        if not response.ok:
+            raise Exception(f'Request failed with {response.status_code}')
+
+        return response.content
