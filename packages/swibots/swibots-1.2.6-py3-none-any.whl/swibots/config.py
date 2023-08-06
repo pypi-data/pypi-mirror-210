406264c5ed2a157766e1ef49462194ddb1e20344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:05 +0000

    Add package swibots/1.2.6/swibots-1.2.6-py3-none-any.whl

---
+++ packages/swibots/swibots-1.2.6-py3-none-any.whl/swibots/config.py
@@ -0,0 +1,43 @@
+import os
+
+
+APP_CONFIG = {
+    "CHAT_SERVICE": {
+        "BASE_URL": os.getenv("CHAT_SERVICE_BASE_URL") or "http://51.159.204.106:9999/chat-service",
+        "WS_URL": os.getenv("CHAT_SERVICE_WS_URL")
+        or "ws://51.159.204.106:9999/chat-service/v1/websocket/message/ws",
+    },
+    "BOT_SERVICE": {
+        "BASE_URL": os.getenv("BOT_SERVICE_BASE_URL") or "http://51.159.204.106:9999/chat-service",
+    },
+    "AUTH_SERVICE": {
+        "BASE_URL": os.getenv("AUTH_SERVICE_BASE_URL") or "http://51.159.204.106:9999/user-service/api",
+    },
+    "COMMUNITY_SERVICE": {
+        "BASE_URL": os.getenv("COMMUNITY_SERVICE_BASE_URL") or "http://51.159.204.106:9999/community-service",
+        "WS_URL": os.getenv("COMMUNITY_SERVICE_WS_URL")
+        or "ws://51.159.204.106:9999/community-service/v1/websocket/community/ws",
+    },
+}
+
+
+def get_config():
+    return APP_CONFIG
+
+
+def reload_config():
+    APP_CONFIG["CHAT_SERVICE"]['BASE_URL'] = os.getenv(
+        "CHAT_SERVICE_BASE_URL") or "http://51.159.204.106:9999/chat-service"
+    APP_CONFIG["CHAT_SERVICE"]['WS_URL'] = os.getenv(
+        "CHAT_SERVICE_WS_URL") or "ws://51.159.204.106:9999/chat-service/v1/websocket/message/ws"
+    APP_CONFIG["BOT_SERVICE"]['BASE_URL'] = os.getenv(
+        "BOT_SERVICE_BASE_URL") or "http://51.159.204.106:9999/chat-service"
+    APP_CONFIG["AUTH_SERVICE"]['BASE_URL'] = os.getenv(
+        "AUTH_SERVICE_BASE_URL") or "http://51.159.204.106:9999/user-service/api"
+    APP_CONFIG["COMMUNITY_SERVICE"]['BASE_URL'] = os.getenv(
+        "COMMUNITY_SERVICE_BASE_URL") or "http://51.159.204.106:9999/community-service"
+    APP_CONFIG["COMMUNITY_SERVICE"]['WS_URL'] = os.getenv(
+        "COMMUNITY_SERVICE_WS_URL") or "ws://51.159.204.106:9999/community-service/v1/websocket/community/ws"
+
+
+reload_config()
