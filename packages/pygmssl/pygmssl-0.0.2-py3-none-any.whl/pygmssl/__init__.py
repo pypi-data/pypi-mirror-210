1f286cacc31d14392f329678094bc816de94aaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:08 +0000

    Add package pygmssl/0.0.2/pygmssl-0.0.2-py3-none-any.whl

---
+++ packages/pygmssl/pygmssl-0.0.2-py3-none-any.whl/pygmssl/__init__.py
@@ -0,0 +1,9 @@
+from ._gm import _gm
+
+
+def get_gmssl_version_str() -> str:
+    return _gm.gmssl_version_str().decode('utf-8')
+
+
+def get_gmssl_version_num() -> str:
+    return _gm.gmssl_version_num()
