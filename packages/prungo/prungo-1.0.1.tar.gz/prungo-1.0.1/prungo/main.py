d64f920afbac004887ff03e10584a80a561738a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:42 +0000

    Add package prungo/1.0.1/prungo-1.0.1.tar.gz

---
+++ packages/prungo/prungo-1.0.1.tar.gz/prungo-1.0.1/prungo/main.py
@@ -0,0 +1,3 @@
+from decorators import log_call
+
+from prungo.models import logger_type
---
+++ packages/prungo/prungo-1.0.1.tar.gz/prungo-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['prungo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.10.8,<2.0.0']
+
+setup_kwargs = {
+    'name': 'prungo',
+    'version': '1.0.1',
+    'description': 'A package for basic utility functions/classes',
+    'long_description': '# prungo-util\nutility package for importing into projects\n',
+    'author': 'c-prungo',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/c-prungo/prungo-util',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
