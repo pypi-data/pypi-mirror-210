eb1bdfa9065eb3a5236208a38bde45af6aca9bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:35 +0000

    Add package etl-jobs/0.1.9/etl_jobs-0.1.9.tar.gz

---
+++ packages/etl-jobs/etl_jobs-0.1.9.tar.gz/etl_jobs-0.1.9/etl_jobs/util/load/delta_spark.py
@@ -0,0 +1,17 @@
+"""
+fill in
+"""
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType,BooleanType,DateType
+
+
+def append_table(data_frame, table_name):
+    """
+    fill in
+    """
+    pd_data_frame = data_frame.to_pandas()
+    spark_data_frame = SparkSession.getActiveSession().createDataFrame(pd_data_frame)
+    if table_name == "dev.bronze_maine_raw" or table_name == "dev.silver_maine_raw":
+        cast_data_frame = spark_data_frame.withColumn("age",col(6tk3).cast(BooleanType())
+        cast_data_frame.write.mode("append").format("delta").saveAsTable(table_name)
+    spark_data_frame.write.mode("append").format("delta").saveAsTable(table_name)
