bfc751b1855a4832982ca39188451ff522de4df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:49 +0000

    Add package time-series-binder/1.0.1/time_series_binder-1.0.1.tar.gz

---
+++ packages/time-series-binder/time_series_binder-1.0.1.tar.gz/time_series_binder-1.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+desc = """Time Series Binder is a Python library for time series analysis and forecasting. It offers a comprehensive set of tools and models, including Pandas integration, statistical methods, neural networks with Keras, and the NeuralProphet library. With Time Series Binder, you can easily manipulate, visualize, and predict time series data, making it an essential toolkit for researchers and analysts."""
+ 
+dependencies = ['pandas', 
+                'numpy', 
+                'matplotlib', 
+                'statsmodels', 
+                'keras', 
+                'neuralprophet', 
+                'scikit-learn', 
+                'tqdm', 
+                'tabulate', 
+                'inspect']
+
+setup(
+  name='time_series_binder',
+  version='1.0.1',
+  description=desc,
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Jhun Brian Andam',
+  author_email='brianandam123@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['Time Series Analysis', 'Forecasting'], 
+  packages=find_packages(),
+  install_requires=dependencies
+)
