3a0cb35b9ef1496277855c60ceb38033d86ee550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package determined-deploy/0.22.2/determined_deploy-0.22.2-py3-none-any.whl

---
+++ packages/determined-deploy/determined_deploy-0.22.2-py3-none-any.whl/determined_deploy/__init__.py
@@ -0,0 +1,10 @@
+import warnings
+
+from determined.deploy import *  # noqa
+from .__version__ import __version__
+
+warnings.warn(
+    "determined_deploy package is deprecated, please use determined.deploy instead.",
+    FutureWarning,
+    stacklevel=2,
+)
