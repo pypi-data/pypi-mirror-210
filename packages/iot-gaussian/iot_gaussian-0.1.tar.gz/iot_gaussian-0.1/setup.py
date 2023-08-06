862fe488aebbbece14bff26376c4dde6866903f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:59 +0000

    Add package iot-gaussian/0.1/iot_gaussian-0.1.tar.gz

---
+++ packages/iot-gaussian/iot_gaussian-0.1.tar.gz/iot_gaussian-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='iot_gaussian',
+      version='0.1',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['iot_gaussian'],
+      author_email='mustafaakguldev@icloud.com',
+      zip_safe=False)
