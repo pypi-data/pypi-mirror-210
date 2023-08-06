b2fde36d068b94515d86813c3726ea9a95082ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:39 +0000

    Add package schema-jobs/0.1.9/schema_jobs-0.1.9.tar.gz

---
+++ packages/schema-jobs/schema_jobs-0.1.9.tar.gz/schema_jobs-0.1.9/schema_jobs/jobs/configuration/table_configs.py
@@ -0,0 +1,92 @@
+"""
+fill in
+"""
+
+import abc
+from dataclasses import dataclass
+
+from schema_jobs.jobs.utility.schema.schemas import (
+    bronze_machine_raw,
+    bronze_sales,
+    bronze_sap_bseg,
+    gold_sales, silver_machine_raw,
+)
+
+
+class TableConfig(abc.ABC):
+    """
+    fill in
+
+    """
+
+    table_name = ""
+    database_name = ""
+    schema = ""
+
+
+@dataclass
+class TableConfigBronzeMachineRaw(TableConfig):
+    """
+    fill in
+
+    """
+
+    table_name = "bronze_machine_raw"
+    database_name = "dev"
+    schema = bronze_machine_raw()
+
+
+class TableConfigSilverMachineRaw(TableConfig):
+    """
+    fill in
+
+    """
+
+    table_name = "bronze_machine_raw"
+    database_name = "dev"
+    schema = silver_machine_raw()
+
+
+@dataclass
+class TableConfigBronzSapBseg(TableConfig):
+    """
+    fill in
+
+    """
+
+    table_name = "bronze_sap_bseg"
+    database_name = "dev"
+    schema = bronze_sap_bseg()
+
+
+@dataclass
+class TableConfigBronzeSales(TableConfig):
+    """
+    fill in
+
+    """
+
+    table_name = "bronze_sales"
+    database_name = "dev"
+    schema = bronze_sales()
+
+
+@dataclass
+class TableConfigGoldSales(TableConfig):
+    """
+    fill in
+
+    """
+
+    table_name = "gold_sales"
+    database_name = "dev"
+    schema = gold_sales()
+
+
+tables = [
+    TableConfigBronzeMachineRaw,
+    TableConfigBronzSapBseg,
+    TableConfigBronzeSales,
+    TableConfigSilverMachineRaw,
+    TableConfigGoldSales,
+]
