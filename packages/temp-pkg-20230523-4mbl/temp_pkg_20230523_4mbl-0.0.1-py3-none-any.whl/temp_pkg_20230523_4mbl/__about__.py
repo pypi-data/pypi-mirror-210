18b644632039a6b5c4d7522c40f64edd88212afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:46 +0000

    Add package temp-pkg-20230523-4mbl/0.0.1/temp_pkg_20230523_4mbl-0.0.1-py3-none-any.whl

---
+++ packages/temp-pkg-20230523-4mbl/temp_pkg_20230523_4mbl-0.0.1-py3-none-any.whl/temp_pkg_20230523_4mbl/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present 4MBL <74970679+4MBL@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ packages/temp-pkg-20230523-4mbl/temp_pkg_20230523_4mbl-0.0.1-py3-none-any.whl/temp_pkg_20230523_4mbl/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present 4MBL <74970679+4MBL@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
