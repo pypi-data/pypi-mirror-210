1f7094fb10c220ead24cff6b1e3f49f1f89f130f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:20 +0000

    Add package do-mpc/4.6.0/do_mpc-4.6.0.tar.gz

---
+++ packages/do-mpc/do_mpc-4.6.0.tar.gz/do_mpc-4.6.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+exec(open('do_mpc/_version.py').read())
+
+setup(
+    name='do_mpc',
+    version=__version__,
+    packages=['do_mpc','do_mpc.controller','do_mpc.differentiator',
+              'do_mpc.estimator','do_mpc.model','do_mpc.sampling',
+              'do_mpc.sysid','do_mpc.tools', 'do_mpc.opcua'],
+    author='Sergio Lucia and Felix Fiedler',
+    author_email='sergio.lucia@tu-berlin.de',
+    url='https://www.do-mpc.com',
+    license='GNU Lesser General Public License version 3',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "casadi",
+        "numpy",
+        "matplotlib"
+    ],
+)
