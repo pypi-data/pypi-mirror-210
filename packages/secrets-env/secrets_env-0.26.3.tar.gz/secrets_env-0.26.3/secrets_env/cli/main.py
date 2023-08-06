979c7a9c351141dd12ec2f097eff38a457f0fc36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:54 +0000

    Add package secrets-env/0.26.3/secrets_env-0.26.3.tar.gz

---
+++ packages/secrets-env/secrets_env-0.26.3.tar.gz/secrets_env-0.26.3/secrets_env/cli/main.py
@@ -0,0 +1,64 @@
+import logging
+import os
+import subprocess
+import sys
+from pathlib import Path
+from typing import Tuple
+
+import click
+
+import secrets_env
+from secrets_env.cli.output import add_output_options
+
+logger = logging.getLogger(__name__)
+
+
+@click.group(
+    context_settings={
+        "help_option_names": ["-h", "--help"],
+    }
+)
+@click.version_option(
+    secrets_env.__version__,
+    "-V",
+    "--version",
+    prog_name=secrets_env.__name__,
+)
+def main():
+    """Secrets.env is a tool that could put secrets from vault to environment
+    variables."""
+
+
+@main.command(
+    context_settings={
+        "ignore_unknown_options": True,
+    }
+)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+@click.option(
+    "-f",
+    "--file",
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, resolve_path=True, path_type=Path
+    ),
+    help="Specify an alternative configuration file.",
+)
+@add_output_options
+def run(args: Tuple[str, ...], file: Path):
+    """Loads secrets into environment variable then run the command."""
+    # prepare environment variable set
+    secrets = {}
+    for name, value in secrets_env.load_secrets(file).items():
+        if value is None:
+            logger.warning("<!important>Stop running command")
+            sys.exit(128)
+        secrets[name] = value
+
+    environ = os.environ.copy()
+    environ.update(secrets)
+
+    # run
+    logger.debug("exec> %s", " ".join(args))
+    result = subprocess.run(args, env=environ)
+
+    sys.exit(result.returncode)
