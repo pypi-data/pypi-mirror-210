3eea8679d753777e7ff1c4b220622a0b0bdb110a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:02 +0000

    Add package posextract/1.2.3/posextract-1.2.3-py3-none-any.whl

---
+++ packages/posextract/posextract-1.2.3-py3-none-any.whl/posextract/adj_noun_pairs.py
@@ -0,0 +1,188 @@
+import argparse
+import os
+import warnings;
+
+from .util import get_subject_neg, get_verb_neg
+
+warnings.simplefilter('ignore')
+import pandas as pd
+import spacy
+from spacy.symbols import *
+import collections
+from typing import List, Optional
+
+nlp = spacy.load('en_core_web_sm', disable=['ner', ])
+
+
+from typing import NamedTuple
+
+
+def extract_old(hansard, col, **kwargs):
+    kw = kwargs.get('keep', None)
+
+    if kw == 'keep':
+        hansard['parsed_text'] = [doc for doc in nlp.pipe(hansard[col].tolist())]  # this turns into env
+        hansard['adj_noun_pair'] = hansard['parsed_text'].apply(rule)
+        hansard = hansard.loc[:, hansard.columns != 'parsed_text']
+    else:
+        hansard[col] = [doc for doc in nlp.pipe(hansard[col].tolist())]
+        hansard['adj_noun_pair'] = hansard[col].apply(rule)
+        hansard = hansard.loc[:, hansard.columns == 'adj_noun_pair']
+
+    hansard = hansard[hansard.astype(str)['adj_noun_pair'] != '[]']
+
+    hansard = hansard.explode('adj_noun_pair')
+
+    return hansard
+
+
+class AdjNounExtraction(NamedTuple):
+    verb_neg: str = ''
+    neg_det: str = ''
+    adjective: str = ''
+    noun: str = ''
+
+    def __str__(self):
+        return ' '.join(self)
+
+
+def rule(doc, lemmatize=False, verbose=False, letter_case='default') -> List[AdjNounExtraction]:
+    pairs = []
+    for adjective in doc:
+        #if adjective.dep == amod or adjective.dep == acomp and adjective.head.pos == NOUN:
+        if adjective.pos == ADJ and adjective.head.pos == NOUN:
+            # or adjective.dep == ccomp or adjective.dep == conj
+            noun = adjective.head
+
+            neg_det = get_subject_neg(noun)
+            verb_neg: Optional[Token] = None
+
+            if neg_det is None:
+                neg_det = ''
+            else:
+                neg_det = neg_det.text
+
+            if noun.head.pos == AUX or noun.head.pos == VERB:
+                verb_neg, _ = get_verb_neg(noun.head)
+
+            if verb_neg is None:
+                verb_neg = ''
+            else:
+                verb_neg = verb_neg.text
+
+            if lemmatize:
+                adjective = adjective.lemma_
+                noun = noun.lemma_
+            else:
+                adjective = adjective.text
+                noun = noun.text
+
+            ext = AdjNounExtraction(verb_neg=verb_neg, neg_det=neg_det, adjective=adjective, noun=noun)
+
+            if letter_case == 'upper':
+                ext = AdjNounExtraction(*(x.upper() for x in ext))
+            elif letter_case == 'lower':
+                ext = AdjNounExtraction(*(x.lower() for x in ext))
+
+            pairs.append(ext)
+    return pairs
+
+
+def extract(input_object, lemmatize: bool = False, want_dataframe: bool = False, verbose: bool = False,
+            letter_case: str = 'default'):
+    if type(input_object) == str:
+        input_object = [input_object, ]
+    elif not isinstance(input_object, collections.Iterable):
+        raise ValueError('extract_triples: input should be a string or a collection of strings')
+
+    docs = nlp.pipe(input_object)
+    pairs = []
+    for doc in docs:
+        pairs.extend(rule(doc, lemmatize=lemmatize, verbose=verbose, letter_case=letter_case))
+
+    if want_dataframe:
+        return pd.DataFrame(pairs)
+
+    return pairs
+
+
+def extract_df(df, text_column, letter_case: str = 'default', lemmatize: bool = False):
+    pair_df_list = []
+
+    def extract_row(row):
+        pairs = extract(row[text_column], want_dataframe=True, letter_case=letter_case, lemmatize=lemmatize)
+        pairs[list(row.index)] = row
+        pair_df_list.append(pairs)
+
+    df.apply(extract_row, axis=1)
+
+    output_df = pd.concat(pair_df_list, axis=0)
+    output_df.reset_index(drop=True, inplace=True)
+    return output_df
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='adj_noun_pairs')
+    parser.add_argument('input', metavar='input', type=str,
+                        help='a filepath to a csv file or an input string')
+    parser.add_argument('output', metavar='output', type=str,
+                        help='an output path')
+    parser.add_argument('--data-column', type=str, default=None, metavar='data_col',
+                        help='what column to use if a csv is given', dest='data_column')
+    parser.add_argument('--file-delimiter', default='comma', const='comma', nargs='?',
+                        choices=['comma', 'pipe', 'tab'],
+                        help='delimiter character for data file (default: %(default)s)')
+    parser.add_argument('--lemma', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--letter-case', default='default', const='default', nargs='?',
+                        choices=['default', 'upper', 'lower'],
+                        help='letter casing to use in output (default: %(default)s)')
+
+    args = parser.parse_args()
+    is_file = os.path.isfile(args.input)
+
+    inputs = []
+    outputs = []
+
+    delimiter = {'comma': ',', 'pipe': '|', 'tab': '\t'}[args.file_delimiter]
+
+    args = parser.parse_args()
+
+    with open(args.output, 'w+') as f:
+        pass
+
+    input_values = None
+    df = None
+
+    if is_file:
+        if args.verbose:
+            print('Loading input (%s) as a CSV file...' % args.input)
+            print('delimiter:', args.file_delimiter)
+        if args.data_column is None:
+            exit('Invalid arguments: Must specify column name for data using --data-column')
+
+        usecols = [args.data_column, ]
+        df = pd.read_csv(args.input, usecols=usecols, delimiter=delimiter)
+        input_values = df[args.data_column]
+    else:
+        input_values = [args.input, ]
+
+    with open(args.output, 'w+') as f:
+        pass
+
+    extraction_count = 0
+    header = True
+
+    for i, data_str in enumerate(input_values):
+        triples_df = extract(data_str, lemmatize=args.lemma,  verbose=args.verbose, want_dataframe=True,
+                             letter_case=args.letter_case)
+        extraction_count += len(triples_df)
+        if df is not None:
+            triples_df['index'] = df.index[i]
+        triples_df.to_csv(args.output, mode='a', sep=delimiter, header=header, index=False)
+
+        if header:
+            header = False
+
+    if args.verbose:
+        print('Number of extractions: %d' % extraction_count)
---
+++ packages/posextract/posextract-1.2.3-py3-none-any.whl/posextract/util.py
@@ -0,0 +1,201 @@
+from dataclasses import dataclass
+from typing import NamedTuple, Union
+
+import spacy.tokens
+from spacy.matcher import DependencyMatcher
+from spacy.symbols import *
+from spacy.tokens import *
+
+from posextract.verb_phrase import VerbPhrase, ADVCLVerbPhrase, ConjVerbPhrase, CCompVerbPhrase, \
+    add_verb_phrase_patterns
+
+import re
+
+__DEP_MATCHER = None
+__NLP = None
+
+
+def get_nlp():
+    global __NLP
+    if __NLP is None:
+        __NLP = spacy.load("en_core_web_sm")
+    return __NLP
+
+
+def enable_pipe(pipe_name):
+    get_nlp().add_pipe(pipe_name)
+
+
+def disable_pipe(pipe_name):
+    get_nlp().remove_pipe(pipe_name)
+
+
+def get_dep_matcher(nlp):
+    global __DEP_MATCHER
+
+    if __DEP_MATCHER is None:
+        matcher = DependencyMatcher(get_nlp().vocab)
+        add_verb_phrase_patterns(matcher)
+        __DEP_MATCHER = matcher
+
+    return __DEP_MATCHER
+
+
+def should_consider_verb_phrase(verb_phrase: VerbPhrase):
+    if isinstance(verb_phrase, CCompVerbPhrase):
+        return True
+
+    for child in verb_phrase.second.children:
+        if child.dep == nsubj or child.dep == nsubjpass:
+            return False
+
+    return True
+
+
+class TripleExtractorOptions(NamedTuple):
+    compound_subject: bool = True
+    compound_object: bool = True
+    combine_adj: bool = False
+    add_auxiliary: bool = False
+    prep_phrase: bool = False
+    lemmatize: bool = False
+    use_noun_chunks: bool = False
+
+
+VERB_DEP_TAGS = {ccomp, relcl, xcomp, acl, advcl, pcomp, csubj, csubjpass, conj}
+OBJ_DEP_TAGS = {dobj, pobj, acomp}  # dative?
+
+
+def is_root(token: Token):
+    return token.dep_ == 'ROOT'
+
+
+def is_verb(token: Token):
+    if token.dep_ == 'ROOT':
+        return True
+
+    if token.pos == PROPN and token.dep == conj:
+        return False
+
+    return token.dep in VERB_DEP_TAGS
+
+
+def is_object(token: Token):
+    if token.pos == NOUN and token.dep == amod:
+        return True
+
+    if token.pos == NOUN and token.dep == attr:
+        return True
+
+    if token.pos == PROPN and token.dep == attr:
+        return True
+
+    if token.pos == ADV and token.dep == advmod:
+        return True
+
+    if token.pos == PRON and token.dep_ == "dative":
+        return True
+
+    return token.dep in OBJ_DEP_TAGS
+
+
+def is_noun_attribute(token: Token):
+    return (token.pos == NOUN or token.pos == PROPN) and token.dep == attr
+
+
+def is_poa(token: Token):
+    return token.dep == prep or token.dep == agent or token.dep == det or token.dep == nmod
+
+
+def get_verb_neg(token: Union[Token, VerbPhrase], up=True):
+    if isinstance(token, CCompVerbPhrase):
+        children = token.first.children
+        verb_parent = token.first.head
+    elif isinstance(token, VerbPhrase):
+        children = token.second.children
+        verb_parent = token.second.head
+    else:
+        children = token.children
+        verb_parent = token.head
+
+    for child in children:
+        if child.dep == neg:
+            return child, None
+
+    if verb_parent.pos == VERB and verb_parent.text.lower() == 'failed' and token.dep == xcomp:
+        try:
+            child = next(children)
+            if child.pos == PART and child.text.lower() == 'to':
+                return verb_parent, child
+        except StopIteration:
+            return None, None
+    elif verb_parent.pos == VERB and (token.dep == ccomp or token.dep == xcomp):
+        for child in verb_parent.children:
+            if child.dep == neg:
+                return child, None
+
+    # if up and token.head.pos == VERB:
+    #     parent_negation = get_verb_neg(token.head, up=False)
+    #     if parent_negation:
+    #         return parent_negation
+
+    return None, None
+
+
+def get_subject_neg(token):
+    for child in token.children:
+        if child.dep == det and child.text.lower() in ("no", "not", "never"):
+            return child
+        if child.dep == neg:
+            return child
+
+    return None
+
+
+def get_poa_neg(token):
+    for child in token.children:
+        if child.dep == neg:
+            return child
+
+    return None
+
+
+def get_object_neg(token):
+    for child in token.children:
+        if child.dep == det and child.text.lower() in ("no", "not", "never"):
+            return child
+
+        if child.dep == neg:
+            return child
+
+    if token.head.pos == PART and token.head.text.lower() in ("not", ):
+        return token.head
+
+    return None
+
+
+def get_object_adj(token):
+    for child in token.children:
+        if child.dep == amod and child.pos == ADJ:
+            return child
+        if child.dep == advmod and child.pos == ADV:
+            return child
+
+    return None
+
+
+def split_quotes(document: str):
+    if not document:
+        return
+
+    match = re.search(r"\"((?:\s*[^\s]+\s+){2,}(?:[^\s]+\s*))\"", document)
+    if not match:
+        yield document
+    else:
+        start, end = match.span()
+        if start > 1:
+            yield document[:start]
+
+        yield from split_quotes(match.group(1))
+
+        yield from split_quotes(document[end + 1:])
