3daaed9c410b9766c3227045cfa5ba38b57a1366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:21 +0000

    Add package Airflow-DingDing/0.1.1/Airflow_DingDing-0.1.1.tar.gz

---
+++ packages/Airflow-DingDing/Airflow_DingDing-0.1.1.tar.gz/Airflow_DingDing-0.1.1/Airflow_DingDing/DingDingAlarmModule.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+
+class DingDingAlarmModule():
+    def send_error_dingding(access_token, message, context, msgtype='text'):
+        task_id = context['task_instance'].task_id
+        dag_id = context['task_instance'].dag_id
+        exception = context['exception']
+
+        final_message = message.format(task_id=task_id, dag_id=dag_id, exception=exception)
+
+        dingding_url = "https://oapi.dingtalk.com/robot/send?access_token="+access_token  # Replace with your Dingding webhook URL
+        headers = {"Content-Type": "application/json"}
+        payload = {"msgtype": msgtype, "text": {"content": final_message}}
+
+
+        response = requests.post(dingding_url, json=payload, headers=headers)
+        response.raise_for_status()
+        logging.info("通知已发送")
---
+++ packages/Airflow-DingDing/Airflow_DingDing-0.1.1.tar.gz/Airflow_DingDing-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding:UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+ name="Airflow_DingDing",
+ version="0.1.1",
+ author="WangBensen",
+ author_email="bensen.wang@ycgame.com",
+ license="Apache License",
+ url="https://xxxxxxxxx",
+ packages=["Airflow_DingDing"],
+ install_requires=["requests<=2.28.1"],
+ classifiers=[
+ "Environment :: Web Environment",
+ "Intended Audience :: Developers",
+ "Operating System :: OS Independent",
+ "Topic :: Text Processing :: Indexing",
+ "Topic :: Utilities",
+ "Topic :: Internet",
+ "Topic :: Software Development :: Libraries :: Python Modules",
+ "Programming Language :: Python",
+ "Programming Language :: Python :: 3.8"
+ ],
+)
+
+#name:打包的名称，以后会根据这个包名去下载包
+#version:版本号
+#author:作者
+#author_email:作者email
+#license:LiCENSE文件,授权方式
+#url:项目地址
+#package:打包的项目包名
+#install_requires:依赖的包以及版本号
+#classifieers:是要求的环境
