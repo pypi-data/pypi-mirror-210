81a7e3dae9ee599b8aef870b827471fd5abc979f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:27 +0000

    Add package starwhale/0.4.6/starwhale-0.4.6.tar.gz

---
+++ packages/starwhale/starwhale-0.4.6.tar.gz/starwhale-0.4.6/starwhale/web/ui/assets/color.442ace62.js
@@ -0,0 +1,1 @@
+import{aG as o}from"./index.ec2afa6c.js";const t=r=>o[r%o.length];export{t as g};
---
+++ packages/starwhale/starwhale-0.4.6.tar.gz/starwhale-0.4.6/starwhale/web/ui/assets/index16.ae914e64.js
@@ -0,0 +1,1 @@
+import{aE as s}from"./index.ec2afa6c.js";const o=["static"];export{s as Component,o as modes};
