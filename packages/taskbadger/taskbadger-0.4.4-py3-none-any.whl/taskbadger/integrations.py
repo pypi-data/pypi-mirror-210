fd7fc6edf804821200a1a3b4b7e02b4d7965f861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:49 +0000

    Add package taskbadger/0.4.4/taskbadger-0.4.4-py3-none-any.whl

---
+++ packages/taskbadger/taskbadger-0.4.4-py3-none-any.whl/taskbadger/integrations.py
@@ -0,0 +1,41 @@
+import dataclasses
+from enum import Enum
+from typing import Any, Dict
+
+from taskbadger.internal.models import ActionRequest, ActionRequestConfig
+
+
+class Integrations(str, Enum):
+    email = "email"
+
+
+def from_config(integration: Integrations, config: str):
+    if integration == Integrations.email:
+        split_ = [tuple(item.split(":", 1)) for item in config.split(",")]
+        kwargs = dict(split_)
+        return EmailIntegration(**kwargs)
+
+
+class Integration:
+    name: str
+
+    def request_config(self):
+        raise NotImplementedError
+
+
+@dataclasses.dataclass
+class Action:
+    trigger: str
+    integration: Integration
+
+    def to_dict(self) -> Dict[str, Any]:
+        return ActionRequest(self.trigger, self.integration.name, self.integration.request_config()).to_dict()
+
+
+@dataclasses.dataclass
+class EmailIntegration(Integration):
+    name = "email"
+    to: str  # custom type
+
+    def request_config(self) -> ActionRequestConfig:
+        return ActionRequestConfig.from_dict({"to": self.to})
