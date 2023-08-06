157d64d7530674ef47c24e6dcfdcabb635ce1cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:36 +0000

    Add package vcspull/1.21.1/vcspull-1.21.1-py3-none-any.whl

---
+++ packages/vcspull/vcspull-1.21.1-py3-none-any.whl/vcspull/types.py
@@ -0,0 +1,32 @@
+import pathlib
+import typing as t
+
+from typing_extensions import NotRequired, TypedDict
+
+from libvcs._internal.types import StrPath, VCSLiteral
+from libvcs.sync.git import GitSyncRemoteDict
+
+
+class RawConfigDict(t.TypedDict):
+    vcs: VCSLiteral
+    name: str
+    dir: StrPath
+    url: str
+    remotes: GitSyncRemoteDict
+
+
+RawConfigDir = dict[str, RawConfigDict]
+RawConfig = dict[str, RawConfigDir]
+
+
+class ConfigDict(TypedDict):
+    vcs: t.Optional[VCSLiteral]
+    name: str
+    dir: pathlib.Path
+    url: str
+    remotes: NotRequired[t.Optional[GitSyncRemoteDict]]
+    shell_command_after: NotRequired[t.Optional[t.List[str]]]
+
+
+ConfigDir = dict[str, ConfigDict]
+Config = dict[str, ConfigDir]
