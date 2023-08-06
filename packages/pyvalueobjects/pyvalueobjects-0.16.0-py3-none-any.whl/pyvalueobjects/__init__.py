3439da0b45d42ac0b7a7e95a4fccac9b83e8fc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:55 +0000

    Add package pyvalueobjects/0.16.0/pyvalueobjects-0.16.0-py3-none-any.whl

---
+++ packages/pyvalueobjects/pyvalueobjects-0.16.0-py3-none-any.whl/pyvalueobjects/__init__.py
@@ -0,0 +1,30 @@
+# ----------------------------
+# ---------- Basics ----------
+# ----------------------------
+
+from pyvalueobjects.abstract.valueobject import ValueObject
+from pyvalueobjects.errors.ValueObjectError import ValueObjectError
+
+
+# -----------------------------
+# ---------- Numbers ----------
+# -----------------------------
+
+from pyvalueobjects.numbers.int import Int
+from pyvalueobjects.numbers.nullable_int import NullableInt
+from pyvalueobjects.numbers.positive_int import PositiveInt
+from pyvalueobjects.numbers.negative_int import NegativeInt
+from pyvalueobjects.numbers.nullable_negative_int import NullableNegativeInt
+from pyvalueobjects.numbers.positive_or_zero_int import PositiveOrZeroInt
+from pyvalueobjects.numbers.nullable_positive_or_zero_int import NullablePositiveOrZeroInt
+from pyvalueobjects.numbers.negative_or_zero_int import NegativeOrZeroInt
+from pyvalueobjects.numbers.nullable_negative_or_zero_int import NullableNegativeOrZeroInt
+
+# -----------------------------
+# ---------- Strings ----------
+# -----------------------------
+
+from pyvalueobjects.strings.string import String
+from pyvalueobjects.strings.nullable_string import NullableString
+from pyvalueobjects.strings.uuid4 import Uuid4
+from pyvalueobjects.dates.isodate import IsoDate
