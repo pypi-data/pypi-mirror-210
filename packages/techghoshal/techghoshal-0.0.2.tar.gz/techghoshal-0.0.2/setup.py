000c8f7464fcb857f70c79bd85eda975f08d0bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:02 +0000

    Add package techghoshal/0.0.2/techghoshal-0.0.2.tar.gz

---
+++ packages/techghoshal/techghoshal-0.0.2.tar.gz/techghoshal-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Basic Hello package'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="techghoshal",
+    version=VERSION,
+    author="techghoshal",
+    author_email="techghoshal@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['hello-wordl', 'testing']
+)
+
