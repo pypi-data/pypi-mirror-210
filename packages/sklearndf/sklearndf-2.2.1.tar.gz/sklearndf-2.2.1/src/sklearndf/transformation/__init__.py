1755091b4fc1fd55f58122a88d8b3db4aec107b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:12 +0000

    Add package sklearndf/2.2.1/sklearndf-2.2.1.tar.gz

---
+++ packages/sklearndf/sklearndf-2.2.1.tar.gz/sklearndf-2.2.1/src/sklearndf/transformation/__init__.py
@@ -0,0 +1,17 @@
+"""
+Extended versions of all `scikit-learn` transformers with enhanced support for data
+frames.
+"""
+
+from .. import __sklearn_0_24__, __sklearn_1_0__, __sklearn_1_1__, __sklearn_version__
+from ._transformation import *
+from ._transformation_v0_22 import *
+
+if __sklearn_version__ >= __sklearn_0_24__:
+    from ._transformation_v0_24 import *
+
+if __sklearn_version__ >= __sklearn_1_0__:
+    from ._transformation_v1_0 import *
+
+if __sklearn_version__ >= __sklearn_1_1__:
+    from ._transformation_v1_1 import *
