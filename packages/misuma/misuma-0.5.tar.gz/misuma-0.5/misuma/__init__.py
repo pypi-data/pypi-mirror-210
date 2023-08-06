7e431c821d5f8be54cb2d35dd44a290e4575c51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:29 +0000

    Add package misuma/0.5/misuma-0.5.tar.gz

---
+++ packages/misuma/misuma-0.5.tar.gz/misuma-0.5/misuma/__init__.py
@@ -0,0 +1,10 @@
+from scipy.integrate import quad
+import numpy as np
+
+def suma(a, b):
+    """Make a miau.
+
+    Based on: https://math.stackexchange.com/questions/2305597/integral-that-evaluates-to-42
+    """
+    sum = a + b
+    return sum
