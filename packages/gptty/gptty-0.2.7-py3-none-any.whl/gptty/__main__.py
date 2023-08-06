2404b44de44eff10ad5fee66506165207e58d12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:53 +0000

    Add package gptty/0.2.7/gptty-0.2.7-py3-none-any.whl

---
+++ packages/gptty/gptty-0.2.7-py3-none-any.whl/gptty/__main__.py
@@ -0,0 +1,218 @@
+
+"""
+gptty: context-preserving chatGPT CLI wrapper
+
+"""
+
+__name__ = "gptty"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "0.2.7"
+__license__ = "MIT"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
+# general packages
+import click
+import os
+import asyncio
+import pandas as pd
+import nltk
+import socket
+from contextlib import closing
+
+# app specific requirements
+from gptty.config import get_config_data
+from gptty.gptty import create_chat_room, run_query
+
+# Define color codes
+CYAN = "\033[1;36m"
+RED = "\033[1;31m"
+RESET = "\033[0m"
+
+# download nltk corpora if they are not already downloaded
+def download_nltk_data_if_needed(data_name):
+    try:
+        nltk.data.find(data_name)
+    except LookupError:
+        nltk.download(data_name.split('/')[-1])
+
+# Download 'stopwords', 'punkt', and 'brown' if they haven't been downloaded yet
+download_nltk_data_if_needed('corpora/stopwords')
+download_nltk_data_if_needed('tokenizers/punkt')
+download_nltk_data_if_needed('corpora/brown')
+
+# return a simple pandas df of the logged questions
+def return_log_as_df(configs):
+    # here we add a pandas df reference object, see 
+    # https://github.com/signebedi/gptty/issues/15
+    try:
+        df = pd.read_csv(configs['output_file'], header=None,sep='|').fillna('')
+        df.columns = ['timestamp','tag','question','response']
+    except:
+        df = pd.DataFrame(columns=['timestamp','tag','question','response'])
+    return df
+
+# Check if the system has a valid internet connection
+
+def has_internet_connection(host="google.com", port=443, timeout=3):
+    """Check if the system has a valid internet connection.
+
+    Args:
+        host (str, optional): A well-known website to test the connection. Default is 'www.google.com'.
+        port (int, optional): The port number to use for the connection. Default is 80.
+        timeout (int, optional): The time in seconds to wait for a response before giving up. Default is 3.
+
+    Returns:
+        bool: True if the system has an internet connection, False otherwise.
+    """
+    try:
+        with closing(socket.create_connection((host, port), timeout=timeout)):
+            return True
+    except OSError:
+        return False
+
+# borrowed version callback from https://click.palletsprojects.com/en/7.x/options/#callbacks-and-eager-options
+def print_version(ctx, param, value, version=__version__):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(f'gptty version {version}')
+    ctx.exit()
+
+@click.group()
+@click.option('--version', '-v', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True, help="Show app version.")
+def main():
+  pass
+
+
+@click.command()
+@click.option('--config_path', '-c', default=os.path.join(os.getcwd(),'gptty.ini'), help="Path to config file.")
+@click.option('--verbose', '-v', is_flag=True, help="Show debug data.")
+def chat(config_path:str, verbose:bool):
+  
+  """
+  Run the gptty chat client
+  """
+
+  asyncio.run(chat_async_wrapper(config_path, verbose))
+
+async def chat_async_wrapper(config_path:str, verbose:bool):
+  title = r"""
+                 _   _         
+     ____  ____ | | | |        
+    / __ `/ __ \| |_| |_ _   _ 
+   / /_/ / /_/ /| __| __| | | |
+   \__, / .___/ | |_| |_| |_| |
+  /____/_/       \__|\__|\__, |
+                          __/ |
+                         |___/ 
+  """
+
+  # Print the text in cyan
+  click.echo(f"{CYAN}{title}\nWelcome to gptty (v.{__version__}), a ChatGPT wrapper in your TTY. Type :help in the chat interface if you need help getting started.{' Verbose / Debug mode is on. Query prompts will be preceded by your daily API usage in the format (query count, query tokens, response tokens).' if verbose else ''}{RESET}\n")
+  
+  if not os.path.exists(config_path):
+      click.echo(f"{RED}FAILED to access app config file at {config_path}. Are you sure this is a valid config file? Run `gptty chat --help` for more information. You can get a sample config at <https://github.com/signebedi/gptty/blob/master/assets/gptty.ini.example>.")
+      return
+
+  # load the app configs
+  configs = get_config_data(config_file=config_path)
+
+  # Here, we verify that we have a wifi connection and if not, exit
+  if not has_internet_connection(configs['verify_internet_endpoint']):
+    click.echo(f"{RED}FAILED to verify connection at {configs['verify_internet_endpoint']}. Are you sure you are connected to the internet?")
+    return
+
+  # create the output file if it doesn't exist
+  with open (configs['output_file'], 'a'): pass
+  
+  # Authenticate with OpenAI using your API key
+  # click.echo (configs['api_key'])
+  if configs['api_key'].rstrip('\n') == "":
+      click.echo(f"{RED}FAILED to initialize connection to OpenAI. Have you added an API token? See gptty docs <https://github.com/signebedi/gptty#configuration> or <https://platform.openai.com/account/api-keys> for more information.")
+      return
+
+  # Run the main function
+  # create_chat_room(configs=configs, config_path=config_path)
+  # asyncio.run(create_chat_room(configs=configs, config_path=config_path))
+  await create_chat_room(configs=configs, config_path=config_path, verbose=verbose)
+
+
+@click.command()
+# @click.option('--log', '-l', is_flag=True, callback=print_version,
+#               expose_value=False, is_eager=True, help="Show question log.")
+@click.option('--config_path', '-c', default=os.path.join(os.getcwd(),'gptty.ini'), help="Path to config file.")
+@click.option('--additional_context', '-a', default="", help="Pass more context to your questions.")
+@click.option('--question', '-q', multiple=True, help='Repeatable list of questions.')
+@click.option('--tag', '-t', default="", help='Tag to categorize your query. [optional]')
+@click.option('--verbose', '-v', is_flag=True, help="Show debug data.")
+@click.option('--json', '-j', is_flag=True, help="Return query as JSON object.")
+@click.option('--quiet', is_flag=True, help="Don't write to stdout.")
+def query(config_path:str, additional_context:str, question:str, tag:str, verbose:bool, json:bool, quiet:bool):
+  """
+  Submit a gptty query
+  """
+
+  asyncio.run(query_async_wrapper(config_path, question, tag, additional_context, verbose, json, quiet))
+
+
+async def query_async_wrapper(config_path:str, question:str, tag:str, additional_context:str, verbose:bool, json:bool, quiet:bool):
+
+  if not os.path.exists(config_path):
+      click.echo(f"{RED}FAILED to access app config file at {config_path}. Are you sure this is a valid config file? Run `gptty chat --help` for more information.")
+      return
+
+  # load the app configs
+  configs = get_config_data(config_file=config_path)
+
+  # Here, we verify that we have a wifi connection and if not, exit
+  if not has_internet_connection(configs['verify_internet_endpoint']):
+    click.echo(f"{RED}FAILED to verify connection at {configs['verify_internet_endpoint']}. Are you sure you are connected to the internet?")
+    return
+
+  # create the output file if it doesn't exist
+  with open (configs['output_file'], 'a'): pass
+
+  # Authenticate with OpenAI using your API key
+  # click.echo (configs['api_key'])
+  if configs['api_key'].rstrip('\n') == "":
+      click.echo(f"{RED}FAILED to initialize connection to OpenAI. Have you added an API token? See gptty docs <https://github.com/signebedi/gptty#configuration> or <https://platform.openai.com/account/api-keys> for more information.")
+      return
+
+  if len(questions) < 1 or not isinstance(questions, tuple):
+      click.echo(f"{RED}FAILED to query ChatGPT. Did you forget to ask a question? Run `gptty chat --help` for more information.")
+      return
+
+  await run_query(questions=question, tag=tag, configs=configs, additional_context=additional_context, config_path=config_path, verbose=verbose, return_json=json, quiet=quiet)
+
+
+@click.command()
+@click.option('--config_path', '-c', default=os.path.join(os.getcwd(),'gptty.ini'), help="Path to config file.")
+def log(config_path):
+  """
+  Get log of past queries
+  """
+  # load the app configs
+  configs = get_config_data(config_file=config_path)
+  
+  # create the output file if it doesn't exist
+  with open (configs['output_file'], 'a'): pass
+
+  if not os.path.exists(config_path):
+      click.echo(f"{RED}FAILED to access app config file at {config_path}. Are you sure this is a valid config file? Run `gptty chat --help` for more information.")
+      return
+
+
+  df = return_log_as_df(configs)
+
+  click.echo(df)
+
+
+
+main.add_command(chat)
+main.add_command(query)
+main.add_command(log)
+
+if __name__ == "__main__":
+  main()
---
+++ packages/gptty/gptty-0.2.7-py3-none-any.whl/gptty/context.py
@@ -0,0 +1,241 @@
+__name__ = "gptty.context"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "0.2.7"
+__license__ = "MIT"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
+
+import click
+import tiktoken
+from textblob import TextBlob
+from collections import Counter, defaultdict
+from nltk.corpus import stopwords
+
+
+YELLOW = "\033[1;33m"
+RESET = "\033[0m"
+
+def get_token_count(s, model_name):
+
+    """
+    Returns the number of tokens in a text string encoded using a specified model.
+
+    Args:
+
+        s (str): The input text string.
+        model_name (str): The name of the model used for encoding.
+
+    Returns:
+
+        num_tokens (int): The number of tokens in the encoded text string.
+    """
+
+    encoding = tiktoken.encoding_for_model(model_name)
+    num_tokens = len(encoding.encode(s))
+    return num_tokens
+
+def return_most_common_phrases(text:str, weight_recent=True) -> list:
+
+    """
+    Returns a list of the most common noun phrases in the input text, with an option to weight more recent phrases more heavily.
+
+    Args:
+    - text (str): The input text.
+    - weight_recent (bool): If True, more recent phrases are weighted more heavily.
+
+    Returns:
+    - list: A list of the most common noun phrases in the input text. Each item in the list is a string representing a noun phrase.
+    """
+
+    # Extract noun phrases using TextBlob
+    blob = TextBlob(text)
+    noun_phrases = blob.noun_phrases
+
+    # Remove stopwords from noun phrases
+    stop_words = set(stopwords.words('english'))
+    filtered_noun_phrases = []
+    for np in noun_phrases:
+        words = np.split()
+        filtered_words = [word for word in words if word not in stop_words]
+        if filtered_words:
+            filtered_noun_phrases.append(' '.join(filtered_words))
+
+    if not weight_recent:
+
+        # Count the frequency of the noun phrases
+        noun_phrase_counts = Counter(filtered_noun_phrases)
+
+        # Get the most frequent key phrases
+        return [phrase for phrase, count in noun_phrase_counts.most_common()]
+
+    # Count the weighted frequency of the noun phrases
+    noun_phrase_weighted_counts = defaultdict(int)
+    total_phrases = len(filtered_noun_phrases)
+
+    for i, phrase in enumerate(filtered_noun_phrases):
+        weight = (i + 1) / total_phrases  # Assign a higher weight to phrases that appear later in the text
+        noun_phrase_weighted_counts[phrase] += weight
+
+    # Get the most frequent key phrases
+    return [phrase for phrase, count in sorted(noun_phrase_weighted_counts.items(), key=lambda x: x[1], reverse=True)]
+
+def get_context(tag: str, 
+                max_context_length: int, 
+                output_file: str, 
+                model_name:str, 
+                context_keywords_only: bool = True, 
+                additional_context: str = "",
+                model_type: str = None, 
+                question: str = None, 
+                debug: bool = False):
+
+
+    """
+    Returns a full query context for a given tag, question and additional context.
+    
+    Parameters:
+        tag: str
+            Tag to identify a conversation with a specific topic
+        max_context_length: int
+            Maximum length of the context to return.
+        output_file: str
+            Path to the file to read the context from.
+        model_name: str
+            Name of the language model to use
+        context_keywords_only: bool, optional
+            If True, use only the most common phrases and words from the context and additional context.
+            Default is True.
+        additional_context: str, optional
+            Additional context to add to the context.
+            Default is an empty string.
+        model_type: str, optional
+            Type of the language model. If 'v1/chat/completions', return a list of dicts with 'role' and 'content' keys
+            If not, return a string.
+            Default is None.
+        question: str, optional
+            Question to add to the context. If None, return only the context.
+            Default is None.
+        debug: bool, optional
+            If True, print debug information.
+            Default is False.
+    
+    Returns:
+        If `model_type` is 'v1/chat/completions', returns a list of dicts with 'role' and 'content' keys
+        If not, returns a string.
+    """
+
+
+    if len(tag) < 1:
+        if model_type == 'v1/chat/completions':
+
+            context = [{"role": "user", "content": question}]
+
+            if len(additional_context) > 0:
+                # at this point we've added all the elements to context that we believe we should, so let's add any 
+                # additional context that we passed.
+                remaining_tokens = max_context_length - len(question)
+                context = [{"role": "system", "content": ' '.join(additional_context.split()[:remaining_tokens])}] + context
+
+
+            if debug:
+                click.echo(YELLOW + '-' * 25)
+                click.echo(f'[debug]\nmodel: {model_name}\ntokens: {get_token_count(question, model_name)}\nwords: {len(question.split()) }\ntext: {question}') # debug - print the context to see what it looks like
+                click.echo('-' * 25 + RESET)
+                
+            return context
+
+        else:
+
+            if len(additional_context) > 0:
+
+                remaining_tokens = max_context_length - (len(question.split()))
+                if remaining_tokens > 0:
+                    question = ' '.join(additional_context.split()[:remaining_tokens]) + " " + question
+
+
+            if debug:
+                click.echo(YELLOW + '-' * 25)
+                click.echo(f'[debug]\nmodel: {model_name}\ntokens: {get_token_count(question, model_name)}\nwords: {len(question.split())}\ntext: {question}') # debug - print the context to see what it looks like
+                click.echo('-' * 25 + RESET)
+
+            return question
+
+    with open(output_file, 'r') as f:
+        text = f.read().strip().split('\n')
+
+    if model_type == 'v1/chat/completions':
+        context = []
+
+        for row in reversed(text):
+            data = [item.strip() for item in row.split('|')]
+
+            if (sum(len(item["content"].split()) for item in context) + len(data[2].split()) + len(data[3].split()) + len(question.split())) > max_context_length:
+                break
+
+            if data[1] == tag:
+                context = [{"role": "assistant", "content": data[3]}] + context
+                context = [{"role": "user", "content": data[2]}] + context
+
+        context.append({"role": "user", "content": question})
+        
+        if len(additional_context) > 0:
+            # at this point we've added all the elements to context that we believe we should, so let's add any 
+            # additional context that we passed.
+            remaining_tokens = max_context_length - (sum(len(item["content"].split()) for item in context))
+            context = [{"role": "system", "content": ' '.join(additional_context.split()[:remaining_tokens])}] + context
+
+        if debug:
+            token_count = " ".join([x['content'] for x in context])
+            click.echo(YELLOW + '-' * 25)
+            click.echo(f'[debug]\nmodel: {model_name}\ntokens: {get_token_count(token_count, model_name)}\nwords: {sum(len(item["content"].split()) for item in context)}\ntext: {context}') # debug - print the context to see what it looks like
+            click.echo('-' * 25 + RESET)
+
+
+    else:
+        context = ""
+        for row in text:
+            data = [item.strip() for item in row.split('|')]
+
+            if data[1] == tag:
+                context += ' ' + data[2] + ' ' + data[3]
+
+        if context_keywords_only:
+            phrases = return_most_common_phrases(additional_context+context) # here we prepend the context with the additional_context string
+            context = "" # maybe not the cleanest way to do this, but we are resetting the context here
+
+            for phrase in phrases:
+                if (len(context.split()) + len(phrase.split()) + len(question.split())) > max_context_length:
+                    break
+                context += " " + phrase
+
+        else:
+            c = ""
+            context_words = context.split()
+
+            for i in range(len(context_words)):
+                if (len(c.split()) + len(question.split())) >= max_context_length:
+                    break
+                c += ' ' + context_words[i]
+
+            context = c.strip()
+
+            # prepend `context` with `additional_context` if we have any tokens remaining.
+            # WARNING - this may create unexpected behavior, especially if a question is 
+            # contained within the additional context passed, that may provide seemingly 
+            # inexplicable responses.
+            remaining_tokens = max_context_length - (len(context.split()) + len(question.split()))
+            if remaining_tokens > 0:
+                context = ' '.join(additional_context.split()[:remaining_tokens]) + " " + context
+
+
+        context = context.strip() + ' ' + question
+        
+
+        if debug:
+            click.echo(YELLOW + '-' * 25)
+            click.echo(f'[debug]\nmodel: {model_name}\ntokens: {get_token_count(context, model_name)}\nwords: {len(context.split())}\ntext: {context}') # debug - print the context to see what it looks like
+            click.echo('-' * 25 + RESET)
+
+    return context
