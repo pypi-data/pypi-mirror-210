e4ade5926c347f4a46485b8ef0ed7de5b4d236d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:22 +0000

    Add package tkinter-layout-helpers/0.2.0/tkinter_layout_helpers-0.2.0-py3-none-any.whl

---
+++ packages/tkinter-layout-helpers/tkinter_layout_helpers-0.2.0-py3-none-any.whl/tkinter_layout_helpers/parent_manager.py
@@ -0,0 +1,26 @@
+import contextlib
+import tkinter as tk
+from typing import ContextManager
+
+
+class DefaultRootWrapper:  # pragma: no cover
+    @property
+    def default_root(self) -> tk.Widget:
+        return tk._default_root  # type: ignore # noqa
+
+    @default_root.setter
+    def default_root(self, value: tk.Widget):
+        tk._default_root = value  # type: ignore
+
+
+_default_root_wrapper = DefaultRootWrapper()
+
+
+@contextlib.contextmanager
+def set_parent(parent: tk.Widget) -> ContextManager[tk.Widget]:
+    old_root = _default_root_wrapper.default_root
+    _default_root_wrapper.default_root = parent
+    try:
+        yield parent
+    finally:
+        _default_root_wrapper.default_root = old_root
