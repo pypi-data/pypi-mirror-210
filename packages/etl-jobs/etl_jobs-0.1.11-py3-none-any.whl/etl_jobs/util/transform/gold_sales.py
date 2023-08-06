80292f7e1839d88a7e16cf7bd1116c41dbe21d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:58 +0000

    Add package etl-jobs/0.1.11/etl_jobs-0.1.11-py3-none-any.whl

---
+++ packages/etl-jobs/etl_jobs-0.1.11-py3-none-any.whl/etl_jobs/util/transform/gold_sales.py
@@ -0,0 +1,8 @@
+"""
+fill in
+"""
+from pyspark.sql.functions import avg, sum
+
+
+def transform_gold_sales(data_frame):
+    return data_frame.select(avg("SALE").alias("AVG"),col("CUSTOMERNAME"),sum("SALE").alias("TOTAL"))
