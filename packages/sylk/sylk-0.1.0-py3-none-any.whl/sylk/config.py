1d00de4839412bd59e9eec6969997f1803de8169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:49 +0000

    Add package sylk/0.1.0/sylk-0.1.0-py3-none-any.whl

---
+++ packages/sylk/sylk-0.1.0-py3-none-any.whl/sylk/config.py
@@ -0,0 +1,30 @@
+from sylk.commons.protos.SylkConfigs_pb2 import SylkCliConfigs
+"""
+               ____  
+   _______  __/ / /__
+  / ___/ / / / / //_/
+ (__  ) /_/ / / ,<   
+/____/\__, /_/_/|_|  
+     /____/ 
+     
+sylk cli module configuration"""
+configs=SylkCliConfigs(
+    host="localhost",
+    port=44880,
+    # Analytic gathering approval
+    analytics=False,
+    # First run flag
+    first_run=False,
+    token="sylk_36f2bff2-7fd8-4322-bc56-402ea9378ecc",
+    # Supported builtins templates
+    sylk_templates = [
+        "@sylk/Blank",
+        "@sylk/TodoAppPy"
+        # "@sylk/io",
+        # "@sylk/SamplePy",
+        # "@sylk/SampleTs",
+        # "@sylk/PubSubTs",
+        # "@sylk/HelloWorldPy",
+        # "@sylk/HelloWorldTs"
+    ]
+)
