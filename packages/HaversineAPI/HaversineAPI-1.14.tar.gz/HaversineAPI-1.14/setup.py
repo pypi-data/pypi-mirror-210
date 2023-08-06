1b9c8fd306f6d79f4b7deb425b71584b5af520bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:37 +0000

    Add package HaversineAPI/1.14/HaversineAPI-1.14.tar.gz

---
+++ packages/HaversineAPI/HaversineAPI-1.14.tar.gz/HaversineAPI-1.14/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import codecs
+from os import path
+from setuptools import setup
+
+pwd = path.abspath(path.dirname(__file__))
+with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
+    long_description = input.read()
+
+version='1.14'
+	
+setup(
+	name='HaversineAPI',
+	version=version,
+	license='MIT',
+    long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://github.com/eddo888/HaversineAPI',
+	download_url='https://github.com/eddo888/HaversineAPI/archive/%s.tar.gz'%version,
+	author='David Edson',
+	author_email='eddo888@tpg.com.au',
+	packages=[
+		'Haversine',
+	],
+	install_requires=[
+		'dotmap',
+		'tqdm'
+		'pygeohash',
+		'Baubles',
+		'Perdy',
+		'Argumental',
+	],
+	scripts=[
+		"Haversine/haversine.py",
+		"Haversine/haversine.sh",
+	],
+)
