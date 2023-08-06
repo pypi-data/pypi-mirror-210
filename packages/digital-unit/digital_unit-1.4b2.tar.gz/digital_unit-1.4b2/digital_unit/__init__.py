563e49008041432dace90afab2b6faeee09d4e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:55 +0000

    Add package digital-unit/1.4b2/digital_unit-1.4b2.tar.gz

---
+++ packages/digital-unit/digital_unit-1.4b2.tar.gz/digital_unit-1.4b2/digital_unit/__init__.py
@@ -0,0 +1,5 @@
+from digital_unit.root import *
+from digital_unit.Area import *
+from digital_unit.Lenth import *
+
+print('This is a beta version.')
---
+++ packages/digital-unit/digital_unit-1.4b2.tar.gz/digital_unit-1.4b2/digital_unit/root.py
@@ -0,0 +1,26 @@
+class DigitalUnit:
+    def __init__(self) -> None:
+        self.type = "NONE"
+
+
+class Number:
+    def __init__(self, number, unit) -> None:
+        self.digital_part = number
+        if unit.__class__.__base__.__base__ == DigitalUnit:
+            self.unit = unit
+        else:
+            print(unit.__class__.__base__.__base__)
+            raise TypeError('You should set the unit parameter to numerical units.')
+
+    def change_unit(self, new_unit):
+        if new_unit.__class__.__base__.__base__ == DigitalUnit:
+            if new_unit.type == self.unit.type:
+                self.digital_part = self.digital_part * self.unit.measure / new_unit.measure
+                self.unit = new_unit
+            else:
+                raise TypeError('Different unit types')
+        else:
+            raise TypeError('You should set the unit parameter to numerical units.')
+    
+    def __str__(self) -> str:
+        return f'{self.digital_part}{self.unit}'
