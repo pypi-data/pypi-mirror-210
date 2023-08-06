2ff24e7ebca93f4d7508ca29fd4faf7778daf70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:25 +0000

    Add package starwhale/0.4.6/starwhale-0.4.6-py3-none-any.whl

---
+++ packages/starwhale/starwhale-0.4.6-py3-none-any.whl/starwhale/web/ui/assets/index11.c9107931.js
@@ -0,0 +1,1 @@
+import{C as p}from"./Column.88200c6a.js";import"./index.ec2afa6c.js";const t=["static"];export{p as Component,t as modes};
---
+++ packages/starwhale/starwhale-0.4.6-py3-none-any.whl/starwhale/web/ui/assets/index33.4967db91.js
@@ -0,0 +1,1 @@
+import{Z as s}from"./index.ec2afa6c.js";const o=["static"];export{s as Component,o as modes};
