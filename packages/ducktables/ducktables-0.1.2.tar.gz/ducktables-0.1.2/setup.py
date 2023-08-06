2348298507c65834a89b22d2d91e44b127573d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:36 +0000

    Add package ducktables/0.1.2/ducktables-0.1.2.tar.gz

---
+++ packages/ducktables/ducktables-0.1.2.tar.gz/ducktables-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ducktables',
+    version='0.1.2',
+    description='Python Table Functions for DuckDB',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    )
