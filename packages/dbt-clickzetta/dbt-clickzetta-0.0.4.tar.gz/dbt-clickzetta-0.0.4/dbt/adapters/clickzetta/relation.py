1ef268c0c1b6097fcf4a54a4fbaf713007bdb1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:31 +0000

    Add package dbt-clickzetta/0.0.4/dbt-clickzetta-0.0.4.tar.gz

---
+++ packages/dbt-clickzetta/dbt-clickzetta-0.0.4.tar.gz/dbt-clickzetta-0.0.4/dbt/adapters/clickzetta/relation.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from dbt.adapters.base.relation import BaseRelation, Policy
+
+
+@dataclass
+class ClickZettaQuotePolicy(Policy):
+    database: bool = False
+    schema: bool = False
+    identifier: bool = False
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class ClickZettaRelation(BaseRelation):
+    quote_policy: ClickZettaQuotePolicy = field(default_factory=lambda: ClickZettaQuotePolicy())
+    quote_character = "`"
