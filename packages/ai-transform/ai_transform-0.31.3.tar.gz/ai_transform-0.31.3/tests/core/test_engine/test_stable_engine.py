451944ef7b43350d1d003b5b31c904a7ebca13e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:40 +0000

    Add package ai-transform/0.31.3/ai_transform-0.31.3.tar.gz

---
+++ packages/ai-transform/ai_transform-0.31.3.tar.gz/ai_transform-0.31.3/tests/core/test_engine/test_stable_engine.py
@@ -0,0 +1,20 @@
+from ai_transform.dataset.dataset import Dataset
+from ai_transform.engine.stable_engine import StableEngine
+from ai_transform.engine.small_batch_stable_engine import SmallBatchStableEngine
+
+from ai_transform.operator.abstract_operator import AbstractOperator
+from ai_transform.workflow.abstract_workflow import AbstractWorkflow
+
+
+class TestStableEngine:
+    def test_stable_engine(self, full_dataset: Dataset, test_operator: AbstractOperator):
+        engine = StableEngine(full_dataset, test_operator, worker_number=0)
+        workflow = AbstractWorkflow(name="workflow_test123", engine=engine, job_id="test_job123")
+        workflow.run()
+        assert engine.success_ratio == 1
+
+    def test_small_batch_stable_engine(self, full_dataset: Dataset, test_operator: AbstractOperator):
+        engine = SmallBatchStableEngine(full_dataset, test_operator)
+        workflow = AbstractWorkflow(name="workflow_test123", engine=engine, job_id="test_job123")
+        workflow.run()
+        assert engine.success_ratio == 1
