87cbb56eaf1b7b1ff7aca5a9a7fa4e530bb778a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:55 +0000

    Add package t8s/0.1.7/t8s-0.1.7.tar.gz

---
+++ packages/t8s/t8s-0.1.7.tar.gz/t8s-0.1.7/src/t8s/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023-present João Antonio Ferreira <joao.parana@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .__about__ import __version__
+# from abc import ABC, abstractmethod
+#
+# class ITimeSerie(ABC):
+#    pass
