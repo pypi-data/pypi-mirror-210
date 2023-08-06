3b16793b5729ee08e31ef4e2ee700f187e529c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:39 +0000

    Add package sonatype-nexus-sdk/0.1.3/sonatype_nexus_sdk-0.1.3.tar.gz

---
+++ packages/sonatype-nexus-sdk/sonatype_nexus_sdk-0.1.3.tar.gz/sonatype_nexus_sdk-0.1.3/nexus_sdk/apis/paths/v1_security_atlassian_crowd_verify_connection.py
@@ -0,0 +1,9 @@
+from nexus_sdk.paths.v1_security_atlassian_crowd_verify_connection.post import (
+    ApiForpost,
+)
+
+
+class V1SecurityAtlassianCrowdVerifyConnection(
+    ApiForpost,
+):
+    pass
---
+++ packages/sonatype-nexus-sdk/sonatype_nexus_sdk-0.1.3.tar.gz/sonatype_nexus_sdk-0.1.3/nexus_sdk/apis/paths/v1_security_privileges_privilege_name.py
@@ -0,0 +1,9 @@
+from nexus_sdk.paths.v1_security_privileges_privilege_name.delete import ApiFordelete
+from nexus_sdk.paths.v1_security_privileges_privilege_name.get import ApiForget
+
+
+class V1SecurityPrivilegesPrivilegeName(
+    ApiForget,
+    ApiFordelete,
+):
+    pass
