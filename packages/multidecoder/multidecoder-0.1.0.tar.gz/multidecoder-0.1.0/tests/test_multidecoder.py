66986b2ee15a4dc1e462638f363826405718012e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:37 +0000

    Add package multidecoder/0.1.0/multidecoder-0.1.0.tar.gz

---
+++ packages/multidecoder/multidecoder-0.1.0.tar.gz/multidecoder-0.1.0/tests/test_multidecoder.py
@@ -0,0 +1,59 @@
+import pytest
+
+from multidecoder.multidecoder import Multidecoder
+
+
+@pytest.fixture
+def md():
+    return Multidecoder()
+
+
+def test_scan_empty(md):
+    assert md.scan(b'') == []
+
+
+def test_analyze_data_url(md):
+    assert md.scan(b'https://some.domain.com') == [{
+        'obfuscation': '',
+        'type': 'network.url',
+        'value': b'https://some.domain.com/',
+        'start': 0,
+        'end': 23,
+        'children': [{
+            'obfuscation': '',
+            'type': 'network.domain',
+            'value': b'some.domain.com',
+            'start': 8,
+            'end': 23,
+            'children': []
+        }]
+    }]
+
+
+def test_scan_no_overlap(md):
+    assert md.scan(b'google.com, amazon.com, 8.8.8.8') == [
+        {
+            'obfuscation': '',
+            'type': 'network.domain',
+            'value': b'google.com',
+            'start': 0,
+            'end': 10,
+            'children': []
+        },
+        {
+            'obfuscation': '',
+            'type': 'network.domain',
+            'value': b'amazon.com',
+            'start': 12,
+            'end': 22,
+            'children': []
+        },
+        {
+            'obfuscation': '',
+            'type': 'network.ip',
+            'value': b'8.8.8.8',
+            'start': 24,
+            'end': 31,
+            'children': []
+        }
+    ]
