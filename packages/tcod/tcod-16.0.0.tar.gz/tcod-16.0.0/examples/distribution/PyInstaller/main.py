a8adf9c712e3b013f4c41bfb0e9b8a2766e7541a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:48 +0000

    Add package tcod/16.0.0/tcod-16.0.0.tar.gz

---
+++ packages/tcod/tcod-16.0.0.tar.gz/tcod-16.0.0/examples/distribution/PyInstaller/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# To the extent possible under law, the libtcod maintainers have waived all
+# copyright and related or neighboring rights for the "hello world" PyInstaller
+# example script.  This work is published from: United States.
+# https://creativecommons.org/publicdomain/zero/1.0/
+"""PyInstaller main script example."""
+import sys
+from pathlib import Path
+
+import tcod
+
+WIDTH, HEIGHT = 80, 60
+
+# The base directory, this is sys._MEIPASS when in one-file mode.
+BASE_DIR = Path(getattr(sys, "_MEIPASS", "."))
+
+FONT_PATH = BASE_DIR / "data/terminal8x8_gs_ro.png"
+
+
+def main() -> None:
+    """Entry point function."""
+    tileset = tcod.tileset.load_tilesheet(FONT_PATH, 16, 16, tcod.tileset.CHARMAP_CP437)
+    with tcod.context.new(columns=WIDTH, rows=HEIGHT, tileset=tileset) as context:
+        while True:
+            console = tcod.console.Console(WIDTH, HEIGHT)
+            console.print(0, 0, "Hello World")
+            context.present(console)
+            for event in tcod.event.wait():
+                if isinstance(event, tcod.event.Quit):
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/tcod/tcod-16.0.0.tar.gz/tcod-16.0.0/tests/test_random.py
@@ -0,0 +1,40 @@
+"""Test random number generators."""
+import copy
+import pickle
+from pathlib import Path
+
+import tcod
+
+# ruff: noqa: D103
+
+SCRIPT_DIR = Path(__file__).parent
+
+
+def test_tcod_random() -> None:
+    rand = tcod.random.Random(tcod.random.COMPLEMENTARY_MULTIPLY_WITH_CARRY)
+    assert 0 <= rand.randint(0, 100) <= 100  # noqa: PLR2004
+    assert 0 <= rand.uniform(0, 100) <= 100  # noqa: PLR2004
+    rand.guass(0, 1)
+    rand.inverse_guass(0, 1)
+
+
+def test_tcod_random_copy() -> None:
+    rand = tcod.random.Random(tcod.random.MERSENNE_TWISTER)
+    rand2 = copy.copy(rand)
+    assert rand.uniform(0, 1) == rand2.uniform(0, 1)
+    assert rand.uniform(0, 1) == rand2.uniform(0, 1)
+    assert rand.uniform(0, 1) == rand2.uniform(0, 1)
+
+
+def test_tcod_random_pickle() -> None:
+    rand = tcod.random.Random(tcod.random.MERSENNE_TWISTER)
+    rand2 = pickle.loads(pickle.dumps(rand))
+    assert rand.uniform(0, 1) == rand2.uniform(0, 1)
+    assert rand.uniform(0, 1) == rand2.uniform(0, 1)
+    assert rand.uniform(0, 1) == rand2.uniform(0, 1)
+
+
+def test_load_rng_v13_1() -> None:
+    rand: tcod.random.Random = pickle.loads((SCRIPT_DIR / "data/random_v13.pkl").read_bytes())
+    assert rand.randint(0, 0xFFFF) == 56422  # noqa: PLR2004
+    assert rand.randint(0, 0xFFFF) == 15795  # noqa: PLR2004
