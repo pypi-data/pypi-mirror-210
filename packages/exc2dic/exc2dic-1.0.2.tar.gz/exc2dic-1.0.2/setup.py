2354e6100b5a2dc7a3e270eb8345fefb1bd7f7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:46 +0000

    Add package exc2dic/1.0.2/exc2dic-1.0.2.tar.gz

---
+++ packages/exc2dic/exc2dic-1.0.2.tar.gz/exc2dic-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='exc2dic',
+    version='1.0.2',
+    description='A package for reading Excel files',
+    author='Mingyuan',
+    author_email='mingyuanhuang@qq.com',
+    url='https://github.com/MingyuanHuang/Excel2Dict',
+    packages=['exc2dic'],
+    install_requires=[
+        'pandas',
+    ],
+)
