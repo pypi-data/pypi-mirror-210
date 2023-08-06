24f074cd38b5292fde482a8230fbc2fc2577d9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:10 +0000

    Add package minet/1.0.0a31/minet-1.0.0a31-py3-none-any.whl

---
+++ packages/minet/minet-1.0.0a31-py3-none-any.whl/minet/crowdtangle/post.py
@@ -0,0 +1,30 @@
+# =============================================================================
+# Minet CrowdTangle Post
+# =============================================================================
+#
+# Function used to retrieve information per post by id.
+#
+from ebbe import getpath
+
+from minet.crowdtangle.exceptions import CrowdTangleMissingTokenError
+from minet.crowdtangle.formatters import format_post
+
+URL_TEMPLATE = "https://api.crowdtangle.com/post/%s?token=%s"
+
+
+def crowdtangle_post(request, post_id, token=None, raw=False):
+    if token is None:
+        raise CrowdTangleMissingTokenError
+
+    # Fetching
+    api_url = URL_TEMPLATE % (post_id, token)
+    data = request(api_url)
+    post = getpath(data, ["posts", 0])
+
+    if post is None:
+        return
+
+    if not raw:
+        return format_post(post)
+
+    return post
