e4ab7d0e3d0d4102a3fe0fe028ef50120ccea188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:32 +0000

    Add package timona/0.0.1/timona-0.0.1.tar.gz

---
+++ packages/timona/timona-0.0.1.tar.gz/timona-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['timona']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['timona = timona.main:main']}
+
+setup_kwargs = {
+    'name': 'timona',
+    'version': '0.0.1',
+    'description': 'Tool to automate Helm deployments',
+    'long_description': '# timona\n\nA tool to automate Helm deployments\n',
+    'author': 'mihaiush',
+    'author_email': 'mihaiush@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/mihaiush/timona',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
