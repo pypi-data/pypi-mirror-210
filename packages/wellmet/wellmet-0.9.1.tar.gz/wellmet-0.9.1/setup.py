fe4e65c261f70be56b4246a1bc75a0a05966f71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:45 +0000

    Add package wellmet/0.9.1/wellmet-0.9.1.tar.gz

---
+++ packages/wellmet/wellmet-0.9.1.tar.gz/wellmet-0.9.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.9.1' 
+DESCRIPTION = 'a pure Python framework for spatial structural reliability analysis'
+LONG_DESCRIPTION = "WellMet is a pure Python framework for failure probability estimation and detection of failure surfaces by adaptive sequential decomposition of the design domain"
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="wellmet",
+        version=VERSION,
+        author="Gerasimov Aleksei",
+        author_email="<ger-alex@seznam.cz>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        url="https://rocketgit.com/iam-git/WellMet",
+        license="MIT",
+        install_requires=[
+		  "numpy",
+		  "scipy",
+		  "mpmath",
+		  "quadpy",
+		  "pandas",
+		  "PyQt5",
+		  "pyqtgraph>=0.13.1",
+		],
+        
+        keywords=['failure probability', 'Monte Carlo', 'surrogate model'],
+        classifiers = [
+			"Topic :: Scientific/Engineering",
+		    "Intended Audience :: Science/Research",
+		    "Programming Language :: Python :: 3",
+		    "License :: OSI Approved :: MIT License",
+		    "Operating System :: OS Independent",
+		]
+)
+
