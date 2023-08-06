79f9d3b2f25004bfee34306187003586a890b7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:53 +0000

    Add package rahaut3/0.0.1/rahaut3-0.0.1.tar.gz

---
+++ packages/rahaut3/rahaut3-0.0.1.tar.gz/rahaut3-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="rahaut3",
+    version="0.0.1",
+    author="fox",
+    author_email="abcdef1234567890user@gmail.com",
+    packages=["rahaut3"],
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ARISTODUMES/LP6",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[]
+)
