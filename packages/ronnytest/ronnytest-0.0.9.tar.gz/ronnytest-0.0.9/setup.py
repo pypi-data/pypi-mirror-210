3da3a42dc63a9294c60ea0a2098caefe63991597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:51 +0000

    Add package ronnytest/0.0.9/ronnytest-0.0.9.tar.gz

---
+++ packages/ronnytest/ronnytest-0.0.9.tar.gz/ronnytest-0.0.9/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from setuptools.command.install import install as _install
+
+try:
+  from pip.commands.install import logger
+except Exception as e:
+   print('don not install!')
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+notice = \
+    "[!!!] NOTICE: mynotice"
+
+class install_with_warning(_install):
+    def run(self):
+        _install.run(self)
+        logger.warning(notice)
+        logger.error(notice)
+
+logger.error(notice)
+
+setuptools.setup(
+  name="ronnytest",
+  version="0.0.9",
+  author="wxpay_sec_team",
+  author_email="wxpay_sec_team@tencent.com",
+  description="ronnytest",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypi/ronnytest",
+  packages=setuptools.find_packages(),
+  cmdclass={'install': install_with_warning},
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
