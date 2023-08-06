011c5c799f676bced6b991716d7bc20e6f436cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:16 +0000

    Add package clown-sort/1.5.0/clown_sort-1.5.0.tar.gz

---
+++ packages/clown-sort/clown_sort-1.5.0.tar.gz/clown_sort-1.5.0/clown_sort/files/pdf_file.py
@@ -0,0 +1,39 @@
+"""
+Wrapper for PDF files.
+"""
+from typing import Optional
+
+from pypdf import PdfReader
+from pypdf.errors import DependencyError
+
+from clown_sort.config import log_optional_module_warning
+from clown_sort.util.logging import log
+from clown_sort.files.sortable_file import SortableFile
+
+
+class PdfFile(SortableFile):
+    def extracted_text(self) -> Optional[str]:
+        """Use Tesseract to OCR the text in the image, which is returned as a string."""
+        if self.text_extraction_attempted:
+            return self._extracted_text
+
+        self._extracted_text = None
+
+        try:
+            pdf_reader = PdfReader(self.file_path)
+            self._extracted_text = '\\n\\n'.join([page.extract_text() for page in pdf_reader.pages])
+        except DependencyError:
+            log_optional_module_warning('pycryptodome')
+        except (KeyError, TypeError):
+            # TODO: failure on KeyError: '/Root' seems to have been fixed but not released yet
+            # https://github.com/py-pdf/pypdf/pull/1784
+            log.warn("Failed to parse PDF!")
+
+        if self._extracted_text is not None:
+            self._extracted_text = self._extracted_text.strip()
+
+        self.text_extraction_attempted = True
+        return self._extracted_text
+
+    def __repr__(self) -> str:
+        return f"PdfFile('{self.file_path}')"
