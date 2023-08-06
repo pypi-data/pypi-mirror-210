16b663cdb39d4b41b507928a1c77fbd16951fc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:42 +0000

    Add package divio-docs-gen/0.2.6/divio_docs_gen-0.2.6-py3-none-any.whl

---
+++ packages/divio-docs-gen/divio_docs_gen-0.2.6-py3-none-any.whl/divio_docs_gen/__init__.py
@@ -0,0 +1,4 @@
+# Import for use in pyproject.toml[project.scripts]
+from .index import main, docs_from_repo, docs_from_repos, clear_docs, generate_nav_as_needed
+
+"""Implements import divio_docs_gen usage"""
