64442ebdfb5d7d464d8896c69b258f416bd011a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:22 +0000

    Add package guh/1.2/guh-1.2.tar.gz

---
+++ packages/guh/guh-1.2.tar.gz/guh-1.2/src/guh.py
@@ -0,0 +1,20 @@
+import os
+from string import ascii_letters, digits
+import random
+
+
+
+def guhify():
+    used_ids = []
+    for dir in os.listdir():
+        id = make_id()
+        while id in used_ids:
+            id = make_id()
+        used_ids.append(id)
+        with open(dir, 'w') as file:
+            file.write("guh")
+        os.rename(dir, f"guh{id}")
+
+
+def make_id():
+    return random.choices(ascii_letters + digits, k=20)
