fcc8585655bb6718439ee3d7293190a5d45860c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package mkdocs-simple-tags-plugin/0.7/mkdocs-simple-tags-plugin-0.7.tar.gz

---
+++ packages/mkdocs-simple-tags-plugin/mkdocs-simple-tags-plugin-0.7.tar.gz/mkdocs-simple-tags-plugin-0.7/setup.py
@@ -0,0 +1,20 @@
+"""
+Setup the plugin
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-simple-tags-plugin',
+    version='0.7',
+    python_requires='>=3.8',
+    install_requires=[
+        'mkdocs>=1.4.3',
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    package_data={'tags': ['templates/*.md.template']},
+    entry_points={
+        'mkdocs.plugins': [
+            'simple-tags = tags.plugin:TagsPlugin'
+        ]
+    }
+)
