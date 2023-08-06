1da00de7b1341aa4e8903f1f14222706de53ed19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:43 +0000

    Add package strawberry-django-pubsub/0.0.1a0/strawberry_django_pubsub-0.0.1a0.tar.gz

---
+++ packages/strawberry-django-pubsub/strawberry_django_pubsub-0.0.1a0.tar.gz/strawberry_django_pubsub-0.0.1a0/strawberry_django_pubsub/exceptions.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+
+class WebSocketAcceptConnection(Exception):
+    """
+    Raised during a websocket.connect (or other supported connection) handler
+    to accept the connection.
+    """
+
+    pass
+
+
+class WebSocketDenyConnection(Exception):
+    """
+    Raised during a websocket.connect (or other supported connection) handler
+    to deny the connection.
+    """
+
+    pass
+
+
+class WebSocketConnectionDisconnect(Exception):
+    """
+    Raised when connection is dropped; this exception should only be called
+    explicit in debug mode only.
+    """
+
+    def __init__(self, code: int = 1000, reason: Optional[str] = None) -> None:
+        self.code = code
+        self.reason = reason or ""
