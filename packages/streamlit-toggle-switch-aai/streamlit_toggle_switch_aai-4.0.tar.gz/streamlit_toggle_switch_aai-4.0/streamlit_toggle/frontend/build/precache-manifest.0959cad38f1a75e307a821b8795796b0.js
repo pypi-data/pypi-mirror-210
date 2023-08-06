17f50fa10d71643627b79b0411adb8f077f0dc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:57 +0000

    Add package streamlit-toggle-switch-aai/4.0/streamlit_toggle_switch_aai-4.0.tar.gz

---
+++ packages/streamlit-toggle-switch-aai/streamlit_toggle_switch_aai-4.0.tar.gz/streamlit_toggle_switch_aai-4.0/streamlit_toggle/frontend/build/precache-manifest.0959cad38f1a75e307a821b8795796b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf117520c671719b36f9abd6c3400b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76b67cc7883c1debe40e",
+    "url": "./static/js/2.fd184217.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.fd184217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed47cea2ba6aee1f7f7a",
+    "url": "./static/js/main.0725fe9a.chunk.js"
+  },
+  {
+    "revision": "5ab337871dec8eeaf644",
+    "url": "./static/js/runtime-main.fd5c39a2.js"
+  }
+]);
