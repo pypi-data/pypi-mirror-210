47e966a890261c81725a0baf71c97fd56cb7a58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:33 +0000

    Add package cleanvision/0.3.0/cleanvision-0.3.0-py3-none-any.whl

---
+++ packages/cleanvision/cleanvision-0.3.0-py3-none-any.whl/cleanvision/utils/constants.py
@@ -0,0 +1,30 @@
+from typing import List
+
+IMAGE_PROPERTY: str = "image_property"
+DUPLICATE: str = "duplicate"
+IMAGE_PROPERTY_ISSUE_TYPES_LIST: List[str] = [
+    "dark",
+    "light",
+    "odd_aspect_ratio",
+    "low_information",
+    "blurry",
+    "grayscale",
+]
+DUPLICATE_ISSUE_TYPES_LIST: List[str] = ["exact_duplicates", "near_duplicates"]
+SETS: str = "sets"
+
+# max number of processes that can be forked/spawned for multiprocessing
+MAX_PROCS = 5000
+MAX_RESOLUTION_FOR_BLURRY_DETECTION = 64
+
+IMAGE_FILE_EXTENSIONS: List[str] = [
+    "*.jpg",
+    "*.jpeg",
+    "*.gif",
+    "*.jp2",
+    "*.TIFF",
+    "*.WebP",
+    "*.PNG",
+    "*.JPEG",
+    "*.png",
+]  # filetypes supported by PIL
