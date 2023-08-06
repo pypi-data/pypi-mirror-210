9548bb770bd8693b2f2693e6384bfaaab23eadb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:44 +0000

    Add package bemserver-core/0.15.2/bemserver-core-0.15.2.tar.gz

---
+++ packages/bemserver-core/bemserver-core-0.15.2.tar.gz/bemserver-core-0.15.2/bemserver_core/settings.py
@@ -0,0 +1,12 @@
+"""Default configuration"""
+
+
+DEFAULT_CONFIG = {
+    # SQLAlchemy parameters
+    "SQLALCHEMY_DATABASE_URI": "",
+    # Unit definitions
+    "UNIT_DEFINITION_FILES": [],
+    # Weather data client config
+    "WEATHER_DATA_CLIENT_API_URL": "https://api.oikolab.com/weather",
+    "WEATHER_DATA_CLIENT_API_KEY": "",
+}
