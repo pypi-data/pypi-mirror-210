db28310638263d26c02aacc1a294380dd1863fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:45 +0000

    Add package shrug-viz/0.1.3/shrug_viz-0.1.3.tar.gz

---
+++ packages/shrug-viz/shrug_viz-0.1.3.tar.gz/shrug_viz-0.1.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+from setuptools import setup,find_packages
+
+
+# In[1]:
+
+
+setup(
+    name='shrug_viz',
+    version='0.1.3',
+    author='Kashmira L, Maithili K, Vibhuti D, Pradnya K',
+    author_email='kashmira.lodha@cumminscollege.in, pradnya.kanale@cumminscollege.in, vibhuti.dhande@cumminscollege.in, maithili.karlekar@cumminscollege.in',
+    description='Choropleth visualization for SHRUG data platform (SAMOSA version)',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'plotly',
+        'numpy',
+        'jellyfish',
+        'matplotlib',
+        'geopandas',
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*']},
+)
+
+
+# In[ ]:
+
+
+
+
