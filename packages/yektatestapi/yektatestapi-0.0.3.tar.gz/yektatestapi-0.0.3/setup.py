dd7659c1fd046baf78f706902ff9aa00befe0076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:56 +0000

    Add package yektatestapi/0.0.3/yektatestapi-0.0.3.tar.gz

---
+++ packages/yektatestapi/yektatestapi-0.0.3.tar.gz/yektatestapi-0.0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#
+# Copyright 2020 Kaggle Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='yektatestapi',
+    version='0.0.3',
+    description='test API',
+    long_description=
+    ('my first python test API.'),
+    author='yekta',
+    author_email='yekta1@live.com',
+    keywords=['test', 'API'],
+    entry_points={'console_scripts': ['kaggle = kaggle.cli:main']},
+    install_requires=[
+        'six >= 1.10',
+        'certifi',
+        'python-dateutil',
+        'requests',
+        'tqdm',
+        'python-slugify',
+        'urllib3',
+    ],
+    packages=find_packages(),
+    license='Apache 2.0')
