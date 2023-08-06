db8a14fbf97ea6c2887ff74cd4476f4cd0338a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:44 +0000

    Add package ezsession/0.0.4/ezsession-0.0.4.tar.gz

---
+++ packages/ezsession/ezsession-0.0.4.tar.gz/ezsession-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ezsession",
+    version="0.0.4",
+    description="A small useful Python module to abstract away the common auth header methods used by different software vendors. The output is a requests session with the authentication headers built in.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Josh XT",
+    author_email="josh@devxt.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+)
