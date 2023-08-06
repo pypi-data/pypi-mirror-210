2cdf202247ead9cba65fce7c7b494c0e21eac20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:53 +0000

    Add package transpose-dit/0.1.0/transpose_dit-0.1.0.tar.gz

---
+++ packages/transpose-dit/transpose_dit-0.1.0.tar.gz/transpose_dit-0.1.0/tests/core/sync/test_sync_save_load_metadata.py
@@ -0,0 +1,65 @@
+import os
+
+from transpose import (
+    DedicatedInstance,
+    db_host,
+    db_name,
+    db_password,
+    db_port,
+    db_sslmode,
+    db_username,
+)
+from transpose.util.testing import process_single_batch
+
+
+def test_sync_save_metadata():
+    db = DedicatedInstance(
+        host=db_host,
+        port=db_port,
+        user=db_username,
+        password=db_password,
+        database=db_name,
+        sslmode=db_sslmode,
+    )
+    assert isinstance(db, (DedicatedInstance,))
+
+    db.sync.run(process_single_batch, "ethereum.token_transfers", 1000)
+    db.sync.save_metadata()
+
+    # assert metadata.json exists
+    assert os.path.exists("metadata.json")
+    os.remove("metadata.json")
+
+
+def test_sync_load_metadata():
+    db = DedicatedInstance(
+        host=db_host,
+        port=db_port,
+        user=db_username,
+        password=db_password,
+        database=db_name,
+        sslmode=db_sslmode,
+    )
+    assert isinstance(db, (DedicatedInstance,))
+
+    db.sync.run(process_single_batch, "ethereum.token_transfers", 1000)
+    db.sync.save_metadata()
+
+    # assert metadata.json exists
+    assert os.path.exists("metadata.json")
+
+    db2 = DedicatedInstance(
+        host=db_host,
+        port=db_port,
+        user=db_username,
+        password=db_password,
+        database=db_name,
+        sslmode=db_sslmode,
+    )
+    assert isinstance(db2, (DedicatedInstance,))
+    db2.sync.load_metadata()
+
+    os.remove("metadata.json")
+
+    # assert the metadatas match
+    assert db.sync.metadata == db2.sync.metadata
