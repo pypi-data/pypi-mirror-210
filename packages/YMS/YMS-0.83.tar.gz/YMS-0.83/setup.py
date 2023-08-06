6f3253b5bc7cd4446ea886ba5bda6aa8a41bc261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package YMS/0.83/YMS-0.83.tar.gz

---
+++ packages/YMS/YMS-0.83.tar.gz/YMS-0.83/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="YMS",
+    version="0.83",
+    author="Yarin Bekor",
+    author_email="yarin.bekor@gmail.com",
+    description="YOLO Microbiome Analysis System",
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'yamas = yamas:main',
+        ]
+    },
+    packages=setuptools.find_packages(),
+    package_data={
+        'yamas': ['config.json']
+    },
+    install_requires=[
+        'tqdm',
+        'MetaPhlAn'
+    ]
+)
