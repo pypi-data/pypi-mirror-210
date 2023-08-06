afbb4a5723389c3915318c77a1f0c867329c6bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:32 +0000

    Add package my_first_upload_project/1.0/my_first_upload_project-1.0.tar.gz

---
+++ packages/my_first_upload_project/my_first_upload_project-1.0.tar.gz/my_first_upload_project-1.0/my_first_upload_project/Dog.py
@@ -0,0 +1,8 @@
+class Dog:
+    def __init__(self, name):
+        self.__name = name
+
+    def __str__(self):
+        return self.__name
+
+
---
+++ packages/my_first_upload_project/my_first_upload_project-1.0.tar.gz/my_first_upload_project-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='my_first_upload_project',
+    version='1.0',
+    description='我的第一次',
+    author='黄德俊',
+    author_email='biantaihuang@qq.com',
+    py_modules=['my_first_upload_project.Animal','my_first_upload_project.Dog']
+
+)
