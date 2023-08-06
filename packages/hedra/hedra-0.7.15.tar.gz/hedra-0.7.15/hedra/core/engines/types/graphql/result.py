110ac247786ac29d50c6a37293fe19dd27b99d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:48 +0000

    Add package hedra/0.7.15/hedra-0.7.15.tar.gz

---
+++ packages/hedra/hedra-0.7.15.tar.gz/hedra-0.7.15/hedra/core/engines/types/graphql/result.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from typing import Dict, Union
+from hedra.core.engines.types.common.types import RequestTypes
+from hedra.core.engines.types.http.result import HTTPResult
+from .action import GraphQLAction
+
+
+class GraphQLResult(HTTPResult):
+
+    def __init__(self, action: GraphQLAction, error: Exception = None) -> None:
+        super().__init__(action, error)
+
+        self.type = RequestTypes.GRAPHQL
+
+    @classmethod
+    def from_dict(
+        cls, 
+        results_dict: Dict[str, Union[int, float, str,]]
+    ) -> GraphQLResult:
+        
+        action = GraphQLAction(
+            results_dict.get('name'),
+            results_dict.get('url'),
+            method=results_dict.get('method'),
+            user=results_dict.get('user'),
+            tags=results_dict.get('tags'),
+        )
+
+        response = GraphQLResult(action, error=results_dict.get('error'))
+        
+
+        response.headers.update(results_dict.get('headers', {}))
+        response.data = results_dict.get('data')
+        response.status = results_dict.get('status')
+        response.reason = results_dict.get('reason')
+        response.checks = results_dict.get('checks')
+     
+        response.wait_start = results_dict.get('wait_start')
+        response.start = results_dict.get('start')
+        response.connect_end = results_dict.get('connect_end')
+        response.write_end = results_dict.get('write_end')
+        response.complete = results_dict.get('complete')
+
+        return response
