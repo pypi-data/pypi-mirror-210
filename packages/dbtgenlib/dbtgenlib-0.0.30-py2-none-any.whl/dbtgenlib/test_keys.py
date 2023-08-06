d335cad6a065cc767fdbf7a206643ad9383aa4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:54 +0000

    Add package dbtgenlib/0.0.30/dbtgenlib-0.0.30-py2-none-any.whl

---
+++ packages/dbtgenlib/dbtgenlib-0.0.30-py2-none-any.whl/dbtgenlib/test_keys.py
@@ -0,0 +1,44 @@
+import json
+import click
+from genyml import dbtdocgen
+
+with open('manifest.json') as manifest_file:
+        manifest_data = json.load(manifest_file)
+
+def find_keys():
+    keys_dict = {}
+    for node,data in manifest_data['nodes'].items():
+        if 'unrendered_config' in data.keys():
+            for keys, value in data['unrendered_config'].items():
+                node = node.split('.')[-1]
+                if 'unique_key' in keys:
+                    keys_dict[node] = value.strip()
+    return keys_dict
+
+@Test
+def check_skeys():
+    model_keys = dbtdocgen()
+    skeys = find_keys()
+    non_unique_keys = {}
+
+    flag = True
+
+    for model_key in model_keys:
+        for model_name, skey in skeys.items():
+            if skey.strip() == model_key.strip():
+                pass:
+            else:
+                non_unique_keys[model_name] = skey.strip()
+                flag = False
+
+    return flag, non_unique_keys
+
+if __name__ == '__main__':
+    flag, non_unique_keys = check_skeys()
+
+    if flag == False:
+        print("The test failed!")
+        for model_name, skey in non_unique_keys.items():
+            print(f"For the model: {model_name}, the key: {skey} is not unique!")
+    else:
+        print("The test has been successful!")
