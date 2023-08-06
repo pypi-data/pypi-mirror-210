95d62e5f3cca79a5dcd813893f23d3a0c31fefd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:25 +0000

    Add package finddd/0.2.1/finddd-0.2.1-py3-none-any.whl

---
+++ packages/finddd/finddd-0.2.1-py3-none-any.whl/finddd/const.py
@@ -0,0 +1,3 @@
+SUFFIXES_IMAGE = ("jpg", "jpeg", "webp", "png", "bmp", "avif")
+SUFFIXES_AUDIO = ("wav", "mp3", "m4a", "aac", "flac", "tak", "tta")
+SUFFIXES_VIDEO = ("mp4", "mkv", "ts", "avi", "wmv")
