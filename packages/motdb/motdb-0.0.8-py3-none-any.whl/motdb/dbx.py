eb537fd46b8dbdcc922eef9014e8a452381668eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:04 +0000

    Add package motdb/0.0.8/motdb-0.0.8-py3-none-any.whl

---
+++ packages/motdb/motdb-0.0.8-py3-none-any.whl/motdb/dbx.py
@@ -0,0 +1,37 @@
+# batteries included imports
+from collections import namedtuple
+from pathlib import Path as P
+
+# pypi imports
+import tomli
+
+# custom imports
+from motdb.tools.cli_tools import Cli
+from motdb.tools.path_tools import resolve, resolve_dir, THISD
+
+# load toml
+def load_toml():
+  try:
+    
+    path_toml = P('dbx.toml')
+    
+    if not path_toml.is_file():
+      from motdb.samples import sample
+      cli = Cli()
+      print(THISD)
+      with open('dbx.toml','wb') as f:
+         f.write(sample.toml_file+'\n')
+
+    with open("dbx.toml", "rb") as f:
+        cfg = tomli.load(f)
+        globals()['cfg'] = cfg
+
+  except Exception as e:
+     print(str(e))
+
+class DB:
+    """ Base Database Class """
+    def __init__(self, *kw):
+        load_toml()
+        #default = namedtuple('default',['type'])
+        self.type = cfg.get('default').get('type')
