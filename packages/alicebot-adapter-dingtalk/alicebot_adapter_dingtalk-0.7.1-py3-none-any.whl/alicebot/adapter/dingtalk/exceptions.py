80db2475b261e1936d199b1f5d3984c76fe5fb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:45 +0000

    Add package alicebot-adapter-dingtalk/0.7.1/alicebot_adapter_dingtalk-0.7.1-py3-none-any.whl

---
+++ packages/alicebot-adapter-dingtalk/alicebot_adapter_dingtalk-0.7.1-py3-none-any.whl/alicebot/adapter/dingtalk/exceptions.py
@@ -0,0 +1,14 @@
+"""DingTalk 适配器异常。"""
+from alicebot.exceptions import AdapterException
+
+
+class DingTalkException(AdapterException):
+    """DingTalk 异常基类。"""
+
+
+class NetworkError(DingTalkException):
+    """网络异常。"""
+
+
+class WebhookExpiredError(DingTalkException):
+    """Webhook 地址已到期。"""
