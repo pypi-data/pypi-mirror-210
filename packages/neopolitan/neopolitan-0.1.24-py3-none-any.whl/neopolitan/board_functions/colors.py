5040d613752708926c9f760e9404a96436574f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:15 +0000

    Add package neopolitan/0.1.24/neopolitan-0.1.24-py3-none-any.whl

---
+++ packages/neopolitan/neopolitan-0.1.24-py3-none-any.whl/neopolitan/board_functions/colors.py
@@ -0,0 +1,13 @@
+"""Predefined colors"""
+
+# pylint: disable=fixme
+# Todo: use a color library?
+
+WHITE   = (255,255,255)
+RED     = (255,  0,  0)
+GREEN   = (  0,255,  0)
+BLUE    = (  0,  0,255)
+GRAY    = ( 50, 50, 50)
+
+ON = WHITE
+OFF = GRAY
