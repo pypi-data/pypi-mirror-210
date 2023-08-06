afc96dbc5697062100af8341826ea33ac3719ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:49 +0000

    Add package transaction-indexer-quality-checker/0.0.1/transaction-indexer-quality-checker-0.0.1.tar.gz

---
+++ packages/transaction-indexer-quality-checker/transaction-indexer-quality-checker-0.0.1.tar.gz/transaction-indexer-quality-checker-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transaction-indexer-quality-checker',
+    version='0.0.1',
+    url='https://github.com/keyperlabs/transaction-indexer-quality-checker.git',
+    author='Palmera',
+    author_email='general@palmeradao.xyz',
+    description='Transaction indexer quality checker',
+    packages=find_packages(),    
+    install_requires=[],
+)
