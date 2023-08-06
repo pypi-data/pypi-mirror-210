d51bac6670b8611a4dd8869ea6c7c017a850806f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:24 +0000

    Add package mesh-sandbox/0.1.33/mesh_sandbox-0.1.33-py3-none-any.whl

---
+++ packages/mesh-sandbox/mesh_sandbox-0.1.33-py3-none-any.whl/mesh_sandbox/routers/admin.py
@@ -0,0 +1,84 @@
+from fastapi import APIRouter, BackgroundTasks, Depends, status
+
+from ..dependencies import EnvConfig, get_env_config, normalise_mailbox_id_path
+from ..handlers.admin import AdminHandler
+from ..views.admin import PutReportRequest
+from .request_logging import RequestLoggingRoute
+
+router = APIRouter(
+    route_class=RequestLoggingRoute,
+)
+
+TESTING_ONLY = "This is not part of the real api!, just for testing purposes"
+
+
+@router.get("/health", status_code=status.HTTP_200_OK, include_in_schema=False, response_model_exclude_none=True)
+@router.get("/_status", status_code=status.HTTP_200_OK, include_in_schema=False, response_model_exclude_none=True)
+@router.get("/_ping", status_code=status.HTTP_200_OK, include_in_schema=False, response_model_exclude_none=True)
+@router.get("/_ping", status_code=status.HTTP_200_OK, include_in_schema=False, response_model_exclude_none=True)
+@router.get("/healthcheck", status_code=status.HTTP_200_OK, include_in_schema=False, response_model_exclude_none=True)
+@router.get(
+    "/messageexchange/_ping", status_code=status.HTTP_200_OK, include_in_schema=False, response_model_exclude_none=True
+)
+@router.get(
+    "/messageexchange/deepping",
+    status_code=status.HTTP_200_OK,
+    include_in_schema=False,
+    response_model_exclude_none=True,
+)
+async def ping(config: EnvConfig = Depends(get_env_config)):
+    return {"env": config.env, "build_label": config.build_label, "status": "running", "outcome": "Yes"}
+
+
+@router.delete(
+    "/admin/reset",
+    summary=f"Reset in memory storage completely, will reload from disk. {TESTING_ONLY}",
+    status_code=status.HTTP_200_OK,
+    response_model_exclude_none=True,
+)
+@router.delete(
+    "/messageexchange/reset",
+    status_code=status.HTTP_200_OK,
+    include_in_schema=False,
+    response_model_exclude_none=True,
+)
+async def reset(
+    handler: AdminHandler = Depends(AdminHandler),
+):
+    await handler.reset()
+    return {"message": "all mailboxes reset"}
+
+
+@router.delete(
+    "/admin/reset/{mailbox_id}",
+    summary=f"Clear messages in a particular inbox. {TESTING_ONLY}",
+    status_code=status.HTTP_200_OK,
+    response_model_exclude_none=True,
+)
+@router.delete(
+    "/messageexchange/reset/{mailbox_id}",
+    status_code=status.HTTP_200_OK,
+    include_in_schema=False,
+    response_model_exclude_none=True,
+)
+async def reset_mailbox(
+    mailbox_id: str = Depends(normalise_mailbox_id_path),
+    handler: AdminHandler = Depends(AdminHandler),
+):
+    await handler.reset(mailbox_id)
+    return {"message": f"mailbox {mailbox_id} reset"}
+
+
+@router.post(
+    "/messageexchange/report",
+    summary=f"Put a report messages into a particular inbox. {TESTING_ONLY}",
+    status_code=status.HTTP_200_OK,
+    response_model_exclude_none=True,
+)
+async def put_report(
+    request: PutReportRequest,
+    background_tasks: BackgroundTasks,
+    handler: AdminHandler = Depends(AdminHandler),
+):
+    message = await handler.put_report(request, background_tasks)
+    return {"message_id": message.message_id}
---
+++ packages/mesh-sandbox/mesh_sandbox-0.1.33-py3-none-any.whl/mesh_sandbox/views/admin.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
+
+from mesh_sandbox.models.message import MessageStatus
+
+
+class PutReportRequest(BaseModel):
+    mailbox_id: str = Field(description="mailbox id to put the report in")
+    code: str = Field(description="error code")
+    description: str = Field(description="error description")
+    workflow_id: str = Field(description="report workflow id")
+    subject: Optional[str] = Field(description="report workflow id", default=None)
+    local_id: Optional[str] = Field(description="report message local id", default=None)
+    status: str = Field(description="report status (error/undeliverable)", default=MessageStatus.UNDELIVERABLE)
+    file_name: Optional[str] = Field(description="file name", default=None)
+    linked_message_id: Optional[str] = Field(description="linked message id", default=None)
