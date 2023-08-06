4fa7d79075d8fcc92a8d1b75802b818df779b7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:03 +0000

    Add package yambs/1.7.1/yambs-1.7.1.tar.gz

---
+++ packages/yambs/yambs-1.7.1.tar.gz/yambs-1.7.1/yambs/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=fcea3768b468c73ee0b69f58af61a177
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Yet another meta build-system."
+PKG_NAME = "yambs"
+VERSION = "1.7.1"
