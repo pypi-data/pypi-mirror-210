f2513123eda2c45e3bd95e34f13d16221fedfe21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:30 +0000

    Add package torchbones/1.0.4/torchbones-1.0.4.tar.gz

---
+++ packages/torchbones/torchbones-1.0.4.tar.gz/torchbones-1.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torchbones',
+    version='1.0.4',
+    author='Olive Ross',
+    author_email='ogr8@cornell.edu',
+    description='A user friendly interface for implementing and training a deep neural network in pytorch',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'torch',
+        'numpy',
+        'matplotlib'
+        # Add any other dependencies here
+    ]
+)
