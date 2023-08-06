935839ff621174a78fe0be9528922e9a719664ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:31 +0000

    Add package kaioretry/0.9.4/kaioretry-0.9.4.tar.gz

---
+++ packages/kaioretry/kaioretry-0.9.4.tar.gz/kaioretry-0.9.4/kaioretry/types.py
@@ -0,0 +1,51 @@
+"""Custom types used by kaioretry"""
+
+
+from logging import Logger, getLogger
+from typing import TypeAlias, TypeVar, ParamSpec, Any
+from collections.abc import Callable
+
+from typing_extensions import Protocol
+
+# Protocols do not have public methods. This module will not define any
+# otherwise valid class.
+# pylint: disable=too-few-public-methods
+
+
+ExceptionType: TypeAlias = type[BaseException]
+ExceptionList: TypeAlias = tuple[ExceptionType, ...]
+Exceptions: TypeAlias = ExceptionList | ExceptionType
+
+
+Number: TypeAlias = int | float
+
+
+# Until better implementation.
+NonNegative: TypeAlias = Number   # >= 0
+Positive: TypeAlias = Number      # >  0
+
+
+# jitter parameter type is a bit specific
+JitterTuple: TypeAlias = tuple[Number, Number]
+Jitter: TypeAlias = Number | JitterTuple
+
+
+FuncParam = ParamSpec('FuncParam')
+FuncRetVal = TypeVar('FuncRetVal')
+
+Function: TypeAlias = Callable[..., Any]
+
+
+UpdateDelayFunc: TypeAlias = Callable[[NonNegative], NonNegative]
+
+
+class RetryDecorator(Protocol):
+    """Retry Decorator Type"""
+    def __call__(self,
+                 exceptions: Exceptions = Exception, tries: int = -1, *,
+                 delay: NonNegative = 0, backoff: Number = 1,
+                 jitter: Jitter = 0,  max_delay: NonNegative | None = None,
+                 min_delay: NonNegative = 0,
+                 logger: Logger = getLogger(__name__)) \
+            -> Callable[FuncParam, FuncRetVal]:
+        ...                     # pragma: nocover
