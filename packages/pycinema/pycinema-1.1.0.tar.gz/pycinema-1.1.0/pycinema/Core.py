4334c908f750d8eb93c2b80efb4a012d64dfffb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:56 +0000

    Add package pycinema/1.1.0/pycinema-1.1.0.tar.gz

---
+++ packages/pycinema/pycinema-1.1.0.tar.gz/pycinema-1.1.0/pycinema/Core.py
@@ -0,0 +1,180 @@
+import time
+
+class Image():
+    def __init__(self, channels=None, meta=None):
+        self.channels = channels or {}
+        self.meta = meta or {}
+
+    def copy(self):
+        return Image(
+            self.channels.copy(),
+            self.meta.copy()
+        )
+
+    @property
+    def shape(self):
+        # get first channel
+        for c in self.channels:
+            return self.channels[c].shape
+
+        return (0,0,0)
+
+def isNumber(s):
+    t = type(s)
+    if t == int or t == float:
+        return True
+    if t == str:
+        try:
+            sf = float(s)
+            return True
+        except ValueError:
+            return False
+    return False
+
+class Port():
+    def __init__(self, name, value, parent, is_input = False):
+        self.parent = parent
+        self.name = name
+        self.is_input = is_input
+        self.connections = []
+        self.time = -1
+        if is_input:
+            self.connections.append(self.parent)
+        self._value = value
+
+    def get(self):
+        if isinstance(self._value, Port):
+            return self._value.get()
+        return self._value;
+
+    def getTime(self):
+        if isinstance(self._value, Port):
+            return self._value.time
+        return self.time;
+
+    def set(self, value, update = True):
+        if Filter._debug and update:
+            print(type(self.parent).__name__+"->"+self.name, str(value)[:40])
+
+        if self._value == value:
+            return
+
+        self.time = time.time()
+
+        # if old value is a port stop listing for push events
+        if isinstance(self._value, Port):
+            self._value.connections.remove(self)
+
+        # replace old value with new value
+        self._value = value
+
+        # if new value is a port listen for push events
+        if isinstance(self._value, Port):
+            self._value.connections.append(self)
+
+        # if value of a port was changed trigger update of listeners
+        if update and not Filter._processing:
+            self.parent.update()
+
+class PortList():
+    def __init__(self):
+        return
+
+class Filter():
+
+    _debug = False
+    _processing = False
+    _filters = {}
+
+    def __init__(self):
+        self.inputs = PortList()
+        self.outputs = PortList()
+        self.time = -2
+        self._filters[self] = self
+
+    def __del__(self):
+        del self._filters[self]
+
+    def addInputPort(self, name, value):
+        setattr(self.inputs, name, Port(name, value, self, True))
+
+    def addOutputPort(self, name, value):
+        setattr(self.outputs, name, Port(name, value, self))
+
+    def _update(self):
+        # needs to be overriden
+        return 1
+
+    def computeDAG(self,edges):
+        for f in self._filters:
+            edges[f] = set({})
+
+        for f in self._filters:
+            for name in [o for o in dir(f.outputs) if not o.startswith('__')]:
+                port = getattr(f.outputs, name)
+                for listener in port.connections:
+                    edges[f].add(listener.parent)
+
+        return 1
+
+    def computeTopologicalOrdering(self,edges):
+
+      edgesR = {}
+      for n in edges:
+        for m in edges[n]:
+          if not m in edgesR:
+              edgesR[m] = 0
+          edgesR[m]+=1
+
+      L = []
+      S = []
+      for f in self._filters:
+          if f not in edgesR or not edgesR[f]:
+              S.append(f)
+
+      while len(S)>0:
+          n = S.pop()
+          L.append(n)
+          for m in edges[n]:
+              edgesR[m]-=1
+              if edgesR[m]<1:
+                  S.append(m)
+
+      return L
+
+    def update(self):
+
+        Filter._processing = True
+
+        dagt = time.time()
+        edges = {}
+        self.computeDAG(edges)
+        filters = self.computeTopologicalOrdering(edges)
+
+        if Filter._debug:
+            for k, v in edges.items():
+              print(k,v)
+            print("DAG (%.2fs)" % (time.time()-dagt))
+
+        for i,f in enumerate(filters):
+            lt = f.time
+            needsUpdate = False
+            for name in [o for o in dir(f.inputs) if not o.startswith('__')]:
+                iport = getattr(f.inputs, name)
+                if lt<iport.getTime():
+                    needsUpdate = True
+            if f==self or needsUpdate:
+                t0 = time.time()
+                if Filter._debug:
+                    print('PROCESS',f)
+                f._update()
+                f.time = time.time()
+                if Filter._debug:
+                    print(" -> Done (%.2fs)" % (f.time-t0))
+            elif Filter._debug:
+                print('SKIP',f)
+
+        Filter._processing = False
+
+    def help(self):
+        print('Documentation Missing')
---
+++ packages/pycinema/pycinema-1.1.0.tar.gz/pycinema-1.1.0/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+# read the description file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'doc/description.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version_text = ""
+with open(path.join(this_directory, 'version.md'), encoding='utf-8') as f:
+    version_text = f.read().strip()
+
+setuptools.setup(
+    name="pycinema",
+    version=version_text,
+    author="David H. Rogers",
+    author_email="dhr@lanl.gov",
+    description="Cinema scientific toolset.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/cinemascience/pycinema",
+    include_package_data=True,
+    packages=[  "pycinema"  ],
+    install_requires=[
+        "numpy==1.24.2",
+        "scipy==1.10.0",
+        "h5py==3.8.0",
+        "matplotlib==3.6.0",
+        "py==1.11.0",
+        "Pillow==9.4.0",
+        "moderngl<6",
+        "opencv-python==4.7.0.68",
+        "ipycanvas==0.13.1",
+        "ipywidgets==8.0.6"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        'doc/description.md',
+        'version.md'
+    ],
+)
