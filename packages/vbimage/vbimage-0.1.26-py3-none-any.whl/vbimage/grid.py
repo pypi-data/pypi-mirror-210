06f5db48f04b1cc42fd2fdee4a29e78a3f9fbb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:45 +0000

    Add package vbimage/0.1.26/vbimage-0.1.26-py3-none-any.whl

---
+++ packages/vbimage/vbimage-0.1.26-py3-none-any.whl/vbimage/grid.py
@@ -0,0 +1,64 @@
+
+import click
+import os
+from PIL import Image, ImageDraw
+
+
+
+@click.command(
+        help="Adds background to any transparent image"
+        )
+@click.option(
+        '-i', 
+        '--inputimage', 
+        type=click.Path(),
+        default="./main.png",
+        show_default=True,
+        help="Front Image"
+        )
+@click.option(
+        '-o', 
+        '--outputimage', 
+        type=click.Path(),
+        default="./main_grided.png",
+        show_default=True,
+        help="Resized output image"
+        )
+@click.option(
+        '-s',
+        '--step',
+        type=click.Tuple([int, int]),
+        default=(10, 10),
+        show_default=True,
+        help='Number of division.'
+        )
+def grid(inputimage, outputimage, step):
+    inputimage = Image.open(inputimage)
+    img = Image.new(inputimage)
+    draw = ImageDraw.Draw(img)
+    for i in range(step[0]):
+        W = inputimage.width
+        H = inputimage.height
+        draw.line([(i*W/step[0], H), (i*W/step[0], 0)], fill='black', width=2)
+    
+    for j in range(step[1]):
+        W = inputimage.width
+        H = inputimage.height
+        draw.line([(0, i*H/step[1]), (W, i*H/step[1])], fill='black', width=2)
+
+
+    img.save(outputimage, format="png")
+
+    click.echo(f'{inputimage} is grided to step size {step} as {outputimage}.')
+
+#    with click.progressbar([1, 2, 3]) as bar:
+#        for x in bar:
+#            print(f"sleep({x})...")
+#            time.sleep(x)
+
+
+
+
+
+
+
