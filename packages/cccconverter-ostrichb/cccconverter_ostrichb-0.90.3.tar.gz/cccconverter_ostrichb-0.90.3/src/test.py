a4c39714afd0de718194ceae56650e997e9bfc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:59 +0000

    Add package cccconverter-ostrichb/0.90.3/cccconverter_ostrichb-0.90.3.tar.gz

---
+++ packages/cccconverter-ostrichb/cccconverter_ostrichb-0.90.3.tar.gz/cccconverter_ostrichb-0.90.3/src/test.py
@@ -0,0 +1,39 @@
+import unittest
+from cccconverter.convert import *
+from cccconverter.parse_dict import *
+
+
+class TestConvert(unittest.TestCase):
+    def test_convert_single_syllable(self):
+        self.assertEqual(convert_single_syllable('jung'), ['yung', 'yong'])
+
+    def test_convert_single_character(self):
+        self.assertEqual(convert_single_character('慈'), 'tsz/tszi/tszu/tsi/tsee/chz/chzi/chzu/chi/chee')
+
+    def test_convert_sentence(self):
+        self.assertEqual(convert_sentence('慈雲山子', only_keep_first_spell=False, only_keep_first_pronunciation=False),
+                         'tsz/tszi/tszu/tsi/tsee/chz/chzi/chzu/chi/chee wan/wun/wen'
+                         ' shaan/shan/saan/san tsz/tszi/tszu/tsi/tsee/chz/chzi/chzu'
+                         '/chi/chee||chz/chzi/chzu/chi/chee/tsz/tszi/tszu/tsi/tsee')
+        self.assertEqual(convert_sentence('慈雲山子', only_keep_first_spell=True, only_keep_first_pronunciation=False),
+                         'tsz wan shaan tsz||chz')
+        self.assertEqual(convert_sentence('慈雲山子', only_keep_first_spell=True, only_keep_first_pronunciation=True),
+                         'tsz wan shaan tsz')
+
+    def test_convert_sentence_map(self):
+        self.assertEqual(convert_sentence_map('慈雲山子'),
+                         [('慈', 'tsz/tszi/tszu/tsi/tsee/chz/chzi/chzu/chi/chee'),
+                          ('雲', 'wan/wun/wen'),
+                          ('山', 'shaan/shan/saan/san'),
+                          ('子',
+                           'tsz/tszi/tszu/tsi/tsee/chz/chzi/chzu/chi/chee||'
+                           'chz/chzi/chzu/chi/chee/tsz/tszi/tszu/tsi/tsee')])
+
+
+class TestParseDict(unittest.TestCase):
+    def test_get_yuet_dict(self):
+        logging.info(get_yuet_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
