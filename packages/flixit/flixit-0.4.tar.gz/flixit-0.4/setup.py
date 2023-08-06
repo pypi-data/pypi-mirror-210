61ae536921f4bfdd743a928e4b834a6b3a722a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:18 +0000

    Add package flixit/0.4/flixit-0.4.tar.gz

---
+++ packages/flixit/flixit-0.4.tar.gz/flixit-0.4/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='flixit',
+    version='0.4',
+    license='MIT',
+    author="Aniket Chopade",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Aniket29-shiv/flixit_pypi',
+    keywords='example project',
+    install_requires=[
+          
+      ],
+
+)
