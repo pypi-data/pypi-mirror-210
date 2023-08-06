2089ba8faf793b36e2c907d82a7045db08942834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:35 +0000

    Add package egnn-jax/0.3/egnn_jax-0.3-py3-none-any.whl

---
+++ packages/egnn-jax/egnn_jax-0.3-py3-none-any.whl/egnn_jax/__init__.py
@@ -0,0 +1,11 @@
+from .egnn import EGNN, EGNNLayer
+from .variants import EGNN_vel, EGNNLayer_vel
+
+__all__ = [
+    "EGNN",
+    "EGNNLayer",
+    "EGNN_vel",
+    "EGNNLayer_vel",
+]
+
+__version__ = "0.3"
