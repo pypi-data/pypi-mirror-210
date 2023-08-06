47aeb6fbb912ad424a64fbc7fd19f0b40f3f1ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:37 +0000

    Add package jwt-helper/0.0.11/jwt_helper-0.0.11.tar.gz

---
+++ packages/jwt-helper/jwt_helper-0.0.11.tar.gz/jwt_helper-0.0.11/README.rst
@@ -0,0 +1,23 @@
+================
+jwt_helper
+================
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://pypi.org/project/black
+
+.. image:: https://app.codacy.com/project/badge/Grade/bfc97f9667f64c4a9f7d26f937393260
+    :target: https://www.codacy.com/gh/bad-microservices/jwt_helper/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=bad-microservices/jwt_helper&amp;utm_campaign=Badge_Grade
+
+.. image:: https://badge.fury.io/py/jwt_helper.svg
+    :target: https://badge.fury.io/py/jwt_helper
+
+.. image:: https://github.com/bad-microservices/jwt_helper/actions/workflows/documentation.yaml/badge.svg
+   :target: https://github.com/bad-microservices/jwt_helper/actions?query=workflow:Docs
+
+.. image:: https://github.com/bad-microservices/jwt_helper/actions/workflows/pypi_upload.yaml/badge.svg
+    :target: https://github.com/bad-microservices/jwt_helper/actions?query=workflow:pypi
+
+Introduction
+=============
+
+jwt_helper aims to be a simple to use wrapper around `pyjwt` with some predefined functions to make life easier.
---
+++ packages/jwt-helper/jwt_helper-0.0.11.tar.gz/jwt_helper-0.0.11/jwt_helper/_jwtvalidator.py
@@ -0,0 +1,149 @@
+import jwt
+import datetime
+
+from typing import Optional, List, Dict
+from dataclasses import dataclass, field
+
+from jwt_helper._issuer import Issuer
+
+
+@dataclass
+class JWTValidator:
+    """A class to help verify JWT Tokens
+
+    Attributes:
+        issuers (dict): A dictionary storing our Trusted Issuers and their methods and secrets
+    """
+
+    issuers: dict[str, Issuer] = field(default_factory=dict)
+
+    def __repr__(self):
+        return f"JWTValidator(Issuers[{', '.join(self.issuers)}])"
+
+    def add_issuer(self, issuer: Issuer) -> bool:
+        """Adds an issuer to the list of trusted issuers
+
+        Args:
+            issuer (Issuer): the issuer we want to add to our list
+
+        Returns:
+            bool: False if the issuer name is already known to us
+        """
+        try:
+            _ = self.issuers[issuer.name]
+            return False
+        except KeyError:
+            pass
+        self.issuers[issuer.name] = issuer
+        return True
+
+    def verify_jwt(self, token: str, not_before_grace_time: Optional[int] = 30) -> bool:
+        """Function to check if a token is valid
+
+        This Function checks a JWT Token for it's validity. The Tokens 'iss' Header entry gets used to determine
+        the Issuer from our Issuers list.
+
+        Important:
+            Requires the following claims to exist in the JWT
+            * :code:`exp`
+            * :code:`iss`
+            * :code:`nbf`
+
+        Args:
+            token (str): JWT Token we want to verify
+            not_before_grace_time (int): Seconds tolerance to the nbf header. Defaults to 30 seconds.
+
+        Raises:
+            ValueError: If the Issuer is not known to us throw this
+
+        Returns:
+            bool: Validity of the Token
+        """
+
+        # get token header
+        headers = jwt.get_unverified_header(token)
+
+        # get_issuer
+        try:
+            issuer = self.issuers[headers["iss"]]
+        except KeyError:
+            return False
+
+        now = datetime.datetime.now().timestamp()
+
+        if now > headers["exp"]:
+            return False
+
+        if headers["nbf"] - not_before_grace_time > now:
+            return False
+
+        # validate token
+        jwt.decode(
+            token,
+            key=issuer.secret,
+            algorithms=[method.value for method in issuer.methods],
+            options={"verify_exp": True},
+        )
+
+        return True
+
+    @staticmethod
+    def get_jwt_as_dict_untrusted(jwt_token: str) -> dict:
+        """Returns the content of the JWT without validating its signature.
+
+        Args:
+            jwt_token (str): jwt to decode
+
+        TODO:
+            * add example code here
+
+        Returns:
+            dict: JWT Content as dict
+        """
+
+        return {
+            "headers": jwt.get_unverified_header(jwt_token),
+            "payload": jwt.decode(jwt_token, options={"verify_signature": False}),
+        }
+
+    @staticmethod
+    def from_dict(cfg_dict: List[Dict[str, str]]):
+        jwt_validator = JWTValidator()
+
+        for issuer in cfg_dict:
+            jwt_validator.add_issuer(Issuer.from_dict(issuer))
+
+        return jwt_validator
+
+    def get_jwt_as_dict(self, jwt_token: str) -> dict:
+        """Gets JWT Content as dict but verifies the JWT itself
+
+        Args:
+            jwt_token (str): JWT Token to get the data from
+
+        Raises:
+            ValueError: If jwt could not be validated
+
+        Returns:
+            dict: JWT Content as dict
+        """
+
+        if not self.verify_jwt(jwt_token):
+            raise ValueError("JWT Invalid")
+
+        return JWTValidator.get_jwt_as_dict_untrusted(jwt_token)
+
+    def remove_issuer(self, issuer: str) -> bool:
+        """Remove and issuer (by name) from our dictionary of trusted issuers
+
+        Args:
+            issuer (str): Name of the Issuer to remove
+
+        Returns:
+            bool: Removal success
+        """
+        try:
+            self.issuers.pop(issuer)
+            return True
+        except KeyError:
+            return False
