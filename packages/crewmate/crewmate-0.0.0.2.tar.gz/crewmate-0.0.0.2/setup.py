55e9caff356aaf393ecfcacdb60ded90538e2fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:12 +0000

    Add package crewmate/0.0.0.2/crewmate-0.0.0.2.tar.gz

---
+++ packages/crewmate/crewmate-0.0.0.2.tar.gz/crewmate-0.0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='crewmate',
+    version='0.0.0.2',
+    url="https://github.com/Qcrew/crewmate",
+    author="Qcrew",
+    author_email="general.qcrew@gmail.com",
+    description='',
+    py_modules=['hello', 'utils/utils'],
+    package_dir={'': 'src'},
+    install_requires=[
+        "numpy"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+            "check-manifest>=0.49"
+        ]
+    }
+)
