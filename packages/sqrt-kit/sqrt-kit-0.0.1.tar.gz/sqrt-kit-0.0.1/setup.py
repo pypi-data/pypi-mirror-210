6e1c32617477c3ef100e29433c959a81123ae142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:42 +0000

    Add package sqrt-kit/0.0.1/sqrt-kit-0.0.1.tar.gz

---
+++ packages/sqrt-kit/sqrt-kit-0.0.1.tar.gz/sqrt-kit-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="sqrt-kit",
+    version="0.0.1",
+    license='MIT',
+    author=".",
+    author_email="",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+
+    ],
+)
