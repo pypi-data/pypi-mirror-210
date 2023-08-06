cb59dc928656467978556b1a1052c3f521582ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:12 +0000

    Add package sayql/0.1.0/sayql-0.1.0.tar.gz

---
+++ packages/sayql/sayql-0.1.0.tar.gz/sayql-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sayql']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['duckdb>=0.8.0,<0.9.0', 'pandas>=2.0.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'sayql',
+    'version': '0.1.0',
+    'description': 'Talk to your data',
+    'long_description': None,
+    'author': 'Ryan',
+    'author_email': 'ryan.sudhakaran@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
