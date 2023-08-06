1768c74f3570cd3d1839aa9429fc1ac993b6b11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:34 +0000

    Add package vanty/0.0.6/vanty-0.0.6-py3-none-any.whl

---
+++ packages/vanty/vanty-0.0.6-py3-none-any.whl/vanty/project.py
@@ -0,0 +1,33 @@
+import rich
+import typer
+
+from vanty._client import Client
+from vanty.config import config
+
+app = typer.Typer(name="project", help="Manage project files", no_args_is_help=True)
+
+
+@app.command(help="Downloads project files from advantch.com.")
+def download():
+    """
+    Download project files from advantch.com.
+    :return:
+    """
+
+    token_id = config.get("token_id")
+    if token_id is None:
+        rich.print("[red]Please run `vanty auth set` to set your token.[/red]")
+        raise typer.Exit(code=1)
+
+    rich.print("[green]Downloading project files...[/green]")
+    Client().download()
+
+
+@app.command(help="Print current configuration.")
+def print_config():
+    """
+    Print current configuration.
+    :return:
+    """
+    rich.print("[green]Current configuration:[/green]")
+    rich.print(config)
