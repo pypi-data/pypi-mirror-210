a8c34b3a8bacfa30c4f7b09c62d29389dc0f7097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:51 +0000

    Add package python-openblas-build/0.3.23rc2/python_openblas_build-0.3.23rc2-py2.py3-none-macosx_10_9_x86_64.whl

---
+++ packages/python-openblas-build/python_openblas_build-0.3.23rc2-py2.py3-none-macosx_10_9_x86_64.whl/python_openblas_build/__init__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+if sys.platform == "win32":
+    static_library = "openblas_64.lib"
+else:
+    static_library = "libopenblas.a"
+
+__dir__ = os.path.dirname(__file__)
+if os.path.isdir(os.path.join(__dir__, 'include', 'openblas')):
+    include_dirs = os.path.join(__dir__, 'include', 'openblas')
+elif os.path.isdir(os.path.join(__dir__, 'include', 'openblas64')):
+    include_dirs = os.path.join(__dir__, 'include', 'openblas64')
+else:
+    raise RuntimeError("Unsupported include layout")
+
+if os.path.isdir(os.path.join(__dir__, 'lib')):
+    library_dir = os.path.join(__dir__, 'lib')
+elif os.path.isdir(os.path.join(__dir__, 'lib64')):
+    library_dir = os.path.join(__dir__, 'lib64')
+else:
+    raise RuntimeError("Unsupported library layout")
