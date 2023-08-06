2745f69d74ff36af141fa6e83c79d6db50bcdb5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:31 +0000

    Add package django-modshib-certic/0.1.2/django-modshib-CERTIC-0.1.2.tar.gz

---
+++ packages/django-modshib-certic/django-modshib-CERTIC-0.1.2.tar.gz/django-modshib-CERTIC-0.1.2/modshib/context_processors.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+_default_conf = {"MODSHIB_SP_LOGOUT_URL": "/Shibboleth.sso/Logout"}
+
+for k in _default_conf.keys():
+    try:
+        val = getattr(settings, k)
+        _default_conf[k] = val
+    except AttributeError:
+        continue
+
+
+def modshib_context(request):
+    return _default_conf
