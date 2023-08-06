069a4e98d3e061935c80df407f5eb34f7b100c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:48 +0000

    Add package fedbase/0.10.1/fedbase-0.10.1-py3-none-any.whl

---
+++ packages/fedbase/fedbase-0.10.1-py3-none-any.whl/fedbase/baselines/ifca_cam.py
@@ -0,0 +1,121 @@
+from fedbase.utils.data_loader import data_process, log
+from fedbase.nodes.node import node
+from fedbase.utils.tools import add_
+from fedbase.server.server import server_class
+from fedbase.baselines import fedavg
+import torch
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from fedbase.model.model import CNNCifar, CNNMnist
+import os
+import sys
+import inspect
+from functools import partial
+
+def run(dataset_splited, batch_size, K, num_nodes, model, objective, optimizer, warmup_rounds, global_rounds, local_steps, \
+    reg = None, device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'), finetune=False, finetune_steps = None):
+    train_splited, test_splited, split_para = dataset_splited
+    # warm up
+    model_g = fedavg.run(dataset_splited, batch_size, num_nodes, model, objective, optimizer, warmup_rounds, local_steps, device, log_file = False)
+
+    # initialize
+    server = server_class(device)
+    server.assign_model(model_g)
+
+    nodes = [node(i, device) for i in range(num_nodes)]
+
+    for i in range(num_nodes):
+        # data
+        # print(len(train_splited[i]), len(test_splited[i]))
+        nodes[i].assign_train(DataLoader(train_splited[i], batch_size=batch_size, shuffle=True))
+        nodes[i].assign_test(DataLoader(test_splited[i], batch_size=batch_size, shuffle=False))
+        # objective
+        nodes[i].assign_objective(objective())
+        nodes[i].assign_model(model())
+        nodes[i].model_g = model().to(device)
+        nodes[i].assign_optim({'local_0': optimizer(nodes[i].model.parameters()),\
+                'local_1': optimizer(nodes[i].model_g.parameters()),\
+                    'all': optimizer(list(nodes[i].model.parameters())+list(nodes[i].model_g.parameters()))})
+
+    del train_splited, test_splited
+
+    # initialize K cluster model
+    cluster_models = [model().to(device) for i in range(K)]
+
+    weight_list = [nodes[i].data_size/sum([nodes[i].data_size for i in range(num_nodes)]) for i in range(num_nodes)]
+    
+    # distribute global model to model_g
+    server.distribute([nodes[i].model_g for i in range(num_nodes)])
+
+    # train!
+    for i in range(global_rounds - warmup_rounds):
+        print('-------------------Global round %d start-------------------' % (i))
+
+        # local update model_0 for cluster model
+        for j in range(num_nodes):
+            nodes[j].local_update_steps(local_steps, partial(nodes[j].train_single_step_res, optimizer = nodes[j].optim['local_0'], \
+                model_opt = nodes[j].model, model_fix = nodes[j].model_g)) 
+        
+        # local update model_g for global model
+        for j in range(num_nodes):
+            nodes[j].local_update_steps(local_steps, partial(nodes[j].train_single_step_res, optimizer = nodes[j].optim['local_1'],\
+                model_opt = nodes[j].model_g, model_fix = nodes[j].model))
+
+        # server clustering
+        assignment = [[] for _ in range(K)]
+        for i in range(num_nodes):
+            m = 0
+            for k in range(1, K):
+                # if i <=5:
+                #     print(nodes[i].local_train_acc(cluster_models[m]), nodes[i].local_train_acc(cluster_models[k]))
+                # if nodes[i].local_train_loss(cluster_models[m])>=nodes[i].local_train_loss(cluster_models[k]):
+                if nodes[i].local_train_acc(cluster_models[m])<=nodes[i].local_train_acc(cluster_models[k]):
+                    m = k
+            assignment[m].append(i)
+            nodes[i].label = m
+
+        server.clustering['label'].append(assignment) 
+        # print(assignment)
+        print([len(assignment[i]) for i in range(len(assignment))]) 
+
+        # server aggregation and distribution by cluster
+        for k in range(K):
+            if len(assignment[k])>0:
+                model_k = server.aggregate([nodes[i].model for i in assignment[k]], \
+                    [nodes[i].data_size/sum([nodes[i].data_size for i in assignment[k]]) for i in assignment[k]])
+                server.distribute([nodes[i].model for i in assignment[k]], model_k)
+                cluster_models[k].load_state_dict(model_k)
+            
+        # aggregate model_g
+        server.model.load_state_dict(server.aggregate([nodes[i].model_g for i in range(num_nodes)], weight_list))
+        # distribute global model to model_g
+        server.distribute([nodes[i].model_g for i in range(num_nodes)])
+ 
+        # test accuracy
+        for j in range(num_nodes):
+             nodes[j].local_test(model_res = nodes[j].model_g)
+        server.acc(nodes, weight_list)
+
+    if not finetune:
+        assign = [[i for i in range(num_nodes) if nodes[i].label == k] for k in range(K)]
+        # log
+        log(os.path.basename(__file__)[:-3] + add_(K) + add_(reg) + add_(split_para), nodes, server)
+        return cluster_models, assign
+    else:
+        if not finetune_steps:
+            finetune_steps = local_steps
+        # fine tune
+        # local update model for cluster model
+        for j in range(num_nodes):
+            nodes[j].local_update_steps(local_steps, partial(nodes[j].train_single_step_res, optimizer = nodes[j].optim['local_0'], \
+                model_opt = nodes[j].model, model_fix = nodes[j].model_g)) 
+        
+        # local update model_g for global model
+        for j in range(num_nodes):
+            nodes[j].local_update_steps(local_steps, partial(nodes[j].train_single_step_res, optimizer = nodes[j].optim['local_1'],\
+                model_opt = nodes[j].model_g, model_fix = nodes[j].model))
+            nodes[j].local_test()
+        server.acc(nodes, weight_list)
+        # log
+        log(os.path.basename(__file__)[:-3] + add_('finetune') + add_(K) + add_(reg) + add_(split_para), nodes, server)
+        return [nodes[i].model for i in range(num_nodes)]
---
+++ packages/fedbase/fedbase-0.10.1-py3-none-any.whl/fedbase/nodes/node.py
@@ -0,0 +1,347 @@
+import torch
+from torch import linalg as LA
+from fedbase.utils.model_utils import save_checkpoint, load_checkpoint
+from fedbase.model.model import CNNMnist, MLP
+from sklearn.metrics import accuracy_score, f1_score
+from fedbase.utils.tools import unpack_args
+from functools import partial
+from statistics import mode
+import torch.nn.functional as F
+
+class node():
+    def __init__(self, id, device):
+        self.id = id
+        self.test_metrics = []
+        self.step = 0
+        self.device = device
+        self.grads = []
+        # self.train_steps = 0
+
+    def assign_train(self, data):
+        self.train = data
+        self.data_size = len(data.dataset)
+    
+    def assign_test(self,data):
+        self.test = data
+
+    def assign_model(self, model):
+        try:
+            self.model.load_state_dict(model.state_dict())
+        except:
+            self.model = model
+        self.model.to(self.device)
+        # try:
+        #     self.model = torch.compile(self.model)
+        # except:
+        #     pass
+
+    def assign_objective(self, objective):
+        self.objective = objective
+
+    def assign_optim(self, optim):
+        self.optim = optim
+    
+    def local_update_steps(self, local_steps, train_single_step_func):
+        # print(len(self.train), self.step)
+        if len(self.train) - self.step > local_steps:
+            for k, (inputs, labels) in enumerate(self.train):
+                if k < self.step or k >= self.step + local_steps:
+                    continue
+                train_single_step_func(inputs, labels)
+            self.step = self.step + local_steps
+        else:
+            for k, (inputs, labels) in enumerate(self.train):
+                if k < self.step:
+                    continue
+                train_single_step_func(inputs, labels)
+            for j in range((local_steps-len(self.train)+self.step)//len(self.train)):
+                for k, (inputs, labels) in enumerate(self.train):
+                    train_single_step_func(inputs, labels)
+            for k, (inputs, labels) in enumerate(self.train):
+                if k >=(local_steps-len(self.train)+self.step)%len(self.train):
+                    continue
+                train_single_step_func(inputs, labels)
+            self.step = (local_steps-len(self.train)+self.step)%len(self.train)
+        # torch.cuda.empty_cache()
+
+    def local_update_epochs(self, local_epochs, train_single_step_func):
+        # local_steps may be better!!
+        running_loss = 0
+        for j in range(local_epochs):
+            for k, (inputs, labels) in enumerate(self.train):
+                train_single_step_func(inputs, labels)
+        # torch.cuda.empty_cache()
+
+    def train_single_step(self, inputs, labels):
+        inputs = inputs.to(self.device)
+        labels = torch.flatten(labels)
+        labels = labels.to(self.device, dtype = torch.long)
+        # print(labels)
+        # zero the parameter gradients
+        # self.model.zero_grad(set_to_none=True)
+        self.optim.zero_grad()
+        # forward + backward + optimize
+        outputs = self.model(inputs)
+        # optim
+        loss = self.objective(outputs, F.one_hot(labels, outputs.shape[1]).float())
+        loss.backward()
+
+        # calculate accumulate gradients
+        # grads = torch.tensor([])
+        # for index, param in enumerate(self.model.parameters()):
+        #     # param.grad = torch.tensor(grads[index])
+        #     grads= torch.cat((grads, torch.flatten(param.grad).cpu()),0)
+        # self.grads.append(grads)
+
+        self.optim.step()
+        # self.train_steps+=1
+
+    # for fedprox and ditto
+    def train_single_step_fedprox(self, inputs, labels, reg_lam = None, reg_model = None):
+        inputs = inputs.to(self.device)
+        labels = torch.flatten(labels)
+        labels = labels.to(self.device, dtype = torch.long)
+        # zero the parameter gradients
+        # self.model.zero_grad(set_to_none=True)
+        self.optim.zero_grad()
+        # forward + backward + optimize
+        outputs = self.model(inputs)
+        # optim
+        if reg_lam:
+            reg_model.to(self.device)
+            reg = torch.square(torch.norm(torch.cat(tuple([torch.flatten(self.model.state_dict()[k] - reg_model.state_dict()[k])\
+                    for k in self.model.state_dict().keys()]),0),2))
+        else:
+            reg, reg_lam = 0, 0
+        self.loss = self.objective(outputs, labels) + reg_lam*reg/2
+        # print(self.objective(outputs, labels))
+        self.loss.backward()
+        self.optim.step()
+        # print('after', self.objective(self.model(inputs), labels))
+    
+    def train_single_step_res(self, inputs, labels, optimizer, model_opt, model_fix, reg_lam = None, reg_model = None):
+        inputs = inputs.to(self.device)
+        labels = torch.flatten(labels)
+        labels = labels.to(self.device, dtype = torch.long)
+        # zero the parameter gradients
+        # model_opt.zero_grad(set_to_none=True)
+        # model_fix.zero_grad(set_to_none=True)
+        optimizer.zero_grad()
+        # model_2.zero_grad(set_to_none=True)
+        # forward + backward + optimize
+        # loss 1
+        # m = torch.nn.LogSoftmax(dim=1)
+        # ls = torch.nn.NLLLoss()
+        # outputs = (m(model_opt(inputs)) + m(model_fix(inputs)))/2
+        # loss = ls(outputs, labels)
+
+        # loss 2
+        # reg = 0
+        # for p,q in zip(model_opt.parameters(), model_fix.parameters()):
+        #     reg += torch.norm((p-q),2)
+
+        outputs = model_opt(inputs) + model_fix(inputs)
+        
+        if reg_lam:
+            reg_model.to(self.device)
+            reg = torch.square(torch.norm(torch.cat(tuple([torch.flatten(model_opt.state_dict()[k] - reg_model.state_dict()[k])\
+                 for k in model_opt.state_dict().keys()]),0),2))
+        else:
+            reg, reg_lam = 0, 0
+
+        loss = self.objective(outputs, labels) + reg_lam/2 * reg
+        # loss 3
+        # reg = 0
+        # for p,q in zip(model_opt.parameters(), model_fix.parameters()):
+        #     reg += torch.square(torch.norm((p-q),2))
+        # # print(reg)
+        # outputs = model_opt(inputs)
+        # loss = self.objective(outputs, labels) + 0.001*reg
+        # # optim
+        # outputs = torch.norm(model_opt(inputs) - model_fix(inputs), p = 2)
+        # self.loss = outputs
+        loss.backward()        
+        optimizer.step()
+
+    def train_single_step_con(self, inputs, labels, model_sim, model_all, tmp, mu, base = 'representation', reg_lam = None, reg_model = None):
+        inputs = inputs.to(self.device)
+        labels = torch.flatten(labels)
+        labels = labels.to(self.device, dtype = torch.long)
+        # zero the parameter gradients
+        # model_opt.zero_grad(set_to_none=True)
+        self.optim.zero_grad()
+        # forward + backward + optimize
+
+        # contrastive loss
+        output_con_dn = 0
+        if base == 'representation':
+            # for i in model_all:
+            #     i.to(self.device)
+            #     output_con_dn += torch.exp(F.cosine_similarity(self.intermediate_output(inputs, self.model, self.model.conv2, 'conv2')\
+            #          , self.intermediate_output(inputs, i, i.conv2, 'conv2'), dim = -1)/tmp)
+            # output_con_n = torch.exp(F.cosine_similarity(self.intermediate_output(inputs, self.model, self.model.conv2, 'conv2')\
+            #      , self.intermediate_output(inputs, model_sim, model_sim.conv2, 'conv2'), dim = -1)/tmp)
+            for i in model_all:
+                output_con_dn += torch.exp(F.cosine_similarity(self.model(inputs), i(inputs))/tmp)
+            output_con_n = torch.exp(F.cosine_similarity(self.model(inputs), model_sim(inputs))/tmp)
+            con_loss = torch.mean(-torch.log(output_con_n/output_con_dn))
+
+        elif base == 'parameter':
+            negative = [torch.cat(tuple([torch.flatten(i.state_dict()[k]) for k in i.state_dict().keys() if 'fc' in k]),0) for i in model_all]
+            positive = torch.cat(tuple([torch.flatten(self.model.state_dict()[k]) for k in self.model.state_dict().keys() if 'fc' in k]),0)
+
+            for i in range(len(model_all)):
+                tmp = torch.exp(F.cosine_similarity(positive, negative[i], dim=0)/tmp)
+                output_con_dn += tmp
+                if i == self.label:
+                    output_con_n = tmp
+            con_loss = -torch.log(output_con_n/output_con_dn) 
+        else:
+            con_loss = 0
+        
+        # knowledge sharing
+        if reg_lam:
+            reg = torch.square(torch.norm(torch.cat(tuple([torch.flatten(self.model.state_dict()[k] - reg_model.state_dict()[k])\
+                 for k in self.model.state_dict().keys() if 'fc' not in k]),0),2))
+        else:
+            reg, reg_lam = 0, 0
+                
+        loss = self.objective(self.model(inputs), labels) + con_loss * mu + reg_lam * reg
+        # if self.id == 0:
+        #     # print(self.model.state_dict()['fc1.bias'])
+        #     # print(self.label)
+        #     # for i in range(len(model_all)):
+        #     #     print(i, model_all[i].state_dict()['fc1.bias'])
+        #     print(self.intermediate_output(inputs, self.model, self.model.conv2, 'conv2').shape)
+        #     # print(self.intermediate_output(inputs, model_sim, model_sim.conv2, 'conv2'))
+            # print(output_con_n, output_con_dn)
+        
+        loss.backward()        
+        self.optim.step()
+
+    def intermediate_output(self, inputs, model, model_layer, layer_name):
+        activation = {}
+        def get_activation(name):
+            def hook(model, input, output):
+                activation[name] = output.detach()
+            return hook
+        
+        model_layer.register_forward_hook(get_activation(layer_name))
+        out = model(inputs)
+        return torch.flatten(activation[layer_name],1)
+
+    # for IFCA
+    def local_train_loss(self, model):
+        model.to(self.device)
+        train_loss = 0
+        i = 0
+        with torch.no_grad():
+            for data in self.train:
+                inputs, labels = data
+                inputs = inputs.to(self.device)
+                labels = torch.flatten(labels)
+                labels = labels.to(self.device, dtype = torch.long)
+                # forward
+                outputs = model(inputs)
+                train_loss += self.objective(outputs, labels)
+                i+=1
+                if i>=10:
+                    break
+        # return train_loss/len(self.train)
+        return train_loss/i
+    
+    def local_train_acc(self, model):
+        model.to(self.device)
+        predict_ts = torch.empty(0).to(self.device)
+        label_ts = torch.empty(0).to(self.device)
+        i = 0
+        with torch.no_grad():
+            for data in self.train:
+                inputs, labels = data
+                inputs = inputs.to(self.device)
+                labels = torch.flatten(labels)
+                labels = labels.to(self.device, dtype = torch.long)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                predict_ts = torch.cat([predict_ts, predicted], 0)
+                label_ts = torch.cat([label_ts, labels], 0)
+                i+=1
+                if i>=10:
+                    break
+        acc = accuracy_score(label_ts.cpu(), predict_ts.cpu())
+        return acc
+
+    def local_test(self, model_res = None):
+        predict_ts = torch.empty(0).to(self.device)
+        label_ts = torch.empty(0).to(self.device)
+        with torch.no_grad():
+            for data in self.test:
+                inputs, labels = data
+                inputs = inputs.to(self.device)
+                labels = torch.flatten(labels)
+                labels = labels.to(self.device, dtype = torch.long)
+                if model_res:
+                    model_res.to(self.device)
+                    outputs = model_res(inputs) + self.model(inputs)
+                else:
+                    outputs = self.model(inputs) 
+                # print(outputs.data.dtype)
+                _, predicted = torch.max(outputs.data, 1)
+                predict_ts = torch.cat([predict_ts, predicted], 0)
+                label_ts = torch.cat([label_ts, labels], 0)
+        acc = accuracy_score(label_ts.cpu(), predict_ts.cpu())
+        macro_f1 = f1_score(label_ts.cpu(), predict_ts.cpu(), average='macro')
+        # micro_f1 = f1_score(label_ts.cpu(), predict_ts.cpu(), average='micro')
+        # print('Accuracy, Macro F1, Micro F1 of Device %d on the %d test cases: %.2f %%, %.2f, %.2f' % (self.id, len(label_ts), acc*100, macro_f1, micro_f1))
+        print('Accuracy, Macro F1 of Device %d on the %d test cases: %.2f %%, %.2f %%' % (self.id, len(label_ts), acc*100, macro_f1*100))
+        self.test_metrics.append([acc, macro_f1])
+
+
+    def local_ensemble_test(self, model_list, voting = 'soft'):
+        predict_ts = torch.empty(0).to(self.device)
+        label_ts = torch.empty(0).to(self.device)
+        with torch.no_grad():
+            for data in self.test:
+                inputs, labels = data
+                inputs = inputs.to(self.device)
+                labels = torch.flatten(labels)
+                labels = labels.to(self.device, dtype = torch.long)
+                out_hard = []
+                if voting == 'soft':
+                    out = torch.zeros(self.model(inputs).data.shape).to(self.device)
+                    for model in model_list:
+                        outputs = model(inputs)
+                        out = out + outputs.data/len(model_list)
+                        _, predicted = torch.max(out, 1)
+                elif voting == 'hard':
+                    out_hard = []
+                    for model in model_list:
+                        outputs = model(inputs)
+                        _, predicted = torch.max(outputs.data, 1)
+                        out_hard.append(predicted)       
+                    predicted = torch.tensor([mode([out_hard[i][j] for i in range(len(out_hard))]) for j in range(len(out_hard[0]))]).to(self.device)
+
+                predict_ts = torch.cat([predict_ts, predicted], 0)
+                label_ts = torch.cat([label_ts, labels], 0)
+        acc = accuracy_score(label_ts.cpu(), predict_ts.cpu())
+        macro_f1 = f1_score(label_ts.cpu(), predict_ts.cpu(), average='macro')
+        # micro_f1 = f1_score(label_ts.cpu(), predict_ts.cpu(), average='micro')
+        # print('Accuracy, Macro F1, Micro F1 of Device %d on the %d test cases: %.2f %%, %.2f, %.2f' % (self.id, len(label_ts), acc*100, macro_f1, micro_f1))
+        print('Accuracy, Macro F1 of Device %d on the %d test cases: %.2f %%, %.2f %%' % (self.id, len(label_ts), acc*100, macro_f1*100))
+        self.test_metrics.append([acc, macro_f1])
+
+
+#     def model_representation(self, test_set, repr='output'):
+#         self.model_repr = self.model(test_set)/len(test_set)
+#         return self.model_repr
+
+#     def log(self):
+#         pass
+
+
+# class nodes_control(node):
+#     def __init__(self, id_list):
+#         pass
+
+#     def assign_one_model(self, model):
+#         pass
