80fd3d5f5599a7d2f3919ff924a1ea3ce826c88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:38 +0000

    Add package bitproto/1.1.1/bitproto-1.1.1.tar.gz

---
+++ packages/bitproto/bitproto-1.1.1.tar.gz/bitproto-1.1.1/bitproto/__init__.py
@@ -0,0 +1,12 @@
+"""
+bitproto
+~~~~~~~~
+
+Bit level data interchange format.
+
+:copyright: (c) 2020~2023 by Chao Wang <hit9@icloud.com>.
+
+"""
+
+__version__ = "1.1.1"
+__description__ = "bit level data interchange format."
