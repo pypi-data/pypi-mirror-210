c232ac6e1c1e72e69d2ee18dc795fa6d038f6f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:49 +0000

    Add package convert-case/1.2.2/convert_case-1.2.2-py3-none-any.whl

---
+++ packages/convert-case/convert_case-1.2.2-py3-none-any.whl/convert_case/__init__.py
@@ -0,0 +1,11 @@
+from .__main__ import pascal_case  # noqa
+from .__main__ import (camel_case, kebab_case, lower_case, sentence_case,
+                       snake_case, title_case, upper_case)
+from .camel import is_camel_case  # noqa
+from .kebab import is_kebab_case  # noqa
+from .lower import is_lower_case  # noqa
+from .pascal import is_pascal_case  # noqa
+from .sentence import is_sentence_case  # noqa
+from .snake import is_snake_case  # noqa
+from .title import is_title_case  # noqa
+from .upper import is_upper_case  # noqa
