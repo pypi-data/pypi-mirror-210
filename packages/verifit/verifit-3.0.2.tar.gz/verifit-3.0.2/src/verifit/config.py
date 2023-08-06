33c3c040baa8bdc1c6be279dd6d18bd7f23979c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:20 +0000

    Add package verifit/3.0.2/verifit-3.0.2.tar.gz

---
+++ packages/verifit/verifit-3.0.2.tar.gz/verifit-3.0.2/src/verifit/config.py
@@ -0,0 +1,40 @@
+import os
+
+from dotenv import dotenv_values
+
+from .prop import get_prop
+from .memoize import create_memoizer
+
+
+def config():
+    environment_name = os.environ.get('ENV', 'dev')
+    os.environ['ENV'] = environment_name
+    values = {
+        **dotenv_values(f".{environment_name}.env"),
+        **os.environ
+    }
+    return values
+
+
+memoize = create_memoizer(config)
+
+
+def get_store_reader():
+    store = memoize()
+
+    def get_env(key):
+        return get_prop(store)(key)
+
+    return get_env
+
+
+def get_store_writer():
+    store = memoize()
+
+    def get_env_key_writer(key):
+        def set_env_key(value):
+            store[key] = value
+
+        return set_env_key
+
+    return get_env_key_writer
---
+++ packages/verifit/verifit-3.0.2.tar.gz/verifit-3.0.2/src/verifit/memoize.py
@@ -0,0 +1,16 @@
+from .prop import get_prop
+
+
+def create_memoizer(func):
+    func_result_cache = {}
+
+    def memoize(arg=None):
+        cached_value = get_prop(func_result_cache)(arg)
+        if cached_value is None:
+            if arg is None:
+                func_result_cache[arg] = func()
+            else:
+                func_result_cache[arg] = func(arg)
+        return func_result_cache.get(arg)
+
+    return memoize
