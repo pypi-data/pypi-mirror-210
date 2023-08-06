c091ec7fe33324dc11bca110c616a8336214d0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:31 +0000

    Add package python-teamcity/0.1.5/python-teamcity-0.1.5.tar.gz

---
+++ packages/python-teamcity/python-teamcity-0.1.5.tar.gz/python-teamcity-0.1.5/examples/sample.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+from teamcity import TeamCity
+
+TEAMCITY_SERVER = os.environ.get('TEAMCITY_SERVER', None)
+TEAMCITY_TOKENS = os.environ.get('TEAMCITY_TOKENS', None)
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    tc = TeamCity(server=TEAMCITY_SERVER, tokens=TEAMCITY_TOKENS)
+    # s = tc.get_build_details(1)
+    # print(tc.get_build_details(23929740))
+    # print(tc.get_all_builds(build_type_id='Hk4eAsset_Streaming_38devAssignerTools'))
+    test = tc.get_builds_by_since_build(23948144,count = 50)
+
+    pass
