f997fb12610327bcf430e301f27d805878ee8491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:09 +0000

    Add package remla23-team17-lib/1.0.6/remla23_team17_lib-1.0.6.tar.gz

---
+++ packages/remla23-team17-lib/remla23_team17_lib-1.0.6.tar.gz/remla23_team17_lib-1.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='remla23_team17_lib',
+    version='v1.0.6',
+    author='remla23-team17',
+    author_email='remyduijsens@gmail.com',
+    description='Library for retrieving repo versions',
+    long_description_content_type="text/markdown",
+    long_description='Library for retrieving repo versions',
+    url='https://github.com/remla23-team17/lib',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages()
+)
