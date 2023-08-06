4317d33f7814e753abf77bcbce1eaeca65b47abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:12 +0000

    Add package etl-jobs/0.1.12/etl_jobs-0.1.12-py3-none-any.whl

---
+++ packages/etl-jobs/etl_jobs-0.1.12-py3-none-any.whl/etl_jobs/util/load/delta_spark.py
@@ -0,0 +1,18 @@
+"""
+fill in
+"""
+from pyspark.sql import SparkSession
+from pyspark.sql.types import BooleanType
+from pyspark.sql.functions import col
+
+
+def append_table(data_frame, table_name):
+    """
+    fill in
+    """
+    pd_data_frame = data_frame.to_pandas()
+    spark_data_frame = SparkSession.getActiveSession().createDataFrame(pd_data_frame)
+    if table_name == "dev.bronze_maine_raw" or table_name == "dev.silver_maine_raw":
+        cast_data_frame = spark_data_frame.withColumn("age", col("6tk3").cast(BooleanType()))
+        cast_data_frame.write.mode("append").format("delta").saveAsTable(table_name)
+    spark_data_frame.write.mode("append").format("delta").saveAsTable(table_name)
