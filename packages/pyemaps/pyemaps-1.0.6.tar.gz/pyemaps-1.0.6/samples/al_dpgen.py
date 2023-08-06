ccbd6441c9db6da84c7a83f443e1ace8f6d759b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:08 +0000

    Add package pyemaps/1.0.6/pyemaps-1.0.6.tar.gz

---
+++ packages/pyemaps/pyemaps-1.0.6.tar.gz/pyemaps-1.0.6/samples/al_dpgen.py
@@ -0,0 +1,28 @@
+
+def al_dpdb(cname='Aluminium'):
+    from pyemaps import Crystal as cr
+    from pyemaps import EMC, SIMC
+    import os
+   
+    
+    cryst = cr.from_builtin(cname)
+
+    #  first generate a DP database file
+    xa0=(2,0,0)   # x-axis, will be folded into EMC object
+    res = 200
+    ret, dbfn = cryst.generateDPDB(emc=EMC(zone=(0,0,1), 
+                                   simc=SIMC(gmax=3.9)), 
+                                   xa = xa0,
+                                   res = res)
+#   DP == Diffraction Pattern
+    if ret != 0:
+        print(f'failed to generate a DP databaes')
+        return -1
+
+    if dbfn is None or not os.path.exists(dbfn):
+        print(f'Error finding generated DP database file')
+        return -1
+
+
+if __name__ == '__main__':
+    al_dpdb()
