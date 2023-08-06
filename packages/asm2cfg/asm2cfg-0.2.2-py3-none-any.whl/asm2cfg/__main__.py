3a2c23622e58bcc1f0e1cb1abbdc49c649480c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:38 +0000

    Add package asm2cfg/0.2.2/asm2cfg-0.2.2-py3-none-any.whl

---
+++ packages/asm2cfg/asm2cfg-0.2.2-py3-none-any.whl/asm2cfg/__main__.py
@@ -0,0 +1,9 @@
+"""
+Let this module to be executed from the command line with python -m src.asm2cfg
+from root of the project
+"""
+
+from . import command_line
+
+if __name__ == '__main__':
+    command_line.main()
