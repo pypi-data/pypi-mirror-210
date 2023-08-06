f7767eaa5e0a2adb24af84694e267bfca0d6b8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:34 +0000

    Add package whylogs/1.1.42.dev6/whylogs-1.1.42.dev6.tar.gz

---
+++ packages/whylogs/whylogs-1.1.42.dev6.tar.gz/whylogs-1.1.42.dev6/whylogs/core/constraints/factories/condition_counts.py
@@ -0,0 +1,49 @@
+from ..metric_constraints import MetricConstraint, MetricsSelector
+
+
+def condition_meets(column_name: str, condition_name: str) -> MetricConstraint:
+    """Checks that all values in column match predicate
+
+    Parameters
+    ----------
+    column_name : str
+        Name of the column to apply the constraint to
+    condition_name : str
+        Name of the condition that will be applied to each value of the column
+    """
+
+    def matches_all(x) -> bool:
+        if x.matches[condition_name].value != x.total.value:
+            return False
+        return True
+
+    constraint = MetricConstraint(
+        name=f"{column_name} meets condition {condition_name}",
+        condition=matches_all,
+        metric_selector=MetricsSelector(column_name=column_name, metric_name="condition_count"),
+    )
+    return constraint
+
+
+def condition_never_meets(column_name: str, condition_name: str) -> MetricConstraint:
+    """Checks that no values in column match predicate
+
+    Parameters
+    ----------
+    column_name : str
+        Name of the column to apply the constraint to
+    condition_name : str
+        Name of the condition that will be applied to each value of the column
+    """
+
+    def matches_none(x) -> bool:
+        if x.matches[condition_name].value != 0:
+            return False
+        return True
+
+    constraint = MetricConstraint(
+        name=f"{column_name} never meets condition {condition_name}",
+        condition=matches_none,
+        metric_selector=MetricsSelector(column_name=column_name, metric_name="condition_count"),
+    )
+    return constraint
