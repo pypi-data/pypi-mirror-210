9c8235625860c0c1493433544d9d5d32041e18c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:03 +0000

    Add package satcfdi/4.0.26/satcfdi-4.0.26-py3-none-any.whl

---
+++ packages/satcfdi/satcfdi-4.0.26-py3-none-any.whl/satcfdi/create/cfd/aieps10.py
@@ -0,0 +1,27 @@
+"""aieps http://www.sat.gob.mx/acreditamiento"""
+from decimal import Decimal
+from datetime import datetime, date, time
+from collections.abc import Sequence
+from ... import CFDI, XElement, ScalarMap
+
+
+class AcreditamientoIEPS(CFDI):
+    """
+    Nodo requerido para expresar los detalles de la descripción del concepto para efectos de poder determinar el monto del estímulo aplicable.
+    
+    :param tar: Atributo requerido para expresar la clave de la Terminal de Almacenamiento y Reparto (CVE TAR), conforme al catálogo publicado en la página de Internet del SAT, mismo que servirá para identificar la cuota por litro conforme a las tablas que publique la Secretaría de Hacienda y Crédito Público para determinar el monto del estímulo fiscal.
+    """
+    
+    tag = '{http://www.sat.gob.mx/acreditamiento}acreditamientoIEPS'
+    version = '1.0'
+    
+    def __init__(
+            self,
+            tar: str,
+    ): 
+        super().__init__({
+            'Version': self.version,
+            'TAR': tar,
+        })
+        
+
