7fd6959a02eb789857a8e94df05ca60d3caf82d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:55 +0000

    Add package hades-cli/0.0.5/hades-cli-0.0.5.tar.gz

---
+++ packages/hades-cli/hades-cli-0.0.5.tar.gz/hades-cli-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("requirements.txt") as f:
+    requirements = f.read()
+
+
+setuptools.setup(
+    name="hades-cli",
+    version="0.0.5",
+    author="Wilson Mendoza",
+    author_email="mreyeswilson@gmail.com",
+    description="A CLI for generating projects",
+    url="https://github.com/mreyeswilson/mycli",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    entry_points='''
+        [console_scripts]
+        hades=app.start:main
+    ''',
+    include_dirs=["app"],
+    py_modules=["app"],
+    python_requires=">=3.8"
+)
