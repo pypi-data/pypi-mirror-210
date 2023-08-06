8d8f1ef05bc7ea1f2bd23b3a2f8a0c9f808501ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:25 +0000

    Add package aiobtclientrpc/5.0.0/aiobtclientrpc-5.0.0.tar.gz

---
+++ packages/aiobtclientrpc/aiobtclientrpc-5.0.0.tar.gz/aiobtclientrpc-5.0.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import re
+
+def get_long_description():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+def get_var(name):
+    with open('aiobtclientrpc/__init__.py') as f:
+        content = f.read()
+        match = re.search(rf'''^{name}\s*=\s*['"]([^'"]*)['"]''',
+                          content, re.MULTILINE)
+        if match:
+            return match.group(1)
+        else:
+            raise RuntimeError(f'Unable to find {name}')
+
+setuptools.setup(
+    name='aiobtclientrpc',
+    version=get_var('__version__'),
+    author=get_var('__author__'),
+    author_email=get_var('__author_email__'),
+    description=get_var('__description__'),
+    long_description=get_long_description(),
+    long_description_content_type='text/x-rst',
+    url=get_var('__homepage__'),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'async-timeout==4.*',
+        'httpx==0.*,>=0.20.0',
+        'httpx-socks==0.7.*',
+        'python-socks[asyncio]==2.*',
+        'rencode==1.0.*',
+    ],
+)
