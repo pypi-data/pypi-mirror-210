b622050d81c8f52e292192aac9b7f0a4ae17d499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:04 +0000

    Add package scaneo/0.0.9.post3/scaneo-0.0.9.post3.tar.gz

---
+++ packages/scaneo/scaneo-0.0.9.post3.tar.gz/scaneo-0.0.9.post3/scaneo/main.py
@@ -0,0 +1,24 @@
+import typer
+import os
+import sys
+
+# add scaneo to path
+# scaneo_dir = os.path.dirname(os.path.realpath(__file__))
+# sys.path.append(os.path.join(scaneo_dir))
+
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    port: int = typer.Option(8000, help="Port to run the server on"),
+    reload: bool = typer.Option(False, help="Reload the server when files change"),
+    host: str = typer.Option("localhost", help="Host to run the server on"),
+):
+    os.system(
+        f"python api.py --port {port} --host {host} {'--reload' if reload else ''}"
+    )
+
+
+if __name__ == "__main__":
+    app()
