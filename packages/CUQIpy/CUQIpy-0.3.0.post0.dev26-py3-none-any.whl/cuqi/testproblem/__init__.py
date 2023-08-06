dee2ea4008f22f780a58b28bdf5efa6c48252d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:00 +0000

    Add package CUQIpy/0.3.0.post0.dev26/CUQIpy-0.3.0.post0.dev26-py3-none-any.whl

---
+++ packages/CUQIpy/CUQIpy-0.3.0.post0.dev26-py3-none-any.whl/cuqi/testproblem/__init__.py
@@ -0,0 +1,12 @@
+from ._testproblem import (
+    _Deblur,
+    Deconvolution1D,
+    Deconvolution2D,
+    Poisson_1D,
+    Heat_1D,
+    Abel_1D,
+    _Deconv_1D,
+    WangCubic,
+    _getCirculantMatrix,
+    _getExactSolution
+)
