5a26ed5d6fc10bbe3d74e7715f3dfe5418455a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:18 +0000

    Add package dbpyman/0.1.22/dbpyman-0.1.22-py3-none-any.whl

---
+++ packages/dbpyman/dbpyman-0.1.22-py3-none-any.whl/py_discord_db_management/classes/column.py
@@ -0,0 +1,53 @@
+
+class Column:
+    def __init__(self, field, type, null, key, default, extra):
+        self.__field = field
+        self.__type = type
+        self.__null = null
+        self.__key = key
+        self.__default = default
+        self.__extra = extra
+
+        # variable that is used to attach values to that specific column
+        self.__attached_data = None
+
+    def set_attached_data(self, attached_data):
+        # value is empty
+        if not attached_data:
+            self.__attached_data = self.__default
+        else:
+            self.__attached_data = attached_data
+
+    def set_default(self, new_default):
+        self.__default = new_default
+
+    def get_attached_data(self):
+        return self.__attached_data
+
+    def get_field(self):
+        return self.__field
+
+    def get_type(self):
+        return self.__type
+
+    def get_nullable(self):
+        # allow to force a custom primary key
+        if self.__extra == 'auto_increment':
+            return True
+
+        if self.__null == 'YES':
+            return True
+
+        if self.__null == 'NO':
+            if self.__default or self.__default == 0:
+                return True
+            return False
+
+    def get_key(self):
+        return self.__key
+
+    def get_default(self):
+        return self.__default
+
+    def get_extra(self):
+        return self.__extra
---
+++ packages/dbpyman/dbpyman-0.1.22-py3-none-any.whl/py_discord_db_management/views/table_categories_view.py
@@ -0,0 +1,67 @@
+import discord
+
+class TableCategoryView(discord.ui.View):
+    def __init__(self, database, table):
+        super().__init__()
+
+        self.add_item(self.ReturnOverviewButton(database))
+        self.add_item(self.AddDataButton(database, table))
+        self.add_item(self.DeleteDataButton(database, table))
+
+    class ReturnOverviewButton(discord.ui.Button):
+        def __init__(self, database):
+            super().__init__(label="Return")
+            self.database = database
+
+        async def callback(self, interaction: discord.Interaction):
+            await interaction.response.defer()
+
+            from py_discord_db_management.views.table_overview_view import TableOverviewView
+            await interaction.message.edit(view=TableOverviewView(database=self.database))
+    class AddDataButton(discord.ui.Button):
+        def __init__(self, database, table):
+            super().__init__(label="Add Data", style=discord.ButtonStyle.green)
+            self.database = database
+            self.table = table
+
+        async def callback(self, interaction: discord.Interaction):
+            await interaction.response.defer()
+
+            # we create an empty array with the size of the table columns count
+            columns = self.table.get_columns()
+
+            from py_discord_db_management.views.table_add_data_view import TableAddDataView
+            await interaction.message.edit(view=TableAddDataView(database=self.database, table=self.table, columns=columns))
+
+    class DeleteDataButton(discord.ui.Button):
+        def __init__(self, database, table):
+            super().__init__(label="Delete Data", style=discord.ButtonStyle.danger)
+            self.database = database
+            self.table = table
+
+        async def callback(self, interaction: discord.Interaction):
+            await interaction.response.defer()
+
+            # we create an empty array with the size of the table columns count
+            columns = self.table.get_columns()
+
+            await interaction.response.send_modal(self.DeleteDataModal(database=self.database, table=self.table))
+
+        class DeleteDataModal(discord.ui.Modal):
+            def __init__(self, database, table):
+                super().__init__(title=f"Adding to {table.get_table_name()}")
+                self.database = database
+                self.table = table
+
+                self.add_item(discord.ui.TextInput(label=f"Primary Key", required=True))
+
+            async def on_submit(self, interaction: discord.Interaction):
+                await interaction.response.defer()
+
+                # retrieve the data from each input
+                input = self.children[0]
+                primary_key = None if not str(input) else str(input)
+
+                self.database.remove_data_from_table(table=self.table, primary_key=primary_key)
+                print("Deleted data")
+
