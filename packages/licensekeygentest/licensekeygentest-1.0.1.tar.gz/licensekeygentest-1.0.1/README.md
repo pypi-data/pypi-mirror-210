58d994efec7844b2ec875a2ab7d899986fb01ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package licensekeygentest/1.0.1/licensekeygentest-1.0.1.tar.gz

---
+++ packages/licensekeygentest/licensekeygentest-1.0.1.tar.gz/licensekeygentest-1.0.1/README.md
@@ -0,0 +1,9 @@
+# Licensed Library KeyGen test
+
+This is test environment for lisenced libraries over PyPi.
+
+To use this poc:
+```bash
+pip install licensekeygentest
+```
+
---
+++ packages/licensekeygentest/licensekeygentest-1.0.1.tar.gz/licensekeygentest-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.1'
+DESCRIPTION = 'POC for license validation checks'
+LONG_DESCRIPTION = 'A POC package that allows lisencing of python libraries.'
+
+# Setting up
+setup(
+    name="licensekeygentest",
+    version=VERSION,
+    author="Rahul R",
+    author_email="<rahulranjan25.RR@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'authentication', 'licensing'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
