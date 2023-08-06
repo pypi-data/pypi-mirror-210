7c8c2a15490d5d4bf410f8eed12c3ae91bfc1baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:29 +0000

    Add package text2tags-lib/0.0.1/text2tags-lib-0.0.1.tar.gz

---
+++ packages/text2tags-lib/text2tags-lib-0.0.1.tar.gz/text2tags-lib-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="text2tags-lib",
+    version="0.0.1",
+    license='MIT',
+    description='Use a finetune of Llama-7b and llama cpp to predict Danbooru tags from natural text.',
+    author='Pantai Suyasri',
+    author_email='pantaisuyasri@gmail.com',
+    url='https://github.com/DatboiiPuntai/text2tags-lib',
+    download_url='https://github.com/DatboiiPuntai/text2tags-lib/archive/refs/tags/v0.0.1.zip',
+    packages=find_packages(),
+    install_requires=["editdistance", "llama-cpp-python==0.1.50", "wget"],
+    package_data={"text2tags": ["tags.txt"]},
+)
