fa636159ae36ca6c81917a0c2a0e410cb9a7b935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:41 +0000

    Add package edc-subject-dashboard/0.3.36/edc-subject-dashboard-0.3.36.tar.gz

---
+++ packages/edc-subject-dashboard/edc-subject-dashboard-0.3.36.tar.gz/edc-subject-dashboard-0.3.36/edc_subject_dashboard/views/__init__.py
@@ -0,0 +1,5 @@
+from .refresh_appointments_view import RefreshAppointmentsView
+from .refresh_metadata_actions_view import RefreshMetadataActionsView
+from .requisition_print_actions_view import RequisitionPrintActionsView
+from .requisition_verify_actions_view import RequisitionVerifyActionsView
+from .subject_dashboard_view import SubjectDashboardView
