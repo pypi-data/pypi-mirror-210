11da88783ad1d878a26ebf45f3db13ce4774a088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:28 +0000

    Add package sslcompare/1.7.0/sslcompare-1.7.0-py3-none-any.whl

---
+++ packages/sslcompare/sslcompare-1.7.0-py3-none-any.whl/sslcompare/sslcompare.py
@@ -0,0 +1,144 @@
+#!/bin/env python3
+
+"""
+Compare cipher suites to baselines.
+
+Authors :
+  - Arthur Le Corguille
+  - William Gougam
+  - Adrian Kalmar
+  - Alexandre Janvrin.
+"""
+import re
+import shlex
+import subprocess
+import sys
+from functools import partial
+from importlib import resources
+
+import click
+import yaml
+
+with resources.path("sslcompare", "anssi.yaml") as default_baseline_path:
+    DEFAULT_BASELINE_PATH = default_baseline_path
+
+
+@click.command()
+@click.argument("url")
+@click.option("--baseline", default=DEFAULT_BASELINE_PATH, help="baseline file (yaml).")
+def main(url, baseline):
+    """Scan URL's cipher suites and compares it to the BASELINE.
+
+    Cipher suites are retrieved with the testssl.sh shell script
+    (https://github.com/drwetter/testssl.sh)
+
+    Examples :
+        sslcompare https://mytargetsite.com -b /path/to/baseline.yaml
+        sslcompare 127.0.0.1:8080
+    """
+    strip_ansi = partial(re.compile(r"\x1b\[\d*m").sub, "")
+
+    with open(baseline) as baseline_path:
+        baseline_suites = yaml.safe_load(baseline_path)
+
+    click.secho(
+        f'running "testssl.sh -E -U {url}", and showing the full output:',
+        fg="black",
+        bg="white",
+        bold=True,
+    )
+    output = []
+    with resources.path("sslcompare", "testssl.sh") as testssl_path:
+        with subprocess.Popen(
+            shlex.split(f"{testssl_path / 'testssl.sh'} -E -U {url}"),
+            # bufsize=1,
+            # universal_newlines=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        ) as testssl:
+            # The following is to ensure that testssl's output is immediately shown
+            # to the user, even if no newline is present.
+            # For instance, a case was encountered where testssl would ask the user
+            # something like "type 'yes' to continue".
+            # Because the output was line-buffered, sslcompare would hang
+            # and the user would not see anything.
+            # The following code should fix that
+            for chunk in iter(partial(testssl.stdout.read, 1), b""):
+                if hasattr(sys.stdout, "buffer"):
+                    sys.stdout.buffer.write(chunk)
+                    sys.stdout.flush()
+                else:
+                    sys.stdout.write(chunk.decode())
+                output.append(chunk)
+
+    current_protocol = None
+    interesting_lines = False
+
+    click.secho(
+        "Here is the summarized output with ANSSI's recommandations:",
+        fg="black",
+        bg="white",
+        bold=True,
+    )
+    for line in b"".join(output).decode().split("\n"):
+        if "Start" in line:
+            click.echo(line)
+
+        elif "Done" in line:
+            click.echo(line)
+            break
+
+        elif "Testing" in line:
+            if not interesting_lines:
+                click.echo("")
+
+            click.echo(line)
+            interesting_lines = True
+
+        elif "Cipher Suite Name (IANA/RFC)" in line:
+            click.echo(f" {'Cipher Suite Name (IANA/RFC)':41} Evaluation")
+
+        elif strip_ansi(line) in [
+            "SSLv2",
+            "SSLv3",
+            "TLS 1",
+            "TLS 1.1",
+            "TLS 1.2",
+            "TLS 1.3",
+        ]:
+            current_protocol = strip_ansi(line)
+            click.echo(line)
+            continue
+
+        elif current_protocol is not None and line != "":
+            if line.strip() == "-":
+                if current_protocol in [
+                    "SSLv2",
+                    "SSLv3",
+                    "TLS 1",
+                    "TLS 1.1",
+                ]:
+                    click.echo(
+                        f" {'NOT OFFERED':<41} "
+                        + click.style("[RECOMMENDED]", fg="green")
+                    )
+                else:
+                    click.echo(f" {'NOT OFFERED':<41}")
+
+            else:
+                cipher_suite = line.split()[-1]
+
+                try:
+                    click.echo(
+                        f" {cipher_suite:<41} "
+                        + click.style(**baseline_suites[current_protocol][cipher_suite])
+                    )
+
+                except KeyError:
+                    click.echo(
+                        f" {cipher_suite: <41} "
+                        + click.style("[DEPRECATED ]", fg="red")
+                    )
+
+        elif interesting_lines:
+            click.echo(line)
