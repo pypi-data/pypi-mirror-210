5a2cdefd29e359517fad8a938ad9e5b9b0475a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:54 +0000

    Add package joyrl/0.2.10/joyrl-0.2.10.tar.gz

---
+++ packages/joyrl/joyrl-0.2.10.tar.gz/joyrl-0.2.10/joyrl/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JiangJi
+Email: johnjim0816@gmail.com
+Date: 2023-01-01 16:20:49
+LastEditor: JiangJi
+LastEditTime: 2023-05-28 20:27:35
+Discription: 
+'''
+from joyrl import algos, config, framework, envs, utils
+from joyrl.run import run
+__version__ = "0.2.10"
+
+__all__ = [
+    "algos",
+    "config",
+    "envs",
+    "framework",
+    "utils",
+    "run"
+]
