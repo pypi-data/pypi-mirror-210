4524b65f5558dd643513e0bbba948c50f95825bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:06 +0000

    Add package coso/1.1.4/coso-1.1.4.tar.gz

---
+++ packages/coso/coso-1.1.4.tar.gz/coso-1.1.4/src/__init__.py
@@ -0,0 +1,27 @@
+"""
+Part of the CoSo distribution.
+
+Copyright 2022 KU Leuven, DTAI Research Group
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Pietro Totis'
+__credits__ = 'KU Leuven'
+
+
+
+# from . import cola_parser
+# from . import configuration
+# from . import count
+# from . import gen_plots
+# from . import level1
+# from . import level2
+# from . import logger
+# from . import problem
+# from . import sharpCSP
+# from . import solver
+# from . import tester
+# from . import util
+# from . import venn
+# from . import viscoso
+# from . import viscoso_widget
