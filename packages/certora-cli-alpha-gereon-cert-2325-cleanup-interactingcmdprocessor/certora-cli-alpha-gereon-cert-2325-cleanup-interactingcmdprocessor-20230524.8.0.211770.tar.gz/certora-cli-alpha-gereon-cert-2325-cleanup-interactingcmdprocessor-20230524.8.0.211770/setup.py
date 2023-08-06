49ebdd2f84b18a48c3255934d44b9022f61e13d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:26 +0000

    Add package certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor/20230524.8.0.211770/certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor-20230524.8.0.211770.tar.gz

---
+++ packages/certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor/certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor-20230524.8.0.211770.tar.gz/certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor-20230524.8.0.211770/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor",
+        version="20230524.8.0.211770",
+        author="Certora",
+        author_email="support@certora.com",
+        description="Runner for the Certora Prover",
+        long_description="Commit c81cda3. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.",
+        long_description_content_type="text/markdown",
+        url="https://pypi.org/project/certora-cli-alpha-gereon-cert-2325-cleanup-interactingcmdprocessor",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=['tabulate', 'requests', 'pycryptodome', 'tqdm', 'click', 'sly', 'argcomplete', 'json5'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={
+            "console_scripts": [
+                "certoraRun = certora_cli.certoraRun:entry_point",
+                "certoraMutate = certora_cli.certoraMutate:ext_gambit_entry_point"
+            ]
+        },
+        python_requires='>=3.8.16',
+    )
+        
