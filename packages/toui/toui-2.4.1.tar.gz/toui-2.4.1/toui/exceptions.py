acbf5e70956471e9687b89877aa9516f77e00567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:11 +0000

    Add package toui/2.4.1/toui-2.4.1.tar.gz

---
+++ packages/toui/toui-2.4.1.tar.gz/toui-2.4.1/toui/exceptions.py
@@ -0,0 +1,11 @@
+"""
+A module that contains exceptions.
+"""
+
+class ToUIWrongPlaceException(Exception):
+    """Raised when the user calls a function in the wrong place."""
+
+
+class ToUINotAddedError(Exception):
+    """Raised when the user calls a function which requires an extension that is not yet added to the app."""
+    
