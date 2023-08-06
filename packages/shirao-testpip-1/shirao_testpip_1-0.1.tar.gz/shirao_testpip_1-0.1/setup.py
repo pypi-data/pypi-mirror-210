610588ecaedf621ef2f2e0d4773589d64f448a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:16 +0000

    Add package shirao-testpip-1/0.1/shirao_testpip_1-0.1.tar.gz

---
+++ packages/shirao-testpip-1/shirao_testpip_1-0.1.tar.gz/shirao_testpip_1-0.1/setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup
+
+setup(
+    name="shirao_testpip_1",
+    version="0.1",
+    packages=["shirao_testpip_1"],
+    description="A simple library to double the input",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/mylibrary",
+)
