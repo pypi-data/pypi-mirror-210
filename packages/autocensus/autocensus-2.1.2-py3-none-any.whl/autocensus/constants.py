05abda85d410f081286558223f439510ac2a98bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:42 +0000

    Add package autocensus/2.1.2/autocensus-2.1.2-py3-none-any.whl

---
+++ packages/autocensus/autocensus-2.1.2-py3-none-any.whl/autocensus/constants.py
@@ -0,0 +1,25 @@
+"""Constants and types for use throughout other modules."""
+
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple, Union
+
+from appdirs import user_cache_dir
+from pandas import DataFrame
+from typing_extensions import Literal
+
+# Cache directory path
+CACHE_DIRECTORY_PATH = Path(user_cache_dir('autocensus'))
+
+# Query parameters
+ESTIMATES = [1, 3, 5]
+QueryEstimate = Literal[1, 3, 5]
+GEOMETRIES = ['points', 'polygons']
+QueryGeometry = Literal['points', 'polygons']
+RESOLUTIONS = ['500k', '5m', '20m']
+QueryResolution = Literal['500k', '5m', '20m']
+
+# Types representing data tables, variables, and geometry files returned from from Census API
+Table = List[List[Union[int, str]]]
+Variables = Dict[Tuple[int, str], dict]
+GazetteerFile = List[Optional[DataFrame]]
+Shapefile = Optional[Path]
