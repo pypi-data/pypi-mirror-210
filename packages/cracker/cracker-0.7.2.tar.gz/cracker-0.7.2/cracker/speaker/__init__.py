bb90931ea8e472f3ad90927d7c2ff976957eb019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:38 +0000

    Add package cracker/0.7.2/cracker-0.7.2.tar.gz

---
+++ packages/cracker/cracker-0.7.2.tar.gz/cracker-0.7.2/cracker/speaker/__init__.py
@@ -0,0 +1,57 @@
+ESPEAK_LANGUAGES = {
+    "English": [
+        "English"
+    ],
+    "Polish": [
+        "Polish"
+    ],
+    "Spanish": [
+        "Spanish"
+    ]
+}
+
+GOOGLE_LANGUAGES = {
+    "English": [
+        "en-US",
+        "en-GB",
+        "en-AU",
+        "en-CA",
+        "en-IN"
+    ],
+    "Polish": [
+        "pl-PL"
+    ],
+    "Spanish": [
+        "es-ES",
+        "es-US"
+    ]
+}
+
+POLLY_LANGUAGES = {
+    "English": [
+        "Joanna",
+        "Salli",
+        "Kimberly",
+        "Kendra",
+        "Ivy",
+        "Matthew",
+        "Justin",
+        "Joey"
+    ],
+    "Polish": [
+        "Ewa",
+        "Maja",
+        "Jan",
+        "Jacek"
+    ],
+    "Spanish": [
+        "Penelope",
+        "Miguel"
+    ]
+}
+
+LANGUAGES = {
+    "polly": POLLY_LANGUAGES,
+    "google": GOOGLE_LANGUAGES,
+    "espeak": ESPEAK_LANGUAGES,
+}
