7490e066dc65a24532413071889a095cb90a62d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:57 +0000

    Add package scaneo/0.0.2/scaneo-0.0.2-py3-none-any.whl

---
+++ packages/scaneo/scaneo-0.0.2-py3-none-any.whl/scaneo/api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/test")
+def test():
+    return "hello"
+
+
+# this needs to be last
+
+app.mount("/", StaticFiles(directory="build", html=True), name="ui")
