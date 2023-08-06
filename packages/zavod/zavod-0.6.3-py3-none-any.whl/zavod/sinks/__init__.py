94f694dc8fa94506d9de404692f92b4910ea089c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:17 +0000

    Add package zavod/0.6.3/zavod-0.6.3-py3-none-any.whl

---
+++ packages/zavod/zavod-0.6.3-py3-none-any.whl/zavod/sinks/__init__.py
@@ -0,0 +1,5 @@
+from zavod.sinks.common import Sink
+from zavod.sinks.json_entity import JSONEntitySink
+from zavod.sinks.json_statement import JSONStatementSink
+
+__all__ = ["Sink", "JSONEntitySink", "JSONStatementSink"]
