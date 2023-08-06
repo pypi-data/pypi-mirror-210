a92faef53f6e11c19762e4831820a7b543cc5046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:20 +0000

    Add package megadata/0.0.56/megadata-0.0.56.tar.gz

---
+++ packages/megadata/megadata-0.0.56.tar.gz/megadata-0.0.56/megadata/myeval.py
@@ -0,0 +1,128 @@
+# eval tool by wanjo 20230511
+from .mypy import *
+load_time = now()
+
+# TODO:
+# loadapi(c,m,request=request)(*args,**kwargs)
+# fix lambda:__builtins__ bug??
+
+# old way, deprecated, will remove soon
+def fwd(c,m,param_a): return tryx(lambda:getattr(sys_import(c).api(param_a),m)(*param_a),lambda ex:{'errmsg':str(ex)})
+
+# working version but not yet support server objects
+def fwdapi(c,m,*args,**kwargs):
+  rt = tryx(lambda:getattr(sys_import(f'api{c}').api(),m)(*args,**kwargs),lambda ex:{'errmsg':str(ex)})
+  #print('DEBUG fwdapi',[c,m,args],'=>',rt)
+  return rt
+
+def myeval(s,g={},l={},debug=False):
+    rt = None
+
+    if type(s) is bytes: # try pickle/loads_func/...
+        import pickle
+        # try pickle...
+        o = tryx(lambda:pickle.loads(s),False)
+        if o is None: # not pickle, try bytes str
+            o = tryx(lambda:s.decode(),False)
+        if o is None: # try rpc function...
+            o = tryx(lambda:loads_func(s,g)) # load by ctx g
+            if o is not None:
+              return tryx(o,True)
+        s = o
+
+    s = f'{s}'.strip()
+    if len(s)<1: return None
+    a = s2o(s)
+    if debug: print(f'===In: {a or s}')
+    #flg_right = False
+    call_id = None
+    call_param = None
+    call_style = None
+    #if type(a) is list: # list-come-list-go
+
+    if s[0]=='[':
+        # list mode w+/- call_id
+        # [ call_id:Optional, call_entry, [call_param...] ]
+        # => [call_param, call_result]
+
+        call_style = 1
+        len_a = len(a)
+        a0i = tryx(lambda:int(a[0]),False)
+        if len_a>1 and type(a0i) is int:
+            call_entry = a[1]
+            call_param = tryx(lambda:a[2:],False)
+            call_id = a[0]
+            #flg_right = True
+        elif len_a>0:
+            call_entry = a[0]
+            call_param = tryx(lambda:a[1:],False)
+            #flg_right = True
+
+    elif s[0]=='(' or s[0]=='/': # pyql ;)
+        s=s.replace('__builtins__','') # safe-guard ;)
+        if s[0]=='/': s = s[1:]
+        #return str(tryx(lambda:eval(s,g,l),True)) # DEBUG: testing no str...
+        #return tryx(lambda:eval(s,g,l),True)
+        rt = tryx(lambda:eval(s,g,l),True)
+        #return rt
+
+    # dict-come-dict-go, old and please try not to use...
+    elif s[0]=='{':
+        # deprecated ,dict mode is not good too
+        call_style = 2
+        call_entry = a.get('entry',None)
+        call_param = a.get('param',[])
+        call_id = a.get('id',None)
+        #flg_right = True
+
+    elif a is None: # assume: quick console mode sep by comma (not good for special case...)
+        # deprecated, quick-console-mode is not good ;)
+        call_style = 3
+        #a = s.split('\t')
+        s=s.replace('\t','')
+        a = s.split(',') # not good for some quote case! for that should /api(...)
+        len_a = len(a)
+        a0i = tryx(lambda:int(a[0]),False)
+        if len_a>1 and type(a0i) is int:
+            call_style = 1 # rollback to list-mode
+            call_entry = a[1]
+            call_param = tryx(lambda:a[2:],False)
+            call_id = a0i or a[0]
+            #flg_right = True
+        elif len_a>0:
+            call_entry = a[0]
+            call_param = tryx(lambda:a[1:],False)
+            #flg_right = True
+
+    #if flg_right:
+    if rt is None:
+        a = call_entry.split('.')
+        if len(a)<2:
+            rt = {'errmsg':'wrong entry {}'.format(call_entry)}
+        else:
+            # TODO remove fwd() and using fwdapi instead...
+            # TMP fwd to api{c}.m(call_param)
+            # TODO should using *args,**kwargs
+            rt = fwd(f'api{a[0]}',a[1],call_param)
+
+    if is_awaitable(rt): return rt
+
+    if debug:print(f'===Out <{type(rt).__name__}>',len(rt) if type(rt) in [bytes,str,dict,list,tuple] else rt)
+
+    if call_style==1: # list mode
+        if call_id is None:
+            return [rt] # 
+        else:
+            return [call_id,rt]
+    else: return rt
+    #elif call_style in [2,3]: # dict mode or quick console mode
+    #    return rt
+    #else:
+    #    return None
+
+#async def myevalasync(*args,**kwargs):
+#  # return await try_await( await try_asyncio_async(lambda:myeval(*args,**kwargs)) )
+#  rt = await try_asyncio_async(lambda:myeval(*args,**kwargs))
+#  if is_awaitable(rt): rt = await rt
+#  return rt
+myevalasync = lambda *args,**kwargs:try_asyncio_async(lambda:myeval(*args,**kwargs))
---
+++ packages/megadata/megadata-0.0.56.tar.gz/megadata-0.0.56/megadata/svr_ipc_bin.py
@@ -0,0 +1,143 @@
+#-*- coding: utf-8 -*-
+
+# server ipc mode (return str or json-str only)
+
+# TODO merge logic with svr_ipcx.py => svr_ipc_bin.py [ipc/ipcx]
+
+from .mypy import *
+from .myeval import myeval,myevalasync,fwdapi
+
+white_list = tryx(lambda:load('../tmp/white_list.json'))
+black_list = tryx(lambda:load('../tmp/black_list.json')) or []
+
+#my_encode = lambda rt: o2s(rt) if type(rt) is not str else rt
+
+get_builtins_default = lambda:{
+  #'type':type,# directly exposing type is dangerous ;)
+  'type':lambda v:str(type(v)), # safer
+  'api':fwdapi,
+  'ping':now(),
+  'print':print,
+}
+
+import pickle
+
+# TODO: merge handle_ipc and my_main_ipc as new class later, and then add debug=False
+
+def handle_ipc(param):
+    #print('param=',param)
+    conn,client,get_builtins = param
+    # client could be None when it came from local...
+    assert client is None or len(client)==0 or (white_list is None) or (white_list and client[0] in white_list), f'banned {client}'
+
+    loop = new_event_loop()
+    # TODO if closed
+    while True:
+      #print('recv[')
+      #EOFError
+      #data = tryx(conn.recv,lambda ex:print(ex,client))
+      data = tryx(conn.recv,lambda ex:(log1('#'),flush1()))
+      #print('recv]')
+      if data is None:
+        #print('break')
+        #print('close[')
+        tryx(conn.close)
+        #print('close]')
+        break
+
+      #print('TMP DEBUG handle_ipc',data)
+
+      #rt_eval = tryx(lambda:myeval(data,{"__builtins__":get_builtins()},{}),True)
+      #rt_eval = loop.run_until_complete(try_await(tryx(lambda:myevalasync(data,{"__builtins__":get_builtins()},{}),True)))
+      rt_eval = tryx(lambda:loop.run_until_complete(myevalasync(data,{"__builtins__":get_builtins()},{})),True)
+
+      #print('TMP DEBUG rt_eval',rt_eval)
+
+      if type(rt_eval) in [list,tuple,dict]:
+        #rt = my_encode(rt_eval)
+        rt = o2s(rt_eval)
+        #print('debug rt_eval',data,type(rt_eval),type(rt),rt)
+        if rt is None or "null"==rt: rt = rt_eval
+      else: rt = rt_eval
+      #print('debug rt_eval',data,type(rt_eval),type(rt),rt)
+
+      if type(rt) not in [str,bytes]:
+        rt = tryx(lambda:pickle.dumps(rt),True)
+
+      # https://docs.python.org/3/library/multiprocessing.html
+      conn.send(rt)
+
+      # TODO print() will delay 25%
+      # TODO --silent
+      #print(f'{client}=>{len(rt) if len(rt)>999 else rt_eval}')
+
+    # TODO no-close if reuse is True, for now no auto close...
+    #tryx(conn.close)
+    #return rt
+
+def on_quit(*a):
+  print('on_quit',a)
+  os._exit(0)
+
+"""
+ipc svr as example and quick usage only
+mode: thread | asyncio | pool
+svr_mode: ipc | ipcx
+"""
+def my_main_ipc(address,svr_mode='ipc',authkey=None,mode='pool',pool_size=None,get_builtins=get_builtins_default,debug=False):
+
+  print('my_main_ipc()','svr_mode=',svr_mode,'mode=',mode)
+
+  from multiprocessing.connection import Listener
+  server = Listener(address=address,authkey=authkey)
+
+  if 'thread'==mode:
+    while True:
+      conn = tryx(server.accept)
+      if conn is None: print('.')
+      else: try_async(lambda:handle_ipc((conn,server.last_accepted,get_builtins)))
+
+  elif 'asyncio'==mode:
+    while True:
+      conn = tryx(server.accept)
+      if conn is None: print('.')
+      else:
+        #print('server.last_accepted',server.last_accepted)
+        try_asyncio(lambda:handle_ipc((conn,server.last_accepted,get_builtins)),new=True)
+
+  else:# pool mode
+
+    if svr_mode=='ipcx':# multiprocess-mode
+      from multiprocessing import Pool
+    else: # multithread-mode
+      from multiprocessing.dummy import Pool
+
+    with Pool(os.cpu_count()) as pool:
+      while True:
+        conn = tryx(server.accept)
+        if conn is None: print('.')
+        else: pool.map_async(handle_ipc, [(conn,server.last_accepted,get_builtins)])
+
+def start_stdin(get_builtins=get_builtins_default):
+  #for line in sys.stdin: print(myeval(line))
+  loop = new_event_loop()
+  for line in sys.stdin:
+    if line.startswith(';'): # god mode (danger for no masking __builtins__)
+      #print('=>', tryx(lambda:eval(line[1:])) )
+      #print(loop.run_until_complete(try_await(tryx(lambda:myevalasync(line[1:])) )))
+      print(tryx(lambda:loop.run_until_complete(myevalasync(line[1:])) ))
+    else: # craft mode
+      #r = myeval(line,{"__builtins__":get_builtins()},{})
+      #r = loop.run_until_complete(try_await(tryx(lambda:myevalasync(line,{"__builtins__":get_builtins()},{}))))
+      r = tryx(lambda:loop.run_until_complete(myevalasync(line,{"__builtins__":get_builtins()},{})))
+      print(type(r),r)
+
+# quick test on main
+if __name__ == '__main__':
+  hook_quit(on_quit)
+
+  address = build_address(argv[1], argv[2] if argc>2 else None)
+  print('listening',address)
+
+  try_async(lambda:my_main_ipc(address,'ipc'))
+
