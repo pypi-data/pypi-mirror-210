77dbd15305861b8af0385e3e1f3f4a6fad6fdbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:27 +0000

    Add package kedro-graphql/0.3.0/kedro_graphql-0.3.0-py3-none-any.whl

---
+++ packages/kedro-graphql/kedro_graphql-0.3.0-py3-none-any.whl/tests/test_schema_mutation.py
@@ -0,0 +1,69 @@
+"""
+
+"""
+
+
+import pytest
+from kedro_graphql.schema import build_schema
+
+schema = build_schema()
+
+@pytest.mark.usefixtures('celery_session_app')
+@pytest.mark.usefixtures('celery_session_worker')
+class TestSchemaMutations:
+    @pytest.mark.asyncio
+    async def test_pipeline(self, mock_info_context, mock_text_in, mock_text_out):
+
+        mutation = """
+        mutation TestMutation($pipeline: PipelineInput!) {
+          pipeline(pipeline: $pipeline) {
+            name
+            describe
+            inputs {
+              name
+              filepath
+              type
+            }
+            nodes {
+              name
+              inputs
+              outputs
+              tags
+            }
+            outputs {
+              filepath
+              name
+              type
+            }
+            parameters {
+              name
+              value
+            }
+            status
+            tags {
+              key
+              value
+            }
+            taskId
+            taskName
+            taskArgs
+            taskKwargs
+            taskRequest
+            taskException
+            taskTraceback
+            taskEinfo
+            taskResult
+          }
+        }
+        """
+
+        resp = await schema.execute(mutation, 
+                                    variable_values = {"pipeline": {
+                                      "name": "example00",
+                                      "inputs": [{"name": "text_in", "type": "text.TextDataSet", "filepath": str(mock_text_in)}],
+                                      "outputs": [{"name": "text_out", "type": "text.TextDataSet", "filepath": str(mock_text_out)}],
+                                      "parameters": [{"name":"example", "value":"hello"}],
+                                      "tags": [{"key": "author", "value": "opensean"},{"key":"package", "value":"kedro-graphql"}]
+                                    }})
+        
+        assert resp.errors is None
