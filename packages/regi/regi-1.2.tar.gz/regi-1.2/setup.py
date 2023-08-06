ff3c24fd045a02c35277d26299964551bcdbc87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:13 +0000

    Add package regi/1.2/regi-1.2.tar.gz

---
+++ packages/regi/regi-1.2.tar.gz/regi-1.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='regi',
+    version='1.2',
+    description='Different Types of Regression',
+    author='Sahraoui Tarek Ziad',
+    author_email='tziad2027@gmail.com',
+    packages=setuptools.find_packages(),
+    py_modules=['main.regi'],
+)
