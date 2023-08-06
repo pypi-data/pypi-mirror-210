ffdca3d907125f0d09b65ddb93afc4afb586c582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:33 +0000

    Add package rtb-toolbox/0.1.47/rtb_toolbox-0.1.47.tar.gz

---
+++ packages/rtb-toolbox/rtb_toolbox-0.1.47.tar.gz/rtb_toolbox-0.1.47/src/rtb_toolbox/robots/comau.py
@@ -0,0 +1,23 @@
+import sympy as sp
+
+from rtb_toolbox.forward_kinematics import ForwardKinematic
+from rtb_toolbox.link import Link
+from rtb_toolbox.frame import xyz_rotation_matrix
+
+from sympy import pi
+
+q1, q2, q3, q4, q5, q6 = sp.symbols('q_1 q_2 q_3 q_4 q_5 q_6')
+
+v1 = xyz_rotation_matrix(pi, 0, 0)
+v2 = xyz_rotation_matrix(0, pi, 0)
+
+j0 = Link([q1, -260,     150,    pi/2]  , v=v1)
+j1 = Link([q2,  0,     590,          pi], offset=-pi/2)
+j2 = Link([q3,  0,     130,     -pi / 2], offset=pi/2)
+j3 = Link([q4, -647.07,  0,     -pi / 2])
+j4 = Link([q5,  0,       0,      pi / 2])
+j5 = Link([q6,-95,       0,           0], v=v2)
+
+comau_fk = ForwardKinematic(
+    [j0, j1, j2, j3, j4, j5],
+)
