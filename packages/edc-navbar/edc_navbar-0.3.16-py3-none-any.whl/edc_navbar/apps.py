5439ff5b0cb9898d40eefde445d9b7c5c9396f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:13 +0000

    Add package edc-navbar/0.3.16/edc_navbar-0.3.16-py3-none-any.whl

---
+++ packages/edc-navbar/edc_navbar-0.3.16-py3-none-any.whl/edc_navbar/apps.py
@@ -0,0 +1,21 @@
+import sys
+
+from django.apps import AppConfig as DjangoAppConfig
+from django.conf import settings
+from django.core.management.color import color_style
+
+style = color_style()
+
+
+class AppConfig(DjangoAppConfig):
+    name = "edc_navbar"
+    verbose_name = "Edc Navbar"
+    register_default_navbar = True
+    default_navbar_name = getattr(settings, "DEFAULT_NAVBAR_NAME", "default")
+
+    def ready(self):
+        from .site_navbars import site_navbars
+
+        sys.stdout.write(f"Loading {self.verbose_name} ...\n")
+        site_navbars.autodiscover()
+        sys.stdout.write(f" Done loading {self.verbose_name}.\n")
