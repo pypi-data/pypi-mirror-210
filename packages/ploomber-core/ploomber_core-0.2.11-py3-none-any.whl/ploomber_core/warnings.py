52f2368f573a32f05ee462f401adc43e85b94403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:41 +0000

    Add package ploomber-core/0.2.11/ploomber_core-0.2.11-py3-none-any.whl

---
+++ packages/ploomber-core/ploomber_core-0.2.11-py3-none-any.whl/ploomber_core/warnings.py
@@ -0,0 +1,9 @@
+class PloomberDeprecationWarning(FutureWarning):
+    """
+    Notes
+    -----
+    .. versionchanged:: 0.1
+        Changed superclass from UserWarning to FutureWarning
+    """
+
+    pass
