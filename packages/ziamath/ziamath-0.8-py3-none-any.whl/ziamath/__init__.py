79e43f4cc361af3f4ad5f17f8e46a16cc3ea810b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:48 +0000

    Add package ziamath/0.8/ziamath-0.8-py3-none-any.whl

---
+++ packages/ziamath/ziamath-0.8-py3-none-any.whl/ziamath/__init__.py
@@ -0,0 +1,18 @@
+from .mathtable import MathTable
+from .styles import styledchr, styledstr
+from .zmath import Math, Latex, Text, declareoperator
+from .config import config
+
+__version__ = '0.8'
+
+
+declareoperator(r'\tg')
+declareoperator(r'\ctg')
+declareoperator(r'\arcctg')
+declareoperator(r'\arctg')
+declareoperator(r'\arg')
+declareoperator(r'\cotg')
+declareoperator(r'\sh')
+declareoperator(r'\ch')
+declareoperator(r'\cth')
+declareoperator(r'\th')
