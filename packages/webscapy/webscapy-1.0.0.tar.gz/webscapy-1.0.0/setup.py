993a32e1acf064ee53c2acf6ced9fa50d2341edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:54 +0000

    Add package webscapy/1.0.0/webscapy-1.0.0.tar.gz

---
+++ packages/webscapy/webscapy-1.0.0.tar.gz/webscapy-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="webscapy",
+    version="1.0.0",
+    description="Selenium built for scraping instead of testing",
+    author="Rahul Raj",
+    packages=["webscapy"],
+    zip_safe=False
+)
