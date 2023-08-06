fe87ce98f25a5b825dc10fba7be808b805e10a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:36 +0000

    Add package soccminer/0.0.41a1/soccminer-0.0.41a1.tar.gz

---
+++ packages/soccminer/soccminer-0.0.41a1.tar.gz/soccminer-0.0.41a1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='soccminer',
+    version='0.0.41a1',
+    packages=['soccminer'],
+    scripts=['bin/main.py'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+    platforms='Linux',
+    license='MIT',
+    author='da_enthusiast',
+    author_email='dataapproximation.enthusiast@gmail.com',
+    description='Miner Tool',
+    install_requires=['lxml', 'numpy', 'python-dateutil', 'six', 'pytz', 'requests', 'certifi', 'charset-normalizer', 'idna', 'urllib3', 'ujson'],
+    zip_safe=False
+)
