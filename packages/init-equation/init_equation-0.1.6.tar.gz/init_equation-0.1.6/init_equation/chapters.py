0706c8294c9de8611e8c3e57a9daebc815829ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:37 +0000

    Add package init-equation/0.1.6/init_equation-0.1.6.tar.gz

---
+++ packages/init-equation/init_equation-0.1.6.tar.gz/init_equation-0.1.6/init_equation/chapters.py
@@ -0,0 +1,53 @@
+
+modules = [
+        'mechanics',
+        'electrodynamics',
+        'modern-physics',
+        'optics',
+        'vectors'
+        ]
+
+chapters_mechanics = [
+        'kinematics',
+        'projectile',
+        'laws-of-motion',
+        'work-energy-power',
+        'circular-motion',
+        'rotation',
+        'center-of-mass',
+        'gravitation',
+        'simple-harmonic-motion',
+        'elasticity',
+        'fluid-mechanics',
+        ]
+chapters_electrodynamics = [
+        'electrostatics',
+        'current-electricity',
+        'capacitors',
+        'magnetics',
+        'electromagnetic-induction',
+        'alternating-current',
+        'electromagnetic-waves'
+        ]
+chapters_optics = [
+        'ray-optics',
+        'wave-optics',
+        ]
+chapters_modern_physics = [
+        'atomic-structure',
+        'photoelectric-effect',
+        'x-rays',
+        'nuclear-physics',
+        'semiconductors',
+        'communiation-system'
+        ]
+
+chapters_vectors = [
+        'vector-analysis'
+        ]
+
+chapters = [chapters_mechanics, chapters_electrodynamics, chapters_modern_physics, chapters_optics, chapters_vectors]
+
+chapters_list = chapters_mechanics + chapters_electrodynamics + chapters_modern_physics + chapters_optics + chapters_vectors
+
+
