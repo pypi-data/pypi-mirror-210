259e97be169a030ae975074b22d29e4eb067cfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package llm-client/0.1.0/llm_client-0.1.0.tar.gz

---
+++ packages/llm-client/llm_client-0.1.0.tar.gz/llm_client-0.1.0/tests/llm_api_client/openai_client/conftest.py
@@ -0,0 +1,42 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+import pytest_asyncio
+from aiohttp import ClientSession
+
+from llm_client.llm_api_client.base_llm_api_client import LLMAPIClientConfig
+from llm_client.llm_api_client.openai_client import OpenAIClient
+
+
+@pytest.fixture
+def model_name():
+    return "ada"
+
+
+@pytest_asyncio.fixture
+async def client_session():
+    session = ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest.fixture
+def openai_mock():
+    with patch("llm_client.llm_api_client.openai_client.openai") as openai_mock:
+        yield openai_mock
+
+
+@pytest.fixture
+def config(model_name):
+    return LLMAPIClientConfig("fake-api-key", MagicMock(ClientSession), default_model=model_name)
+
+
+@pytest.fixture
+def open_ai_client(config):
+    return OpenAIClient(config)
+
+
+@pytest.fixture
+def tiktoken_mock():
+    with patch("llm_client.llm_api_client.openai_client.tiktoken") as tiktoken_mock:
+        yield tiktoken_mock
