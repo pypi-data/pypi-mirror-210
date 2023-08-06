6813c2d4264cde6b05a937bbae036a219421243d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:13 +0000

    Add package kuto/0.0.22/kuto-0.0.22.tar.gz

---
+++ packages/kuto/kuto-0.0.22.tar.gz/kuto-0.0.22/demo/tests/test_ios.py
@@ -0,0 +1,32 @@
+import kuto
+from kuto import IosElem
+
+
+class IndexPage(kuto.Page):
+    """首页"""
+    ad = IosElem(text='close white big')
+    my = IosElem(text='我的')
+
+
+class MyPage(kuto.Page):
+    """我的页"""
+    setting = IosElem(text='settings navi')
+
+
+class TestSearch(kuto.TestCase):
+    """设置页测试"""
+
+    def start(self):
+        self.index_page = IndexPage(self.driver)
+        self.my_page = MyPage(self.driver)
+
+    def test_1(self):
+        """进入设置页"""
+        self.index_page.ad.click_exists(timeout=5)
+        self.index_page.my.click()
+        self.my_page.setting.click()
+
+
+if __name__ == '__main__':
+    kuto.main(did='00008101-000E646A3C29003A', pkg='com.qizhidao.company')
+
