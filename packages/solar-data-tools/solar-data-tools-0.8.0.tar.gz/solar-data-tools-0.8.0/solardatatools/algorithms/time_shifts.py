7544c2021f8802e554eeb163b7912b20dadd85b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:18 +0000

    Add package solar-data-tools/0.8.0/solar-data-tools-0.8.0.tar.gz

---
+++ packages/solar-data-tools/solar-data-tools-0.8.0.tar.gz/solar-data-tools-0.8.0/solardatatools/algorithms/time_shifts.py
@@ -0,0 +1,282 @@
+""" Time Shift Algorithm Module
+
+This module contains the algorithm for detecting time shifts in an unlabeled PV
+power production data sets. These occur because of the local clock on the data
+logging system being changed or by incorrect handling of daylight savings.
+The algorithm works as follows:
+
+    - Estimate solar noon on each day from the data
+    - Fit a signal demixing model, assuming a seasonal component and a piecewise
+      constant component
+    - Polish the L1 heuristic used to estimate piecewise constant component
+      using iterative reweighting
+    - Use piecewise constance component to detect shift points in time and
+      correction amounts
+
+"""
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from solardatatools.solar_noon import energy_com, avg_sunrise_sunset
+from solardatatools.signal_decompositions import l2_l1d1_l2d2p365
+
+
+class TimeShift:
+    def __init__(self):
+        self.metric = None
+        self.s1 = None
+        self.s2 = None
+        self.index_set = None
+        self.corrected_data = None
+        self.roll_by_index = None
+        self.normalized_holdout_error = None
+        self.normalized_train_error = None
+        self.tv_metric = None
+        self.jumps_per_year = None
+        self.best_c1 = None
+        self.best_ix = None
+        self.baseline = None
+        self.periodic_detector = None
+        self.__recursion_depth = 0
+
+    def run(
+        self,
+        data,
+        use_ixs=None,
+        c1=None,
+        c2=200.0,
+        solar_noon_estimator="com",
+        threshold=0.005,
+        periodic_detector=False,
+        solver=None,
+    ):
+        if solar_noon_estimator == "com":
+            metric = energy_com(data)
+        elif solar_noon_estimator == "srss":
+            metric = avg_sunrise_sunset(data, threshold=threshold)
+        self.metric = metric
+        if use_ixs is None:
+            use_ixs = ~np.isnan(metric)
+        else:
+            use_ixs = np.logical_and(use_ixs, ~np.isnan(metric))
+        self.use_ixs = use_ixs
+        # Optimize c1
+        if c1 is None:
+            c1s = np.logspace(-1, 2, 11)
+            hn, rn, tv_metric, jpy, best_ix = self.optimize_c1(
+                metric, c1s, use_ixs, c2, periodic_detector, solver=solver
+            )
+            if tv_metric[best_ix] >= 0.009:
+                # rerun the optimizer with a new random data selection
+                hn, rn, tv_metric, jpy, best_ix = self.optimize_c1(
+                    metric, c1s, use_ixs, c2, periodic_detector, solver=solver
+                )
+            # if np.isclose(hn[best_ix], hn[-1]):
+            #     best_ix = np.argmax(hn * rn)
+            best_c1 = c1s[best_ix]
+        else:
+            best_c1 = c1
+            hn = None
+            rn = None
+            tv_metric = None
+            jpy = None
+            c1s = None
+            best_ix = None
+        s1, s2 = self.estimate_components(
+            metric, best_c1, c2, use_ixs, periodic_detector, solver=solver
+        )
+        # find indices of transition points
+        index_set = np.arange(len(s1) - 1)[np.round(np.diff(s1, n=1), 3) != 0]
+        # print(len(index_set), len(index_set) / (len(metric) / 365))
+        s1, s2 = self.estimate_components(
+            metric,
+            best_c1,
+            c2,
+            use_ixs,
+            periodic_detector,
+            transition_locs=index_set,
+            solver=solver,
+        )
+        jumps_per_year = len(index_set) / (len(metric) / 365)
+        cond1 = jumps_per_year >= 5
+        cond2 = c1 is None
+        cond3 = self.__recursion_depth < 2
+        if cond1 and cond2 and cond3:
+            # Unlikely that  there are more than 5 time shifts per year. Try a
+            # different random sampling
+            self.__recursion_depth += 1
+            self.run(
+                data,
+                use_ixs=use_ixs,
+                c1=c1,
+                c2=c2,
+                solar_noon_estimator=solar_noon_estimator,
+                threshold=threshold,
+                periodic_detector=periodic_detector,
+                solver=solver,
+            )
+            return
+        # Apply corrections
+        my_set = set(s1)
+        key_func = lambda x: abs(x - 12)
+        closest_element = min(my_set, key=key_func)
+        roll_by_index = np.round((closest_element - s1) * data.shape[0] / 24, 0)
+        correction_metric = np.average(np.abs(roll_by_index))
+        if correction_metric < 0.01:
+            roll_by_index[:] = 0
+        self.roll_by_index = roll_by_index
+        index_set = np.arange(len(roll_by_index) - 1)[
+            np.round(np.diff(roll_by_index, n=1), 3) != 0
+        ]
+        Dout = self.apply_corrections(data)
+
+        # save results
+        self.normalized_holdout_error = hn
+        self.normalized_train_error = rn
+        self.tv_metric = tv_metric
+        self.jumps_per_year = jpy
+        self.c1_vals = c1s
+        self.best_c1 = best_c1
+        self.best_ix = best_ix
+        self.periodic_detector = periodic_detector
+        self.s1 = s1
+        self.s2 = s2
+        self.index_set = index_set
+        self.baseline = closest_element
+        self.corrected_data = Dout
+        self.__recursion_depth = 0
+
+    def optimize_c1(self, metric, c1s, use_ixs, c2, periodic_detector, solver=None):
+        # set up train/test split with sklearn
+        ixs = np.arange(len(metric))
+        ixs = ixs[use_ixs]
+        train_ixs, test_ixs = train_test_split(ixs, train_size=0.85)
+        train = np.zeros(len(metric), dtype=bool)
+        test = np.zeros(len(metric), dtype=bool)
+        train[train_ixs] = True
+        test[test_ixs] = True
+        # initialize results objects
+        train_r = np.zeros_like(c1s)
+        test_r = np.zeros_like(c1s)
+        tv_metric = np.zeros_like(c1s)
+        jpy = np.zeros_like(c1s)
+        rms_s2 = np.zeros_like(c1s)
+        # iterate over possible values of c1 parameter
+        for i, v in enumerate(c1s):
+            s1, s2 = self.estimate_components(
+                metric, v, c2, train, periodic_detector, n_iter=5, solver=solver
+            )
+            y = metric
+            # collect results
+            train_r[i] = np.average(np.power((y - s1 - s2)[train], 2))
+            test_r[i] = np.average(np.power((y - s1 - s2)[test], 2))
+            tv_metric[i] = np.average(np.abs(np.diff(s1, n=1)))
+            count_jumps = np.sum(~np.isclose(np.diff(s1), 0, atol=1e-4))
+            jumps_per_year = count_jumps / (len(metric) / 365)
+            jpy[i] = jumps_per_year
+            rms_s2[i] = np.sqrt(np.mean(np.square(s2)))
+
+        def zero_one_scale(x):
+            return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+        hn = zero_one_scale(test_r)  # holdout error metrix
+        rn = zero_one_scale(train_r)
+        ixs = np.arange(len(c1s))
+        # Detecting more than 5 time shifts per year is extremely uncommon,
+        # and is considered non-physical
+        if not periodic_detector:
+            slct = np.logical_and(jpy <= 5, hn <= 0.1)
+        else:
+            slct = np.logical_and(np.logical_and(jpy <= 5, hn <= 0.05), rms_s2 <= 0.25)
+        subset_ixs = ixs[slct]
+        if len(subset_ixs)>0:
+            # choose index of lowest holdout error
+            best_ix = np.max(subset_ixs)
+        else:
+            best_ix = np.nanargmin(hn)
+        return hn, rn, tv_metric, jpy, best_ix
+
+    def estimate_components(
+        self,
+        metric,
+        c1,
+        c2,
+        use_ixs,
+        periodic_detector,
+        transition_locs=None,
+        n_iter=5,
+        solver=None,
+    ):
+        # Iterative reweighted L1 heuristic
+        w = np.ones(len(metric) - 1)
+        eps = 0.1
+        for i in range(n_iter):
+            s1, s2 = l2_l1d1_l2d2p365(
+                metric,
+                c1=c1,
+                c2=c2,
+                tv_weights=w,
+                use_ixs=use_ixs,
+                yearly_periodic=periodic_detector,
+                transition_locs=transition_locs,
+                solver=solver,
+            )
+            w = 1 / (eps + np.abs(np.diff(s1, n=1)))
+        return s1, s2
+
+    def plot_analysis(self, figsize=None):
+        if self.metric is not None:
+            import matplotlib.pyplot as plt
+
+            fig = plt.figure(figsize=figsize)
+            plt.plot(self.metric, linewidth=1, label="metric")
+            plt.plot(self.s1, label="shift detector")
+            plt.plot(self.s1 + self.s2, ls="--", label="SD model")
+            plt.legend()
+            plt.xlabel("day number")
+            plt.ylabel("solar noon [hours]")
+            return fig
+
+    def plot_optimization(self, figsize=None):
+        if self.best_ix is not None:
+            c1s = self.c1_vals
+            hn = self.normalized_holdout_error
+            rn = self.normalized_train_error
+            best_c1 = self.best_c1
+            import matplotlib.pyplot as plt
+
+            fig, ax = plt.subplots(nrows=4, sharex=True, figsize=figsize)
+            ax[0].plot(c1s, hn, marker=".")
+            ax[0].axvline(best_c1, ls="--", color="red")
+            ax[0].set_title("holdout validation")
+            ax[1].plot(c1s, self.jumps_per_year, marker=".")
+            ax[1].axvline(best_c1, ls="--", color="red")
+            ax[1].set_title("jumps per year")
+            ax[2].plot(c1s, rn, marker=".")
+            ax[2].axvline(best_c1, ls="--", color="red")
+            ax[2].set_title("training residuals")
+            ax[3].plot(c1s, self.tv_metric, marker=".")
+            ax[3].axvline(best_c1, ls="--", color="red")
+            ax[3].set_xscale("log")
+            ax[3].set_title("Total variation metric")
+            plt.tight_layout()
+            return fig
+
+    def apply_corrections(self, data):
+        roll_by_index = self.roll_by_index
+        Dout = np.copy(data)
+        for roll in np.unique(roll_by_index):
+            if roll != 0:
+                ixs = roll_by_index == roll
+                Dout[:, ixs] = np.roll(data, int(roll), axis=0)[:, ixs]
+        return Dout
+
+    def invert_corrections(self, data):
+        roll_by_index = self.roll_by_index
+        Dout = np.copy(data)
+        for roll in np.unique(roll_by_index):
+            if roll != 0:
+                ixs = roll_by_index == roll
+                Dout[:, ixs] = np.roll(data, -int(roll), axis=0)[:, ixs]
+        return Dout
---
+++ packages/solar-data-tools/solar-data-tools-0.8.0.tar.gz/solar-data-tools-0.8.0/solardatatools/signal_decompositions.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+""" Signal Decompositions Module
+
+This module contains standardized signal decomposition models for use in the
+SDT algorithms. The defined signal decompositions are:
+
+1) 'l2_l1d1_l2d2p365': separating a piecewise constant component from a smooth
+and seasonal component, with Gaussian noise
+    - l2: gaussian noise, sum-of-squares small or l2-norm squared
+    - l1d1: piecewise constant heuristic, l1-norm of first order differences
+    - l2d2p365: small second order diffs (smooth) and 365-periodic
+2) 'tl1_l2d2p365': similar to (2), estimating a smooth, seasonal component with
+an asymmetric laplacian noise model, fitting a local quantile instead of a
+local average
+    - tl1: 'tilted l1-norm,' also known as quantile cost function
+    - l2d2p365: small second order diffs (smooth) and 365-periodic
+3) 'tl1_l1d1_l2d2p365': like (1) but with an asymmetric residual cost instead
+of Gaussian residuals
+    - tl1: 'tilted l1-norm,' also known as quantile cost function
+    - l1d1: piecewise constant heuristic, l1-norm of first order differences
+    - l2d2p365: small second order diffs (smooth) and 365-periodic
+4) 'make_l2_l1d2_constrained':
+    - l2: gaussian noise, sum-of-squares small or l2-norm squared
+    - l1d2: piecewise linear heuristic
+    - constrained to have first val at 0 and last val at 1
+
+"""
+import sys
+import numpy as np
+import cvxpy as cvx
+
+
+def l2_l1d1_l2d2p365(
+    signal,
+    c0=10, # "hard-coded"
+    c1=50, # optimized
+    c2=1e5,
+    solver=None,
+    verbose=False,
+    tv_weights=None,
+    use_ixs=None,
+    yearly_periodic=False,
+    transition_locs=None,
+    return_all=False
+):
+    """
+    This performs total variation filtering with the addition of a seasonal
+    baseline fit. This introduces a new signal to the model that is smooth and
+    periodic on a yearly time frame. This does a better job of describing real,
+    multi-year solar PV power data sets, and therefore does an improved job of
+    estimating the discretely changing signal.
+
+    :param signal: A 1d numpy array (must support boolean indexing) containing
+    the signal of interest
+    :param c1: The regularization parameter to control the total variation in
+    the final output signal
+    :param c2: The regularization parameter to control the smoothness of the
+    seasonal signal
+    :return: A 1d numpy array containing the filtered signal
+    """
+    if tv_weights is None:
+        tv_weights = np.ones(len(signal) - 1)
+    if use_ixs is None:
+        index_set = ~np.isnan(signal)
+    else:
+        index_set = np.logical_and(use_ixs, ~np.isnan(signal))
+    s_hat = cvx.Variable(len(signal))
+    s_seas = cvx.Variable(len(signal))
+    s_error = cvx.Variable(len(signal))
+    c0 = cvx.Constant(value=c0)
+    c1 = cvx.Constant(value=c1)
+    c2 = cvx.Constant(value=c2)
+
+    if transition_locs is None: # TODO: this should be two separate sd problems
+        objective = cvx.Minimize(
+            c0 * cvx.sum_squares(s_error)
+            + c1 * cvx.norm1(cvx.multiply(tv_weights, cvx.diff(s_hat, k=1)))
+            + c2 * cvx.sum_squares(cvx.diff(s_seas, k=2))
+        )
+    else:
+        objective = cvx.Minimize(
+            c0 * cvx.norm(s_error)
+           + c2 * cvx.sum_squares(cvx.diff(s_seas, k=2))
+        )
+    # Consistency constraints
+    constraints = [
+        signal[index_set] == s_hat[index_set] + s_seas[index_set] + s_error[index_set],
+        cvx.sum(s_seas[:365]) == 0,
+    ]
+    if len(signal) > 365:
+        constraints.append(s_seas[365:] - s_seas[:-365] == 0)
+        if yearly_periodic:
+            constraints.append(s_hat[365:] - s_hat[:-365] == 0)
+    if transition_locs is not None:
+        loc_mask = np.ones(len(signal) - 1, dtype=bool)
+        loc_mask[transition_locs] = False
+        constraints.append(cvx.diff(s_hat, k=1)[loc_mask] == 0)
+
+    problem = cvx.Problem(objective=objective, constraints=constraints)
+    problem.solve(solver=solver, verbose=verbose)
+
+    if return_all:
+        return s_hat.value, s_seas.value, s_error.value, problem.objective.value
+
+    return s_hat.value, s_seas.value
+
+def tl1_l2d2p365(
+    signal,
+    use_ixs=None,
+    tau=0.75, # passed as 0.05 (sunrise), 0.95 (sunset), 0.9, 0.85
+    c1=500, # good default for sunrise sunset estimates (4 calls), incr from 100 on 5/9
+    solver=None,
+    yearly_periodic=True, # passed as False once
+    verbose=False,
+    return_all=False
+):
+    """
+    https://colab.research.google.com/github/cvxgrp/cvx_short_course/blob/master/applications/quantile_regression.ipynb
+
+    :param signal: 1d numpy array
+    :param use_ixs: optional index set to apply cost function to
+    :param tau: float, parameter for quantile regression
+    :param c1: float
+    :param solver: string
+    :return: median fit with seasonal baseline removed
+    """
+    if use_ixs is None:
+        use_ixs = ~np.isnan(signal)
+    x = cvx.Variable(len(signal))
+    r = signal[use_ixs] - x[use_ixs]
+    objective = cvx.Minimize(
+        cvx.sum(0.5 * cvx.abs(r) + (tau - 0.5) * r) + c1 * cvx.sum_squares(cvx.diff(x, k=2))
+    )
+    if len(signal) > 365 and yearly_periodic:
+        constraints = [x[365:] == x[:-365]]
+    else:
+        constraints = []
+    problem = cvx.Problem(objective, constraints=constraints)
+    problem.solve(solver=solver, verbose=verbose)
+
+    if return_all:
+        return x.value, problem.objective.value
+
+    return x.value
+
+def tl1_l1d1_l2d2p365(
+    signal,
+    use_ixs=None,
+    tau=0.995, # passed as 0.5
+    c1=1e3, # passed as 15, l1d1 term
+    c2=6000,
+    c3=1e2, # passed as 300, linear term
+    solver=None,
+    verbose=False,
+    tv_weights=None,
+    return_all=False
+):
+    """
+    This performs total variation filtering with the addition of a seasonal baseline fit. This introduces a new
+    signal to the model that is smooth and periodic on a yearly time frame. This does a better job of describing real,
+    multi-year solar PV power data sets, and therefore does an improved job of estimating the discretely changing
+    signal.
+
+    :param signal: A 1d numpy array (must support boolean indexing) containing the signal of interest
+    :param c1: The regularization parameter to control the total variation in the final output signal
+    :param c2: The regularization parameter to control the smoothness of the seasonal signal
+    :return: A 1d numpy array containing the filtered signal
+    """
+    n = len(signal)
+    if tv_weights is None:
+        tv_weights = np.ones(len(signal) - 1)
+    if use_ixs is None:
+        use_ixs = ~np.isnan(signal)
+    else:
+        use_ixs = np.logical_and(use_ixs, ~np.isnan(signal))
+
+    s_hat = cvx.Variable(n)
+    s_seas = cvx.Variable(max(n, 366))
+    s_error = cvx.Variable(n)
+    c1 = cvx.Parameter(value=c1, nonneg=True)
+    c2 = cvx.Parameter(value=c2, nonneg=True)
+    c3 = cvx.Parameter(value=c3, nonneg=True)
+    tau = cvx.Parameter(value=tau)
+    beta = cvx.Variable()
+
+    objective = cvx.Minimize(
+        2
+        * cvx.sum(
+            0.5 * cvx.abs(s_error)
+            + (tau - 0.5) * s_error
+        )
+        + c1 * cvx.norm1(cvx.multiply(tv_weights, cvx.diff(s_hat, k=1)))
+        + c2 * cvx.sum_squares(cvx.diff(s_seas, k=2))
+        + c3 * beta**2 # linear term that has a slope of beta over 1 year, done wrong
+    )
+
+    constraints = [
+        signal[use_ixs] == s_hat[use_ixs] + s_seas[:n][use_ixs] + s_error[use_ixs],
+        cvx.sum(s_seas[:365]) == 0,
+    ]
+    constraints.append(s_seas[365:] - s_seas[:-365] == beta)
+    constraints.extend([beta <= 0.01, beta >= -0.1])
+    problem = cvx.Problem(objective=objective, constraints=constraints)
+    problem.solve(solver=solver, verbose=verbose)
+
+    if return_all:
+        return s_hat.value, s_seas.value, s_error.value, problem.objective.value
+
+    return s_hat.value, s_seas.value[:n]
+
+
+def make_l2_l1d2_constrained(signal,
+                 weight=1e1, # val ok
+                 solver="MOSEK",
+                 use_ixs=None,
+                 verbose=False,
+                 return_all=False
+):
+    """
+    Used in solardatatools/algorithms/clipping.py
+    Added hard-coded constraints on the first and last vals
+    """
+    if use_ixs is None:
+        use_ixs = ~np.isnan(signal)
+    else:
+        use_ixs = np.logical_and(use_ixs, ~np.isnan(signal))
+
+    y_hat = cvx.Variable(len(signal))
+    mu = cvx.Parameter(nonneg=True)
+    mu.value = weight
+    error = cvx.sum_squares(signal[use_ixs] - y_hat[use_ixs])
+    reg = cvx.norm(cvx.diff(y_hat, k=2), p=1)
+
+    objective = cvx.Minimize(error + mu * reg)
+    constraints = [y_hat[0] == 0, y_hat[-1] == 1]
+    problem = cvx.Problem(objective, constraints)
+
+    if return_all:
+        problem.solve(solver=solver, verbose=verbose)
+        return y_hat.value, problem.objective.value
+
+    return problem, signal, y_hat, mu
