4228b9e437b85a906bcac8ee023260327ffdd35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:25 +0000

    Add package meta-matching-tool/0.0.5/meta_matching_tool-0.0.5.tar.gz

---
+++ packages/meta-matching-tool/meta_matching_tool-0.0.5.tar.gz/meta_matching_tool-0.0.5/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/5/21 17:19
+# @Author : Leqi Tian
+# @File : setup.py
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.0.5'
+
+setup(
+    name='meta_matching_tool',  # package name
+    version=VERSION,  # package version
+    description='An integrated deep learning framework for the interpretation of untargeted metabolomics data',  
+    packages=find_packages(),
+    author='Leqi Tian',
+    author_email='leqitian@link.cuhk.edu.cn',
+    python_requires=">=3.9",
+    zip_safe=False,
+    install_requires=[
+        'numpy>=1.21.5',
+        'pandas>=1.5.3',
+        'python_igraph>=0.10.4',
+       'scikit_learn>=1.0.2',
+        'torch>=1.11.0',
+    ],
+)
