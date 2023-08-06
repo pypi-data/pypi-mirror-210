8538fa9bc9e348f72d48bbaf38b25428ecfbd3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:13 +0000

    Add package alpha-filter/0.9.1/alpha-filter-0.9.1.tar.gz

---
+++ packages/alpha-filter/alpha-filter-0.9.1.tar.gz/alpha-filter-0.9.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='alpha-filter',
+    version='0.9.1',
+    description='differential filter',
+    py_modules=['alpha-filter'],
+    python_requires='>=3.8'
+)
