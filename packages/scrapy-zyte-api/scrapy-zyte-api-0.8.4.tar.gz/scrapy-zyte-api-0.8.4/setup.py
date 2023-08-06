56b5c2ee5e3710e316f3b037432dfed7a0c285d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:47 +0000

    Add package scrapy-zyte-api/0.8.4/scrapy-zyte-api-0.8.4.tar.gz

---
+++ packages/scrapy-zyte-api/scrapy-zyte-api-0.8.4.tar.gz/scrapy-zyte-api-0.8.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="scrapy-zyte-api",
+    version="0.8.4",
+    description="Client library to process URLs through Zyte API",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    author="Zyte Group Ltd",
+    author_email="info@zyte.com",
+    url="https://github.com/scrapy-plugins/scrapy-zyte-api",
+    packages=["scrapy_zyte_api"],
+    install_requires=[
+        "packaging>=14.0",
+        "scrapy>=2.0.1",
+        "zyte-api>=0.4.0",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
