e65d46499272db3200b915d2c814137b665aff88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:21 +0000

    Add package disfluency-constituency-parser/0.0.6/disfluency-constituency-parser-0.0.6.tar.gz

---
+++ packages/disfluency-constituency-parser/disfluency-constituency-parser-0.0.6.tar.gz/disfluency-constituency-parser-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="disfluency-constituency-parser",
+    version="0.0.6",
+    author="Li Wang",
+    author_email="li@liwang.info",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liwangd/disfluency-constituency-parser",
+    packages=setuptools.find_packages(),
+    package_data={'': ['*.pyx']},
+    classifiers=(
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing :: Linguistic",
+    ),
+    setup_requires=["cython", "numpy"],
+    install_requires=["cython", "numpy", "nltk>=3.2"],
+)
