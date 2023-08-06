638277cce78f9294e18e1507fc6288607f190266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:06 +0000

    Add package logfrog/0.10/logfrog-0.10.tar.gz

---
+++ packages/logfrog/logfrog-0.10.tar.gz/logfrog-0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logfrog",
+    version="0.10",
+    packages=find_packages(),
+    description="A simple and efficient logging library in Python.",
+    author="Lakshya Gupta",
+    author_email="lakshyagupta831@gmail.com",
+    url="https://github.com/fuushyn/logfrog",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
