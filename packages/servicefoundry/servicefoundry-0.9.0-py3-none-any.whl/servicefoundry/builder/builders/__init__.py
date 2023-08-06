610c4736a1283fe0f3379203085cb3797ea714f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package servicefoundry/0.9.0/servicefoundry-0.9.0-py3-none-any.whl

---
+++ packages/servicefoundry/servicefoundry-0.9.0-py3-none-any.whl/servicefoundry/builder/builders/__init__.py
@@ -0,0 +1,17 @@
+from typing import Callable, Dict
+
+from servicefoundry.builder.builders import dockerfile, tfy_python_buildpack
+
+BUILD_REGISTRY: Dict[str, Callable] = {
+    "dockerfile": dockerfile.build,
+    "tfy-python-buildpack": tfy_python_buildpack.build,
+}
+
+__all__ = ["get_builder"]
+
+
+def get_builder(build_configuration_type: str) -> Callable:
+    if build_configuration_type not in BUILD_REGISTRY:
+        raise NotImplementedError(f"Builder for {build_configuration_type} not found")
+
+    return BUILD_REGISTRY[build_configuration_type]
