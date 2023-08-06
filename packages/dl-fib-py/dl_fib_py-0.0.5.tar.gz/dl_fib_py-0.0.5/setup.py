d064efb87ce492a69ee32a445cf6d3b8d9fbcf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:50 +0000

    Add package dl-fib-py/0.0.5/dl_fib_py-0.0.5.tar.gz

---
+++ packages/dl-fib-py/dl_fib_py-0.0.5.tar.gz/dl_fib_py-0.0.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+import pathlib
+
+with open(
+    str(pathlib.Path(__file__).parent.absolute()) + "/dl_fib_py/version.py", "r"
+) as fh:
+    version = fh.read().split("=")[1].replace("'", "")
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dl_fib_py",
+    version=version,
+    author="Dima Lurie",
+    author_email="dimalurie@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dima-lurie/dl-fib-py.git",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={"dev": ["mypy>=1.3", "twine>=4.0", "requests"]},
+    python_requires=">=3.11",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "fib-number = \
+           dl_fib_py.cmd.fib_numb:fib_numb",
+        ],
+    },
+)
