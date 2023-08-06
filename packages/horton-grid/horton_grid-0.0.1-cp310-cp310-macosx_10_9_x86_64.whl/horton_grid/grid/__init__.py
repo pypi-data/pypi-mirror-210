dcbec458e05d4810cf725f06429a0dbcfdb62e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:35 +0000

    Add package horton-grid/0.0.1/horton_grid-0.0.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/horton-grid/horton_grid-0.0.1-cp310-cp310-macosx_10_9_x86_64.whl/horton_grid/grid/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# HORTON-GRID: GRID for Helpful Open-source Research TOol for N-fermion systems.
+# Copyright (C) 2011-2023 The HORTON-GRID Development Team
+#
+# This file is part of HORTON-GRID
+#
+# HORTON-GRID is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# HORTON-GRID is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+"""Numerical integration grids"""
+
+
+from horton_grid.grid.base import *
+from horton_grid.grid.atgrid import *
+from horton_grid.grid.cext import *
+from horton_grid.grid.molgrid import *
+from horton_grid.grid.ode2 import *
+from horton_grid.grid.poisson import *
+from horton_grid.grid.radial import *
+from horton_grid.grid.visual import *
