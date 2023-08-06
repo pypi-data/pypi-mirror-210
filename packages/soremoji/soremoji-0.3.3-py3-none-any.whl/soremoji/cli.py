6ddf0fe2d1906fc89853251a18d4f94f8bc68553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:52 +0000

    Add package soremoji/0.3.3/soremoji-0.3.3-py3-none-any.whl

---
+++ packages/soremoji/soremoji-0.3.3-py3-none-any.whl/soremoji/cli.py
@@ -0,0 +1,43 @@
+from argparse import REMAINDER, ArgumentParser
+from typing import Any, Callable, List, Optional
+
+from command import (
+    handle_commit,
+    handle_hook,
+    handle_list,
+    handle_no_subcommand,
+    handle_search,
+)
+
+parser = ArgumentParser("soremoji", description="Simple gitmoji-cli written in python")
+parser.set_defaults(func=handle_no_subcommand)
+_subparsers = parser.add_subparsers(title="subcommands")
+
+# hook / 钩子
+hook_parser = _subparsers.add_parser("hook", help="Run as a git hook.")
+hook_parser.add_argument("commit_msg_file", help="Path to the commit message file")
+hook_parser.set_defaults(func=handle_hook)
+
+# commit / 提交
+commit_parser = _subparsers.add_parser("commit", help="Start to commit changes")
+commit_parser.add_argument("-e", "--emoji", help="Emoji to use")
+commit_parser.add_argument("-m", "--message", help="Commit message")
+commit_parser.add_argument(
+    "--", nargs=REMAINDER, help="Argument to pass to the git", dest="args"
+)
+commit_parser.set_defaults(func=handle_commit)
+
+# list / 列表
+list_parser = _subparsers.add_parser("list", help="List all available emoji.")
+list_parser.set_defaults(func=handle_list)
+
+# search / 搜索
+search_parser = _subparsers.add_parser("search", help="Search for an emoji.")
+search_parser.set_defaults(func=handle_search)
+
+
+def main(argv: Optional[List[str]] = None):
+    result = parser.parse_args(argv)
+    result = vars(result)
+    func: Callable[..., Any] = result.pop("func")
+    func(**result)
