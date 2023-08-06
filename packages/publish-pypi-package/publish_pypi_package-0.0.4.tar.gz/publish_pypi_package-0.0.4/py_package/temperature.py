0c6af0509eb458bc1f0b8dfce6c3d140bc7a0f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:15 +0000

    Add package publish-pypi-package/0.0.4/publish_pypi_package-0.0.4.tar.gz

---
+++ packages/publish-pypi-package/publish_pypi_package-0.0.4.tar.gz/publish_pypi_package-0.0.4/py_package/temperature.py
@@ -0,0 +1,8 @@
+def hello() -> None:
+    print('Hello, welcome to py_package.')
+
+def c2f(celcius):
+    return (float(celcius) * 9/5) + 32
+
+def f2c(fahrenheit):
+    return (float(fahrenheit) - 32) * 5/9
---
+++ packages/publish-pypi-package/publish_pypi_package-0.0.4.tar.gz/publish_pypi_package-0.0.4/tests/test.py
@@ -0,0 +1,5 @@
+from py_package import *
+
+hello()
+print(f"{f2c(32)}")  # Result should be 0.0
+print(f"{c2f(0)}")  # Result should be 32.0
