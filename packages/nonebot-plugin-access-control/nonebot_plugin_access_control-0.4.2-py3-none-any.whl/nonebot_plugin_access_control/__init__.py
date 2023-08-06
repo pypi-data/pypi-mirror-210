4f9fe8ab2890405c2a520b91dbd4ab855f82ae44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:03 +0000

    Add package nonebot-plugin-access-control/0.4.2/nonebot_plugin_access_control-0.4.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-access-control/nonebot_plugin_access_control-0.4.2-py3-none-any.whl/nonebot_plugin_access_control/__init__.py
@@ -0,0 +1,35 @@
+from nonebot import require
+
+require("nonebot_plugin_datastore")
+require("nonebot_plugin_apscheduler")
+
+from . import access_control
+from . import config
+from . import errors
+from . import event_bus
+from . import matchers
+from . import models
+from . import service
+from . import subject
+from . import utils
+
+from importlib import import_module
+from nonebot import logger, get_driver
+
+supported_modules = {
+    "OneBot V11": "nonebot_plugin_access_control_onebot",
+    "OneBot V12": "nonebot_plugin_access_control_onebot",
+    "Kaiheila": "nonebot_plugin_access_control_kaiheila",
+}
+
+loaded_modules = []
+
+driver = get_driver()
+for adapter in driver._adapters:
+    if adapter in supported_modules:
+        import_module(supported_modules[adapter])
+        loaded_modules.append(adapter)
+        logger.debug(f"Succeed to loaded plugin for {adapter}")
+
+if len(loaded_modules):
+    logger.success(f"Loaded plugin for: {', '.join(loaded_modules)}")
