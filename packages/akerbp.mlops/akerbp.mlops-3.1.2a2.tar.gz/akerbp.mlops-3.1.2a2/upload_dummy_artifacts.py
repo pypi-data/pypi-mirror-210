bcc0fe832bc94d69209c60099c6936cfbc685047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:16 +0000

    Add package akerbp.mlops/3.1.2a2/akerbp.mlops-3.1.2a2.tar.gz

---
+++ packages/akerbp.mlops/akerbp.mlops-3.1.2a2.tar.gz/akerbp.mlops-3.1.2a2/upload_dummy_artifacts.py
@@ -0,0 +1,11 @@
+import os
+
+import akerbp.mlops.model_manager as mm
+
+model_name = "mlopsdemo"
+folder_path = "model_artifact"
+env = os.environ["MODEL_ENV"]
+metadata = {"Description": "Dummy artifacts for mlops demo model"}
+
+mm.setup()
+folder_info = mm.upload_new_model_version(model_name, env, folder_path, metadata)
