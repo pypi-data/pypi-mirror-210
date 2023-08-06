fc36f5181a2522f6a9db2071fab15cd4f9b7abd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:56 +0000

    Add package oarepo-model-builder/4.0.5/oarepo_model_builder-4.0.5-py3-none-any.whl

---
+++ packages/oarepo-model-builder/oarepo_model_builder-4.0.5-py3-none-any.whl/oarepo_model_builder/datatypes/containers/nested.py
@@ -0,0 +1,14 @@
+from .object import ObjectDataType
+
+
+class NestedDataType(ObjectDataType):
+    model_type = "nested"
+
+    mapping = {"type": "nested"}
+    facets = {
+        "facet_class": "NestedLabeledFacet",
+        "imports": [
+            {"import": "oarepo_runtime.facets.nested_facet.NestedLabeledFacet"}
+        ],
+    }
+
