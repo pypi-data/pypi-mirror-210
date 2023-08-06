d0c8c22e8538f86f5acb15ce6cad091173fc9ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:13 +0000

    Add package pyscada-bacnet/0.8.0/pyscada-bacnet-0.8.0.tar.gz

---
+++ packages/pyscada-bacnet/pyscada-bacnet-0.8.0.tar.gz/pyscada-bacnet-0.8.0/README.rst
@@ -0,0 +1,36 @@
+PyScada BACNet Extension
+========================
+
+This is a extension for PyScada to support BACNet Communication.
+
+
+What is Working
+---------------
+
+ - nothing is test
+
+
+What is not Working/Missing
+---------------------------
+
+ - Test with real hardware
+ - Documentation
+
+Installation
+------------
+
+ - pip install pyscada-bacnet
+
+
+Contribute
+----------
+
+ - Issue Tracker: https://github.com/pyscada/PyScada-BACNet/issues
+ - Source Code: https://github.com/pyscada/PyScada-BACNet
+
+
+License
+-------
+
+The project is licensed under the _GNU AFFERO GENERAL PUBLIC LICENSE Version 3 (AGPLv3)_.
+
---
+++ packages/pyscada-bacnet/pyscada-bacnet-0.8.0.tar.gz/pyscada-bacnet-0.8.0/pyscada/bacnet/admin.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyscada.bacnet import PROTOCOL_ID
+from pyscada.bacnet.models import BACnetDevice, ExtendedBACnetDevice
+from pyscada.bacnet.models import BACnetVariable, BACnetVariableProperty, ExtendedBACnetVariable
+from pyscada.admin import DeviceAdmin
+from pyscada.admin import VariableAdmin
+from pyscada.admin import admin_site
+from pyscada.models import Device, DeviceProtocol
+from pyscada.models import Variable
+from django.contrib import admin
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class BACnetDeviceAdminInline(admin.StackedInline):
+    model = BACnetDevice
+
+
+class BACnetDeviceAdmin(DeviceAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'protocol':
+            kwargs['queryset'] = DeviceProtocol.objects.filter(pk=PROTOCOL_ID)
+            db_field.default = PROTOCOL_ID
+        return super(BACnetDeviceAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        """Limit Pages to those that belong to the request's user."""
+        qs = super(BACnetDeviceAdmin, self).get_queryset(request)
+        return qs.filter(protocol_id=PROTOCOL_ID)
+
+    inlines = [
+        BACnetDeviceAdminInline
+    ]
+
+
+class BACnetVariableAdminInline(admin.StackedInline):
+    model = BACnetVariable
+
+
+class BACnetVariableAdmin(VariableAdmin):
+    list_display = ('id', 'name', 'description', 'unit', 'device_name', 'value_class', 'active', 'writeable', 'address',
+                    'function_code_read',)
+    list_editable = ('active', 'writeable',)
+    list_display_links = ('name',)
+
+    def address(self, instance):
+        return instance.BACnetvariable.address
+
+    def function_code_read(self, instance):
+        return instance.bacnetvariable.function_code_read
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'device':
+            kwargs['queryset'] = Device.objects.filter(protocol=PROTOCOL_ID)
+        return super(BACnetVariableAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        """Limit Pages to those that belong to the request's user."""
+        qs = super(BACnetVariableAdmin, self).get_queryset(request)
+        return qs.filter(device__protocol_id=PROTOCOL_ID)
+
+    inlines = [
+        BACnetVariableAdminInline
+    ]
+
+
+#admin_site.register(ExtendedBACnetDevice, BACnetDeviceAdmin)
+#admin_site.register(ExtendedBACnetVariable, BACnetVariableAdmin)
+admin_site.register(BACnetVariableProperty)
