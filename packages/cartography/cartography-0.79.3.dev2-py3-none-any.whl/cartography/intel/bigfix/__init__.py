a9a4aed0bce1d63a1f40ea5b9c3d0444d8193d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:58 +0000

    Add package cartography/0.79.3.dev2/cartography-0.79.3.dev2-py3-none-any.whl

---
+++ packages/cartography/cartography-0.79.3.dev2-py3-none-any.whl/cartography/intel/bigfix/__init__.py
@@ -0,0 +1,43 @@
+import logging
+
+import neo4j
+import requests
+
+from cartography.config import Config
+from cartography.intel.bigfix.computers import sync
+from cartography.util import timeit
+
+logger = logging.getLogger(__name__)
+
+requests_session = requests.Session()
+
+
+@timeit
+def start_bigfix_ingestion(neo4j_session: neo4j.Session, config: Config) -> None:
+    """
+    If this module is configured, perform ingestion of BigFix data. Otherwise warn and exit
+    :param neo4j_session: Neo4J session for database interface
+    :param config: A cartography.config object
+    :return: None
+    """
+
+    if not config.bigfix_username or not config.bigfix_password:
+        logger.info(
+            'BigFix import is not configured - skipping this module. '
+            'See docs to configure.',
+        )
+        return
+
+    common_job_parameters = {
+        "UPDATE_TAG": config.update_tag,
+        "ROOT_URL": config.bigfix_root_url,
+    }
+    sync(
+        neo4j_session,
+        requests_session,
+        config.bigfix_root_url,
+        config.bigfix_username,
+        config.bigfix_password,
+        config.update_tag,
+        common_job_parameters,
+    )
