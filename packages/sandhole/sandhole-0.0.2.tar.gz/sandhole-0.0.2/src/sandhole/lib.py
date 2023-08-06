46dd6f928d678d94945b779445c97dbc22527702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:14 +0000

    Add package sandhole/0.0.2/sandhole-0.0.2.tar.gz

---
+++ packages/sandhole/sandhole-0.0.2.tar.gz/sandhole-0.0.2/src/sandhole/lib.py
@@ -0,0 +1,80 @@
+import argparse
+import pathlib
+import sys
+import time
+
+import platformdirs
+import yaml
+
+
+def parse_timestamp(timestamp):
+    unit = timestamp[-1]
+    value = int(timestamp[:-1])
+
+    if unit == "h":
+        return value * 60 * 60
+    elif unit == "d":
+        return value * 24 * 60 * 60
+    elif unit == "w":
+        return value * 7 * 24 * 60 * 60
+    elif unit == "m":
+        return value * 30 * 24 * 60 * 60
+    elif unit == "y":
+        return value * 365 * 24 * 60 * 60
+    else:
+        raise argparse.ArgumentTypeError(
+            "Invalid timestamp unit. Use h, d, w, m, or y."
+        )
+
+
+def resolve_path(path):
+    resolved_path = pathlib.Path(path).expanduser()
+    return resolved_path
+
+
+def main(args):
+    current_time = time.time()
+    threshold = current_time - args.age
+
+    list_file = resolve_path(args.list_file)
+
+    if not list_file.exists():
+        print(f"File not found: {list_file}", file=sys.stderr)
+        sys.exit(1)
+
+    ignore_list = []
+
+    ignore_list.extend(args.append_ignore)
+
+    config_dir = pathlib.Path(platformdirs.user_config_dir("sandhole"))
+    config_file = pathlib.Path(config_dir, "config.yaml")
+
+    if not config_file.exists():
+        # Create a sample config file with example ignore_list
+        sample_config = {"ignore_list": ignore_list}
+        config_dir.mkdir(parents=True, exist_ok=True)
+        with config_file.open("w") as file:
+            yaml.safe_dump(sample_config, file)
+
+    else:
+        with config_file.open("r") as file:
+            config = yaml.safe_load(file)
+            if config and "ignore_list" in config:
+                ignore_list.extend(config["ignore_list"])
+
+    with list_file.open("r") as file:
+        file_paths = file.read().splitlines()
+
+    for file_path in file_paths:
+        path = resolve_path(file_path)
+        if path.name in ignore_list:
+            continue
+
+        if not path.exists():
+            print(f"File not found: {path}", file=sys.stderr)
+        elif path.is_file() and path.stat().st_mtime >= threshold:
+            print(file_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/sandhole/sandhole-0.0.2.tar.gz/sandhole-0.0.2/src/sandhole/main.py
@@ -0,0 +1,103 @@
+import argparse
+import logging
+import sys
+
+from sandhole import __version__
+
+from . import lib
+
+__author__ = "Taylor Monacelli"
+__copyright__ = "Taylor Monacelli"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"sandhole {__version__}",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    parser.add_argument(
+        "list_file", help="pathlib.Path to the file containing the list of file paths"
+    )
+    parser.add_argument(
+        "age",
+        type=lib.parse_timestamp,
+        help="Age threshold for file modification time (e.g., 1h, 2d, 1w, 1m, 4y)",
+    )
+    parser.add_argument(
+        "--append-ignore",
+        nargs="+",
+        default=[],
+        help="File names to append to the ignore list",
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    lib.main(args)
+    _logger.info("Script ends here")
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m sandhole.skeleton 42
+    #
+    run()
