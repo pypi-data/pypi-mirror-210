f34de31c6e8e02cb9853b5fac3bd7e440c673ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:29 +0000

    Add package cq23/1.3.0/cq23-1.3.0-py3-none-any.whl

---
+++ packages/cq23/cq23-1.3.0-py3-none-any.whl/main/command.py
@@ -0,0 +1,38 @@
+import sys
+
+from check.command import check
+from cleanup.command import cleanup
+from run_game.command import run_game
+from zip.command import zip
+
+from .utils import restore_cwd
+
+
+def help_message():
+    message = (
+        "Available commands:\n\n"
+        + "> cq23 run\n"
+        + "> cq23 run map=<map name>\n"
+        + "> cq23 zip\n"
+        + "> cq23 check\n"
+        + "> cq23 cleanup\n\n"
+        + "If you need help with the competition, post a message in Discord or email us at info@codequest.club."
+    )
+    print(message)
+
+
+@restore_cwd
+def route_command():
+    command_args = sys.argv[1:]
+
+    first_arg_mapping = {
+        "run": run_game,
+        "cleanup": cleanup,
+        "zip": zip,
+        "check": check,
+    }
+
+    if not command_args or command_args[0].lower() not in first_arg_mapping.keys():
+        help_message()
+    else:
+        first_arg_mapping[command_args[0].lower()](*command_args[1:])
