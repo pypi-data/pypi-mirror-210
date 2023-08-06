24a3b677dd0e9857a6f2392157a22d6e53e0589f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:43 +0000

    Add package physrisk-lib/0.12.0/physrisk_lib-0.12.0-py3-none-any.whl

---
+++ packages/physrisk-lib/physrisk_lib-0.12.0-py3-none-any.whl/physrisk/kernel/calculation.py
@@ -0,0 +1,168 @@
+import logging
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+
+from physrisk.data.inventory import Inventory
+from physrisk.kernel import hazards
+from physrisk.kernel.exposure import Category, DataRequester, ExposureMeasure
+
+from ..data.hazard_data_provider import (
+    SourcePath,
+    get_source_path_generic,
+    get_source_path_osc_chronic_heat,
+    get_source_path_wri_coastal_inundation,
+    get_source_path_wri_riverine_inundation,
+)
+from ..data.pregenerated_hazard_model import ZarrHazardModel
+from ..models import power_generating_asset_models as pgam
+from ..models.chronic_heat_models import ChronicHeatGznModel
+from ..models.real_estate_models import RealEstateCoastalInundationModel, RealEstateRiverineInundationModel
+from ..utils.helpers import get_iterable
+from .assets import Asset, IndustrialActivity, PowerGeneratingAsset, RealEstateAsset, TestAsset
+from .hazard_event_distrib import HazardEventDistrib
+from .hazard_model import HazardDataRequest, HazardDataResponse, HazardModel
+from .hazards import ChronicHeat, CoastalInundation, RiverineInundation
+from .impact_distrib import ImpactDistrib
+from .vulnerability_distrib import VulnerabilityDistrib
+from .vulnerability_model import VulnerabilityModelAcuteBase, VulnerabilityModelBase
+
+
+@dataclass
+class AssetExposureResult:
+    hazard_categories: Dict[type, Tuple[Category, float]]
+
+
+class AssetImpactResult:
+    def __init__(
+        self,
+        impact: ImpactDistrib,
+        vulnerability: Optional[VulnerabilityDistrib] = None,
+        event: Optional[HazardEventDistrib] = None,
+        hazard_data: Optional[Iterable[HazardDataResponse]] = None,
+    ):
+        self.impact = impact
+        # optional detailed results for drill-down
+        self.hazard_data = hazard_data
+        self.vulnerability = vulnerability
+        self.event = event
+
+
+def get_default_zarr_source_paths():
+    return {
+        RiverineInundation: get_source_path_wri_riverine_inundation,
+        CoastalInundation: get_source_path_wri_coastal_inundation,
+        ChronicHeat: get_source_path_osc_chronic_heat,
+    }
+
+
+def get_source_paths_from_inventory(inventory: Inventory, embedded: Optional[Dict[type, SourcePath]] = None):
+    source_paths: Dict[type, SourcePath] = {}
+    for key, resource in inventory.resources.items():
+        source_paths[hazards.hazard_class(resource.type)] = get_source_path_generic(inventory, resource.type, embedded)
+    return source_paths
+
+
+def get_default_hazard_model():
+    # Model that gets hazard event data from Zarr storage
+    return ZarrHazardModel(get_default_zarr_source_paths())
+
+
+def get_default_vulnerability_models():
+    """Get default exposure/vulnerability models for different asset types."""
+    return {
+        PowerGeneratingAsset: [pgam.InundationModel()],
+        RealEstateAsset: [RealEstateCoastalInundationModel(), RealEstateRiverineInundationModel()],
+        IndustrialActivity: [ChronicHeatGznModel()],
+        TestAsset: [pgam.TemperatureModel()],
+    }
+
+
+def calculate_exposures(
+    assets: List[Asset], hazard_model: HazardModel, exposure_measure: ExposureMeasure, scenario: str, year: int
+) -> Dict[Asset, AssetExposureResult]:
+    requester_assets: Dict[DataRequester, List[Asset]] = {exposure_measure: assets}
+    assetRequests, responses = _request_consolidated(hazard_model, requester_assets, scenario, year)
+
+    logging.info(
+        "Applying exposure measure {0} to {1} assets of type {2}".format(
+            type(exposure_measure).__name__, len(assets), type(assets[0]).__name__
+        )
+    )
+    result: Dict[Asset, AssetExposureResult] = {}
+
+    for asset in assets:
+        requests = assetRequests[(exposure_measure, asset)]  # (ordered) requests for a given asset
+        hazard_data = [responses[req] for req in get_iterable(requests)]
+        result[asset] = AssetExposureResult(hazard_categories=exposure_measure.get_exposures(asset, hazard_data))
+
+    return result
+
+
+def calculate_impacts(  # noqa: C901
+    assets: Iterable[Asset],
+    hazard_model: Optional[HazardModel] = None,
+    vulnerability_models: Optional[Any] = None,  #: Optional[Dict[type, Sequence[VulnerabilityModelBase]]] = None,
+    *,
+    scenario: str,
+    year: int,
+) -> Dict[Tuple[Asset, type], AssetImpactResult]:
+    """Calculate asset level impacts."""
+
+    if hazard_model is None:
+        hazard_model = get_default_hazard_model()
+    # the models that apply to asset of a particular type
+    if vulnerability_models is None:
+        vulnerability_models = get_default_vulnerability_models()
+    model_assets: Dict[DataRequester, List[Asset]] = defaultdict(
+        list
+    )  # list of assets to be modelled using vulnerability model
+
+    for asset in assets:
+        asset_type = type(asset)
+        mappings = vulnerability_models[asset_type]
+        for m in mappings:
+            model_assets[m].append(asset)
+    results = {}
+    asset_requests, responses = _request_consolidated(hazard_model, model_assets, scenario, year)
+
+    logging.info("Calculating impacts")
+    for model, assets in model_assets.items():
+        logging.info(
+            "Applying model {0} to {1} assets of type {2}".format(
+                type(model).__name__, len(assets), type(assets[0]).__name__
+            )
+        )
+        for asset in assets:
+            requests = asset_requests[(model, asset)]
+            hazard_data = [responses[req] for req in get_iterable(requests)]
+            if isinstance(model, VulnerabilityModelAcuteBase):
+                impact, vul, event = model.get_impact_details(asset, hazard_data)
+                results[(asset, model.hazard_type)] = AssetImpactResult(
+                    impact, vulnerability=vul, event=event, hazard_data=hazard_data
+                )
+            elif isinstance(model, VulnerabilityModelBase):
+                impact = model.get_impact(asset, hazard_data)
+                results[(asset, model.hazard_type)] = AssetImpactResult(impact, hazard_data=hazard_data)
+    return results
+
+
+def _request_consolidated(
+    hazard_model: HazardModel, requester_assets: Dict[DataRequester, List[Asset]], scenario: str, year: int
+):
+    """As an important performance optimization, data requests are consolidated for all requesters
+    (e.g. vulnerability mode) because different requesters may query the same hazard data sets
+    note that key for a single request is (requester, asset).
+    """
+    # the list of requests for each requester and asset
+    asset_requests: Dict[Tuple[DataRequester, Asset], Union[HazardDataRequest, Iterable[HazardDataRequest]]] = {}
+
+    logging.info("Generating hazard data requests for requesters")
+    for requester, assets in requester_assets.items():
+        for asset in assets:
+            asset_requests[(requester, asset)] = requester.get_data_requests(asset, scenario=scenario, year=year)
+
+    logging.info("Retrieving hazard data")
+    flattened_requests = [req for requests in asset_requests.values() for req in get_iterable(requests)]
+    responses = hazard_model.get_hazard_events(flattened_requests)
+    return asset_requests, responses
---
+++ packages/physrisk-lib/physrisk_lib-0.12.0-py3-none-any.whl/physrisk/models/chronic_heat_models.py
@@ -0,0 +1,128 @@
+from typing import Iterable, Union
+
+import numpy as np
+from scipy.stats import norm
+
+from physrisk.kernel.assets import Asset
+from physrisk.kernel.hazard_model import HazardDataRequest, HazardDataResponse, HazardParameterDataResponse
+from physrisk.kernel.hazards import ChronicHeat
+from physrisk.kernel.impact_distrib import ImpactDistrib, ImpactType
+from physrisk.kernel.vulnerability_model import VulnerabilityModelBase
+
+
+class ChronicHeatGznModel(VulnerabilityModelBase):
+    """Model which estiamtes the labour productivity impact based on chronic heat based on the paper "Neidell M,
+    Graff Zivin J, Sheahan M,  Willwerth J, Fant C, Sarofim M, et al. (2021) Temperature and work:
+    Time allocated to work under varying climate and labor market conditions."
+    Average annual work hours are based on USA values reported by the OECD for 2021."""
+
+    def __init__(self, model: str = "mean_degree_days_above_32c", delta=True):
+        super().__init__(model, ChronicHeat)  # opportunity to give a model hint, but blank here
+        self.time_lost_per_degree_day = 4.671  # This comes from the paper converted to celsius
+        self.time_lost_per_degree_day_se = 2.2302  # This comes from the paper converted to celsius
+        self.total_labour_hours = 107460
+        self.delta = delta
+
+    def get_data_requests(
+        self, asset: Asset, *, scenario: str, year: int
+    ) -> Union[HazardDataRequest, Iterable[HazardDataRequest]]:
+        """Request the hazard data needed by the vulnerability model for a specific asset
+        (this is a Google-style doc string)
+
+        Args:
+            asset: Asset for which data is requested.
+            scenario: Climate scenario of calculation.
+            year: Projection year of calculation.
+
+        Returns:
+            Single data requests.
+        """
+
+        # specify hazard data needed. Model string is hierarchical and '/' separated.
+        model = "mean_degree_days/above/32c"
+
+        return [
+            HazardDataRequest(
+                self.hazard_type,
+                asset.longitude,
+                asset.latitude,
+                scenario="historical",
+                year=1980,
+                model=model,
+            ),
+            HazardDataRequest(
+                self.hazard_type,
+                asset.longitude,
+                asset.latitude,
+                scenario=scenario,
+                year=year,
+                model=model,
+            ),
+        ]
+
+    def get_impact(self, asset: Asset, data_responses: Iterable[HazardDataResponse]) -> ImpactDistrib:
+        """Calcaulate impact (disruption) of asset based on the hazard data returned.
+
+        Args:
+            asset: Asset for which impact is calculated.
+            data_responses: responses to the hazard data requests generated in get_data_requests.
+
+        Returns:
+            Probability distribution of impacts.
+        """
+
+        baseline_dd_above_mean, scenario_dd_above_mean = data_responses
+
+        assert isinstance(baseline_dd_above_mean, HazardParameterDataResponse)
+        assert isinstance(scenario_dd_above_mean, HazardParameterDataResponse)
+
+        delta_dd_above_mean: float = scenario_dd_above_mean.parameter - baseline_dd_above_mean.parameter * self.delta
+
+        hours_worked = self.total_labour_hours
+        fraction_loss_mean = (delta_dd_above_mean * self.time_lost_per_degree_day) / hours_worked
+        fraction_loss_std = (delta_dd_above_mean * self.time_lost_per_degree_day_se) / hours_worked
+
+        return get_impact_distrib(fraction_loss_mean, fraction_loss_std, ChronicHeat, ImpactType.disruption)
+
+
+def get_impact_distrib(
+    fraction_loss_mean: float, fraction_loss_std: float, hazard_type: type, impact_type: ImpactType
+) -> ImpactDistrib:
+    """Calculate impact (disruption) of asset based on the hazard data returned.
+
+    Args:
+        fraction_loss_mean: mean of the impact distribution
+        fraction_loss_std: standard deviation of the impact distribution
+        hazard_type: Hazard Type.
+        impact_type: Impact Type.
+
+    Returns:
+        Probability distribution of impacts.
+    """
+    impact_bins = np.concatenate(
+        [
+            np.linspace(-0.001, 0.001, 1, endpoint=False),
+            np.linspace(0.001, 0.01, 9, endpoint=False),
+            np.linspace(0.01, 0.1, 10, endpoint=False),
+            np.linspace(0.1, 0.999, 10, endpoint=False),
+            np.linspace(0.999, 1.001, 2),
+        ]
+    )
+
+    probs_cumulative = np.vectorize(lambda x: norm.cdf(x, loc=fraction_loss_mean, scale=max(1e-12, fraction_loss_std)))(
+        impact_bins
+    )
+    probs_cumulative[-1] = np.maximum(probs_cumulative[-1], 1.0)
+    probs = np.diff(probs_cumulative)
+
+    probs_norm = np.sum(probs)
+    prob_differential = 1 - probs_norm
+    if probs_norm < 1e-8:
+        if fraction_loss_mean <= 0.0:
+            probs = np.concatenate((np.array([1.0]), np.zeros(len(impact_bins) - 2)))
+        elif fraction_loss_mean >= 1.0:
+            probs = np.concatenate((np.zeros(len(impact_bins) - 2), np.array([1.0])))
+    else:
+        probs[0] = probs[0] + prob_differential
+
+    return ImpactDistrib(hazard_type, impact_bins, probs, impact_type)
