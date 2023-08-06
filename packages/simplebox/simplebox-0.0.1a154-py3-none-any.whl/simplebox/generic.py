5b2e585cf47bb33b41720c53a70c5a1c9118c086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:24 +0000

    Add package simplebox/0.0.1a154/simplebox-0.0.1a154-py3-none-any.whl

---
+++ packages/simplebox/simplebox-0.0.1a154-py3-none-any.whl/simplebox/generic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from typing import TypeVar
+
+C = TypeVar("C")
+D = TypeVar("D")
+T = TypeVar("T")
+R = TypeVar("R")
+K = TypeVar('K')
+V = TypeVar('V')
+U = TypeVar('U')
+
+__all__ = [C, D, T, R, K, V, U]
