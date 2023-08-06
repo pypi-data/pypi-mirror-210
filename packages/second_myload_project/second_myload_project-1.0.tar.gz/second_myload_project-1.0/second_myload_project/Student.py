807708d140696b54b7b07a76ee3daf98a867db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:17 +0000

    Add package second_myload_project/1.0/second_myload_project-1.0.tar.gz

---
+++ packages/second_myload_project/second_myload_project-1.0.tar.gz/second_myload_project-1.0/second_myload_project/Student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def __str__(self):
+        return f"{self.__name}:{self.age}"
---
+++ packages/second_myload_project/second_myload_project-1.0.tar.gz/second_myload_project-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='second_myload_project',  # 发布后的资源名
+    version='1.0',  # 版本
+    description='测试',    # 资源信息
+    author='黄德俊',            # 作者
+    author_email='biantaihuang@qq.com', # 作者邮箱
+    py_modules=['second_myload_project.Student'] # 要发布的模块
+)
