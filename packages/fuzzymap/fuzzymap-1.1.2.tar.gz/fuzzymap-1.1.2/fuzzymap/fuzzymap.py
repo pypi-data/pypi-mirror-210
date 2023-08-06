b0e5b923b5af221693754aa8d7a630b26e4a27fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:06 +0000

    Add package fuzzymap/1.1.2/fuzzymap-1.1.2.tar.gz

---
+++ packages/fuzzymap/fuzzymap-1.1.2.tar.gz/fuzzymap-1.1.2/fuzzymap/fuzzymap.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2022  Artyom Vancyan
+# See full copyright notice at __init__.py
+from fuzzywuzzy import fuzz
+
+
+class FuzzyMap(dict):
+    """
+    FuzzyMap implements a subtype of `dict` with customized descriptors.
+
+    This kind of dictionary returns the value of the exact key  if there is
+    such a key. Otherwise, it will return the value of the most similar key
+    satisfying the given ratio. The same mechanism works when setting a new
+    or replacing an old key in the dictionary.  If the key is not found and
+    does  not  match  any of the keys by the given ratio, it returns `None`.
+    """
+
+    # set the minimum percent of the
+    # diff between the compared keys
+    ratio = 60
+
+    def _closest_key(self, key):
+        """Returns the closest key matched by the given ratio"""
+
+        if len(self):
+            # Calculate matching coefficient of each key via fuzz.ratio
+            coefficients = {k: fuzz.ratio(k, key) for k in self.keys()}
+            matching = max(coefficients, key=lambda k: coefficients[k])
+            if coefficients[matching] >= self.ratio:
+                return matching
+        return key
+
+    def get(self, key, default=None):
+        """Invokes the __getitem__ to keep the `FuzzyMap` behavior"""
+
+        return self[key] or default
+
+    def __missing__(self, key):
+        return super().get(self._closest_key(key))
+
+    def __setitem__(self, key, value):
+        super().__setitem__(self._closest_key(key), value)
---
+++ packages/fuzzymap/fuzzymap-1.1.2.tar.gz/fuzzymap-1.1.2/setup.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2022  Artyom Vancyan
+# See full copyright notice at __init__.py
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
