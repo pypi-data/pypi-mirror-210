4be1019083aaec557b41da1d1303d1c535d22d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:26 +0000

    Add package gcamreader/1.2.7/gcamreader-1.2.7.tar.gz

---
+++ packages/gcamreader/gcamreader-1.2.7.tar.gz/gcamreader-1.2.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    """Return the contents of the project README file."""
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="gcamreader",
+    version="1.2.7",
+    python_requires=">=3.6",
+    packages=find_packages(),
+    description="Tools for importing GCAM output data",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JGCRI/gcamreader",
+    license='BSD 2-Clause',
+    author="Robert Link",
+    author_email="robert.link@pnnl.gov",
+    install_requires=[
+        "requests~=2.20.0",
+        "pandas>=1.2.4",
+        "lxml>=4.6.3"
+    ],
+    include_package_data=True,
+    zip_safe=False
+    )
