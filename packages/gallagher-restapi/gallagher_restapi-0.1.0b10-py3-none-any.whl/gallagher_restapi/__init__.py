c3fde94a80a9e809fb32a41b6b8bdf3cacc8bb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:12 +0000

    Add package gallagher-restapi/0.1.0b10/gallagher_restapi-0.1.0b10-py3-none-any.whl

---
+++ packages/gallagher-restapi/gallagher_restapi-0.1.0b10-py3-none-any.whl/gallagher_restapi/__init__.py
@@ -0,0 +1,18 @@
+"""Gallagher REST api library."""
+
+from .client import Client
+from .exceptions import ConnectError, GllApiError, Unauthorized
+from .models import EventFilter, FTAlarm, FTCardholder, FTEvent
+
+MOVEMENT_EVENT_TYPES = ["20001", "20002", "20003", "20047", "20107", "42415"]
+
+__all__ = [
+    "Client",
+    "EventFilter",
+    "FTAlarm",
+    "FTCardholder",
+    "FTEvent",
+    "ConnectError",
+    "GllApiError",
+    "Unauthorized",
+]
