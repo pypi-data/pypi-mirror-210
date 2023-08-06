5ba01363ff83f7d0218a862a06fa99d536cd5690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:58 +0000

    Add package unyts/0.5.28/unyts-0.5.28.tar.gz

---
+++ packages/unyts/unyts-0.5.28.tar.gz/unyts-0.5.28/src/unyts/units/force.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 14:34:59 2020
+
+@author: Martín Carlos Araya <martinaraya@gmail.com>
+"""
+
+__version__ = '0.5.4'
+__release__ = 20230121
+__all__ = ['Force', 'Pressure', 'Weight', 'Compressibility', 'Viscosity']
+
+from ..dictionaries import dictionary as _dictionary
+from ..unit_class import Unit
+from ..helpers.common_classes import unit_or_str, numeric
+
+
+class Force(Unit):
+    classUnits = _dictionary['Force']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'force'
+        self.kind = Force
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Pressure(Unit):
+    classUnits = _dictionary['Pressure']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'pressure'
+        self.kind = Pressure
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Weight(Unit):
+    classUnits = _dictionary['Weight']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'weight'
+        self.kind = Weight
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Compressibility(Unit):
+    classUnits = _dictionary['Compressibility']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'compressibility'
+        self.kind = Compressibility
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Viscosity(Unit):
+    classUnits = _dictionary['Viscosity']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'viscosity'
+        self.kind = Viscosity
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
---
+++ packages/unyts/unyts-0.5.28.tar.gz/unyts-0.5.28/src/unyts/units/geometry.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 14:34:59 2020
+
+@author: Martín Carlos Araya <martinaraya@gmail.com>
+"""
+
+__version__ = '0.5.3'
+__release__ = 20230118
+__all__ = ['Length', 'Area', 'Volume', 'Permeability']
+
+from ..dictionaries import dictionary as _dictionary
+from ..unit_class import Unit
+from ..helpers.common_classes import unit_or_str, numeric
+
+
+class Length(Unit):
+    classUnits = _dictionary['Length']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'length'
+        self.kind = Length
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Area(Unit):
+    classUnits = _dictionary['Area']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'area'
+        self.kind = Area
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Volume(Unit):
+    classUnits = _dictionary['Volume']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'volume'
+        self.kind = Volume
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+
+class Permeability(Unit):
+    classUnits = _dictionary['Permeability']
+
+    def __init__(self, value: numeric, units: unit_or_str):
+        self.name = 'permeability'
+        self.kind = Permeability
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
