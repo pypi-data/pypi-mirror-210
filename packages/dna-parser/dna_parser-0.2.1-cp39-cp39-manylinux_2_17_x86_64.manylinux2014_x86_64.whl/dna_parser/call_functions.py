c2005d88099c03778345b8badefa7e553e670fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:25 +0000

    Add package dna-parser/0.2.1/dna_parser-0.2.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/dna-parser/dna_parser-0.2.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/dna_parser/call_functions.py
@@ -0,0 +1,21 @@
+from .dna_parser import *
+
+
+def onehot_encoding(sequences, pad_type= "after", pad_length= 0, n_jobs= 1):
+
+    return onehot_encoding_rust(sequences, pad_type, pad_length, n_jobs)
+
+
+
+def ordinal_encoding(sequences, pad_type= "after", pad_length= 0, n_jobs= 1):
+
+    return ordinal_encoding_rust(sequences, pad_type, pad_length, n_jobs)
+
+def cross_encoding(sequences, pad_type= "after", pad_length= 0, n_jobs= 1):
+    
+    return cross_encoding_rust(sequences, pad_type, pad_length, n_jobs)
+
+
+def random_seq(length, nb_of_seq, seq_type= "dna", n_jobs= 1):
+
+    return random_seq_rust(length, nb_of_seq, seq_type, n_jobs)
