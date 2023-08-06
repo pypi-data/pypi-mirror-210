60ce5261d816a234d796d39bb715d5eab372ed0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:20 +0000

    Add package misuma/0.6/misuma-0.6.tar.gz

---
+++ packages/misuma/misuma-0.6.tar.gz/misuma-0.6/setup.py
@@ -0,0 +1,59 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # ######################################################################
+    # BASIC DESCRIPTION
+    # ######################################################################
+    name='misuma',
+    author='Bang1329',
+    author_email='santiago.hg2292@gmail.com',
+    description='Sum two numbers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://pypi.org/project/misuma',
+    keywords='Sum Addition',
+    license='MIT',
+
+    # ######################################################################
+    # CLASSIFIER
+    # ######################################################################
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    version='0.6',
+
+    # ######################################################################
+    # FILES
+    # ######################################################################
+    package_dir={'': '.'},
+    packages=setuptools.find_packages(where='.'),
+    
+    # ######################################################################
+    # ENTRY POINTS
+    # ######################################################################
+    entry_points={
+        'console_scripts': ['install=pryngles.install:main'],
+    },
+
+    # ######################################################################
+    # TESTS
+    # ######################################################################
+    test_suite='nose.collector',
+    tests_require=['nose'],
+
+    # ######################################################################
+    # DEPENDENCIES
+    # ######################################################################
+    install_requires=['scipy','ipython','matplotlib','tqdm'],
+
+    # ######################################################################
+    # OPTIONS
+    # ######################################################################
+    include_package_data=True,
+    package_data={'': ['data/*.*', 'tests/*.*']},
+)
