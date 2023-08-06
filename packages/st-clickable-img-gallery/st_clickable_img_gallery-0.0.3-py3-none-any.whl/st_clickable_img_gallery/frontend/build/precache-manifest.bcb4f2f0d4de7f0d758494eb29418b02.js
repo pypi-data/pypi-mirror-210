bd4d4f28f7c9af38adc6aa1eb84e3fd5bdb27aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:26 +0000

    Add package st-clickable-img-gallery/0.0.3/st_clickable_img_gallery-0.0.3-py3-none-any.whl

---
+++ packages/st-clickable-img-gallery/st_clickable_img_gallery-0.0.3-py3-none-any.whl/st_clickable_img_gallery/frontend/build/precache-manifest.bcb4f2f0d4de7f0d758494eb29418b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f382ae66dc20830101142f1020a5ae38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1a22922c185398fcc72",
+    "url": "./static/js/2.60f28383.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.60f28383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb06ac4342aee78cb3a",
+    "url": "./static/js/main.b6cd0f1f.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
