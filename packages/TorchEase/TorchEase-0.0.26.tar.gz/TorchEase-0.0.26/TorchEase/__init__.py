6d9e12dd005b3c48812b36f75100a2bfa3fb74f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:10 +0000

    Add package TorchEase/0.0.26/TorchEase-0.0.26.tar.gz

---
+++ packages/TorchEase/TorchEase-0.0.26.tar.gz/TorchEase-0.0.26/TorchEase/__init__.py
@@ -0,0 +1,12 @@
+from .trainer import Trainer
+from .utilities import EarlyStopper
+from .fuser import ModelFuser
+from .user import User
+from .comparer import Comparer
+
+
+__all__ = ["Trainer",
+           "EarlyStopper",
+           "ModelFuser",
+           "User",
+           "Comparer"]
