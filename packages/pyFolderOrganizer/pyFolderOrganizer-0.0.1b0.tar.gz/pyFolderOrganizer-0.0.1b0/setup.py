13ba610e2589d585e5ae2a9210779ae3885cce0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:24 +0000

    Add package pyFolderOrganizer/0.0.1b0/pyFolderOrganizer-0.0.1b0.tar.gz

---
+++ packages/pyFolderOrganizer/pyFolderOrganizer-0.0.1b0.tar.gz/pyFolderOrganizer-0.0.1b0/setup.py
@@ -0,0 +1,32 @@
+# from pyFolderOrganizer import constants
+from setuptools import setup
+
+setup(
+    name='pyFolderOrganizer',
+    version= '0.0.1b',
+    description='Python package to organize a folder in sub-directories based on file types.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/kotsiossp97/pyFolderOrganizer",
+    project_urls={  'Bug Tracker': 'https://github.com/kotsiossp97/pyFolderOrganizer/issues',
+                    'Documentation': "https://pyfolderorganizer.readthedocs.io/en/latest/index.html",
+                    "Source Code": "https://github.com/kotsiossp97/pyFolderOrganizer"
+    },
+    author="Konstantinos Andreou",
+    author_email="kotsiossp@gmail.com",
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        "Topic :: Utilities",
+        'Operating System :: Microsoft',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: Microsoft :: Windows :: Windows 11'
+    ],
+    install_requires=["win11toast", "watchdog"],
+    py_modules=['pyFolderOrganizer']
+)
+
+
+#python -m build
+#python -m twine upload dist/*
