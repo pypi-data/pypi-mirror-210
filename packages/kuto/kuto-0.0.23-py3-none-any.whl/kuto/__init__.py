10aef5ff7bd88432b877997de6dcced253cfc0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:13 +0000

    Add package kuto/0.0.23/kuto-0.0.23-py3-none-any.whl

---
+++ packages/kuto/kuto-0.0.23-py3-none-any.whl/kuto/__init__.py
@@ -0,0 +1,16 @@
+from kuto.core.api.request import HttpReq
+from kuto.core.android.element import AdrElem
+from kuto.core.android.driver import AndroidDriver
+from kuto.core.ios.element import IosElem
+from kuto.core.ios.driver import IosDriver
+from kuto.core.web.element import WebElem, FraElem
+from kuto.core.web.driver import PlayWrightDriver
+from kuto.case import TestCase, Page
+from kuto.running.runner import main
+from kuto.utils.config import config
+from kuto.utils.decorate import data
+from kuto.utils.log import logger
+
+
+__version__ = "0.0.23"
+__description__ = "移动、web、接口自动化测试框架"
