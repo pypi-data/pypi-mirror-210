f4251ea54352ea2ac9c1ea80c0345ffc2a83d262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:45 +0000

    Add package diffsims/0.5.2/diffsims-0.5.2-py3-none-any.whl

---
+++ packages/diffsims/diffsims-0.5.2-py3-none-any.whl/diffsims/release_info.py
@@ -0,0 +1,30 @@
+name = "diffsims"
+version = "0.5.2"
+author = "Duncan Johnstone, Phillip Crout"
+copyright = "Copyright 2017-2023, The diffsims developers"
+# Initial committer first, then listed by line additions
+credits = [
+    "Duncan Johnstone",
+    "Phillip Crout",
+    "Håkon Wiik Ånes",
+    "Eric Prestat",
+    "Rob Tovey",
+    "Simon Høgås",
+    "Ben Martineau",
+    "Niels Cautaerts",
+    "Joonatan Laulainen",
+    "Isabel Wood",
+    "Sean Collins",
+    "Stef Smeets",
+    "Alex Borrelli",
+    "Tiarnan Doherty",
+    "Jedrzej Morzy",
+    "Endre Jacobsen",
+    "Tina Bergh",
+    "Tomas Ostasevicius",
+    "Eirik Opheim",
+]
+license = "GPLv3+"
+maintainer = "Duncan Johnstone, Phillip Crout, Håkon Wiik Ånes"
+email = "pyxem.team@gmail.com"
+status = "Development"
