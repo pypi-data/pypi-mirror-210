f4b9962c9cec9aebd7bf1647c20b4e146f1276c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:09 +0000

    Add package noise2read/0.1.3/noise2read-0.1.3-py3-none-any.whl

---
+++ packages/noise2read/noise2read-0.1.3-py3-none-any.whl/noise2read/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Pengyao Ping
+# @Date:   2023-01-24 15:47:23
+# @Last Modified by:   Pengyao Ping
+# @Last Modified time: 2023-05-25 13:20:22
+
+__version__ = "0.1.3"
