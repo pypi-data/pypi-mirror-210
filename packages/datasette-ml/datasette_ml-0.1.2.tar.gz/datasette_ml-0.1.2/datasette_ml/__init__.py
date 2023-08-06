0859c1eb334e203b5b7a412bc7185d1d2bf67ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:20 +0000

    Add package datasette-ml/0.1.2/datasette_ml-0.1.2.tar.gz

---
+++ packages/datasette-ml/datasette_ml-0.1.2.tar.gz/datasette_ml-0.1.2/datasette_ml/__init__.py
@@ -0,0 +1,29 @@
+import sqlite3
+import typing as t
+
+from datasette import hookimpl
+from datasette.database import Database
+
+from sqlite_ml.sqml import SQML
+
+
+if t.TYPE_CHECKING:  # pragma: no cover
+    from datasette.app import Datasette
+
+
+sqml = SQML()
+
+
+@hookimpl
+def startup(datasette: "Datasette") -> None:
+    config = datasette.plugin_config("datasette-ml") or {}
+    db_name = config.get("db", "sqml")
+    db: Database = datasette.get_database(db_name)
+    sqml.setup_schema(db.connect(True))
+
+
+@hookimpl
+def prepare_connection(
+    conn: sqlite3.Connection, database: str, datasette: "Datasette"
+) -> None:
+    sqml.register_functions(conn)
