0778a25dff6e3f47aa32121792d5aa2e61b3a8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:31 +0000

    Add package django-simple-notes/0.0.2/django_simple_notes-0.0.2-py3-none-any.whl

---
+++ packages/django-simple-notes/django_simple_notes-0.0.2-py3-none-any.whl/django_simple_notes/conf/settings.py
@@ -0,0 +1,21 @@
+# Django
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+SIMPLE_NOTES_CUSTOM_CONTENT_TYPE_AND_ID = getattr(
+    settings, "SIMPLE_NOTES_CUSTOM_CONTENT_TYPE_AND_ID", None
+)
+
+SIMPLE_NOTES_ADMIN_URLS = getattr(settings, "SIMPLE_NOTES_ADMIN_URLS", None)
+
+SIMPLE_NOTES_TOOLBAR_MENU_TEXT = getattr(
+    settings, "SIMPLE_NOTES_TOOLBAR_MENU_TEXT", _("Simple notes")
+)
+
+SIMPLE_NOTES_TOOLBAR_EDIT_TEXT = getattr(
+    settings, "SIMPLE_NOTES_TOOLBAR_EDIT_TEXT", _("Edit note for current page")
+)
+
+SIMPLE_NOTES_TOOLBAR_LIST_TEXT = getattr(
+    settings, "SIMPLE_NOTES_TOOLBAR_LIST_TEXT", _("Notes list")
+)
