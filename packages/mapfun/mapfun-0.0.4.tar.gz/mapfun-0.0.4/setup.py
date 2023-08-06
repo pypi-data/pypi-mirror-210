007e7113a13c3acc7515b20b3b3cd5b2c2bd03db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:51 +0000

    Add package mapfun/0.0.4/mapfun-0.0.4.tar.gz

---
+++ packages/mapfun/mapfun-0.0.4.tar.gz/mapfun-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from pathlib import Path
+VERSION = '0.0.4'
+parent = Path(__file__).parent
+setup(
+    name="mapfun",
+    version=VERSION,
+    author="Zakaria Elalaoui",
+    description="mapfun",
+    long_description_content_type="text/markdown",
+    #long_description=(parent / "README.md").read_text(),
+    packages = ['mapfun'],
+    keywords=['zikojs','python', 'map'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
