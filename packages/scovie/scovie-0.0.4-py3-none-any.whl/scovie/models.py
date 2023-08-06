bbce46ce7ee27ab0af005bba65d575ce61d65777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:01 +0000

    Add package scovie/0.0.4/scovie-0.0.4-py3-none-any.whl

---
+++ packages/scovie/scovie-0.0.4-py3-none-any.whl/scovie/models.py
@@ -0,0 +1,124 @@
+import datetime
+import os
+import re
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from solo.models import SingletonModel
+
+
+# Functions
+def format_filename(instance, filename):
+    date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    name, extension = filename.rsplit('.', 1)
+    name = re.sub(r'\W+', '', name)
+    return os.path.join('static/uploads/', f"{date}-{name}.{extension}")
+
+
+# Models
+class Announcement(models.Model):
+    class Meta:
+        verbose_name = _("announcement")
+        ordering = ['type']
+
+    type = models.CharField(max_length=20, unique=True, verbose_name=_("type"))
+    message = models.TextField(verbose_name=_("message"))
+
+    def __str__(self):
+        return self.type + " : " + self.message
+
+
+class Room(models.Model):
+    class Meta:
+        verbose_name = _("room")
+        ordering = ['name']
+
+    name = models.CharField(max_length=15, verbose_name=_("room"))
+
+    @classmethod
+    def get_occupied_rooms(cls):
+        return cls.objects.filter(planning__teacher__isnull=False).distinct()
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    class Meta:
+        verbose_name = _("teacher")
+        ordering = ['name']
+
+    name = models.CharField(max_length=50, verbose_name=_("teacher"))
+
+    def __str__(self):
+        return self.name
+
+
+class Time(models.Model):
+    class Meta:
+        verbose_name = _("time")
+        ordering = ['rank']
+
+    name = models.CharField(max_length=5, verbose_name=_("time"))
+    rank = models.IntegerField(verbose_name=_("display order"))
+
+    def __str__(self):
+        return self.name
+
+
+class Planning(models.Model):
+    class Meta:
+        verbose_name = _("planning")
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name=_("room"))
+    time = models.ForeignKey(Time, on_delete=models.CASCADE, verbose_name=_("time"))
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name=_("teacher"))
+
+    def __str__(self):
+        return _('Planning for {} in {} with {}').format(
+            self.time.name, self.room.name, self.teacher.name
+        )
+
+
+class Configuration(SingletonModel):
+    class Meta:
+        verbose_name = _("parameters")
+
+    enterprise_name = models.CharField(max_length=50, verbose_name=_("enterprise name"))
+
+    emergency_mode = models.BooleanField(default=False, verbose_name=_("emergency mode"))
+    emergency_title = models.CharField(max_length=50, verbose_name=_("emergency title"))
+    emergency_subtitle = models.CharField(max_length=50, verbose_name=_("emergency subtitle"))
+
+    carnival_day = models.BooleanField(default=False, verbose_name=_("carnival day"))
+    carnival_date = models.DateField(
+        default=datetime.date(
+            2023,
+            2,
+            21,
+        ),
+        verbose_name=_("carnival date"),
+    )
+
+    valentine_day = models.BooleanField(default=False, verbose_name=_("valentine day"))
+    valentine_date = models.DateField(
+        default=datetime.date(2023, 2, 14), verbose_name=_("valentine date")
+    )
+
+    def __str__(self):
+        return str(_("Application configuration"))
+
+
+class Media(models.Model):
+    class Meta:
+        verbose_name = _("media")
+
+    name = models.CharField(max_length=30, verbose_name=_("name"))
+    image = models.ImageField(upload_to=format_filename, verbose_name=_("image"))
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return _("Image: {}").format(self.name)
---
+++ packages/scovie/scovie-0.0.4-py3-none-any.whl/scovie/views.py
@@ -0,0 +1,57 @@
+import datetime
+
+from django.shortcuts import render
+
+from scovie.models import Announcement, Configuration, Media, Planning, Room, Time
+
+
+def index(request):
+    # Get configuration
+    config = Configuration.get_solo()
+    # Create a context
+    context = {}
+    # Get emergency status from configuration
+    emergency_mode = config.emergency_mode
+    # If emergency status is True
+    if emergency_mode:
+        # Force screen mode to emergency
+        screen_mode = 'emergency'
+        # Clean screen modes with only emergency
+        screen_modes = ['emergency']
+        mobile_screen_modes = ['emergency']
+        # Emergency context
+        emergency_context = {
+            'emergency_title': config.emergency_title,
+            'emergency_subtitle': config.emergency_subtitle,
+        }
+        # Update context with emergency context
+        context.update(emergency_context)
+    else:
+        # Get screen mode from GET request, default to all
+        screen_mode = request.GET.get('screen_mode', 'all')
+        # Available screen modes
+        screen_modes = ['all', 'informations', 'caroussel']
+        mobile_screen_modes = ['informations']
+        # Saint-Valentine day feature
+        if config.valentine_day and datetime.date.today() == config.valentine_date:
+            screen_modes.append('valentine')
+
+        # Carnival day feature
+        if config.carnival_day and datetime.date.today() == config.carnival_date:
+            screen_modes.append('carnival')
+
+    temp = {
+        'enterprise_name': config.enterprise_name,
+        'planning': Planning.objects.all(),
+        'rooms': Room.objects.filter(planning__teacher__isnull=False).distinct(),
+        'announcements': Announcement.objects.all(),
+        'times': Time.objects.all(),
+        'medias': Media.objects.all(),
+        'mobile_screen_modes': mobile_screen_modes,
+        'screen_modes': screen_modes,
+        'screen_mode': screen_mode,
+    }
+
+    context.update(temp)
+
+    return render(request, 'index.html', context)
