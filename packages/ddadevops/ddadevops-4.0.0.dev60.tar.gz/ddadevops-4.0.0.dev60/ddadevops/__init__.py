85b42d0df3a31da5608385f0e7d46faf28b11209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:41 +0000

    Add package ddadevops/4.0.0.dev60/ddadevops-4.0.0.dev60.tar.gz

---
+++ packages/ddadevops/ddadevops-4.0.0.dev60.tar.gz/ddadevops-4.0.0.dev60/ddadevops/__init__.py
@@ -0,0 +1,17 @@
+"""
+ddadevops provide tools to support builds combining gopass,
+terraform, dda-pallet, aws & hetzner-cloud.
+
+"""
+
+from .provs_k3s_build import ProvsK3sBuild
+from .aws_mfa_mixin import AwsMfaMixin, add_aws_mfa_mixin_config
+from .c4k_build import C4kBuild
+from .hetzner_mixin import HetznerMixin, add_hetzner_mixin_config
+from .devops_image_build import DevopsImageBuild
+from .devops_terraform_build import DevopsTerraformBuild, create_devops_terraform_build_config
+from .devops_build import DevopsBuild, create_devops_build_config, get_devops_build
+from .credential import gopass_password_from_path, gopass_field_from_path
+from .release_mixin import ReleaseMixin
+
+__version__ = "${version}"
