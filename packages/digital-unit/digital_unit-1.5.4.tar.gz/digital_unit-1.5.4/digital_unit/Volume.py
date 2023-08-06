5f9f795209bd23e7d5e5b9f77fa4137b0b60380b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:06 +0000

    Add package digital-unit/1.5.4/digital_unit-1.5.4.tar.gz

---
+++ packages/digital-unit/digital_unit-1.5.4.tar.gz/digital_unit-1.5.4/digital_unit/Volume.py
@@ -0,0 +1,40 @@
+from .root import DigitalUnit
+
+class Volume(DigitalUnit):
+    def __init__(self) -> None:
+        self.type = 'Volume'
+
+class CubicMeter(Volume):
+    def __init__(self) -> None:
+        super().__init__()
+        self.measure = 1.0 
+    def __str__(self) -> str:
+        return 'm³'
+    
+class CubicCentimeter(Volume):
+    def __init__(self) -> None:
+        super().__init__()
+        self.measure = 1e-06
+    def __str__(self) -> str:
+        return 'cm³'
+
+class CubicDecimeter(Volume):
+    def __init__(self) -> None:
+        super().__init__()
+        self.measure = 0.001
+    def __str__(self) -> str:
+        return 'dm³'
+    
+class Liter(Volume):
+    def __init__(self) -> None:
+        super().__init__()
+        self.measure = 0.001
+    def __str__(self) -> str:
+        return 'L'
+    
+class MilliLiter(Volume):
+    def __init__(self) -> None:
+        super().__init__()
+        self.measure = 1e-06
+    def __str__(self) -> str:
+        return 'mL'
