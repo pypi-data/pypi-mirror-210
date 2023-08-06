fade77ccd7bd2e36f6222d45b5f8d59e2159548b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:29 +0000

    Add package genser/0.0.2/genser-0.0.2.tar.gz

---
+++ packages/genser/genser-0.0.2.tar.gz/genser-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(name="genser",
+	version="0.0.2",
+	author="Sergei Zuev",
+	author_email="shoukhov@mail.ru",
+	description="A set of functions for dataset dimension transformations",
+	packages=setuptools.find_packages(),
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Programming Language :: Python :: 3.10",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
+
