54e55a5ede9ddd0f4582fa6c9abffa3bddd2cad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:56 +0000

    Add package pyrosexmod/0.0.4/pyrosexmod-0.0.4.tar.gz

---
+++ packages/pyrosexmod/pyrosexmod-0.0.4.tar.gz/pyrosexmod-0.0.4/pyrosexmod/teledl.py
@@ -0,0 +1,71 @@
+import os
+import yt_dlp
+import asyncio
+from typing import Union
+
+
+class Teledl:
+    async def formats(self, link: str):
+        if "&" in link:
+            link = link.split("&")[0]
+        ytdl_opts = {"quiet": True}
+        ydl = yt_dlp.YoutubeDL(ytdl_opts)
+        with ydl:
+            formats_available = []
+            r = ydl.extract_info(link, download=False)
+            for format in r["formats"]:
+                try:
+                    str(format["format"])
+                except:
+                    continue
+                if not "dash" in str(format["format"]).lower():
+                    try:
+                        format["format"]
+                        format["filesize"]
+                        format["format_id"]
+                        format["ext"]
+                        format["format_note"]
+                    except:
+                        continue
+                    formats_available.append(
+                        {
+                            "format": format["format"],
+                            "filesize": format["filesize"],
+                            "format_id": format["format_id"],
+                            "ext": format["ext"],
+                            "format_note": format["format_note"],
+                            "url": link,
+                        }
+                    )
+        return formats_available, link
+    
+    
+    async def download(
+        self,
+        link: str,
+        format_id: Union[bool, str] = None,
+    ) -> str:
+        loop = asyncio.get_running_loop()
+    
+        def video_dl():
+            formats = f"{format_id}+140"
+            ydl_optssx = {
+                "format": formats,
+                "outtmpl": "downloads/%(title)s",
+                "geo_bypass": True,
+                "nocheckcertificate": True,
+                "quiet": True,
+                "no_warnings": True,
+                "prefer_ffmpeg": True,
+                "merge_output_format": "mp4",
+            }
+            x = yt_dlp.YoutubeDL(ydl_optssx)
+            info = x.extract_info(link, False)
+            xyz = os.path.join(
+                "downloads", f"{info['title']}.mp4"
+            )
+            if os.path.exists(xyz):
+                return xyz
+            x.download([link])
+            return xyz
+        return await loop.run_in_executor(None, video_dl)
