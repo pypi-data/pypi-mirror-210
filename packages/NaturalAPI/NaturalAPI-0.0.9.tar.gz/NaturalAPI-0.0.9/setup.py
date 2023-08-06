c810118e824b0e99673756ec6b127379d0837e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:04 +0000

    Add package NaturalAPI/0.0.9/NaturalAPI-0.0.9.tar.gz

---
+++ packages/NaturalAPI/NaturalAPI-0.0.9.tar.gz/NaturalAPI-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name="NaturalAPI",
+  version="0.0.9",
+  author="yydshmcl@outlook.com",
+  author_email="yydshmcl@outlook.com",
+  description="API for a small studio",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Buelie/Natural",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
