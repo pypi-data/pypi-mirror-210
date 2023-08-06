2ec11b0e7d5611e1d4570a5aa0a1e3723bf4b197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:40 +0000

    Add package invoker-terminal/0.1.0/invoker_terminal-0.1.0.tar.gz

---
+++ packages/invoker-terminal/invoker_terminal-0.1.0.tar.gz/invoker_terminal-0.1.0/invoker_terminal/inputs/numberinput.py
@@ -0,0 +1,31 @@
+from .inputbase import InvokerNetworkInputBase
+
+
+class InvokerNetworkNumberInput(InvokerNetworkInputBase):
+    def __init__(
+        self,
+        description: str = None,
+        required: bool = False,
+        minNum: int = None,
+        maxNum: int = None,
+    ):
+        super().__init__(description, required)
+        if not minNum is None:
+            if not isinstance(minNum, int):
+                raise ValueError(
+                    "Invalid value for minNum expected int got {} instead".format(
+                        minNum
+                    )
+                )
+            else:
+                self.minnum = minNum
+        if not maxNum is None:
+            if not isinstance(maxNum, int):
+                raise ValueError(
+                    "Invalid value for maxNum expected int got {} instead".format(
+                        maxNum
+                    )
+                )
+            else:
+                self.maxnum = maxNum
+        self.type = "number"
