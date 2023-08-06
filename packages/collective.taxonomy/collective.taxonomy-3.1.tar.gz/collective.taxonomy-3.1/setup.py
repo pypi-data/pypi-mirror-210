5fcdb3b06dbbbf24cd5ba80ea19e0643b274e07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:48 +0000

    Add package collective.taxonomy/3.1/collective.taxonomy-3.1.tar.gz

---
+++ packages/collective.taxonomy/collective.taxonomy-3.1.tar.gz/collective.taxonomy-3.1/setup.py
@@ -0,0 +1,77 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = "3.1"
+
+
+def read(*pathnames):
+    with open(os.path.join(os.path.dirname(__file__), *pathnames)) as fh:
+        return fh.read()
+
+
+setup(
+    name="collective.taxonomy",
+    version=version,
+    description="Create, edit and use hierarchical taxonomies in Plone!",
+    url="https://pypi.org/project/collective.taxonomy/",
+    long_description="\n".join(
+        [
+            read("README.rst"),
+            read("CHANGES.rst"),
+        ]
+    ),
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Addon",
+        "Framework :: Zope",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plone taxonomy dexterity",
+    author="Bo Simonsen and Malthe Borch",
+    author_email="bo@headnet.dk",
+    license="GPLv2+",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["collective"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "plone.supermodel",
+        "plone.api >= 1.5",
+        "plone.app.registry",
+        "plone.app.dexterity",
+        "plone.synchronize",
+        "lxml",
+        "six >= 1.12",
+    ],
+    extras_require={
+        "dev": [
+            "zest.releaser[recommended]",
+        ],
+        "test": [
+            "plone.testing",
+            "plone.app.testing",
+            "plone.app.contenttypes",
+            "plone.app.querystring",
+            "plone.app.robotframework[debug]",
+            "plone.restapi"
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
