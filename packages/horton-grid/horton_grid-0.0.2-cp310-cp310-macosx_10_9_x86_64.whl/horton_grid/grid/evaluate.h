b178bd37572c2ad28ebaae37edf219e4fa247bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:46 +0000

    Add package horton-grid/0.0.2/horton_grid-0.0.2-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/horton-grid/horton_grid-0.0.2-cp310-cp310-macosx_10_9_x86_64.whl/horton_grid/grid/evaluate.h
@@ -0,0 +1,38 @@
+// HORTON-GRID: GRID for Helpful Open-source Research TOol for N-fermion systems.
+// Copyright (C) 2011-2023 The HORTON-GRID Development Team
+//
+// This file is part of HORTON-GRID
+//
+// HORTON-GRID is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// HORTON-GRID is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>
+//
+//--
+
+// UPDATELIBDOCTITLE: Evaluation of splines on grids
+
+#ifndef HORTON_GRID_EVALUATE_H
+#define HORTON_GRID_EVALUATE_H
+
+
+#include "../cell.h"
+#include "cubic_spline.h"
+#include "uniform.h"
+
+
+void eval_spline_grid(CubicSpline* spline, double* center, double* output,
+                      double* points, Cell* cell, long npoint);
+
+void eval_decomposition_grid(CubicSpline** splines, double* center,
+                             double* output, double* points, Cell* cell,
+                             long nspline, long npoint);
+#endif
