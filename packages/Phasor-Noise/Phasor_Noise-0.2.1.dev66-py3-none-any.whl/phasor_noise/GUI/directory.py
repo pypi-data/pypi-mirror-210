e7e6f1d569c673789a9ec2d45fb2b5a93f39b156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:53 +0000

    Add package Phasor-Noise/0.2.1.dev66/Phasor_Noise-0.2.1.dev66-py3-none-any.whl

---
+++ packages/Phasor-Noise/Phasor_Noise-0.2.1.dev66-py3-none-any.whl/phasor_noise/GUI/directory.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+import os
+config_directory = Path(os.path.expanduser("~/phasor-generator/config"))
+images_directory = Path(os.path.expanduser("~/phasor-generator/images"))
