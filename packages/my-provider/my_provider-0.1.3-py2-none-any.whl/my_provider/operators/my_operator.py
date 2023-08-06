7002b6da88448b1585f60979e8c94979d2429b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:44 +0000

    Add package my-provider/0.1.3/my_provider-0.1.3-py2-none-any.whl

---
+++ packages/my-provider/my_provider-0.1.3-py2-none-any.whl/my_provider/operators/my_operator.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.context import Context
+
+from my_provider_top.my_provider.hooks.my_hook import MyCustomHook
+
+
+class MyOperator(BaseOperator):
+    def __init__(
+            self,
+            *,
+            flag: str,
+            **kwargs
+    ) -> None:
+        super().__init__(**kwargs)
+        self.flag = flag
+
+    def execute(self, context: Context) -> Any:
+        my_hook = MyCustomHook
+        my_hook.my_method(self=self, flag=self.flag)
