791677ba3d0cbb9b02a1cb42db552cd5c013771f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package jade-config/1.1.1/jade_config-1.1.1.tar.gz

---
+++ packages/jade-config/jade_config-1.1.1.tar.gz/jade_config-1.1.1/src/jade_config/__init__.py
@@ -0,0 +1,13 @@
+"""
+----- Jade Config -----
+A python package to quickly and easily make configs, 
+or other things that your program needs to remember.
+"""
+# Standard library imports
+import shelve
+
+
+# Version of jade-config package
+__version__ = "1.0.1"
+
+# Thanks to https://realpython.com/pypi-publish-python-package/#prepare-your-package-for-publication for help on this
