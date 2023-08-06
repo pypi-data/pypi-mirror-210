ecd67e9e0a5597765f3d872eb8788cb1a10c0377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:04 +0000

    Add package listupy/0.0.0/listupy-0.0.0.tar.gz

---
+++ packages/listupy/listupy-0.0.0.tar.gz/listupy-0.0.0/README.md
@@ -0,0 +1,16 @@
+# listupy
+
+下の方に日本語の説明があります
+
+## Overview
+- A tool that recursively enumerates Python files within a folder, calculates the total number of lines, and compiles them into a single Markdown text.
+
+## Usage
+under construction
+
+
+## 概略
+- フォルダ内のpythonファイルを再帰的に列挙し、合計行数を出すとともに、1つのmarkdowndテキストにまとめるツール
+
+## 利用例
+under construction
---
+++ packages/listupy/listupy-0.0.0.tar.gz/listupy-0.0.0/listupy/__init__.py
@@ -0,0 +1,51 @@
+# pythonファイルのリストアップ [listupy]
+
+import os
+import sys
+import fies
+import argparse
+from sout import sout
+
+# 再帰的にpythonファイルをすべて列挙
+def r_listup(dir_name):
+	dir_fies = fies[dir_name]
+	ret_ls = []
+	for file in dir_fies:
+		fullpath = os.path.join(dir_name, file)
+		if os.path.isdir(fullpath) is True:
+			ret_ls += r_listup(fullpath)
+		else:
+			if file.endswith(".py") is False: continue
+			ret_ls.append((file, dir_fies[file, "text"]))
+	return ret_ls
+
+# 行数合計の計算
+def line_n(file_ls):
+	ret_n = 0
+	for _, content in file_ls:
+		ret_n += len(content.split("\n"))
+	return ret_n
+
+# pythonファイルのリストアップ [listupy]
+def listup(dir_name):
+	# 再帰的にpythonファイルをすべて列挙
+	file_ls = r_listup(dir_name)
+	# 統計情報
+	res = f"# listupy レポート\n\n## サマリ\n合計{line_n(file_ls)}行; {len(file_ls)}ファイル\n\n"
+	# 各ファイルの内容を足していく
+	for filename, content in file_ls:
+		res += f"## {filename}\n```python\n{content}\n```\n\n"
+	return res
+
+# コンソールから利用
+def console_command():
+	# コンソール引数
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", required = True, help = "Target directory name.")
+	parser.add_argument("-o", "--output", required = False, help = "Output text filepath.")
+	args_dic = vars(parser.parse_args())
+	# debug
+	print(args_dic)
+	sys.exit()
+	# pythonファイルのリストアップ [listupy]
+	res = listup(args_dic["input"])
