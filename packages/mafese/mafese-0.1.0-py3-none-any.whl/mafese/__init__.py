496d82ccbe52c4877f10888792e7e43783386b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:05 +0000

    Add package mafese/0.1.0/mafese-0.1.0-py3-none-any.whl

---
+++ packages/mafese/mafese-0.1.0-py3-none-any.whl/mafese/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 06/03/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "0.1.0"
+
+from mafese.utils.data_loader import Data, get_dataset
+from mafese.filter import Filter
+from mafese.wrapper.recursive import Recursive
+from mafese.wrapper.sequential import Sequential
