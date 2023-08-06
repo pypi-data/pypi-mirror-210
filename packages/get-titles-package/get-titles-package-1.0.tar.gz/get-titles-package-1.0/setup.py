8d4bafa3549b100c117741ae620a41b4601ddefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:26 +0000

    Add package get-titles-package/1.0/get-titles-package-1.0.tar.gz

---
+++ packages/get-titles-package/get-titles-package-1.0.tar.gz/get-titles-package-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='get-titles-package',
+      version='1.0',
+      author='Pawel Helisz',
+      author_email='252779@student.pwr.edu.pl',
+      packages=find_packages(),
+      )
