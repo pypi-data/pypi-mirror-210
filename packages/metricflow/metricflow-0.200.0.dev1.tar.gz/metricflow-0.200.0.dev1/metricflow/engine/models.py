4a5a9ae9a7005006e9a5b304a3971ffc32c09715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:30 +0000

    Add package metricflow/0.200.0.dev1/metricflow-0.200.0.dev1.tar.gz

---
+++ packages/metricflow/metricflow-0.200.0.dev1.tar.gz/metricflow-0.200.0.dev1/metricflow/engine/models.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+from dbt_semantic_interfaces.objects.elements.dimension import Dimension
+from dbt_semantic_interfaces.objects.filters.where_filter import WhereFilter
+from dbt_semantic_interfaces.objects.metadata import Metadata
+from dbt_semantic_interfaces.objects.metric import Metric as SemanticManifestMetric
+from dbt_semantic_interfaces.objects.metric import MetricType, MetricTypeParams
+
+
+@dataclass(frozen=True)
+class Metric:
+    """Dataclass representation of a Metric."""
+
+    name: str
+    description: Optional[str]
+    type: MetricType
+    type_params: MetricTypeParams
+    filter: Optional[WhereFilter]
+    metadata: Optional[Metadata]
+    dimensions: List[Dimension]
+
+    @classmethod
+    def from_pydantic(cls, pydantic_metric: SemanticManifestMetric, dimensions: List[Dimension]) -> Metric:
+        """Build from pydantic Metric object and list of Dimensions."""
+        return cls(
+            name=pydantic_metric.name,
+            description=pydantic_metric.description,
+            type=pydantic_metric.type,
+            type_params=pydantic_metric.type_params,
+            filter=pydantic_metric.filter,
+            metadata=pydantic_metric.metadata,
+            dimensions=dimensions,
+        )
