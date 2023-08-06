308824bb41123a2efcc3f069eed0d206c670b202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:00 +0000

    Add package satcfdi/4.0.27/satcfdi-4.0.27-py3-none-any.whl

---
+++ packages/satcfdi/satcfdi-4.0.27-py3-none-any.whl/satcfdi/__init__.py
@@ -0,0 +1,16 @@
+from .exceptions import (
+    CFDIError,
+    # Schema Validation
+    SchemaValidationError,
+    # Request
+    ResponseError,
+    DocumentNotFoundError,
+    CFDIInvalidError,
+    # Transformation
+    NamespaceMismatchError
+)
+from .models import *
+from .utils import iterate
+from .xelement import XElement
+from .cfdi import CFDI
+from .utils import ScalarMap
