a0db67f2fa917b3d09e26c3b5ff4e3dae8d2f6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:56 +0000

    Add package SerizalizatorDeserializator-IGI/0.1.0/SerizalizatorDeserializator_IGI-0.1.0-py3-none-any.whl

---
+++ packages/SerizalizatorDeserializator-IGI/SerizalizatorDeserializator_IGI-0.1.0-py3-none-any.whl/SerizalizatorDeserializator_IGI/__init__.py
@@ -0,0 +1,11 @@
+from SerizalizatorDeserializator_IGI.Constants import \
+                           nonetype, moduletype, codetype, celltype, \
+                           functype, bldinfunctype, smethodtype, cmethodtype, \
+                           mapproxytype, wrapdesctype, metdesctype, getsetdesctype, \
+                           CODE_PROPS, UNIQUE_TYPES
+
+from SerizalizatorDeserializator_IGI.BaseSerializer import BaseSerializer
+from SerizalizatorDeserializator_IGI.DictSerializer import DictSerializer
+from SerizalizatorDeserializator_IGI.JSON import JsonSerializer
+from SerizalizatorDeserializator_IGI.XML import XmlSerializer
+from SerizalizatorDeserializator_IGI.SerializeFactory import SerializersFactory, SerializerType
