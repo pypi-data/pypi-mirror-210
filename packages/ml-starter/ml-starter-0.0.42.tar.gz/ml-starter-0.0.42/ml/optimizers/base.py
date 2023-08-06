962015b0d48fea85b35a5d94beda3ef95b40daaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:09 +0000

    Add package ml-starter/0.0.42/ml-starter-0.0.42.tar.gz

---
+++ packages/ml-starter/ml-starter-0.0.42.tar.gz/ml-starter-0.0.42/ml/optimizers/base.py
@@ -0,0 +1,41 @@
+"""Defines the base optimizer adapter.
+
+This class usually just wraps PyTorch optimizers, providing some common
+hyperparameter configurations.
+"""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Generic, TypeVar
+
+from torch import nn
+from torch.optim.optimizer import Optimizer
+
+from ml.core.config import BaseConfig, BaseObject
+
+
+@dataclass
+class BaseOptimizerConfig(BaseConfig):
+    """Defines the base config for all optimizers."""
+
+
+OptimizerConfigT = TypeVar("OptimizerConfigT", bound=BaseOptimizerConfig)
+
+
+class BaseOptimizer(BaseObject[OptimizerConfigT], Generic[OptimizerConfigT], ABC):
+    """Defines the base optimizer type."""
+
+    @property
+    def common_kwargs(self) -> dict[str, Any]:
+        return {}
+
+    @abstractmethod
+    def get(self, model: nn.Module) -> Optimizer:
+        """Given a base module, returns an optimizer.
+
+        Args:
+            model: The model to get an optimizer for
+
+        Returns:
+            The constructed optimizer
+        """
