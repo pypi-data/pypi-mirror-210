2be4875974427c2ccf38ceb76b5be0235e6c1a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:03 +0000

    Add package c4m-pdk-gf180mcu/0.0.3/c4m_pdk_gf180mcu-0.0.3-py3-none-any.whl

---
+++ packages/c4m-pdk-gf180mcu/c4m_pdk_gf180mcu-0.0.3-py3-none-any.whl/c4m/pdk/gf180mcu/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-or-later OR AGPL-3.0-or-later OR CERN-OHL-S-2.0+
+from .pdkmaster import *
+from .pyspice import *
+from .stdcell import *
+
+__libs__ = [stdcell3v3lib, stdcell5v0lib]
