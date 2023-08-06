d69747f713d855aa939914806b96d75eddd447bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:50 +0000

    Add package prowler/3.5.3/prowler-3.5.3.tar.gz

---
+++ packages/prowler/prowler-3.5.3.tar.gz/prowler-3.5.3/prowler/providers/aws/services/vpc/vpc_flow_logs_enabled/vpc_flow_logs_enabled.py
@@ -0,0 +1,22 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.vpc.vpc_client import vpc_client
+
+
+class vpc_flow_logs_enabled(Check):
+    def execute(self):
+        findings = []
+        for vpc in vpc_client.vpcs.values():
+            report = Check_Report_AWS(self.metadata())
+            report.region = vpc.region
+            report.resource_tags = vpc.tags
+            if vpc.flow_log:
+                report.status = "PASS"
+                report.status_extended = f"VPC {vpc.id} Flow logs are enabled."
+                report.resource_id = vpc.id
+            else:
+                report.status = "FAIL"
+                report.status_extended = f"VPC {vpc.id} Flow logs are disabled."
+                report.resource_id = vpc.id
+            findings.append(report)
+
+        return findings
