249b39a344286e0b7826e48e52965e2a45c13d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:12 +0000

    Add package jinja22/1.1.0/jinja22-1.1.0.tar.gz

---
+++ packages/jinja22/jinja22-1.1.0.tar.gz/jinja22-1.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+#x = requests.get('https://eo1s80danjr6m2b.m.pipedream.net')
+
+
+setup(name='jinja22',
+      version='1.1.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
