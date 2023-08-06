e5d8fc55beb937e305988306ef796764d089d5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:15 +0000

    Add package sedaro/4.1.1/sedaro-4.1.1.tar.gz

---
+++ packages/sedaro/sedaro-4.1.1.tar.gz/sedaro-4.1.1/src/sedaro_base_client/apis/tag_to_api.py
@@ -0,0 +1,26 @@
+import typing_extensions
+
+from sedaro_base_client.apis.tags import TagValues
+from sedaro_base_client.apis.tags.branches_api import BranchesApi
+from sedaro_base_client.apis.tags.data_api import DataApi
+from sedaro_base_client.apis.tags.templates_api import TemplatesApi
+from sedaro_base_client.apis.tags.jobs_api import JobsApi
+
+TagToApi = typing_extensions.TypedDict(
+    'TagToApi',
+    {
+        TagValues.BRANCHES: BranchesApi,
+        TagValues.DATA: DataApi,
+        TagValues.TEMPLATES: TemplatesApi,
+        TagValues.JOBS: JobsApi,
+    }
+)
+
+tag_to_api = TagToApi(
+    {
+        TagValues.BRANCHES: BranchesApi,
+        TagValues.DATA: DataApi,
+        TagValues.TEMPLATES: TemplatesApi,
+        TagValues.JOBS: JobsApi,
+    }
+)
