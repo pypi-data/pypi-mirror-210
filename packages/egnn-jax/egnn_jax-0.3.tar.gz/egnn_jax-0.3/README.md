48556140a106e95847a131fe06f3925379388fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:36 +0000

    Add package egnn-jax/0.3/egnn_jax-0.3.tar.gz

---
+++ packages/egnn-jax/egnn_jax-0.3.tar.gz/egnn_jax-0.3/README.md
@@ -0,0 +1,53 @@
+# E(n) Equivariant GNN in jax
+Reimplementation of [EGNN](https://arxiv.org/abs/2102.09844) in jax. Original work by Victor Garcia Satorras, Emiel Hogeboom and Max Welling.
+
+## Installation
+```
+python -m pip install egnn-jax
+```
+
+Or clone this repository and build locally
+```
+git clone https://github.com/gerkone/egnn-jax
+cd painn-jax
+python -m pip install -e .
+```
+### GPU support
+Upgrade `jax` to the gpu version
+```
+pip install --upgrade "jax[cuda]==0.4.10" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+```
+
+## Validation
+N-body (charged) is included for validation from the original paper. Times are  __model only__ on batches of 100 graphs, in (global) single precision.
+|                  |  MSE  | Inference [ms]* |
+|------------------|-------|-----------------|
+| torch (original) | .0071 |      8.27       |
+| jax (ours)       | .0093 |      0.94       |
+
+\* remeasured (Quadro RTX 4000)
+
+### Validation install
+
+The N-Body experiments are only included in the github repo, so it needs to be cloned first.
+```
+git clone https://github.com/gerkone/egnn-jax
+```
+
+They are adapted from the original implementation, so additionally `torch` and `torch_geometric` are needed (cpu versions are enough).
+```
+python -m pip install -r nbody/requirements.txt
+```
+
+### Valdation usage
+The charged N-body dataset has to be locally generated in the directory [/nbody/data](/nbody/data).
+```
+python -u generate_dataset.py --num-train 3000 --seed 43 --sufix small
+```
+Then, the model can be trained and evaluated with
+```
+python validate.py --epochs=1000 --batch-size=100 --lr=1e-4 --weight-decay=1e-12
+```
+
+## Acknowledgements
+This implementation heavily borrows from the [original pytorch code](https://github.com/vgsatorras/egnn).
---
+++ packages/egnn-jax/egnn_jax-0.3.tar.gz/egnn_jax-0.3/egnn_jax/variants.py
@@ -0,0 +1,77 @@
+from typing import Callable, Optional, Tuple
+
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import jraph
+
+from egnn_jax import EGNN, EGNNLayer
+from egnn_jax.utils import LinearXav
+
+
+class EGNNLayer_vel(EGNNLayer):
+    def __init__(
+        self,
+        hidden_size: int,
+        *args,
+        blocks: int = 1,
+        act_fn: Callable = jax.nn.relu,
+        dt: float = 0.001,
+        **kwargs,
+    ):
+        super().__init__(
+            *args,
+            hidden_size=hidden_size,
+            blocks=blocks,
+            act_fn=act_fn,
+            dt=dt,
+            **kwargs,
+        )
+        # velocity integrator network
+        net = [LinearXav(hidden_size)] * blocks
+        a = dt * jnp.sqrt(6 / hidden_size)
+        net += [
+            act_fn,
+            LinearXav(1, with_bias=False, w_init=hk.initializers.UniformScaling(a)),
+        ]
+        self._vel_correction_mlp = hk.Sequential(net)
+
+    def __call__(
+        self,
+        graph: jraph.GraphsTuple,
+        pos: jnp.ndarray,
+        vel: jnp.ndarray,
+        edge_attribute: Optional[jnp.ndarray] = None,
+        node_attribute: Optional[jnp.ndarray] = None,
+    ) -> jnp.ndarray:
+        graph, pos = super().__call__(graph, pos, edge_attribute, node_attribute)
+        shift = self._vel_correction_mlp(graph.nodes) * vel
+        pos = pos + jnp.clip(shift, -100, 100)
+        return graph, pos
+
+
+class EGNN_vel(EGNN):
+    def __call__(
+        self,
+        graph: jraph.GraphsTuple,
+        pos: jnp.ndarray,
+        vel: jnp.ndarray,
+        edge_attribute: Optional[jnp.ndarray] = None,
+        node_attribute: Optional[jnp.ndarray] = None,
+    ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        # input node embedding
+        h = LinearXav(self._hidden_size, name="embedding")(graph.nodes)
+        graph = graph._replace(nodes=h)
+        # message passing
+        for n in range(self._num_layers):
+            graph, pos = EGNNLayer_vel(
+                layer_num=n,
+                hidden_size=self._hidden_size,
+                output_size=self._hidden_size,
+                act_fn=self._act_fn,
+                residual=self._residual,
+                attention=self._attention,
+                normalize=self._normalize,
+                tanh=self._tanh,
+            )(graph, pos, vel, edge_attribute, node_attribute)
+        return pos
