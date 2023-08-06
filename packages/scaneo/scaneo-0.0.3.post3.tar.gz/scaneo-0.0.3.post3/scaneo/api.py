83be56479c1348ea8de134ed3aa3cf7242e223ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:07 +0000

    Add package scaneo/0.0.3.post3/scaneo-0.0.3.post3.tar.gz

---
+++ packages/scaneo/scaneo-0.0.3.post3.tar.gz/scaneo-0.0.3.post3/scaneo/api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/test")
+def test():
+    return "hello"
+
+
+# this needs to be last
+# get current script path
+current_dir = os.path.dirname(os.path.realpath(__file__))
+print(current_dir)
+app.mount("/", StaticFiles(directory=current_dir + "/build", html=True), name="ui")
