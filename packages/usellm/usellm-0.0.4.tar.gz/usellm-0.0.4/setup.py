eb68d567be1c61b4bd85ca14585eefdeafa4ed5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:52 +0000

    Add package usellm/0.0.4/usellm-0.0.4.tar.gz

---
+++ packages/usellm/usellm-0.0.4.tar.gz/usellm-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='usellm',
+    version='0.0.4',
+    author='Siddhant',
+    author_email='siddhant@jovian.com',
+    description='Use Large Language Models in Python App',
+    packages=['usellm'],
+    install_requires=[
+        'requests>=2.31.0'
+    ],
+)
