24cbb196e69a323b2df09587a58234982be376f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:26 +0000

    Add package adds/0.0.9/adds-0.0.9.tar.gz

---
+++ packages/adds/adds-0.0.9.tar.gz/adds-0.0.9/adds/add_number.py
@@ -0,0 +1,2 @@
+def addm(n=0, m=0):
+    return n + m
---
+++ packages/adds/adds-0.0.9.tar.gz/adds-0.0.9/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+packages = ['adds']
+setup(
+    name='adds',
+    version='0.0.9',
+    author='mirschao',
+    packages=packages
+)
