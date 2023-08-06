270d3669d04d80cf34fa2abb2829001676a22d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:20 +0000

    Add package excel-2-csv-converter/1.0.0/excel_2_csv_converter-1.0.0.tar.gz

---
+++ packages/excel-2-csv-converter/excel_2_csv_converter-1.0.0.tar.gz/excel_2_csv_converter-1.0.0/excel_2_csv_converter/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='excel_2_csv_converter',
+    version='1.0.0',
+    author='Shambhu Jha',
+    author_email='shambhu.job1@gmail.com',
+    description='A package to convert Excel files to CSV',
+    url='https://github.com/yourusername/excel-converter',
+    packages=['excel_2_csv_converter'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
