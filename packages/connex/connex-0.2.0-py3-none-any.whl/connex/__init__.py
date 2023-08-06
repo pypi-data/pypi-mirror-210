945982e4e84ab3a4a4c832ee571f837938797d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:06 +0000

    Add package connex/0.2.0/connex-0.2.0-py3-none-any.whl

---
+++ packages/connex/connex-0.2.0-py3-none-any.whl/connex/__init__.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+
+from . import nn
+from ._network import NeuralNetwork
+from ._plasticity import (
+    add_connections,
+    add_hidden_neurons,
+    add_input_neurons,
+    add_output_neurons,
+    remove_connections,
+    remove_neurons,
+)
