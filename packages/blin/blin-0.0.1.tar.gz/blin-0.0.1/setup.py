26d69a9d393b2a04dbda08368b9364f89ca84d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:59 +0000

    Add package blin/0.0.1/blin-0.0.1.tar.gz

---
+++ packages/blin/blin-0.0.1.tar.gz/blin-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+  name="blin",
+  version="0.0.1",
+  author="Anastasiia Dolzhenko",
+  description="Python client library for Blin API (natural language to API service)",
+  packages=["blin"]
+  )
