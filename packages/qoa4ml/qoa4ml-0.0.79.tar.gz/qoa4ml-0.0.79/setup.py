e154ed0ab9c50262956872f5abd0871d7feb4c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:49 +0000

    Add package qoa4ml/0.0.79/qoa4ml-0.0.79.tar.gz

---
+++ packages/qoa4ml/qoa4ml-0.0.79.tar.gz/qoa4ml-0.0.79/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='qoa4ml',
+    version='0.0.79',
+    description='Quality of Analysis for Machine Learning',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    url='https://rdsea.github.io/',
+    author='Aaltosea',
+    email='tri.m.nguyen@aalto.fi',
+    keyword='Monitoring Machine Learning',
+    install_requires=['pika','requests','paho-mqtt','prometheus-client','psutil', 'docker'],
+    packages=find_packages(),
+    license='Apache License 2.0'
+)
