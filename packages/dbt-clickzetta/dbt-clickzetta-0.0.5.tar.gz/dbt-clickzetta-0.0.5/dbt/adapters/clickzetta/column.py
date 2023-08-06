cbe5863b2559a179fe5570130254108b6e23272e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:03 +0000

    Add package dbt-clickzetta/0.0.5/dbt-clickzetta-0.0.5.tar.gz

---
+++ packages/dbt-clickzetta/dbt-clickzetta-0.0.5.tar.gz/dbt-clickzetta-0.0.5/dbt/adapters/clickzetta/column.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, TypeVar, Union
+
+from dbt.adapters.base.column import Column
+from dbt.exceptions import DbtRuntimeError
+
+
+@dataclass
+class ClickZettaColumn(Column):
+    table_database: Optional[str] = None
+    table_schema: Optional[str] = None
+    table_name: Optional[str] = None
+
+    def is_integer(self) -> bool:
+        return self.dtype.lower() in [
+            "int8",
+            "int16",
+            "int32",
+            "int64",
+        ]
+
+    def is_float(self):
+        return self.dtype.lower() in [
+            "float32",
+            "float64",
+            # TODO(hanmiao.li): decimal is a subclass of float, but we don't want to treat it
+            # "decimal",
+        ]
+
+    def is_string(self) -> bool:
+        return self.dtype.lower() in [
+            "string",
+            "varchar",
+            "char",
+        ]
+
+    def string_size(self) -> int:
+        if not self.is_string():
+            raise DbtRuntimeError("Called string_size() on non-string field!")
+
+        if self.dtype == "string" or self.char_size is None:
+            return 16777216
+        else:
+            return int(self.char_size)
