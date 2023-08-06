689b3f4669fa06db8d98a1a658c8e3010f0faaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:39 +0000

    Add package rubi/1.3.2/rubi-1.3.2-py3-none-any.whl

---
+++ packages/rubi/rubi-1.3.2-py3-none-any.whl/rubi/contracts/__init__.py
@@ -0,0 +1,3 @@
+from .router import RubiconRouter, RubiconRouterSigner
+from .market import RubiconMarket, RubiconMarketSigner
+from .aid import FactoryAid, FactoryAidSigner, MarketAid, MarketAidSigner
