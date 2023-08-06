de6554d8060b057e9c32c1dd49117d9bcc8d8b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:18 +0000

    Add package mpipartition/1.0.5/mpipartition-1.0.5-py3-none-any.whl

---
+++ packages/mpipartition/mpipartition-1.0.5-py3-none-any.whl/mpipartition/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for MPIPartition."""
+
+__author__ = """Michael Buehlmann"""
+__email__ = "buehlmann.michi@gmail.com"
+__version__ = "1.0.5"
+
+
+from .distribute import distribute
+from .exchange import exchange
+from .overload import overload
+from .partition import Partition
