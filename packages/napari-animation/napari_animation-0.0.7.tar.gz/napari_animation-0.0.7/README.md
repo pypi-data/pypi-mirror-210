68a099180728b8737551793d885b84fddc5cd1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:39 +0000

    Add package napari-animation/0.0.7/napari_animation-0.0.7.tar.gz

---
+++ packages/napari-animation/napari_animation-0.0.7.tar.gz/napari_animation-0.0.7/README.md
@@ -0,0 +1,123 @@
+# napari-animation
+
+[![License](https://img.shields.io/pypi/l/napari-animation.svg?color=green)](https://github.com/napari/napari-animation/raw/main/LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/napari-animation.svg?color=green)](https://pypi.org/project/napari-animation)
+[![Python Version](https://img.shields.io/pypi/pyversions/napari-animation.svg?color=green)](https://python.org)
+[![tests](https://github.com/napari/napari-animation/actions/workflows/test_and_deploy.yml/badge.svg)](https://github.com/napari/napari-animation/actions)
+[![codecov](https://codecov.io/gh/napari/napari-animation/branch/main/graph/badge.svg)](https://codecov.io/gh/napari/napari-animation)
+
+**napari-animation** is a plugin for making animations in [napari].
+
+<p align="center">
+  <img width="500" src="https://user-images.githubusercontent.com/7307488/196110138-6c4663b1-67b2-4c79-97b7-57b706d1d49c.gif">
+</p>
+
+----------------------------------
+
+[Merlin Lange](https://twitter.com/Merlin_Lange) used *napari-animation* to create one of [Nature's best science images for September 2022](https://www.nature.com/immersive/d41586-022-03051-6/index.html)
+
+----------------------------------
+
+This plugin is built on [naparimovie](https://github.com/guiwitz/naparimovie) from @guiwitz. naparimovie was submitted to napari in [PR#851](https://github.com/napari/napari/pull/780) before napari plugin infrastructure existed.
+
+----------------------------------
+## Overview
+
+**napari-animation** provides a framework for the creation of animations in napari, the plugin contains:
+- an easy to use GUI for creating animations interactively
+- a Python package for the programmatic creation of animations
+
+This plugin remains under development and contributions are very welcome, please open an issue to discuss potential improvements.
+
+## Installation
+
+### PyPI
+`napari-animation` is available through the Python package index and can be installed using `pip`.
+
+```sh
+pip install napari-animation
+```
+
+### Local
+You can clone this repository and install locally with
+
+    pip install -e .
+
+### Interactive use
+**napari-animation** can be used interactively.
+
+An animation is created by capturing [keyframes](https://en.wikipedia.org/wiki/Key_frame) containing the current viewer state.
+
+<p align="center">
+  <img width="600" src="https://user-images.githubusercontent.com/7307488/196113682-96ce0da3-fa5c-411e-8fb1-52dc3a8f96b6.png">
+</p>
+
+To activate the GUI, select **napari-animation: wizard** from the *plugins menu*
+
+<p align="center">
+  <img width="200" src="https://user-images.githubusercontent.com/7307488/196114466-56cb5985-0d79-4cfa-96f1-38cf3ccfbc48.png">
+</p>
+
+### Headless
+**napari-animation** can also be run headless, allowing for reproducible, scripted creation of animations.
+
+```python
+from napari_animation import Animation
+
+animation = Animation(viewer)
+
+viewer.dims.ndisplay = 3
+viewer.camera.angles = (0.0, 0.0, 90.0)
+animation.capture_keyframe()
+viewer.camera.zoom = 2.4
+animation.capture_keyframe()
+viewer.camera.angles = (-7.0, 15.7, 62.4)
+animation.capture_keyframe(steps=60)
+viewer.camera.angles = (2.0, -24.4, -36.7)
+animation.capture_keyframe(steps=60)
+viewer.reset_view()
+viewer.camera.angles = (0.0, 0.0, 90.0)
+animation.capture_keyframe()
+animation.animate('demo.mov', canvas_only=False)
+```
+
+## Examples
+Examples can be found in our [examples](examples) folder. Simple examples for both interactive and headless 
+use of the plugin follow.
+
+## Contributing
+
+Contributions are very welcome and a detailed contributing guide is coming soon. 
+
+Tests are run with `pytest`.
+
+We use [`pre-commit`](https://pre-commit.com) to sort imports with
+[`isort`](https://github.com/timothycrosley/isort), format code with
+[`black`](https://github.com/psf/black), and lint with
+[`flake8`](https://github.com/PyCQA/flake8) automatically prior to each commit.
+To minmize test errors when submitting pull requests, please install `pre-commit`
+in your environment as follows:
+
+```sh
+pre-commit install
+```
+
+## License
+
+Distributed under the terms of the [BSD-3] license,
+"napari-animation" is free and open source software
+
+## Issues
+
+If you encounter any problems, please [file an issue] along with a detailed description.
+
+[napari]: https://github.com/napari/napari
+[Cookiecutter]: https://github.com/audreyr/cookiecutter
+[@napari]: https://github.com/napari
+[BSD-3]: http://opensource.org/licenses/BSD-3-Clause
+[cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
+[file an issue]: https://github.com/sofroniewn/napari-animation/issues
+[napari]: https://github.com/napari/napari
+[tox]: https://tox.readthedocs.io/en/latest/
+[pip]: https://pypi.org/project/pip/
+[PyPI]: https://pypi.org/
---
+++ packages/napari-animation/napari_animation-0.0.7.tar.gz/napari_animation-0.0.7/napari_animation/_tests/test_animation.py
@@ -0,0 +1,141 @@
+from unittest.mock import patch
+
+import numpy as np
+import pytest
+
+from napari_animation import Animation, ViewerState
+from napari_animation.utils import make_thumbnail
+
+CAPTURED_LAYER_ATTRIBUTES = [
+    "name",
+    "scale",
+    "translate",
+    "rotate",
+    "shear",
+    "opacity",
+    "blending",
+    "visible",
+]
+
+
+def test_animation(make_napari_viewer):
+    """Test creation of an animation class."""
+    viewer = make_napari_viewer()
+    animation = Animation(viewer)
+    assert animation is not None
+
+
+def test_capture_key_frame(empty_animation):
+    """Test capture of key-frame"""
+    animation = empty_animation
+    assert len(animation.key_frames) == 0
+    animation.capture_keyframe()
+    assert len(animation.key_frames) == 1
+    assert animation.key_frames[0].ease
+
+
+def test_set_to_key_frame(animation_with_key_frames):
+    """Test Animation.set_to_key_frame()"""
+    animation = animation_with_key_frames
+    for i in range(2):
+        animation.set_key_frame_index(i)
+        assert animation.key_frames.selection.active == animation.key_frames[i]
+
+
+def test_replace_keyframe(animation_with_key_frames):
+    """Test Animation.set_to_key_frame()"""
+    animation = animation_with_key_frames
+    assert len(animation.key_frames) == 2
+    animation.capture_keyframe(insert=False)
+    animation.capture_keyframe(insert=False)
+    animation.capture_keyframe(insert=False)
+    assert len(animation.key_frames) == 2
+
+
+def test_get_viewer_state(empty_animation):
+    """Test ViewerState.from_viewer()"""
+    animation = empty_animation
+    state = ViewerState.from_viewer(animation.viewer)
+    assert isinstance(state, ViewerState)
+
+
+def test_set_viewer_state(animation_with_key_frames, viewer_state):
+    """Test Animation._set_viewer_state()"""
+    animation: Animation = animation_with_key_frames
+    current_state = ViewerState.from_viewer(animation.viewer)
+    viewer_state.apply(animation.viewer)
+
+    animation_dims_state = animation.viewer.dims.dict()
+    animation_camera_state = animation.viewer.camera.dict()
+
+    assert animation_dims_state == current_state.dims
+    for key in ("center", "angles", "interactive"):
+        assert animation_camera_state[key] == current_state.camera[key]
+
+
+def test_thumbnail_generation(empty_animation):
+    """Test thumbnail generation"""
+    animation = empty_animation
+    shape = (32, 32, 4)
+    thumbnail = make_thumbnail(
+        animation.viewer.screenshot(canvas_only=True), shape
+    )
+
+    assert thumbnail.shape == shape
+    assert thumbnail.dtype == np.uint8
+    assert thumbnail.max() <= 255
+    assert thumbnail.min() >= 0
+
+
+@patch("napari_animation.animation.imsave")
+@patch("imageio.get_writer")
+@patch(
+    "napari_animation.frame_sequence.FrameSequence.iter_frames",
+    return_value=["frame"] * 30,
+)
+@pytest.mark.parametrize("ext", [".mp4", ".mov", ""])
+def test_animate_filenames(
+    frame_gen, get_writer, imsave, animation_with_key_frames, ext, tmp_path
+):
+    """Test that Animation.animate() produces files with correct filenames"""
+    output_filename = tmp_path / f"test{ext}"
+    animation_with_key_frames.animate(output_filename)
+    if ext in (".mp4", ".mov"):
+        expected_filename = output_filename
+        saved_filename = get_writer.call_args[0][0]
+        assert saved_filename == expected_filename
+    elif ext == "":
+        expected = [output_filename / f"test_{i:06d}.png" for i in range(30)]
+        saved_files = [call[0][0] for call in imsave.call_args_list]
+        assert saved_files == expected
+
+
+@pytest.mark.parametrize("ext", [".mp4", ".mov", ".avi"])
+def test_animation_file_metadata(animation_with_key_frames, tmp_path, ext):
+    """Test output video file contians napari version metadata()"""
+    animation = animation_with_key_frames
+    output_filename = tmp_path / f"test{ext}"
+    animation.animate(output_filename)
+    # Read metadata back in, and check for napari version information
+    # We expect to see a metadata line in the metadata like this:
+    # title="napari version 0.4.17 https://napari.org/"
+    with open(output_filename, "rb") as f:
+        content = f.read()
+    assert b"napari version" in content
+    assert b"https://napari.org" in content
+
+
+@pytest.mark.parametrize("attribute", CAPTURED_LAYER_ATTRIBUTES)
+def test_layer_attribute_capture(layer_state, attribute):
+    """Test that 'attribute' is captured in the layer state dictionary"""
+    for layer_state_dict in layer_state.values():
+        assert attribute in layer_state_dict.keys()
+
+
+def test_end_state_reached(image_animation):
+    """Check that animation ends in the same state as the final key-frame"""
+    image_animation.capture_keyframe()
+    image_animation.viewer.dims.current_step = (28, 0)
+    image_animation.capture_keyframe(steps=2)
+    last_state = image_animation._frames[-1]
+    assert last_state == image_animation.key_frames[-1].viewer_state
