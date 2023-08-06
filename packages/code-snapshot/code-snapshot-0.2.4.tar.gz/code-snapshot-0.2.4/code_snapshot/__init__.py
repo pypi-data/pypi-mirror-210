183507bdeb8e1b552b805c3fabd037039ba1ff53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:14 +0000

    Add package code-snapshot/0.2.4/code-snapshot-0.2.4.tar.gz

---
+++ packages/code-snapshot/code-snapshot-0.2.4.tar.gz/code-snapshot-0.2.4/code_snapshot/__init__.py
@@ -0,0 +1,6 @@
+"""code_snapshot - A Python library for interacting with the Showcode api."""
+from __future__ import absolute_import
+
+
+__version__ = "0.2.4"
+__all__ = ['code_snapshot', 'models']
