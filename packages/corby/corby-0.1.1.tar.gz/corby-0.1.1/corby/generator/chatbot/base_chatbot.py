0a72da162f3d7e7541632fc8563c3138fc33893b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:25 +0000

    Add package corby/0.1.1/corby-0.1.1.tar.gz

---
+++ packages/corby/corby-0.1.1.tar.gz/corby-0.1.1/corby/generator/chatbot/base_chatbot.py
@@ -0,0 +1,22 @@
+'''Base class for chatbot generators'''
+from ..base import BaseGenerator
+
+class BaseChatbotGenerator(BaseGenerator):
+    '''Base class for chatbot generators'''
+    def get_chatbot_params(self):
+        '''Returns the specific questions required for each kind of chatbot'''
+        raise NotImplementedError
+
+    def get_generator_params(self, name):
+        '''Returns the specific parameters for chatbots'''
+        template_params = {'chatbot_name': name}
+        if hasattr(self, 'get_chatbot_params'):
+            chatbot_specific_params = self.get_chatbot_params()
+            if bool(chatbot_specific_params):
+                template_params.update(chatbot_specific_params)
+        return template_params
+
+    def create_chatbot(self, name):
+        '''Calls super.create() to generate a new chatbot
+        we may have custom logic here on the future'''
+        return super().create(name)
