1e8d2a1d012e6fad94f9d97f2ed86f2cec6355dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:33 +0000

    Add package cornflakes/3.3.5/cornflakes-3.3.5-cp310-cp310-manylinux_2_28_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/cornflakes/cornflakes-3.3.5-cp310-cp310-manylinux_2_28_x86_64.manylinux_2_24_x86_64.whl/cornflakes/__main__.py
@@ -0,0 +1,61 @@
+"""Command-line interface."""
+from cornflakes.decorator import click_cli
+from cornflakes.decorator.click import RichGroup, bg_process_option, verbose_option
+
+
+@click_cli(
+    OPTION_GROUPS={
+        **{
+            command: [
+                {
+                    "name": "Basic Options",
+                    "options": [
+                        "--name",
+                        "--description",
+                        "--help",
+                        "--log-level",
+                        "--log-config",
+                        "--version",
+                        "--verbose",
+                        "--silent",
+                        "--background-process",
+                    ],
+                },
+            ]
+            for command in ["cornflakes", "cornflakes create"]
+        },
+    },
+    COMMAND_GROUPS={
+        "cornflakes": [
+            {
+                "name": "Commands",
+                "commands": [],
+            },
+        ]
+    },
+    CONTEXT_SETTINGS={"help_option_names": ["-h", "--help"]},
+    HEADER_LOGO="""[blue]
+ ██████╗ ██████╗ ██████╗ ███╗   ██╗███████╗██╗      █████╗ ██╗  ██╗███████╗ ███████╗
+██╔════╝██╔═══██╗██╔══██╗████╗  ██║██╔════╝██║     ██╔══██╗██║ ██╔╝██╔════╝ ██╔════╝
+██║     ██║   ██║██████╔╝██╔██╗ ██║█████╗  ██║     ███████║█████╔╝ █████╗   ███████╗
+██║     ██║   ██║██╔══██╗██║╚██╗██║██╔══╝  ██║     ██╔══██║██╔═██╗ ██╔══╝   ╚════██║
+╚██████╗╚██████╔╝██║  ██║██║ ╚████║██║     ███████╗██║  ██║██║  ██╗███████╗ ███████║
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚══════╝""",
+    # Banner generated with figlet and figlet-fonts/ANSI\ Shadow.flf
+    HEADER_TEXT="Create generic any easy to manage Configs for your Project.",
+    GLOBAL_OPTIONS=[verbose_option, bg_process_option],
+    VERSION_INFO=True,
+)
+def main():
+    """Main CLI Entrypoint Method."""
+
+
+if isinstance(main, RichGroup):
+    for command in []:
+        main.add_command(command)
+        main.config.COMMAND_GROUPS.get("cornflakes")[0].get("commands").append(command.name)  # type: ignore
+
+__all__ = ["main"]
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
