df7e625c42311e2df47437298ed26171af93c968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:19 +0000

    Add package shrug-viz/0.1.1/shrug_viz-0.1.1.tar.gz

---
+++ packages/shrug-viz/shrug_viz-0.1.1.tar.gz/shrug_viz-0.1.1/shrug_viz/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from .shrugviz import csv_to_df
+from .shrugviz import get_column_names
+from .shrugviz import read_shp
+from .shrugviz import get_key_from_value
+from .shrugviz import get_state_map
+from .shrugviz import get_district_map
+from .shrugviz import get_agg_map
+from .shrugviz import get_state_names
+from .shrugviz import get_district_names
+from .shrugviz import shp_corrections
+from .shrugviz import c_plot
+from .shrugviz import choropleth
+
+
+# In[ ]:
+
+
+
+
