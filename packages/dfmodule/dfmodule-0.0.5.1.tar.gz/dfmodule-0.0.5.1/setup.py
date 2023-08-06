8af3b3af5cdd4405d6d0bce1d8bffe45d1557ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package dfmodule/0.0.5.1/dfmodule-0.0.5.1.tar.gz

---
+++ packages/dfmodule/dfmodule-0.0.5.1.tar.gz/dfmodule-0.0.5.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dfmodule',
+    version='0.0.5.1',
+    description='data management project common features module',
+    author='rimmoyee',
+    author_email='rimmoyee@gmail.com',
+    url='',
+    install_requires=['boto3', 'b64uuid', 'cryptography'],
+    packages=find_packages(exclude=[]),
+    keywords=['dfmodule', 'dfmodule777'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
