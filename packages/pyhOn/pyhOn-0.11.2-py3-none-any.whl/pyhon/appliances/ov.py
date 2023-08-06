a5876bc03bebf27986b249c2b39cb475cb399e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:48 +0000

    Add package pyhOn/0.11.2/pyhOn-0.11.2-py3-none-any.whl

---
+++ packages/pyhOn/pyhOn-0.11.2-py3-none-any.whl/pyhon/appliances/ov.py
@@ -0,0 +1,17 @@
+class Appliance:
+    def __init__(self, appliance):
+        self.parent = appliance
+
+    def data(self, data):
+        if data["attributes"]["lastConnEvent"]["category"] == "DISCONNECTED":
+            data["attributes"]["parameters"]["temp"] = "0"
+            data["attributes"]["parameters"]["onOffStatus"] = "0"
+            data["attributes"]["parameters"]["remoteCtrValid"] = "0"
+            data["attributes"]["parameters"]["remainingTimeMM"] = "0"
+
+        data["active"] = data["attributes"]["parameters"]["onOffStatus"] == "1"
+
+        return data
+
+    def settings(self, settings):
+        return settings
