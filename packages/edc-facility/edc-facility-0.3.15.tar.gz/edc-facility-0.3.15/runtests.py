710ffe7e47bae9819d1339ae83a807a338b184e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:07 +0000

    Add package edc-facility/0.3.15/edc-facility-0.3.15.tar.gz

---
+++ packages/edc-facility/edc-facility-0.3.15.tar.gz/edc-facility-0.3.15/runtests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import logging
+import sys
+from os.path import abspath, dirname
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+
+app_name = "edc_facility"
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    APP_NAME=app_name,
+    BASE_DIR=base_dir,
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "edc_sites.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+    ],
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
+    failures = DiscoverRunner(failfast=True, tags=tags).run_tests([f"{app_name}.tests"])
+    sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
