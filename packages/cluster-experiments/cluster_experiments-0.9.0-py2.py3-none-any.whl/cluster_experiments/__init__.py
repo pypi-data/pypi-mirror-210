04191764df2d6e9ce35e9afdfc778fea8594ea75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:28 +0000

    Add package cluster-experiments/0.9.0/cluster_experiments-0.9.0-py2.py3-none-any.whl

---
+++ packages/cluster-experiments/cluster_experiments-0.9.0-py2.py3-none-any.whl/cluster_experiments/__init__.py
@@ -0,0 +1,68 @@
+from cluster_experiments.cupac import EmptyRegressor, TargetAggregation
+from cluster_experiments.experiment_analysis import (
+    ClusteredOLSAnalysis,
+    ExperimentAnalysis,
+    GeeExperimentAnalysis,
+    MLMExperimentAnalysis,
+    OLSAnalysis,
+    PairedTTestClusteredAnalysis,
+    TTestClusteredAnalysis,
+)
+from cluster_experiments.perturbator import (
+    BetaRelativePerturbator,
+    BetaRelativePositivePerturbator,
+    BinaryPerturbator,
+    NormalPerturbator,
+    Perturbator,
+    RelativePositivePerturbator,
+    SegmentedBetaRelativePerturbator,
+    UniformPerturbator,
+)
+from cluster_experiments.power_analysis import PowerAnalysis
+from cluster_experiments.power_config import PowerConfig
+from cluster_experiments.random_splitter import (
+    BalancedClusteredSplitter,
+    BalancedSwitchbackSplitter,
+    ClusteredSplitter,
+    NonClusteredSplitter,
+    RandomSplitter,
+    RepeatedSampler,
+    StratifiedClusteredSplitter,
+    StratifiedSwitchbackSplitter,
+    SwitchbackSplitter,
+)
+from cluster_experiments.washover import ConstantWashover, EmptyWashover, Washover
+
+__all__ = [
+    "ExperimentAnalysis",
+    "GeeExperimentAnalysis",
+    "OLSAnalysis",
+    "BinaryPerturbator",
+    "Perturbator",
+    "UniformPerturbator",
+    "RelativePositivePerturbator",
+    "NormalPerturbator",
+    "BetaRelativePositivePerturbator",
+    "BetaRelativePerturbator",
+    "SegmentedBetaRelativePerturbator",
+    "PowerAnalysis",
+    "PowerConfig",
+    "EmptyRegressor",
+    "TargetAggregation",
+    "BalancedClusteredSplitter",
+    "ClusteredSplitter",
+    "RandomSplitter",
+    "NonClusteredSplitter",
+    "StratifiedClusteredSplitter",
+    "SwitchbackSplitter",
+    "BalancedSwitchbackSplitter",
+    "StratifiedSwitchbackSplitter",
+    "RepeatedSampler",
+    "ClusteredOLSAnalysis",
+    "TTestClusteredAnalysis",
+    "PairedTTestClusteredAnalysis",
+    "EmptyWashover",
+    "ConstantWashover",
+    "Washover",
+    "MLMExperimentAnalysis",
+]
