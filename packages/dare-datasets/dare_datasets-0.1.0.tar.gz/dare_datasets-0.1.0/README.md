10b61f3f229d8d1f7addc0da15f9c4bfca25e41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package dare-datasets/0.1.0/dare_datasets-0.1.0.tar.gz

---
+++ packages/dare-datasets/dare_datasets-0.1.0.tar.gz/dare_datasets-0.1.0/README.md
@@ -0,0 +1,1 @@
+# Darelab Datasets
---
+++ packages/dare-datasets/dare_datasets-0.1.0.tar.gz/dare_datasets-0.1.0/dare_datasets/utils/downloader.py
@@ -0,0 +1,20 @@
+import gdown
+import glob
+import logging
+import os
+
+from pathlib import Path
+print()
+
+
+def get_file_from_gdrive(url: str, folder_name: str, cache_dir: str) -> None:
+    cached_datasets = list(map(os.path.basename, glob.glob(f"{cache_dir}*")))
+
+    if folder_name not in cached_datasets:  # Check if file is already downloaded in cache
+        gdown.download_folder(url, output=cache_dir, quiet=True, use_cookies=False)
+    else:
+        logging.debug(f"Using cached version of {folder_name}")
+
+
+get_file_from_gdrive(url="https://drive.google.com/drive/folders/1LCD4gObeX-PBxkNYNuZxSmrTBvzEK095?usp=sharing",
+                     cache_dir='~/.cache/dare-datasets/', folder_name='QR2T_Benchmark')
