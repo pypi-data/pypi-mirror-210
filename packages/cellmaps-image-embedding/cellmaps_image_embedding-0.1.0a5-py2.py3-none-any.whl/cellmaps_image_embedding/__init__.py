e8b205a616b9083dc0af6b93ec38b422196141d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:36 +0000

    Add package cellmaps-image-embedding/0.1.0a5/cellmaps_image_embedding-0.1.0a5-py2.py3-none-any.whl

---
+++ packages/cellmaps-image-embedding/cellmaps_image_embedding-0.1.0a5-py2.py3-none-any.whl/cellmaps_image_embedding/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cellmaps_image_embedding."""
+
+__author__ = """Gege Qian"""
+__email__ = 'geqian@ucsd.edu'
+__version__ = '0.1.0a5'
+__repo_url__ = 'https://github.com/idekerlab/cellmaps_image_embedding'
+__description__ = 'A tool to generate embeddings from HPA IF images'
+
