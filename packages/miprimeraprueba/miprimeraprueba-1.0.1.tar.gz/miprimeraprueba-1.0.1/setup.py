664bcbb316d6c64c1ba6595cae8fad6acd953983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:54 +0000

    Add package miprimeraprueba/1.0.1/miprimeraprueba-1.0.1.tar.gz

---
+++ packages/miprimeraprueba/miprimeraprueba-1.0.1.tar.gz/miprimeraprueba-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='miprimeraprueba',
+    version='1.0.1',
+    description='A simple hello world package',
+    author='Diego J. Pedregal',
+    author_email='Diego.Pedregal@uclm.es',
+    packages=['miprimeraprueba'],
+)
