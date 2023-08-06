c9bb4cbb512460213800439ddaa845e0852f812d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:51 +0000

    Add package joyrl/0.2.7/joyrl-0.2.7-py3-none-any.whl

---
+++ packages/joyrl/joyrl-0.2.7-py3-none-any.whl/joyrl/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JiangJi
+Email: johnjim0816@gmail.com
+Date: 2023-01-01 16:20:49
+LastEditor: JiangJi
+LastEditTime: 2023-05-28 19:00:13
+Discription: 
+'''
+from joyrl import algos, config, framework, envs, utils
+from joyrl.run import run
+__version__ = "0.2.7"
+
+__all__ = [
+    "algos",
+    "config",
+    "envs",
+    "framework",
+    "utils",
+    "run"
+]
