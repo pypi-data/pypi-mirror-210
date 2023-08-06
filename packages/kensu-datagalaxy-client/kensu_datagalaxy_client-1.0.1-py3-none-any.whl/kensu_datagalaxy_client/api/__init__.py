73db5ae1fa802cb1b467c5f8524d7c9ce24d9503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:42 +0000

    Add package kensu-datagalaxy-client/1.0.1/kensu_datagalaxy_client-1.0.1-py3-none-any.whl

---
+++ packages/kensu-datagalaxy-client/kensu_datagalaxy_client-1.0.1-py3-none-any.whl/kensu_datagalaxy_client/api/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.attributes_api import AttributesApi
+from swagger_client.api.authentication_api import AuthenticationApi
+from swagger_client.api.comments_api import CommentsApi
+from swagger_client.api.containers_api import ContainersApi
+from swagger_client.api.data_processing_api import DataProcessingApi
+from swagger_client.api.data_processing_item_api import DataProcessingItemApi
+from swagger_client.api.fields_api import FieldsApi
+from swagger_client.api.filtered_views_api import FilteredViewsApi
+from swagger_client.api.glossary_api import GlossaryApi
+from swagger_client.api.history_api import HistoryApi
+from swagger_client.api.image_api import ImageApi
+from swagger_client.api.licenses_api import LicensesApi
+from swagger_client.api.links_api import LinksApi
+from swagger_client.api.scim_users_api import SCIMUsersApi
+from swagger_client.api.screens_api import ScreensApi
+from swagger_client.api.search_api import SearchApi
+from swagger_client.api.sources_api import SourcesApi
+from swagger_client.api.structures_api import StructuresApi
+from swagger_client.api.tasks_api import TasksApi
+from swagger_client.api.teams_api import TeamsApi
+from swagger_client.api.technologies_api import TechnologiesApi
+from swagger_client.api.usage_api import UsageApi
+from swagger_client.api.users_api import UsersApi
+from swagger_client.api.workspaces_api import WorkspacesApi
