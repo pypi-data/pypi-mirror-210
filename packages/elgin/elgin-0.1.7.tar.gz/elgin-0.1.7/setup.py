ea47caf4f6a53f0cec3a99601dd28919068dc528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:09 +0000

    Add package elgin/0.1.7/elgin-0.1.7.tar.gz

---
+++ packages/elgin/elgin-0.1.7.tar.gz/elgin-0.1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open('./README.md') as f:
+    long_desc = f.read()
+
+setup(
+     name='elgin'
+    ,version='0.1.7'
+    ,description='Biblioteca responsavel por gerar o monitoramento dos processos da empresa elgin.'
+    ,long_description=long_desc
+    ,long_description_content_type='text/markdown'
+    ,license="MIT"
+    ,classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ]
+    ,packages=['elgin']
+    ,package_dir={'elgin': 'elgin'}
+    ,package_data={'elgin': ['templates/*']}
+    ,include_package_data=True
+    ,install_requires=["mysql-connector-python","requests"]
+)
