eee7d23a7bb0f9b6651f69ae33d32a1b45ff4c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:43 +0000

    Add package aiodiskqueue/0.1.0a1/aiodiskqueue-0.1.0a1.tar.gz

---
+++ packages/aiodiskqueue/aiodiskqueue-0.1.0a1.tar.gz/aiodiskqueue-0.1.0a1/docs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "aiodiskqueue"
+copyright = "2023, Erik Kalkoken"
+author = "Erik Kalkoken"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_theme_options = {
+    "description": "Persistent queues for Python AsyncIO",
+    "fixed_sidebar": True,
+    "badge_branch": "master",
+    "github_button": True,
+    "github_user": "ErikKalkoken",
+    "github_repo": "aiodiskqueue",
+    "show_powered_by": False,
+    "sidebar_collapse": False,
+    "extra_nav_links": {
+        "Report Issues": "https://github.com/ErikKalkoken/aiodiskqueue/issues",
+    },
+}
+
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
+    ],
+}
+
+html_static_path = ["_static"]
