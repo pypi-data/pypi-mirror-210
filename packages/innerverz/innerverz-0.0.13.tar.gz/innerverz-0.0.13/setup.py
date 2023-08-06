3332a93159876a1cec4c7ecb43147d1ec7338a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:04 +0000

    Add package innerverz/0.0.13/innerverz-0.0.13.tar.gz

---
+++ packages/innerverz/innerverz-0.0.13.tar.gz/innerverz-0.0.13/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+requirements =[
+    'Pillow',
+    'numpy',
+    'tqdm',
+    'gdown',
+    'insightface',
+    'opencv-python',
+    
+]
+
+pypandoc_enabled = True
+try:
+    import pypandoc
+    print('pandoc enabled')
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except (IOError, ImportError, ModuleNotFoundError):
+    print('WARNING: pandoc not enabled')
+    long_description = open('README.md').read()
+    pypandoc_enabled = False
+
+setup(
+    name="innerverz",
+    version="0.0.13",
+    author="Innerverz",
+    author_email="study@innerverz.com",
+    description="innerverz package",
+    long_description=long_description,
+    python_requires=">=3.6",
+    install_requires=requirements,
+    packages=find_packages()
+    
+    
+)
