2b6b188110543b91f4628edb791d7229a79231f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package determined-common/0.22.2/determined_common-0.22.2-py3-none-any.whl

---
+++ packages/determined-common/determined_common-0.22.2-py3-none-any.whl/determined_common/__init__.py
@@ -0,0 +1,10 @@
+import warnings
+
+from determined.common import *  # noqa
+from .__version__ import __version__
+
+warnings.warn(
+    "determined_common package is deprecated, please use determined.common instead.",
+    FutureWarning,
+    stacklevel=2,
+)
