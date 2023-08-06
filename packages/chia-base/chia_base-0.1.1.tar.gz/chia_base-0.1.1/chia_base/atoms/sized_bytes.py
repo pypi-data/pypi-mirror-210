aae6666ddbcf2592efe9f5810a0557acaac7a63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:25 +0000

    Add package chia-base/0.1.1/chia_base-0.1.1.tar.gz

---
+++ packages/chia-base/chia_base-0.1.1.tar.gz/chia_base-0.1.1/chia_base/atoms/sized_bytes.py
@@ -0,0 +1,35 @@
+from typing import BinaryIO
+from .hexbytes import hexbytes
+
+
+class SizedBytes(bytes):
+    _size: int
+
+    def __new__(cls, v):
+        v = bytes(v)
+        if not isinstance(v, bytes) or len(v) != cls._size:
+            raise ValueError("bad %s initializer %s" % (cls.__name__, v))
+        return hexbytes.__new__(cls, v)
+
+    @classmethod
+    def parse(cls, f: BinaryIO) -> bytes:
+        b = f.read(cls._size)
+        assert len(b) == cls._size
+        return cls(b)
+
+    @classmethod
+    def _class_stream(cls, obj: bytes, f: BinaryIO) -> None:
+        assert len(obj) == cls._size
+        f.write(obj)
+
+
+class bytes32(SizedBytes):
+    _size = 32
+
+
+class bytes48(SizedBytes):
+    _size = 48
+
+
+class bytes96(SizedBytes):
+    _size = 96
