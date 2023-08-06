d4ddd0eecc8762909e9a5eb6b23e64e5b87810e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:49 +0000

    Add package edaplore/0.2.2/edaplore-0.2.2.tar.gz

---
+++ packages/edaplore/edaplore-0.2.2.tar.gz/edaplore-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='edaplore',
+    version='0.2.2',
+    author='Maksim Zabelin',
+    author_email='mzabelin8@mail.ru',
+    description='EDA helper',
+    long_description='',
+    license='MIT',
+    url='https://github.com/mzabelin8/explore_hse',
+    packages=find_packages(),
+    install_requires=[
+        'Jinja2==3.1.2',
+        'matplotlib==3.7.1',
+        'numpy==1.24.3',
+        'pandas==1.5.3',
+        'scikit_learn==1.2.2',
+        'seaborn==0.12.2',
+        'setuptools==66.0.0',
+        'tqdm==4.65.0',
+    ],
+    python_requires='>=3.6'
+)
