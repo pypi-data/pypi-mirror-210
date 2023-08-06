8fc751ff392cf699922a3c8be6a1af33f6ad5b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:46 +0000

    Add package matrix-bot-lib/0.1.7/matrix_bot_lib-0.1.7.tar.gz

---
+++ packages/matrix-bot-lib/matrix_bot_lib-0.1.7.tar.gz/matrix_bot_lib-0.1.7/matrix_bot_lib/__init__.py
@@ -0,0 +1,9 @@
+from .models import T_Listener, RoomsResponse, TokenResponse
+from .botlib import MatrixBot
+
+__all__ = [
+    'T_Listener',
+    'RoomsResponse',
+    'TokenResponse',
+    'MatrixBot',
+]
